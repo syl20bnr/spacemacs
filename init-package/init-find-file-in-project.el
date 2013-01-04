9f92b48315778fc0fe49c77275e252f134022bc0
syl20bnr <sylvain.benner@gmail.com>
2013-01-04 15:10:53 -0500

    set patterns for ffip

--- init-package/init-find-file-in-project.el
+++ init-package/init-find-file-in-project.el
@@ -2,8 +2,26 @@
 
 ;; Use eproject to find project root
 (setq ffip-project-root-function 'eproject-root)
-;; No need to be stingy
-(setq ffip-limit 4096)
+;; That's a limit...
+(setq ffip-limit 32768)
+
+(setq ffip-patterns
+  '(
+    "*.clj"
+    "*.el"
+    "*.erl"
+    "*.htm"
+    "*.html"
+    "*.java"
+    "*.jelly"
+    "*.js"
+    "*.md"
+    "*.org"
+    "*.pl"
+    "*.py"
+    "*.rb"
+    "*.txt"
+    ))
 
 ;; Helper methods to create local settings
 (defun ffip--create-exclude-find-options (names)
@@ -11,19 +29,9 @@
                (concat "-not -regex \".*" name ".*\"")) names " "))
 
 (defun ffip-local-excludes (&rest names)
-  "Given a set of names, will exclude results with those names in the path.
-Example:
-(ffip-local-excludes \"target\" \"overlays\")
-"
+  "Given a set of names, will exclude results with those names in the path. Example: (ffip-local-excludes \"target\" \"overlays\") "
   (set (make-local-variable 'ffip-find-options)
        (ffip--create-exclude-find-options names)))
-
-(defun ffip-local-patterns (&rest patterns)
-  "An exhaustive list of file name patterns to look for.
-Example:
-(ffip-local-patterns \"*.js\" \"*.jsp\" \"*.css\")
-"
-  (set (make-local-variable 'ffip-patterns) patterns))
 
 ;; Function to create new functions that look for a specific pattern
 (defun ffip-create-pattern-file-finder (&rest patterns)
