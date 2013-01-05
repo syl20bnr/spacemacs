c062fc250c9cf06e2667de11ded2c447810980b4
syl20bnr <sylvain.benner@gmail.com>
2013-01-05 15:29:05 -0500

    better consistency for key chords

--- init-package/init-expand-region.el
+++ init-package/init-expand-region.el
@@ -1,4 +1,1 @@
 (require 'expand-region)
-(require 'evil)
-(key-chord-define evil-visual-state-map "kl" 'er/expand-region)
-(key-chord-define evil-visual-state-map "hj" 'er/contract-region)
--- my-keychords.el
+++ my-keychords.el
@@ -1,38 +1,43 @@
 ;; evil -----------------------------------------------------------------------
 (key-chord-define evil-insert-state-map "jk" 'evil-normal-state)
 (key-chord-define evil-visual-state-map "jk" 'evil-normal-state)
+(key-chord-define evil-emacs-state-map "jk" 'evil-normal-state)
+(key-chord-define evil-motion-state-map "jk" 'evil-normal-state)
 
 ;; applications ---------------------------------------------------------------
 (key-chord-define evil-normal-state-map "ae" 'erc-start-or-switch)
 (key-chord-define evil-normal-state-map "am" 'mu4e-up-to-date-status)
 ;; buffers --------------------------------------------------------------------
-(key-chord-define evil-normal-state-map "bh" 'switch-to-prev-buffer)
 (key-chord-define evil-normal-state-map "bk" 'ido-kill-buffer)
 (key-chord-define evil-normal-state-map "BK" 'delete-current-buffer-file)
-(key-chord-define evil-normal-state-map "bl" 'switch-to-next-buffer)
+(key-chord-define evil-normal-state-map "bn" 'switch-to-next-buffer)
 (key-chord-define evil-normal-state-map "bo" 'kill-other-buffers)
+(key-chord-define evil-normal-state-map "bp" 'switch-to-prev-buffer)
 (key-chord-define evil-normal-state-map "br" 'rename-current-buffer-file)
 (key-chord-define evil-normal-state-map "bs" 'ido-switch-buffer)
 ;; multiple-cursors -----------------------------------------------------------
 (key-chord-define evil-visual-state-map "ca" 'mc/mark-all-like-this)
+(key-chord-define evil-visual-state-map "cb" 'mac-mc-edit-beginnings-of-lines-tb)
+(key-chord-define evil-visual-state-map "CB" 'mac-mc-edit-ends-of-lines-tb)
 (key-chord-define evil-visual-state-map "cc" 'mac-mc-acquire-cursors-at-beginning)
+(key-chord-define evil-visual-state-map "ce" 'mac-mc-edit-beginnings-of-lines-bt)
+(key-chord-define evil-visual-state-map "CE" 'mac-mc-edit-ends-of-lines-bt)
 (key-chord-define evil-normal-state-map "cj" 'mc/mark-next-lines)
-(key-chord-define evil-normal-state-map "ck" 'mc/mark-previous-lines)
-(key-chord-define evil-visual-state-map "cl" 'mc/mark-next-like-this)
-(key-chord-define evil-visual-state-map "cm" 'mac-mc-edit-beginnings-of-lines-tb)
-(key-chord-define evil-visual-state-map "CM" 'mac-mc-edit-ends-of-lines-tb)
-(key-chord-define evil-visual-state-map "cn" 'mac-mc-edit-beginnings-of-lines-bt)
-(key-chord-define evil-visual-state-map "CN" 'mac-mc-edit-ends-of-lines-bt)
+(key-chord-define evil-visual-state-map "cn" 'mc/mark-next-like-this)
+(key-chord-define evil-normal-state-map "cp" 'mc/mark-previous-lines)
 ;; elisp ----------------------------------------------------------------------
 (key-chord-define evil-normal-state-map "ed" 'elisp-slime-nav-find-elisp-thing-at-point)
 (key-chord-define evil-normal-state-map "ev" 'eval-sexp)
 (key-chord-define evil-normal-state-map "EV" 'eval-and-replace)
-(key-chord-define evil-normal-state-map "EV" 'eval-and-replace)
 ;; emacs ----------------------------------------------------------------------
 (key-chord-define evil-normal-state-map "em" 'smex)
+;; expand region --------------------------------------------------------------
+(key-chord-define evil-visual-state-map "re" 'er/contract-region)
+(key-chord-define evil-visual-state-map "rr" 'er/expand-region)
 ;; files ----------------------------------------------------------------------
 (key-chord-define evil-normal-state-map "fo" 'ido-find-file)
 (key-chord-define evil-normal-state-map "fp" 'find-file-in-project)
+(key-chord-define evil-normal-state-map "fs" 'sr-speedbar-toggle-and-select)
 ;; fold-this ------------------------------------------------------------------
 (key-chord-define evil-visual-state-map "fj" 'fold-this)
 (key-chord-define evil-visual-state-map "FJ" 'fold-this-all)
@@ -46,13 +51,13 @@
 (key-chord-define evil-normal-state-map "vk" 'move-text-up)
 (key-chord-define evil-visual-state-map "vk" 'move-text-up)
 ;; errors ---------------------------------------------------------------------
-(key-chord-define evil-normal-state-map "rh" 'previous-error)
-(key-chord-define evil-normal-state-map "rl" 'next-error)
+(key-chord-define evil-normal-state-map "rp" 'previous-error)
+(key-chord-define evil-normal-state-map "rn" 'next-error)
 ;; terminals ------------------------------------------------------------------
+(key-chord-define evil-normal-state-map "tc" 'multi-term)
 (key-chord-define evil-normal-state-map "te" 'eshell)
-(key-chord-define evil-normal-state-map "th" 'multi-term-prev)
-(key-chord-define evil-normal-state-map "tl" 'multi-term-next)
-(key-chord-define evil-normal-state-map "tn" 'multi-term)
+(key-chord-define evil-normal-state-map "tn" 'multi-term-next)
+(key-chord-define evil-normal-state-map "tp" 'multi-term-prev)
 (key-chord-define evil-normal-state-map "tt" 'last-used-multi-term)
 ;; windows --------------------------------------------------------------------
 (key-chord-define evil-normal-state-map "wb" 'split-window-right)
@@ -64,5 +69,6 @@
 (key-chord-define evil-normal-state-map "wm" 'toggle-maximize-buffer)
 (key-chord-define evil-normal-state-map "wr" 'rotate-windows)
 (key-chord-define evil-normal-state-map "wv" 'split-window-below)
+(key-chord-define evil-normal-state-map "ww" 'other-window)
 
 (provide 'my-keychords)
