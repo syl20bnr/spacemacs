a632addab82fff7aa2635758389dfd0f63075c4c
syl20bnr <sylvain.benner@gmail.com>
2013-01-05 15:27:19 -0500

    added automatic switch evil-normal-state when entering evil-motion-state

--- init-package/init-evil.el
+++ init-package/init-evil.el
@@ -24,3 +24,10 @@
 (add-hook 'evil-emacs-state-entry-hook
   (lambda ()
     (add-hook 'post-command-hook 'evil-emacs-state-2-evil-normal-state)))
+;; same thing for motion state
+(defun evil-motion-state-2-evil-normal-state ()
+  (evil-normal-state)
+  (remove-hook 'post-command-hook 'evil-motion-state-2-evil-normal-state))
+(add-hook 'evil-motion-state-entry-hook
+  (lambda ()
+    (add-hook 'post-command-hook 'evil-motion-state-2-evil-normal-state)))
