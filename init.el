89e13dc6645d4e548ab4f1c14287a995379dfffe
syl20bnr <sylvain.benner@gmail.com>
2013-01-05 20:47:49 -0500

    minor change

--- init.el
+++ init.el
@@ -45,9 +45,3 @@
 (progn (when (file-exists-p host-directory)
     (dolist (l (directory-files host-directory nil "^[^#].*el$"))
       (load (concat host-directory l)))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
-)
