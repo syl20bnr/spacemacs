a4cea79cbea5e177bbeb949d26ceea610400fc78
syl20bnr <sylvain.benner@gmail.com>
2013-01-11 11:15:41 -0500

    override default yasnippet snippets for python-mode

--- init-package/init-yasnippet.el
+++ init-package/init-yasnippet.el
@@ -2,4 +2,5 @@
 (yas-global-mode t)
 (setq yas-prompt-functions '(yas-dropdown-prompt yas-ido-prompt yas-x-prompt))
 (setq yas-wrap-around-region 'cua)
+(yas-load-directory (concat user-emacs-directory "/snippets"))
 (add-to-list 'ac-sources 'ac-source-yasnippet)
