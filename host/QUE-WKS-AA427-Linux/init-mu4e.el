dc933f4e30119e5435b963b49b53cadcea9181f2
syl20bnr <sylvain.benner@gmail.com>
2013-01-11 09:33:45 -0500

    email preferences

--- host/QUE-WKS-AA427-Linux/init-mu4e.el
+++ host/QUE-WKS-AA427-Linux/init-mu4e.el
@@ -1,5 +1,13 @@
+;; alternatively, for emacs-24 you can use:
+(setq message-send-mail-function 'smtpmail-send-it
+     smtpmail-stream-type 'starttls
+     smtpmail-default-smtp-server "smtp.gmail.com"
+     smtpmail-smtp-server "smtp-ncsa.ubisoft.org"
+     smtpmail-smtp-service 25)
+
 ;; something about ourselves
 (setq
    user-mail-address "sylvain.benner@ubisoft.com"
    user-full-name  "Sylvain Benner"
-   message-signature "-Sylvain Benner")
+   message-signature (concat "-Sylvain Benner - Team Stratus\n"
+                             "http://stratus"))
