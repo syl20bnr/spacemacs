9d2967b48e6fc8d8626a29e805bd11b308eb0c47
syl20bnr <sylvain.benner@gmail.com>
2013-01-15 09:28:08 -0500

    disabled auto-switch to normal mode

--- init-package/init-evil.el
+++ init-package/init-evil.el
@@ -14,22 +14,22 @@
 ;; (initiated with 'i').
 (setq evil-move-cursor-back nil)
 
-;; evil-emacs-state is annoying, the following function and hook automatically
-;; switch to evil-insert-state whenever the evil-emacs-state is entered.
-;; It allows a more consistent navigation experience among all mode maps.
-(defun evil-emacs-state-2-evil-insert-state ()
-  (evil-insert-state)
-  (remove-hook 'post-command-hook 'evil-emacs-state-2-evil-insert-state))
-(add-hook 'evil-emacs-state-entry-hook
-  (lambda ()
-    (add-hook 'post-command-hook 'evil-emacs-state-2-evil-insert-state)))
-;; same thing for motion state but switch in normal mode instead
-(defun evil-motion-state-2-evil-normal-state ()
-  (evil-normal-state)
-  (remove-hook 'post-command-hook 'evil-motion-state-2-evil-normal-state))
-(add-hook 'evil-motion-state-entry-hook
-  (lambda ()
-    (add-hook 'post-command-hook 'evil-motion-state-2-evil-normal-state)))
+;; ;; evil-emacs-state is annoying, the following function and hook automatically
+;; ;; switch to evil-insert-state whenever the evil-emacs-state is entered.
+;; ;; It allows a more consistent navigation experience among all mode maps.
+;; (defun evil-emacs-state-2-evil-insert-state ()
+;;   (evil-insert-state)
+;;   (remove-hook 'post-command-hook 'evil-emacs-state-2-evil-insert-state))
+;; (add-hook 'evil-emacs-state-entry-hook
+;;   (lambda ()
+;;     (add-hook 'post-command-hook 'evil-emacs-state-2-evil-insert-state)))
+;; ;; same thing for motion state but switch in normal mode instead
+;; (defun evil-motion-state-2-evil-normal-state ()
+;;   (evil-normal-state)
+;;   (remove-hook 'post-command-hook 'evil-motion-state-2-evil-normal-state))
+;; (add-hook 'evil-motion-state-entry-hook
+;;   (lambda ()
+;;     (add-hook 'post-command-hook 'evil-motion-state-2-evil-normal-state)))
 
 ;; from
 ;; Insert state clobbers some useful Emacs keybindings
