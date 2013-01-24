77de700c14c5470dd267c1cc982a6c09e210dd16
syl20bnr <sylvain.benner@gmail.com>
2013-01-23 20:05:37 -0500

    erlang-mode: added default node name and automatic debug info when compiling

--- init-package/init-erlang.el
+++ init-package/init-erlang.el
@@ -1,7 +1,14 @@
-(require 'erlang-start)
 (add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
 (add-to-list 'auto-mode-alist '("\\.hrl?$" . erlang-mode))
 (setq erlang-root-dir "/usr/local/lib/erlang/erts-5.9.2")
 (add-to-list 'exec-path "/usr/local/lib/erlang/erts-5.9.2/bin")
 (setq erlang-man-root-dir "/usr/local/lib/erlang/erts-5.9.2/man")
+(setq erlang-compile-extra-opts '(debug_info))
+(require 'erlang-start)
 
+(add-hook 'erlang-mode-hook
+      (lambda ()
+        ;; when starting an Erlang shell in Emacs, with a custom node name
+        (setq inferior-erlang-machine-options '("-sname" "syl20bnr"))))
+
+(require 'erlang-flymake)
