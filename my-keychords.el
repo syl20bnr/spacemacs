929b23433622c42f24fac1317d1d3e3991403656
syl20bnr <sylvain.benner@gmail.com>
2013-01-24 00:19:37 -0500

    changed key chord for normal mode, it's now fd

--- my-keychords.el
+++ my-keychords.el
@@ -1,8 +1,8 @@
 ;; evil -----------------------------------------------------------------------
-(key-chord-define evil-insert-state-map ",," 'evil-normal-state)
-(key-chord-define evil-visual-state-map ",," 'evil-normal-state)
-(key-chord-define evil-emacs-state-map  ",," 'evil-normal-state)
-(key-chord-define evil-motion-state-map ",," 'evil-normal-state)
+(key-chord-define evil-insert-state-map "fd" 'evil-normal-state)
+(key-chord-define evil-visual-state-map "fd" 'evil-normal-state)
+(key-chord-define evil-emacs-state-map  "fd" 'evil-normal-state)
+(key-chord-define evil-motion-state-map "fd" 'evil-normal-state)
 
 
 (provide 'my-keychords)
