bba3992fd7010532e3b56fbd95bff67246bcc25c
syl20bnr <sylvain.benner@gmail.com>
2013-01-30 17:16:12 -0500

    auto cleanup of recent file every 10 minutes

--- init-package/init-recentf.el
+++ init-package/init-recentf.el
@@ -3,5 +3,5 @@
 (setq recentf-save-file (concat user-emacs-directory "/.recentf"))
 (setq recentf-max-saved-items 100)
 (setq recentf-auto-cleanup 10)
-(setq recentf-auto-save-timer (run-with-idle-timer 30 t 'recentf-save-list))
+(setq recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list))
 (recentf-mode 1)
