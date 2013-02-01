ba06c7c83f86cc538555b22409d34a8d59724653
syl20bnr <sylvain.benner@gmail.com>
2013-02-01 12:02:10 -0500

    disabled auto clean up of recent files in recentf mode

--- init-package/init-recentf.el
+++ init-package/init-recentf.el
@@ -2,6 +2,6 @@
 (setq recentf-exclude '("~/.emacs.d/.recentf"))
 (setq recentf-save-file (concat user-emacs-directory "/.recentf"))
 (setq recentf-max-saved-items 100)
-(setq recentf-auto-cleanup 10)
+(setq recentf-auto-cleanup 'never)
 (setq recentf-auto-save-timer (run-with-idle-timer 600 t 'recentf-save-list))
 (recentf-mode 1)
