3e409f722d6f7f4afd779017100d22ae28a6cfb3
syl20bnr <sylvain.benner@gmail.com>
2013-02-13 16:24:00 -0500

    removed deprecated keychords for expand-region

--- my-keychords.el
+++ my-keychords.el
@@ -1,6 +1,3 @@
-;; expand region --------------------------------------------------------------
-(key-chord-define evil-visual-state-map "re" 'er/expand-region)
-(key-chord-define evil-visual-state-map "rr" 'er/contract-region)
 ;; evil -----------------------------------------------------------------------
 (key-chord-define evil-insert-state-map "fd" 'evil-normal-state)
 (key-chord-define evil-visual-state-map "fd" 'evil-normal-state)
