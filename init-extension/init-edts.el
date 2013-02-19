3c34525bf0eb407bf082954020218706c5e4a36e
syl20bnr <sylvain.benner@gmail.com>
2013-02-19 15:40:09 -0500

    removed old EDTS project configuration

--- init-extension/init-edts.el
+++ init-extension/init-edts.el
@@ -1,10 +1,3 @@
 (require 'edts-start)
 
 ;; (setq edts-log-level 'debug)
-
-(setq edts-projects
-      '(( ;; Erlang tests.
-         (name       . "erlang")
-         (root       . "/home/sbenner/dev/projects/erlang")
-         (node-sname . "erltests"))
-        ))
