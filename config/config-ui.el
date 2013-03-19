da7aadf2b127009db0fd5f7d769ababb37e54fba
syl20bnr <sylvain.benner@gmail.com>
2013-03-18 23:04:15 -0400

    made config works on windows :(

--- config/config-ui.el
+++ config/config-ui.el
@@ -18,8 +18,7 @@
 ;; When emacs asks for "yes" or "no", let "y" or "n" sufficide
 (fset 'yes-or-no-p 'y-or-n-p)
 ;; font
-(if (eq window-system 'x)
-  (set-default-font "DejaVu Sans Mono-10"))
+(set-default-font "DejaVu Sans Mono-10"))
 ;; setup right and left margins
 ;; (add-hook 'window-configuration-change-hook
 ;;           (lambda ()
