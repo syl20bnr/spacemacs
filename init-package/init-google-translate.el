3bf15baaf40e0440d11c0709b20618ae414b99a8
syl20bnr <sylvain.benner@gmail.com>
2013-04-21 22:49:16 -0400

    Add google translate init file

---
+++ init-package/init-google-translate.el
@@ -0,0 +1,4 @@
+(require 'google-translate)
+(setq google-translate-enable-ido-completion t)
+(setq google-translate-default-source-language "En")
+(setq google-translate-default-target-language "Fr")
