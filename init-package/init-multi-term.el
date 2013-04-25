20a8a3a87965ebb8e325136b70fcc5256ffab827
syl20bnr <sylvain.benner@gmail.com>
2013-04-24 23:30:59 -0400

    Make tab completion work within a zsh term.

--- init-package/init-multi-term.el
+++ init-package/init-multi-term.el
@@ -1,3 +1,10 @@
 (require 'multi-term)
 
 (setq multi-term-program "/bin/zsh")
+
+(defun term-send-tab ()
+  "Send tab in term mode."
+  (interactive)
+  (term-send-raw-string "\t"))
+
+(add-to-list 'term-bind-key-alist '("<tab>" . term-send-tab))
