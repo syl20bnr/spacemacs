6b02cd51d8248e88f2f82410558cd4dbc969b71c
syl20bnr <sylvain.benner@gmail.com>
2013-04-25 11:58:40 -0400

    Disabled syntax check on new line.

--- init-extension/init-flycheck.el
+++ init-extension/init-flycheck.el
@@ -7,3 +7,5 @@
 
 (add-hook (intern (concat (symbol-name mode) "-mode-hook"))
 		  'flycheck-mode))
+
+(setq flycheck-check-syntax-automatically (delq 'new-line flycheck-check-syntax-automatically))
