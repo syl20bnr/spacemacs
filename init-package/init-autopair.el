ed42a8863cfbc1502d67b12d7fd5eeec682a7236
syl20bnr <sylvain.benner@gmail.com>
2013-04-26 15:18:02 -0400

    Hook autopair mode for ruby

--- init-package/init-autopair.el
+++ init-package/init-autopair.el
@@ -1,8 +1,10 @@
 (require 'autopair)
 
 (defvar autopair-modes
-  '(python-mode erlang-mode emacs-lisp-mode)
-)
+  '(python-mode
+    emacs-lisp-mode
+    erlang-mode
+    ruby-mode))
 (defun turn-on-autopair-mode () (autopair-mode 1))
 (dolist (mode autopair-modes)
   (add-hook (intern (concat (symbol-name mode) "-hook")) 'turn-on-autopair-mode))
