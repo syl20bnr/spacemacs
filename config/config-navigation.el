67df0c806edb4b1e671c7abed296af81ea97ee8b
syl20bnr <sylvain.benner@gmail.com>
2013-04-30 13:46:18 -0400

    Suppress annoying bell beep on windows as well as visual bell.

--- config/config-navigation.el
+++ config/config-navigation.el
@@ -5,3 +5,7 @@
 ;; Also auto refresh dired, but be quiet about it
 (setq global-auto-revert-non-file-buffers t)
 (setq auto-revert-verbose nil)
+;; no beep pleeeeeease ! (and no visual blinking too please)
+(custom-set-variables '(ring-bell-function 'ignore))
+(setq visible-bell nil)
+
