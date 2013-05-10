81d6cb84df20971b72e31921b6d0b076a7e15821
syl20bnr <sylvain.benner@gmail.com>
2013-05-09 22:45:39 -0400

    Add evil leader to emacs and motion states.

--- init-package/init-evil-leader.el
+++ init-package/init-evil-leader.el
@@ -2,8 +2,10 @@
       evil-leader/leader "SPC"
       evil-leader/non-normal-prefix "s-")
 (require 'evil-leader)
-;; make leader available in visual mode
-(define-key evil-visual-state-map (kbd "SPC") evil-leader/map)
 ;; Unset shortcuts which shadow evil leader
 (eval-after-load "compile"
   (define-key compilation-mode-map (kbd "SPC") nil))
+;; make leader available in visual mode
+(define-key evil-visual-state-map (kbd "SPC") evil-leader/map)
+(define-key evil-motion-state-map (kbd "SPC") evil-leader/map)
+(define-key evil-emacs-state-map  (kbd "SPC") evil-leader/map)
