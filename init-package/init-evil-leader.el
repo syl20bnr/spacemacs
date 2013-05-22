10aa81809479f815c7040734e2c06771866f8503
syl20bnr <sylvain.benner@gmail.com>
2013-05-21 21:25:10 -0400

    Update code to match recent breaking changes in evil-leader.

--- init-package/init-evil-leader.el
+++ init-package/init-evil-leader.el
@@ -6,6 +6,6 @@
 (eval-after-load "compile"
   (define-key compilation-mode-map (kbd "SPC") nil))
 ;; make leader available in visual mode
-(define-key evil-visual-state-map (kbd "SPC") evil-leader/map)
-(define-key evil-motion-state-map (kbd "SPC") evil-leader/map)
-(define-key evil-emacs-state-map  (kbd "SPC") evil-leader/map)
+(define-key evil-visual-state-map (kbd "SPC") evil-leader--default-map)
+(define-key evil-motion-state-map (kbd "SPC") evil-leader--default-map)
+(define-key evil-emacs-state-map  (kbd "SPC") evil-leader--default-map)
