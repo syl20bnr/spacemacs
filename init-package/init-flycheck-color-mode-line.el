620b297dd1b4bb1f9216bdd657912f34b8a62259
sbenner <sylvain.benner@ubisoft.com>
2013-05-22 09:18:32 -0400

    Remove flyckeck check on idle.

--- init-package/init-flycheck-color-mode-line.el
+++ init-package/init-flycheck-color-mode-line.el
@@ -1,4 +1,6 @@
 (require 'flycheck-color-mode-line)
 
+(setq flycheck-check-syntax-automatically '(save mode-enabled))
+
 (eval-after-load "flycheck"
   '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
