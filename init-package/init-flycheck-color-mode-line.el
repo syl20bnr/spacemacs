5546ba7eb4ef3929937460cdeb425c49af4b66c1
syl20bnr <sylvain.benner@gmail.com>
2013-05-23 17:22:08 -0400

    Clean up some messy flycheck configuration.

--- init-package/init-flycheck-color-mode-line.el
+++ init-package/init-flycheck-color-mode-line.el
@@ -1,6 +1,4 @@
 (require 'flycheck-color-mode-line)
 
-(setq flycheck-check-syntax-automatically '(save mode-enabled))
-
 (eval-after-load "flycheck"
   '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
--- init-package/init-flycheck.el
+++ init-package/init-flycheck.el
@@ -4,8 +4,7 @@
                 json
                 python
                 ruby))
-
 (add-hook (intern (concat (symbol-name mode) "-mode-hook"))
 		  'flycheck-mode))
 
-(setq flycheck-check-syntax-automatically (delq 'new-line flycheck-check-syntax-automatically))
+(setq flycheck-check-syntax-automatically '(save mode-enabled))
