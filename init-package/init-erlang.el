8f1e088b0ad8e84976e3385501a90e6a7b008820
sbenner <sylvain.benner@gmail.com>
2013-11-15 12:02:13 -0500

    Update erlang version

--- init-package/init-erlang.el
+++ init-package/init-erlang.el
@@ -1,9 +1,9 @@
 (add-to-list 'auto-mode-alist '("\\.erl?$" . erlang-mode))
 (add-to-list 'auto-mode-alist '("\\.hrl?$" . erlang-mode))
 (add-to-list 'auto-mode-alist '("\\.spec?$" . erlang-mode))
-(setq erlang-root-dir "/usr/local/lib/erlang/erts-5.9.2")
-(add-to-list 'exec-path "/usr/local/lib/erlang/erts-5.9.2/bin")
-(setq erlang-man-root-dir "/usr/local/lib/erlang/erts-5.9.2/man")
+(setq erlang-root-dir "/usr/lib/erlang/erts-5.10.3")
+(add-to-list 'exec-path "/usr/lib/erlang/erts-5.10.3/bin")
+(setq erlang-man-root-dir "/usr/lib/erlang/erts-5.10.3/man")
 (setq erlang-compile-extra-opts '(debug_info))
 (require 'erlang-start)
 
