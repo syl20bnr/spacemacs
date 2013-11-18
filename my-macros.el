cba91baa04346cfcf9a45f9a9ec327a58154341e
syl20bnr <sylvain.benner@gmail.com>
2013-11-18 01:06:08 -0500

    Add after macro

--- my-macros.el
+++ my-macros.el
@@ -1,3 +1,9 @@
+;; http://milkbox.net/note/single-file-master-emacs-configuration/
+(defmacro after (mode &rest body)
+  "`eval-after-load' MODE evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,mode '(progn ,@body)))
+
 ;; Works only with selections from top to bottom
 (fset 'mac-mc-edit-beginnings-of-lines-tb
    (lambda (&optional arg)
