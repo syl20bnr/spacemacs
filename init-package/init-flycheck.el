e5d7472950e6940b015e0771b6546a0864c6b8dc
sbenner <sylvain.benner@gmail.com>
2013-11-21 15:37:07 -0500

    Fix lazy loading of flycheck

--- init-package/init-flycheck.el
+++ init-package/init-flycheck.el
@@ -1,11 +1,6 @@
 (use-package flycheck
   :defer t
   :init
-  (use-package flycheck-color-mode-line
-    :defer t
-    :init
-    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
-  :config
   (progn
     (dolist (mode '(c
                     elixir
@@ -14,7 +9,12 @@
                     ruby))
       (add-hook (intern (concat (symbol-name mode) "-mode-hook"))
                 'flycheck-mode))
+    (use-package flycheck-color-mode-line
+      :defer t
+      :init
+      (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
+  :config
+  (progn
 
     (setq flycheck-check-syntax-automatically '(save mode-enabled))
     (setq flycheck-standard-error-navigation nil)))
-
