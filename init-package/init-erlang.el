08dc76c83cc7a3e8e60e55d2e9494ca57fad31e5
syl20bnr <sylvain.benner@gmail.com>
2013-11-20 22:49:19 -0500

    Fix erlang/EDTS lazy loading

--- init-package/init-erlang.el
+++ init-package/init-erlang.el
@@ -1,4 +1,4 @@
-(use-package erlang-mode
+(use-package erlang
   :mode (("\\.erl?$" . erlang-mode)
          ("\\.hrl?$" . erlang-mode)
          ("\\.spec?$" . erlang-mode))
@@ -9,12 +9,15 @@
     (setq erlang-man-root-dir "/usr/lib/erlang/erts-5.10.3/man")
     (setq erlang-compile-extra-opts '(debug_info))
     (require 'erlang-start)
-
     (add-hook 'erlang-mode-hook
               (lambda ()
                 ;; when starting an Erlang shell in Emacs, with a custom node name
                 (setq inferior-erlang-machine-options '("-sname" "syl20bnr"))
-                ))))
+                ))
+    (require 'edts-start)
+    ;; (setq edts-log-level 'debug)
+    (setq edts-face-inhibit-mode-line-updates t)
+     ))
 
 ;; not needed using EDTS
 ;; (require 'erlang-flymake)
