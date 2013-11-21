8ce2ba9a1e97d4dda1958b2a96e5a279db857650
sbenner <sylvain.benner@gmail.com>
2013-11-21 10:25:29 -0500

    Fix lazy loading of pylookup

--- init-extension/init-pylookup.el
+++ init-extension/init-pylookup.el
@@ -1,9 +1,8 @@
 (use-package pylookup
-  :defer t
+  :commands pylookup-lookup
   :config
   (progn
     (setq pylookup-dir (concat user-extensions-directory "/pylookup"))
     ;; set executable file and db file
     (setq pylookup-program (concat pylookup-dir "/pylookup.py"))
-    (setq pylookup-db-file (concat pylookup-dir "/pylookup.db"))
-))
+    (setq pylookup-db-file (concat pylookup-dir "/pylookup.db"))))
