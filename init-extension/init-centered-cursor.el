da3b21154d4cea9b6d77b714a43efef7172c011a
syl20bnr <sylvain.benner@gmail.com>
2013-11-22 22:13:11 -0500

    Ingore mouse click with centered-cursor mode and evil mode

--- init-extension/init-centered-cursor.el
+++ init-extension/init-centered-cursor.el
@@ -1,5 +1,11 @@
 (use-package centered-cursor-mode
   :commands global-centered-cursor-mode
   :config
-  (custom-set-variables '(ccm-recenter-at-end-of-file t))
+  (custom-set-variables
+   '(ccm-recenter-at-end-of-file t)
+   '(ccm-ignored-commands (quote (mouse-drag-region
+                                  mouse-set-point
+                                  widget-button-click
+                                  scroll-bar-toolkit-scroll
+                                  evil-mouse-drag-region))))
 )
