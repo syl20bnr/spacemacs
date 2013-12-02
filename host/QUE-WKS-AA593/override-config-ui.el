5b83324e6d91c0d7719bcaadeea2eef7b0f0c17e
sbenner <sylvain.benner@gmail.com>
2013-12-02 09:03:59 -0500

    Default theme based on hostname

--- host/QUE-WKS-AA593/override-config-ui.el
+++ host/QUE-WKS-AA593/override-config-ui.el
@@ -1,2 +1,4 @@
 ;; (if window-system
 ;;       (set-frame-size (selected-frame) 220 64))
+
+(load-theme 'solarized-light t)
--- init-extension/init-solarized-theme.el
+++ init-extension/init-solarized-theme.el
@@ -1,12 +1,1 @@
-(use-package solarized
-  :init
-  (progn
-    (deftheme solarized-dark "The dark variant of the Solarized colour theme")
-    (create-solarized-theme 'dark 'solarized-dark)
-    (provide-theme 'solarized-dark)
-
-    (deftheme solarized-light "The light variant of the Solarized colour theme")
-    (create-solarized-theme 'light 'solarized-light)
-    (provide-theme 'solarized-light))
-  )
-
+(use-package solarized-theme)
