2f522f5e18c4f0190ac2ddc542eded9deca41ea3
sbenner <sylvain.benner@gmail.com>
2014-01-07 14:55:16 -0500

    Keep buffers opened when closing an emacsclient

--- config/config-session.el
+++ config/config-session.el
@@ -18,6 +18,8 @@
 (require 'saveplace)
 (setq-default save-place t)
 (setq save-place-file (expand-file-name ".places" user-emacs-directory))
+;; keep buffers opened when leaving an emacs client
+(setq server-kill-new-buffers nil)
 
 ;; ;; save a bunch of variables to the desktop file
 ;; ;; for lists specify the len of the maximal saved data also
