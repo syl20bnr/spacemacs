65a2e3bd4af2d6c6d2b6de12f7d08defc0eff22b
sbenner <sylvain.benner@gmail.com>
2014-03-27 16:34:41 -0400

    Remove deprecated stuff

--- host/QUE-WKS-AA427-Linux/init-erc.el
+++
@@ -1,6 +0,0 @@
-;; joining && autojoing
-
-;; make sure to use wildcards for e.g. freenode as the actual server
-;; name can be be a bit different, which would screw up autoconnect
-(setq erc-autojoin-channels-alist
-  '(("10.140.40.21" "#deployment")))
--- host/QUE-WKS-AA593/override-config-ui.el
+++
@@ -1,4 +0,0 @@
-;; (if window-system
-;;       (set-frame-size (selected-frame) 220 64))
-
-(load-theme 'solarized-light t)
