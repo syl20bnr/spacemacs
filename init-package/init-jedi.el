d93506735e61b6cbec29d526344077dc09deac44
sbenner <sylvain.benner@gmail.com>
2014-04-11 13:31:32 -0400

    Add keybindings for python test suite and fix a jedi loading issue

--- init-package/init-jedi.el
+++
@@ -1,8 +0,0 @@
-(use-package jedi
-  :defer t
-  :init
-  (setq jedi:setup-keys t)
-  :config
-  (progn
-    (setq jedi:complete-on-dot t)
-    (add-hook 'python-mode-hook 'jedi:setup)))
--- init-package/init-python.el
+++ init-package/init-python.el
@@ -1,13 +1,12 @@
 (use-package python
   :defer t
-  :config
+  :init
   (progn
     (add-hook 'python-mode-hook '(lambda()
                                    (setq indent-tabs-mode nil)
                                    (setq indent-level 4)
                                    (setq python-indent 4)
                                    (setq tab-width 4)))
-
     ;; from http://pedrokroger.net/2010/07/configuring-emacs-as-a-python-ide-2/
     (defun annotate-pdb ()
       "Highlight break point lines."
@@ -15,7 +14,17 @@
       (highlight-lines-matching-regexp "import pdb")
       (highlight-lines-matching-regexp "pdb.set_trace()"))
     (add-hook 'python-mode-hook 'annotate-pdb)
-
+    (use-package jedi
+      :defer t
+      :init
+      (progn
+        (setq jedi:setup-keys t)
+        (add-hook 'python-mode-hook 'jedi:setup))
+      :config
+      (progn
+        (setq jedi:complete-on-dot t))))
+  :config
+  (progn
     ;; from http://pedrokroger.net/2010/07/configuring-emacs-as-a-python-ide-2/
     (defun python-add-breakpoint ()
       "Add a break point, highlight it and save the buffer."
@@ -33,4 +42,6 @@
     (evil-leader/set-key "Mpd" 'pylookup-lookup)
     (evil-leader/set-key "Mpf" 'jedi:goto-definition)
     (evil-leader/set-key "Mpm" 'nosetests-module)
-    (evil-leader/set-key "MpM" 'nosetests-pdb-module)))
+    (evil-leader/set-key "MpM" 'nosetests-pdb-module)
+    (evil-leader/set-key "Mps" 'nosetests-suite)
+    (evil-leader/set-key "MpS" 'nosetests-pdb-suite)))
