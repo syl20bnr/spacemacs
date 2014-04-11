37ba24be9dc8408b87cc4ee1c7ada2ec817cb258
sbenner <sylvain.benner@gmail.com>
2014-04-11 13:32:55 -0400

    Add popwin rule for *grep*

--- init-package/init-popwin.el
+++ init-package/init-popwin.el
@@ -1,7 +1,8 @@
 (use-package popwin
   :init
   (progn
-    (setq display-buffer-function 'popwin:display-buffer)
+    (popwin-mode 1)
+    (push '("*grep*" :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
     ;; use popwin with helm
-    (push '("^\*helm.+\*$" :regexp t) popwin:special-display-config)
-    (push '("^\*helm-.+\*$" :regexp t) popwin:special-display-config)))
+    (push '("^\*helm.+\*$" :regexp t :position bottom) popwin:special-display-config)
+    (push '("^\*helm-.+\*$" :regexp t :position bottom) popwin:special-display-config)))
