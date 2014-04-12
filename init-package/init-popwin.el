9f80cdcbc00207f8fab183d34c52ccc1fff152c4
syl20bnr <sylvain.benner@gmail.com>
2014-04-11 21:41:24 -0400

    Make flycheck error window a popup window

--- init-package/init-popwin.el
+++ init-package/init-popwin.el
@@ -2,7 +2,7 @@
   :init
   (progn
     (popwin-mode 1)
-    (push '("*grep*" :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
-    ;; use popwin with helm
-    (push '("^\*helm.+\*$" :regexp t :position bottom) popwin:special-display-config)
-    (push '("^\*helm-.+\*$" :regexp t :position bottom) popwin:special-display-config)))
+    (push '("*grep*"                     :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
+    (push '("^\*Flycheck.+\*$" :regexp t :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
+    (push '("^\*helm.+\*$"     :regexp t              :position bottom) popwin:special-display-config)
+    (push '("^\*helm-.+\*$"    :regexp t              :position bottom) popwin:special-display-config)))
