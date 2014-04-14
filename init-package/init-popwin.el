fb40bf5aa3fbdcbdd93cfa9d0bbc6d24d46340e7
sbenner <sylvain.benner@gmail.com>
2014-04-14 10:36:31 -0400

    Add popwin properties for nosetests window

--- init-package/init-popwin.el
+++ init-package/init-popwin.el
@@ -3,6 +3,7 @@
   (progn
     (popwin-mode 1)
     (push '("*grep*"                     :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
+    (push '("*nosetests*"                :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
     (push '("^\*Flycheck.+\*$" :regexp t :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
-    (push '("^\*helm.+\*$"     :regexp t              :position bottom) popwin:special-display-config)
-    (push '("^\*helm-.+\*$"    :regexp t              :position bottom) popwin:special-display-config)))
+    (push '("^\*helm.+\*$"     :regexp t              :position bottom                     ) popwin:special-display-config)
+    (push '("^\*helm-.+\*$"    :regexp t              :position bottom                     ) popwin:special-display-config)))
