2ac3dac0bec8de85c1af5b0bf5622f51e6c4160e
sbenner <sylvain.benner@gmail.com>
2014-05-16 15:00:20 -0400

    Add popwin rule for WoMan

--- init-package/init-popwin.el
+++ init-package/init-popwin.el
@@ -6,5 +6,6 @@
     (push '("*grep*"                     :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
     (push '("*nosetests*"                :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
     (push '("^\*Flycheck.+\*$" :regexp t :dedicated t :position bottom :stick t :noselect t) popwin:special-display-config)
+    (push '("^\*WoMan.+\*$"    :regexp t              :position bottom                     ) popwin:special-display-config)
     (push '("^\*helm.+\*$"     :regexp t              :position bottom                     ) popwin:special-display-config)
     (push '("^\*helm-.+\*$"    :regexp t              :position bottom                     ) popwin:special-display-config)))
