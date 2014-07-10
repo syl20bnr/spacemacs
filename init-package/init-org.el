128c25babff0c82b71fe429ee9c30379d1a56145
sbenner <sylvain.benner@gmail.com>
2014-07-10 16:52:43 -0400

    Add org files to agenda files list

--- init-package/init-org.el
+++ init-package/init-org.el
@@ -6,6 +6,7 @@
     (define-key global-map "\C-cl" 'org-store-link)
     (define-key global-map "\C-ca" 'org-agenda)
     (setq org-log-done t)
+    (setq org-agenda-files '("~/Dropbox/org"))
     (use-package org-bullets
       :config
       (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
