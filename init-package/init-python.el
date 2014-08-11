834d79cb006e53c1377aff6c77cae339d3437241
sbenner <sylvain.benner@gmail.com>
2014-08-11 17:53:33 -0400

    Add ipython as python inferior process

--- init-package/init-python.el
+++ init-package/init-python.el
@@ -14,6 +14,17 @@
       (highlight-lines-matching-regexp "import pdb")
       (highlight-lines-matching-regexp "pdb.set_trace()"))
     (add-hook 'python-mode-hook 'annotate-pdb)
+        (setq
+         python-shell-interpreter "ipython"
+         ;; python-shell-interpreter-args (if (system-is-mac)
+         ;;                                   "--gui=osx --matplotlib=osx --colors=Linux"
+         ;;                                 (if (system-is-linux)
+         ;;                                     "--gui=wx --matplotlib=wx --colors=Linux"))
+         python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+         python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+         python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
+         python-shell-completion-module-string-code "';'.join(module_completion('''%s'''))\n"
+         python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
     (use-package jedi
       :defer t
       :init
