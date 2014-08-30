db85f861b059a09d87fd8d95efb35409c023f98e
syl20bnr <sylvain.benner@gmail.com>
2014-08-30 01:29:32 -0400

    Add reconnect command to rcirc

--- init-package/init-rcirc.el
+++ init-package/init-rcirc.el
@@ -3,6 +3,7 @@
   (progn
     (add-to-hook 'rcirc-mode-hook '(rcirc-track-minor-mode
                                     rcirc-omit-mode
+                                    ;; rcirc-reconnect-mode
                                     flyspell-mode)))
   :config
   (progn
@@ -14,32 +15,9 @@
              :nick "syl20bnr"
              :full-name "Sylvain Benner"
              :channels ("#emacs" "#nupic" "#python"))))
-    (require 'rcirc-color nil 'noerror)
+    (require 'rcirc-color)
+    (require 'rcirc-reconnect
+             (concat user-extensions-directory "rcirc-reconnect/rcirc-reconnect.el"))
     ;; identify info are stored in a separate location, skip errors
     ;; if the feature cannot be found.
-    (require 'pinit-rcirc)))
-
-;; from the rcirc manual
-(eval-after-load "rcirc"
-  '(defun-rcirc-command reconnect (arg)
-     "Reconnect the server process."
-     (interactive "i")
-     (unless process
-       (error "There's no process for this target"))
-     (let* ((server (car (process-contact process)))
-            (port (process-contact process :service))
-            (nick (rcirc-nick process))
-            channels query-buffers)
-       (dolist (buf (buffer-list))
-         (with-current-buffer buf
-           (when (eq process (rcirc-buffer-process))
-             (remove-hook 'change-major-mode-hook
-                          'rcirc-change-major-mode-hook)
-             (if (rcirc-channel-p rcirc-target)
-                 (setq channels (cons rcirc-target channels))
-               (setq query-buffers (cons buf query-buffers))))))
-       (delete-process process)
-       (rcirc-connect server port nick
-                      rcirc-default-user-name
-                      rcirc-default-full-name
-                      channels))))
+    (require 'pinit-rcirc nil 'noerror)))
