9ed075ae4b4caf19dd62683078f15e1e75ff7a7e
syl20bnr <sylvain.benner@gmail.com>
2014-08-30 03:24:26 -0400

    Minor change (reformat)

--- init-package/init-volatile-highlights.el
+++ init-package/init-volatile-highlights.el
@@ -1,10 +1,7 @@
 (use-package volatile-highlights
-  :init
-  (volatile-highlights-mode t)
+  :init (volatile-highlights-mode t)
   :config
   (progn
-  ;; define extensions for evil mode
-  (vhl/define-extension 'evil_past_after 'evil-paste-after)
-  (vhl/install-extension 'evil_past_after)
-  )
-)
+    ;; define extensions for evil mode
+    (vhl/define-extension 'evil_past_after 'evil-paste-after)
+    (vhl/install-extension 'evil_past_after)))
