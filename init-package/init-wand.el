d5132253effe15adef72d4a2927c217a0ecdd2e7
syl20bnr <sylvain.benner@gmail.com>
2014-08-30 03:15:09 -0400

    Disable wand mode

--- init-package/init-wand.el
+++ init-package/init-wand.el
@@ -1,7 +1,8 @@
 (use-package wand
- :init
- (progn
-   (require 'wand)
-   (wand:add-rule (wand:create-rule :match "https?://"
-                                    :capture :whole
-                                    :action message))))
+  :disabled t
+  :init
+  (progn
+    (require 'wand)
+    (wand:add-rule (wand:create-rule :match "https?://"
+                                     :capture :whole
+                                     :action message))))
