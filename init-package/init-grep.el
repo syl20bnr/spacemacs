03167a1bd5a3dd10b428d3bc9dea80b8b54da72a
syl20bnr <sylvain.benner@gmail.com>
2014-08-30 03:37:04 -0400

    Add comment to init-grep.el

--- init-package/init-grep.el
+++ init-package/init-grep.el
@@ -1,6 +1,7 @@
 (use-package grep
   :init
   (progn 
+    ;; add fish shell compatibility with grep command
     (defadvice grep-compute-defaults
       (after grep-default-fish-compatibility)
       (grep-apply-setting 'grep-find-command "find . ! -name \"*~\" ! -name \"#*#\" -type f -print0 | xargs -0 -e grep -nH -e ")
