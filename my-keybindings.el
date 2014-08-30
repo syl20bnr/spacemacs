78192c3dba6c3a9402b2e043c5bbcab80f6c048f
syl20bnr <sylvain.benner@gmail.com>
2014-08-30 03:25:16 -0400

    Lazy loading of helm
    With the previous loading optimizations, the total
    load time of Spacemacs is now 2.4s instead of 4.8s

--- my-keybindings.el
+++ my-keybindings.el
@@ -12,9 +12,6 @@
   (lambda () (interactive) (fd-trigger 'keyboard-quit)))
 (define-key minibuffer-local-map (kbd "f")
   (lambda () (interactive) (fd-trigger 'abort-recursive-edit)))
-;; the original hot key of helm-keyboard-quit is "C-g"
-(define-key helm-map (kbd "f")
-  (lambda () (interactive) (fd-trigger 'helm-keyboard-quit)))
 ;; evil -----------------------------------------------------------------------
 ;; easier toggle for emacs-state
 (evil-set-toggle-key "s-`")
@@ -42,6 +39,10 @@
 ;; use home row keys
 (eval-after-load "helm"
   '(progn
+     ;; the original hot key of helm-keyboard-quit is "C-g"
+     (define-key helm-map (kbd "f")
+       (lambda () (interactive) (fd-trigger 'helm-keyboard-quit)))
+     ;; helm navigation on hjkl
      (define-key helm-map (kbd "C-j") 'helm-next-line)
      (define-key helm-map (kbd "C-k") 'helm-previous-line)
      (define-key helm-map (kbd "C-h") 'helm-next-source)
@@ -64,7 +65,7 @@
 ;; evil-leader shortcuts ======================================================
 
 ;; M-x ------------------------------------------------------------------------
-(evil-leader/set-key ":" 'execute-extended-command)
+(evil-leader/set-key ":" 'helm-M-x)
 ;; Key bindings help ----------------------------------------------------------
 (evil-leader/set-key "?" 'helm-descbinds)
 ;; ace-jump -------------------------------------------------------------------
