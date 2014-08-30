0191d1bba88c98eb6e4627301e02c3750af2ed42
syl20bnr <sylvain.benner@gmail.com>
2014-08-30 03:13:50 -0400

    Remove unused evil plugin for mocur

--- init-extension/init-evil-plugins.el
+++ init-extension/init-evil-plugins.el
@@ -1,9 +1,5 @@
 (use-package evil-little-word)
 
-(use-package evil-operator-moccur
-  :init
-    (global-evil-operator-moccur-mode 1))
-
 (use-package evil-operator-comment
   :init
   (global-evil-operator-comment-mode 1))
