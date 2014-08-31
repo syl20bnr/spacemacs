c3f12a793c709ca5954e0fe7c4a4a0160c3dd32d
syl20bnr <sylvain.benner@gmail.com>
2014-08-31 12:59:31 -0400

    Remove evil config for volatile highlight (not working)

--- init-package/init-volatile-highlights.el
+++ init-package/init-volatile-highlights.el
@@ -1,7 +1,2 @@
 (use-package volatile-highlights
-  :init (volatile-highlights-mode t)
-  :config
-  (progn
-    ;; define extensions for evil mode
-    (vhl/define-extension 'evil_past_after 'evil-paste-after)
-    (vhl/install-extension 'evil_past_after)))
+  :init (volatile-highlights-mode t)) 
