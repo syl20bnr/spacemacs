4ccd9e2249759ec9aba6670ba764d7148f03af78
syl20bnr <sylvain.benner@gmail.com>
2014-08-31 12:58:42 -0400

    Defer loading of bookmark configuration

--- init-package/init-bookmark.el
+++ init-package/init-bookmark.el
@@ -1,5 +1,9 @@
 (use-package bookmark
-  :init
+  :commands (bookmark-delete
+             bookmark-jump
+             bookmark-rename
+             bookmark-set)
+  :config
   (setq 
   bookmark-default-file "~/.emacs.d/bookmarks"  ; keep my ~/ clean
   bookmark-save-flag 1)                         ; autosave each change
