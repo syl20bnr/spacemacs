7151ae10ff2da622e42f0c7057451018c8211c75
sbenner <sylvain.benner@gmail.com>
2014-09-02 14:41:01 -0400

    Uneeded creation of an untitled buffer at startup
    In order to reduce even more the boot time of Spacemacs
    Now we keep the focus on the *scratch* buffer at startup

--- spacemacs/config.el
+++ spacemacs/config.el
@@ -195,10 +195,10 @@
           (lambda ()
             ;; No splash screen
             (setq inhibit-startup-screen t)
-            ;; If the *scratch* buffer is the current one, then create a new
-            ;; empty untitled buffer to hide *scratch*
-            (if (string= (buffer-name) "*scratch*")
-                (new-empty-buffer))
+            ;; ;; If the *scratch* buffer is the current one, then create a new
+            ;; ;; empty untitled buffer to hide *scratch*
+            ;; (if (string= (buffer-name) "*scratch*")
+            ;;     (new-empty-buffer))
             )
           t) ;; append this hook to the tail
 
