ebdab6b05a255954cfd650e5ef60257c03860f21
syl20bnr <sylvain.benner@gmail.com>
2014-09-04 23:34:36 -0400

    Fix ace-jump-mode autoload

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -339,12 +339,13 @@ which require an initialization must be listed explicitly in the list."
   (use-package ace-jump-mode
     :defer t
     :init
-    (add-hook 'ace-jump-mode-end-hook 'golden-ratio)
+    (progn 
+      (add-hook 'ace-jump-mode-end-hook 'golden-ratio)
+      (define-key evil-normal-state-map "," 'ace-jump-mode)
+      (define-key evil-normal-state-map (kbd "C-,") 'ace-jump-word-mode))
     :config
     (progn 
       ;; ace-jump quick access
-      (define-key evil-normal-state-map "," 'ace-jump-mode)
-      (define-key evil-normal-state-map (kbd "C-,") 'ace-jump-word-mode)
       (evil-leader/set-key "," 'ace-jump-mode-pop-mark))))
 
 (defun spacemacs/init-auto-complete ()
