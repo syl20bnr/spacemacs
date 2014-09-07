583e605d4491188fe9ad04cebdcbcf5413f7486f
syl20bnr <sylvain.benner@gmail.com>
2014-09-07 10:02:18 -0400

    Reactivate marmalade repo.

--- contribsys.el
+++ contribsys.el
@@ -2,7 +2,7 @@
 (require 'package)
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
-                         ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 (setq warning-minimum-level :error)
