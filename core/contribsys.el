c0c464a39b40c5ea46ea2c4769f5d178661952c6
syl20bnr <sylvain.benner@gmail.com>
2014-09-26 14:08:58 -0400

    Temporarily reactivate marmalade for python
    Seems that python is not a built-in package in at least
    Ubuntu packages

--- core/contribsys.el
+++ core/contribsys.el
@@ -2,6 +2,8 @@
 (require 'package)
 (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")
+                         ;; temporarily reactivate marmalade because of python
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 (setq warning-minimum-level :error)
