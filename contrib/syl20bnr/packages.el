1ddf042f28c091c5283dda55448c109929281b10
syl20bnr <sylvain.benner@gmail.com>
2014-09-28 18:56:38 -0400

    Add mini-buffer doc for change color saturation and lightness mini-mode
    In syl20bnr configuration layer

--- contrib/syl20bnr/packages.el
+++ contrib/syl20bnr/packages.el
@@ -21,8 +21,19 @@ which require an initialization must be listed explicitly in the list.")
     :config
     (progn
       (syl20bnr/tweak-theme-colors 'solarized-light)
+
       ;; functions to change saturation and lightness of colors
-      (defun syl20bnr/rainbow-identifier-change-color-component
+      (defun syl20bnr/change-color-mini-mode-doc (component)
+        "Display a short documentation in the mini buffer."
+        (let ((var (intern (format
+                            "rainbow-identifiers-cie-l*a*b*-%s" component))))
+          (message "Change color %s mini-mode (value: %s)
+  + to increase %s
+  - to decrease %s
+  = to reset
+Press any other key to exit." component (eval var) component component)))
+
+      (defun syl20bnr/change-color-component
         (component inc reset)
         "Set a temporary overlay map to easily change a color COMPONENT from
  rainbow-identifier mode. The color COMPONENT can be 'saturation' or
@@ -31,28 +42,24 @@ which require an initialization must be listed explicitly in the list.")
         (set-temporary-overlay-map
          (let ((map (make-sparse-keymap)))
            (define-key map (kbd "+")
-             `(lambda () (interactive)
-                (syl20bnr/rainbow-identifier-change-color-component-func
-                 ,component ,inc)))
+             `(lambda () (interactive) (syl20bnr/change-color-component-func
+                                        ,component ,inc)))
            (define-key map (kbd "-")
-             `(lambda () (interactive)
-                (syl20bnr/rainbow-identifier-change-color-component-func
-                 ,component ,(- inc))))
+             `(lambda () (interactive) (syl20bnr/change-color-component-func
+                                        ,component ,(- inc))))
            (define-key map (kbd "=")
-             `(lambda () (interactive)
-                (syl20bnr/rainbow-identifier-change-color-component-func
-                 ,component ,reset t)))
+             `(lambda () (interactive) (syl20bnr/change-color-component-func
+                                        ,component ,reset t)))
            map) t)
-        (message "Type +/- to increase or decrease %s (= to reset)" component))
+        (syl20bnr/change-color-mini-mode-doc component))
 
-      (defun syl20bnr/rainbow-identifier-change-color-component-func
+      (defun syl20bnr/change-color-component-func
         (component inc &optional reset)
-        "Change the color component by adding INC value to it. If RESET is not nil
- the color component is set to INC."
+        "Change the color component by adding INC value to it. If RESET is not
+ nil the color component is set to INC."
         (interactive)
-        (message component)
-        (let* ((var (intern (format "rainbow-identifiers-cie-l*a*b*-%s"
-                                    component)))
+        (let* ((var (intern (format
+                             "rainbow-identifiers-cie-l*a*b*-%s" component)))
                (new-value (+ (eval var) inc)))
           (if reset
               (set var inc)
@@ -60,15 +67,13 @@ which require an initialization must be listed explicitly in the list.")
               (if (< new-value 0)
                   (setq new-value 0))
               (set var new-value)))
-          (message "Color %s: %s" component (eval var))
-          (font-lock-fontify-buffer)))
+          (font-lock-fontify-buffer)
+          (syl20bnr/change-color-mini-mode-doc component)))
       ;; key bindings
       (evil-leader/set-key "cs"
         '(lambda () (interactive)
-           (syl20bnr/rainbow-identifier-change-color-component "saturation" 5 100)))
+           (syl20bnr/change-color-component "saturation" 5 100)))
       (evil-leader/set-key "cl"
         '(lambda () (interactive)
-           (syl20bnr/rainbow-identifier-change-color-component "lightness" 5 40)))
- 
-      )))
+           (syl20bnr/change-color-component "lightness" 5 40))))))
 
