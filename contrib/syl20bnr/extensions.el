b8a4b85d434121e0a6ab2f7539b613013c70451f
syl20bnr <sylvain.benner@gmail.com>
2014-09-30 22:57:51 -0400

    Add o-blog to syl20bnr configuration layer

--- contrib/syl20bnr/extensions.el
+++ contrib/syl20bnr/extensions.el
@@ -1,5 +1,11 @@
-;; Pre extensions are loaded *before* the co/packages
+;; Pre extensions are loaded *before* the packages
 (defvar syl20bnr-pre-extensions '())
 
-;; Pre extensions are loaded *after* the co/packages
-(defvar syl20bnr-post-extensions '())
+;; Pre extensions are loaded *after* the packages
+(defvar syl20bnr-post-extensions '(o-blog))
+
+(defun syl20bnr/init-o-blog ()
+  (let* ((dir (contribsys/get-layer-property 'syl20bnr :ext-dir)))
+    (add-to-list 'load-path (format "%so-blog/lisp/" dir)))
+  (use-package o-blog
+    :commands o-blog-publish))
