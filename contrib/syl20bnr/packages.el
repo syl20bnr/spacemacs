d278e6312b075923f18646fd0049215e462e80a0
syl20bnr <sylvain.benner@gmail.com>
2014-10-17 23:04:00 -0400

    Fixes typo in xxx-packages docstring

--- contrib/syl20bnr/packages.el
+++ contrib/syl20bnr/packages.el
@@ -2,7 +2,7 @@
   '(
     rainbow-identifiers
     )
-  "List of all packages to install and/or initialized. Built-in packages
+  "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
 (defun syl20bnr/init-rainbow-identifiers ()
