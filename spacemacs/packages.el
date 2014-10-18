5f46d67a252558ae44fc12da8a68b5948b8f5cf7
Tristan Hume <tris.hume@gmail.com>
2014-10-18 15:17:57 -0400

    Detect more ledger files

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -1277,7 +1277,7 @@ inserted in the buffer (if it is not read-only)."
 
 (defun spacemacs/init-ledger-mode ()
   (use-package ledger-mode
-    :mode ("\\.ledger\\'" . ledger-mode)
+    :mode ("\\.\\(ledger\\|ldg\\)\\'" . ledger-mode)
     :init
     (progn
       (setq ledger-post-amount-alignment-column 62)
