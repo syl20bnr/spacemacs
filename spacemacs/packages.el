dab680f7792110f7775f34e99cb5f3070f0bbd75
syl20bnr <sylvain.benner@gmail.com>
2014-10-30 22:48:57 -0400

    Fixes #40 and #53 Move evil-nerd-commenter to extensions
    Work-around a current bug in evil-nc with 24.4 byte-compilation

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -34,7 +34,6 @@
     evil-surround
     evil-terminal-cursor-changer
     evil-visualstar
-    evil-nerd-commenter
     exec-path-from-shell
     expand-region
     fill-column-indicator
@@ -364,20 +363,6 @@ DELETE-FUNC when calling CALLBACK.
       ;; load surround
       (use-package evil-surround
         :init (global-evil-surround-mode 1))
-      ;; load nerd-commenter
-      (if (version< emacs-version "24.4")
-          (use-package evil-nerd-commenter
-            :init
-            (progn
-              (evil-leader/set-key
-                "ncl" 'evilnc-comment-or-uncomment-lines
-                "nct" 'evilnc-quick-comment-or-uncomment-to-the-line
-                "ncy" 'evilnc-copy-and-comment-lines
-                "ncp" 'evilnc-comment-or-uncomment-paragraphs
-                "ncr" 'comment-or-uncomment-region
-                "nci" 'evilnc-toggle-invert-comment-line-by-line
-                "ncc" 'evilnc-comment-operator
-                ))))
       ;; load evil-exchange
       (use-package evil-exchange
         :init (evil-exchange-install))
