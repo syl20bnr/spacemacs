d60dffda1ca7fc9cedc6b079911e69f777852f8b
syl20bnr <sylvain.benner@gmail.com>
2014-11-06 00:05:30 -0500

    Add function echo
    Display a message in echo area without logging it in *Messages* buffer

--- spacemacs/funcs.el
+++ spacemacs/funcs.el
@@ -11,6 +11,12 @@
   (dolist (fun funs)
     (add-hook hook fun)))
 
+(defun echo (msg &rest args)
+  "Display MSG in echo-area without logging it in *Messages* buffer."
+  (interactive)
+  (let ((message-log-max nil))
+    (apply 'message msg args)))
+
 (defun system-is-mac ()
   (interactive)
   (string-equal system-type "darwin"))
