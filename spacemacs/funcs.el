a21daaac4aff528b992e64d1d330aff41217a140
ralesi <scio62@gmail.com>
2014-11-14 10:25:44 -0700

    Added ido-invoke options - open in other window - open in other frame

--- spacemacs/funcs.el
+++ spacemacs/funcs.el
@@ -354,6 +354,61 @@ changed to THEME."
          (delete-windows-on buf)
          (message "compilation ok.")))))
 
+;; from https://gist.github.com/timcharper/493269
+(defun split-window-vertically-and-switch ()
+  (interactive)
+  (split-window-vertically)
+  (other-window 1))
+
+(defun split-window-horizontally-and-switch ()
+  (interactive)
+  (split-window-horizontally)
+  (other-window 1))
+
+(defun ido-invoke-in-other-window ()
+  "signals ido mode to switch to (or create) another window after exiting"
+  (interactive)
+  (setq ido-exit-minibuffer-target-window 'other)
+  (ido-exit-minibuffer))
+
+(defun ido-invoke-in-horizontal-split ()
+  "signals ido mode to split horizontally and switch after exiting"
+  (interactive)
+  (setq ido-exit-minibuffer-target-window 'horizontal)
+  (ido-exit-minibuffer))
+
+(defun ido-invoke-in-vertical-split ()
+  "signals ido mode to split vertically and switch after exiting"
+  (interactive)
+  (setq ido-exit-minibuffer-target-window 'vertical)
+  (ido-exit-minibuffer))
+
+(defun ido-invoke-in-new-frame ()
+  "signals ido mode to create a new frame after exiting"
+  (interactive)
+  (setq ido-exit-minibuffer-target-window 'frame)
+  (ido-exit-minibuffer))
+
+(defadvice ido-read-internal (around ido-read-internal-with-minibuffer-other-window activate)
+  (let* (ido-exit-minibuffer-target-window
+         (this-buffer (current-buffer))
+         (result ad-do-it))
+    (cond
+     ((equal ido-exit-minibuffer-target-window 'other)
+      (if (= 1 (count-windows))
+          (split-window-horizontally-and-switch)
+        (other-window 1)))
+     ((equal ido-exit-minibuffer-target-window 'horizontal)
+      (split-window-horizontally-and-switch))
+
+     ((equal ido-exit-minibuffer-target-window 'vertical)
+      (split-window-vertically-and-switch))
+     ((equal ido-exit-minibuffer-target-window 'frame)
+      (make-frame)))
+    (switch-to-buffer this-buffer) ;; why? Some ido commands, such as textmate.el's textmate-goto-symbol don't switch the current buffer
+    result))
+
+
 ;; from https://gist.github.com/cofi/3013327
 (defun cofi/helm-flyspell-correct ()
     "Use helm for flyspell correction.
