7b422e0597e26495344b7a92ab3e8eca3382b871
Diego Berrocal <cestdiego@gmail.com>
2014-11-27 14:59:24 -0500

    Add restclient contribution layer

---
+++ contrib/restclient/packages.el
@@ -0,0 +1,25 @@
+(defvar restclient-packages
+  '(
+    restclient
+    )
+  )
+
+(defun restclient/init-restclient ()
+  (use-package restclient
+    :mode ("\\.http\\'" . restclient-mode)
+    :defer t
+    :init
+    (progn
+
+      (defun restclient-http-send-current-raw-stay-in-window ()
+        (interactive)
+        (restclient-http-send-current t t))
+
+      (evil-leader/set-key-for-mode 'restclient-mode
+        "ms" 'restclient-http-send-current-stay-in-window
+        "mS" 'restclient-http-send-current
+        "mr" 'restclient-http-send-current-raw-stay-in-window
+        "mR" 'restclient-http-send-current-raw
+        ))
+    )
+  )
