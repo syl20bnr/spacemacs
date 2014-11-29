7a40b67d1d50409d64730d10266a58da27758fb4
Joris Engbers <info@jorisengbers.nl>
2014-11-29 12:28:57 +0100

    Pylookup package in python now uses correct directory

--- contrib/lang/python/extensions.el
+++ contrib/lang/python/extensions.el
@@ -47,7 +47,7 @@
     (progn
       (add-to-list 'evil-emacs-state-modes 'pylookup-mode)
       (evil-add-hjkl-bindings pylookup-mode-map 'emacs)
-      (let ((dir (config-system/get-layer-property 'spacemacs :ext-dir)))
+      (let ((dir (config-system/get-layer-property 'python :ext-dir)))
         (setq pylookup-dir (concat dir "/pylookup")
               pylookup-program (concat pylookup-dir "/pylookup.py")
               pylookup-db-file (concat pylookup-dir "/pylookup.db"))))))
