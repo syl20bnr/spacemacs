f53d671971493c19885b1b8bffae7d68a2b409c7
nmerino <noheliams1@gmail.com>
2014-11-29 20:57:09 -0500

    Adding pcre2el
    Spaces
    
    Fixing wrong spaces
    
    Spaces fixed
    
    Fixing spaces
    
    Fixing org
    
    Fixing file
    
    Return previous
    
    Fixing file org
    
    fixing org file
    
    Fixing files
    
    Changing
    
    Returning
    
    Fixing format
    
    Updating and fixing

---
+++ contrib/pcre2el/packages.el
@@ -0,0 +1,35 @@
+(defvar pcre2el-packages
+  '(
+    pcre2el
+    ))
+
+
+(defun pcre2el/init-pcre2el ()
+  (use-package pcre2el
+    :defer t
+    :commands rxt-fontify-regexp-at-point
+    :init
+    (progn
+      ;; 
+      (define-prefix-command 'pcre2el-prefix)
+      (evil-leader/set-key "R" 'pcre2el-prefix)
+      (evil-leader/set-key
+        "R/"  'rxt-explain
+        "Rc"  'rxt-convert-syntax
+        "Rx"  'rxt-convert-to-rx
+        "R'"  'rxt-convert-to-strings
+        "Rpe" 'rxt-pcre-to-elisp
+        "R%"  'pcre-query-replace-regexp
+        "Rpx" 'rxt-pcre-to-rx
+        "Rps" 'rxt-pcre-to-sre  
+        "Rp'" 'rxt-pcre-to-strings
+        "Rp/" 'rxt-explain-pcre
+        "Re/" 'rxt-explain-elisp
+        "Rep" 'rxt-elisp-to-pcre
+        "Rex" 'rxt-elisp-to-rx
+        "Res" 'rxt-elisp-to-sre
+        "Re'" 'rxt-elisp-to-strings
+        "Ret" 'rxt-toggle-elisp-rx
+        "Rt"  'rxt-toggle-elisp-rx
+        "Rh"  'rxt-fontify-regexp-at-point))))
+
