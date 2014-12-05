528c4efec3bfe3edbb73e74088b9c5c6ee40a801
syl20bnr <sylvain.benner@gmail.com>
2014-12-05 15:32:41 -0500

    Add `p` to iedit state to replace occurrence with last yanked (copied) text

--- contrib/iedit/extensions/evil-iedit-state/evil-iedit-state.el
+++ contrib/iedit/extensions/evil-iedit-state/evil-iedit-state.el
@@ -42,6 +42,12 @@
   (evil-iedit-state)  ; required to correctly update the cursors
   (evil-iedit-insert-state))
 
+(defun evil-iedit-state/paste-replace (count)
+  "Replace the selection with the yanked text."
+  (interactive "P")
+  (iedit-delete-occurrences)
+  (evil-paste-before count))
+
 ;; expand-region integration, add an "e" command
 (eval-after-load 'expand-region
   '(progn
@@ -64,6 +70,31 @@
                      (add-to-list 'default-bindings
                                   '("e" evil-iedit-state/iedit-mode-from-expand-region))))))))
 
+;; redefine iedit-done to prevent iedit from putting the occurrence on the
+;; kill-ring for no useful reason.
+(defun iedit-done ()
+  "Exit Iedit mode.
+Save the current occurrence string locally and globally.  Save
+the initial string globally."
+  (when iedit-buffering
+      (iedit-stop-buffering))
+  (setq iedit-last-occurrence-local (iedit-current-occurrence-string))
+  (setq iedit-last-occurrence-global iedit-last-occurrence-local)
+  (setq iedit-last-initial-string-global iedit-initial-string-local)
+  ;; this is the hack
+  ;; (if iedit-last-occurrence-local
+  ;; (kill-new iedit-last-occurrence-local)) ; Make occurrence the latest kill in the kill ring.
+  (setq iedit-num-lines-to-expand-up 0)
+  (setq iedit-num-lines-to-expand-down 0)
+  (iedit-cleanup)
+  (setq iedit-initial-string-local nil)
+  (setq iedit-mode nil)
+  (force-mode-line-update)
+  (remove-hook 'kbd-macro-termination-hook 'iedit-done t)
+  (remove-hook 'change-major-mode-hook 'iedit-done t)
+  (remove-hook 'iedit-aborting-hook 'iedit-done t)
+  (run-hooks 'iedit-mode-end-hook))
+
 (define-key evil-iedit-state-map "#"  'iedit-number-occurrences)
 (define-key evil-iedit-state-map "b"  'iedit-blank-occurrences)
 (define-key evil-iedit-state-map "B"  'iedit-toggle-buffering)
@@ -84,6 +115,7 @@
 (define-key evil-iedit-state-map "L"  'iedit-restrict-current-line)
 (define-key evil-iedit-state-map "n"  'iedit-next-occurrence)
 (define-key evil-iedit-state-map "N"  'iedit-prev-occurrence)
+(define-key evil-iedit-state-map "p"  'evil-iedit-state/paste-replace)
 (define-key evil-iedit-state-map "r"  'evil-iedit-state/replace)
 (define-key evil-iedit-state-map "v"  'iedit-toggle-unmatched-lines-visible)
 (define-key evil-iedit-state-map "u"  'undo-tree-undo)
