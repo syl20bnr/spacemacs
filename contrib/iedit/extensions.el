981810118865f57910cfe3891dfb43c214af5727
syl20bnr <sylvain.benner@gmail.com>
2014-12-05 22:15:17 -0500

    Enable evil-escape `fd` in iedit states to exit to `normal state`

--- contrib/iedit/extensions.el
+++ contrib/iedit/extensions.el
@@ -8,11 +8,19 @@
     (setq evil-iedit-state-cursor `(,(spacemacs/state-color 'iedit) box))   
     (setq evil-iedit-insert-state-cursor `((spacemacs/state-color 'iedit-insert) (bar . 2)))
     (evil-leader/set-key "se" 'evil-iedit-state/iedit-mode)
-    ;; override the basic edit mode from ahs everywhere
-    (eval-after-load 'auto-highlight-symbol
-      '(progn
-         (evil-leader/set-key "se" 'evil-iedit-state/iedit-mode)
-         (defalias 'ahs-edit-mode 'evil-iedit-state/iedit-mode))))
+    (when (and (boundp 'evil-escape-mode)
+               (symbol-value evil-escape-mode))
+      (key-chord-define evil-iedit-state-map
+                        evil-escape-key-sequence
+                        'evil-iedit-state/quit-iedit-mode)
+      (key-chord-define evil-iedit-insert-state-map
+                        evil-escape-key-sequence
+                        'evil-iedit-state/quit-iedit-mode)))
+  ;; override the basic edit mode from ahs everywhere
+  (eval-after-load 'auto-highlight-symbol
+    '(progn
+       (evil-leader/set-key "se" 'evil-iedit-state/iedit-mode)
+       (defalias 'ahs-edit-mode 'evil-iedit-state/iedit-mode)))
   (add-to-hooks 'iedit//lazy-load '(prog-mode-hook markdown-mode-hook)))
 
 
