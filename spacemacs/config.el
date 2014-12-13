1a44b042deb4c8f6242a37781cc83cfcd97a200c
syl20bnr <sylvain.benner@gmail.com>
2014-12-13 14:16:50 -0500

    Fixes #258 C-u to scroll up doesn't work anymore

--- spacemacs/config.el
+++ spacemacs/config.el
@@ -55,8 +55,6 @@
 ;; no beep pleeeeeease ! (and no visual blinking too please)
 (custom-set-variables '(ring-bell-function 'ignore))
 (setq visible-bell nil)
-;; use C-u as scroll-up
-(setq-default evil-want-C-u-scroll t)
 
 ;; Hack to fix a bug with tabulated-list.el
 ;; see: http://redd.it/2dgy52
