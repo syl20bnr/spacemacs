2e42dc999d7c2700ebacf047d8789c76f4a97fcd
syl20bnr <sylvain.benner@gmail.com>
2014-12-17 21:48:43 -0500

    Fix spacemacs|evilify `n` and `N` key bindings

--- spacemacs/funcs.el
+++ spacemacs/funcs.el
@@ -84,8 +84,8 @@ a key sequence. NAME is a symbol name used as the prefix command."
 bindings contained in BODY."
   `(evil-add-hjkl-bindings ,map 'emacs
     "/" 'evil-search-forward
-    "n" ',(lookup-key evil-normal-state-map "n")
-    "N" ',(lookup-key evil-normal-state-map "N")
+    "n" ',(lookup-key evil-motion-state-map "n")
+    "N" ',(lookup-key evil-motion-state-map "N")
     "v" 'evil-visual-char
     "V" 'evil-visual-line
     (kbd "C-v") 'evil-visual-block
