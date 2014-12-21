9b03d63d65cdafc72342c2cd5758cda89e8fa627
Rich Alesi <scio62@gmail.com>
2014-12-21 15:07:22 -0700

    Update AutoHotkey layer, fix bugs, improve indentation

---
+++ contrib/lang/autohotkey/packages.el
@@ -0,0 +1,7 @@
+(defvar autohotkey-packages
+  '(
+    )
+  "List of all packages to install and/or initialize. Built-in packages
+which require an initialization must be listed explicitly in the list."
+  ) 
+
