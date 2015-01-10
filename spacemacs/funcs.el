d719b78e93cbd47a56f83fdf74b7ad760e253b73
Daniel Wu <zw339@nyu.edu>
2015-01-10 10:13:09 -0500

    added open file in external application function

--- spacemacs/funcs.el
+++ spacemacs/funcs.el
@@ -840,3 +840,14 @@ If ASCII si not provided then UNICODE is used instead."
   (save-excursion
     (evil-end-of-line)
     (eval-last-sexp nil)))
+
+(defun spacemacs/open-in-external-app ()
+  "Open current file in external application."
+  (interactive)
+  (let ((file-path (buffer-file-name)))
+    (cond
+     ((system-is-mswindows) (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" file-path)))
+     ((system-is-mac) (shell-command (format "open \"%s\"" file-path)))
+     ((system-is-linux) (let ((process-connection-type nil))
+                          (start-process "" nil "xdg-open" file-path)))
+     )))
