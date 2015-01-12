035dcee7339c1f63894f2ed15e4dd203b683d3bd
sooheon <tngjs0@gmail.com>
2015-01-12 22:01:32 +0900

    Add vi-tilde-fringe to excluded packages.

--- contrib/vim-empty-lines/packages.el
+++ contrib/vim-empty-lines/packages.el
@@ -2,7 +2,7 @@
   '(vim-empty-lines-mode))
 
 (defvar vim-empty-lines-mode-excluded-packages
-  '()
+  '(vi-tilde-fringe)
   "This contrib layer is meant to be an alternative/direct replacement to `vi-tilde-fringe'.")
 
 (defun vim-empty-lines/init-vim-empty-lines-mode ()
