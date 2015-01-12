0562d38a849a4a668e28c5519191a72974cd04ad
sooheon <tngjs0@gmail.com>
2015-01-12 21:59:32 +0900

    Add a vim-empty-lines contrib layer.

---
+++ contrib/vim-empty-lines/packages.el
@@ -0,0 +1,15 @@
+(defvar vim-empty-lines-packages
+  '(vim-empty-lines-mode))
+
+(defvar vim-empty-lines-mode-excluded-packages
+  '()
+  "This contrib layer is meant to be an alternative/direct replacement to `vi-tilde-fringe'.")
+
+(defun vim-empty-lines/init-vim-empty-lines-mode ()
+  (use-package vim-empty-lines-mode
+    :defer t
+    :init
+    (progn
+      (add-hook 'prog-mode-hook 'vim-empty-lines-mode) 
+      (add-hook 'text-mode-hook 'vim-empty-lines-mode))
+    ))
