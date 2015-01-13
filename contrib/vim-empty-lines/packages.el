60282adad15509eae16e21adff55f49d80a86e8a
syl20bnr <sylvain.benner@gmail.com>
2015-01-12 23:23:23 -0500

    Tweak vim-empty-lines layer

--- contrib/vim-empty-lines/packages.el
+++ contrib/vim-empty-lines/packages.el
@@ -1,15 +1,14 @@
 (defvar vim-empty-lines-packages
   '(vim-empty-lines-mode))
 
-(defvar vim-empty-lines-mode-excluded-packages
-  '(vi-tilde-fringe)
-  "This contrib layer is meant to be an alternative/direct replacement to `vi-tilde-fringe'.")
+(defvar vim-empty-lines-excluded-packages
+  '(vi-tilde-fringe))
 
 (defun vim-empty-lines/init-vim-empty-lines-mode ()
   (use-package vim-empty-lines-mode
     :defer t
     :init
     (progn
-      (add-hook 'prog-mode-hook 'vim-empty-lines-mode) 
-      (add-hook 'text-mode-hook 'vim-empty-lines-mode))
-    ))
+      (add-to-hooks 'vim-empty-lines-mode '(prog-mode-hook
+                                            erlang-mode-hook
+                                            text-mode-hook)))))
