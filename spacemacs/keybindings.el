f212dfbfd81224087fa4e390ead1409663a638cf
mkcode <chrisewald@gmail.com>
2015-01-14 17:10:13 +1100

    added "fj" to dired-jump keybinding

--- spacemacs/keybindings.el
+++ spacemacs/keybindings.el
@@ -72,6 +72,7 @@
   "fec" 'find-contrib-file
   "fed" 'find-dotfile
   "fev" 'spacemacs/display-and-copy-version
+  "fj" 'dired-jump
   "fo" 'spacemacs/open-in-external-app
   "fS" 'evil-write-all
   "fs" 'evil-write
