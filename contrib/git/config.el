916444d486e0b493c234621ffe16b40567defccf
syl20bnr <sylvain.benner@gmail.com>
2015-01-14 23:13:06 -0500

    Move setq magit-completing-read-function to init-magit function

--- contrib/git/config.el
+++ contrib/git/config.el
@@ -14,8 +14,6 @@
   "If non nil the Github packages and extensions are enabled.")
 
 (setq git/key-binding-prefixes '(("gh" . "gutter-hunks/highlight")))
-(setq magit-completing-read-function 'magit-ido-completing-read)
-
 (when git-enable-github-support
   (push (cons "gf" "file") git/key-binding-prefixes)
   (push (cons "gg" "gist") git/key-binding-prefixes))
