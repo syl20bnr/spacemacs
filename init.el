3d02adf755842810898dc44cd46d5b7fb27caa50
syl20bnr <sylvain.benner@gmail.com>
2015-01-20 00:47:05 -0500

    Better check for emacs version prerequisite
    Move the check at the very top of init.el
    Resolves #453

--- init.el
+++ init.el
@@ -9,13 +9,16 @@
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
+(defconst spacemacs-version     "0.48.2" "Spacemacs version.")
+(defconst spacemacs-min-version   "24.3" "Mininal required version of Emacs.")
 
-(add-to-list 'load-path (concat user-emacs-directory "core/"))
-
-(require 'spacemacs-mode)
-(require 'configuration-layer)
+(defun spacemacs/emacs-version-ok ()
+  (version<= spacemacs-min-version emacs-version))
 
 (when (spacemacs/emacs-version-ok)
+  (add-to-list 'load-path (concat user-emacs-directory "core/"))
+  (require 'spacemacs-mode)
+  (require 'configuration-layer)
   (dotspacemacs/load)
   (spacemacs/initialize)
   ;; Initializing configuration from ~/.spacemacs
