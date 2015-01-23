07511c7307d44198e3a54f7af3fce13286c2fb47
syl20bnr <sylvain.benner@gmail.com>
2015-01-22 22:39:26 -0500

    Missing unit test file

---
+++ core/test/configuration-layer-utest.el
@@ -0,0 +1,50 @@
+;;; configuration-layer-test.el --- Spacemacs Unit Test File
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+(require 'mocker)
+
+(ert-deftest dummy-test ()
+  "dummy"
+  (should (string-equal "" "")))
+
+(ert-deftest dummy-test1 ()
+  "dummy"
+  (should (string-equal "" "")))
+
+(ert-deftest dummy-test2 ()
+  "dummy"
+  (should (string-equal "" "")))
+
+(ert-deftest dummy-test3 ()
+  "dummy"
+  (should (string-equal "" "")))
+
+(ert-deftest dummy-test4 ()
+  "dummy"
+  (should (string-equal "" "")))
+
+(ert-deftest dummy-test5 ()
+  "dummy"
+  (should (string-equal "" "")))
+
+(ert-deftest dummy-test6 ()
+  "dummy"
+  (should (string-equal "" "")))
+
+(ert-deftest dummy-test7 ()
+  "dummy"
+  (should (string-equal "" "")))
+
+(ert-deftest dummy-test8 ()
+  "dummy"
+  (should (string-equal "" "")))
+
+
