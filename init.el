c2f846c47797940f1fd695b85dc03bb2341c745a
syl20bnr <sylvain.benner@gmail.com>
2015-01-27 00:33:10 -0500

    Bump version to 0.50.0

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
-(defconst spacemacs-version     "0.49.2" "Spacemacs version.")
+(defconst spacemacs-version     "0.50.0" "Spacemacs version.")
 (defconst spacemacs-min-version   "24.3" "Minimal required version of Emacs.")
 
 (defun spacemacs/emacs-version-ok ()
