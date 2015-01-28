1b14b0863aa62a6eabbc2f1a9e6a0fdaaccc17b2
Andrew Oppenlander <andrew.oppenlander@zipscene.com>
2015-01-28 10:24:24 -0500

    Add variable `clojure-enable-fancify-symbol` in Clojure mode
    Used to turn on fancify-symbol (default value is nil, i.e. disabled)

---
+++ contrib/lang/clojure/config.el
@@ -0,0 +1,20 @@
+;;; config.el --- Clojure Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; ---------------------------------------------------------------------------
+;; Prefixes
+;; ---------------------------------------------------------------------------
+
+;; Variables
+
+(defvar clojure-enable-fancify-symbols nil
+  "If non nil the `fancify-symbols' function is enabled.")
