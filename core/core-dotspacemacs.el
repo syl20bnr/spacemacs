ba86ae776925ddb9688bf4c40aa1a47781a39d19
syl20bnr <sylvain.benner@gmail.com>
2015-01-27 21:45:39 -0500

    Add leuven monokai and zenburn to default dotspacemacs-themes

--- core/core-dotspacemacs.el
+++ core/core-dotspacemacs.el
@@ -28,7 +28,11 @@ the value is nil then no banner is displayed.")
   "list of contribution to load."
 )
 
-(defvar dotspacemacs-themes '(solarized-light solarized-dark)
+(defvar dotspacemacs-themes '(solarized-light
+                              solarized-dark
+                              leuven
+                              monokai
+                              zenburn)
   "List of themes, the first of the list is loaded when spacemacs starts.
 Press <SPC> T n to cycle to the next theme in the list (works great
 with 2 themes variants, one dark and one light")
