9101b05a369c5e565a63994a45756b600672f8ff
Riccardo Cambiassi <r.cambiassi@gmail.com>
2015-01-31 11:05:37 +0000

    add support for rails to ruby layer

--- contrib/lang/ruby/README.md
+++ contrib/lang/ruby/README.md
@@ -9,7 +9,10 @@
     - [Description](#description)
     - [Install](#install)
         - [Ruby version manager](#ruby-version-manager)
-    - [Key bindings](#key-bindings)
+    - [Prerequisites](#prerequisites)
+    - [Ruby version management](#ruby-version-management)
+    - [Rails support](#rails-support)
+   - [Key bindings](#key-bindings)
         - [enh-ruby-mode](#enh-ruby-mode)
         - [ruby-test-mode](#ruby-test-mode)
 
@@ -30,21 +33,30 @@ To use this contribution add it to your `~/.spacemacs`
 )
 ```
 
-In order to take advantage of `robe-mode` you will probably need to
-install the `pry` gem.
-You can do that via your Gemfile:
+### Prerequisites
+
+Some of the advanced features supported by this layer depend on external gems
+that need to be installed in the context of your project (see below for guidance
+based on your version manager):
+
+* `pry` and `pry-doc` are required for *jump to definition* and *code documentation* (`robe-mode`)
+* `ruby_parser` is required for *goto-step_definition* in `feature-mode`
+
+You can install the gems in the context of your current project by
+adding them to the `Gemfile`, e.g.:
 
 ```ruby
  gem 'pry'
 ```
 
-or on the command line:
+or on the command line (please refer to your ruby version manager
+specific documentation for details and caveats):
 
 ```shell
 $ gem install pry
 ```
 
-### Ruby version manager
+### Ruby version management
 
 This layer supports the use of [RVM][] and [Rbenv][].
 To enable it, set the `ruby-version-manager` var in your `~/.spacemacs`:
@@ -56,6 +68,22 @@ To enable it, set the `ruby-version-manager` var in your `~/.spacemacs`:
 ```
 
 Possible values are `rbenv` and `rvm`.
+
+### Rails support
+
+Rails support is available through [projectile-rails][].
+To enable it, set the `ruby-on-rails-support` var in your
+`~/.spacemacs`:
+
+```elisp
+(defun dotspacemacs/init ()
+  (setq-default ruby-on-rails-support t)
+)
+```
+
+This will also add [haml-mode][] (for templates written in [haml
+language](http://haml.info) and [feature-mode][] for
+[Cucumber](http://cukes.info) support.
 
 ## Key bindings
 
@@ -74,8 +102,72 @@ a couple of useful keybindings:
 <kbd>SPC m t b</kbd> run test file
 <kbd>SPC m t t</kbd> run test at pointer
 
+### projectile-rails
+
+#### GOTO commands
+
+<kbd>SPC m r f</kbd> goto file at point
+<kbd>SPC m r g</kbd> goto Gemfile
+<kbd>SPC m r r</kbd> goto routes file
+<kbd>SPC m r d</kbd> goto DB schema file
+<kbd>SPC m r s</kbd> goto DB seeds file
+<kbd>SPC m r h</kbd> goto spec helper
+
+#### Find commands
+<kbd>SPC m r m</kbd> find rails model
+<kbd>SPC m r M</kbd> find current rails model
+
+<kbd>SPC m r c</kbd> find rails controller
+<kbd>SPC m r C</kbd> find current rails controller
+
+<kbd>SPC m r v</kbd> find rails view
+<kbd>SPC m r V</kbd> find current rails view
+
+<kbd>SPC m r j</kbd> find javascript file
+<kbd>SPC m r J</kbd> find current javascript
+
+<kbd>SPC m r s</kbd> find stylesheet file
+<kbd>SPC m r S</kbd> find current stylesheet
+
+<kbd>SPC m r h</kbd> find rails helper
+<kbd>SPC m r H</kbd> find current rails helper
+
+<kbd>SPC m r p</kbd> find spec file
+<kbd>SPC m r P</kbd> find current spec file
+
+<kbd>SPC m r t</kbd> find test file
+<kbd>SPC m r T</kbd> find current test file
+
+<kbd>SPC m r n</kbd> find migration
+<kbd>SPC m r N</kbd> find current migration
+
+<kbd>SPC m r u</kbd> find fixture
+<kbd>SPC m r U</kbd> find current fixture
+
+<kbd>SPC m r l</kbd> find lib file
+<kbd>SPC m r f</kbd> find feature file
+<kbd>SPC m r i</kbd> find initializer
+<kbd>SPC m r o</kbd> find log
+<kbd>SPC m r e</kbd> find environment file
+<kbd>SPC m r a</kbd> find localization file
+<kbd>SPC m r @</kbd> find mailer
+<kbd>SPC m r y</kbd> find layout
+<kbd>SPC m r k</kbd> find rake task
+
+<kbd>SPC m r x</kbd> extract region into partial
+
+#### RUN commands
+<kbd>SPC mrc</kbd> start rails console
+<kbd>SPC mrs</kbd> start rails server
+<kbd>SPC mrr</kbd> run rake task
+<kbd>SPC mrg</kbd> run rails generator
+
 
 [enh-ruby-mode]: https://github.com/zenspider/enhanced-ruby-mode
 [robe-mode]: https://github.com/dgutov/robe
 [Rbenv]: https://github.com/sstephenson/rbenv
 [RVM]: https://rvm.io/
+[projectile-rails]: https://github.com/asok/projectile-rails
+[haml-mode]: https://github.com/nex3/haml-mode
+[feature-mode]: https://github.com/michaelklishin/cucumber.el
+[rspec-mode]: https://github.com/pezra/rspec-mode
--- contrib/lang/ruby/packages.el
+++ contrib/lang/ruby/packages.el
@@ -6,12 +6,18 @@
     ruby-test-mode
     robe
     yaml-mode
+
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
 (when ruby-version-manager
   (add-to-list 'ruby-packages ruby-version-manager))
+
+(when ruby-on-rails-support
+  (add-to-list 'ruby-packages 'haml-mode)
+  (add-to-list 'ruby-packages 'feature-mode)
+  (add-to-list 'ruby-packages 'projectile-rails))
 
 (defvar ruby-excluded-packages '(ruby-mode))
 
@@ -41,15 +47,82 @@ which require an initialization must be listed explicitly in the list.")
 (defun ruby/init-flycheck ()
   (add-hook 'enh-ruby-mode-hook 'flycheck-mode))
 
+(defun ruby/init-projectile-rails ()
+  (use-package projectile-rails
+    :defer t
+    :init (progn
+            (add-hook 'projectile-mode-hook 'projectile-rails-on))
+    :config (progn
+              (spacemacs|diminish projectile-rails-mode " ⇋" " R")
+              ;; GOTO commands
+              (evil-leader/set-key "mrf" 'projectile-rails-goto-file-at-point)
+              (evil-leader/set-key "mrg" 'projectile-rails-goto-gemfile)
+              (evil-leader/set-key "mrr" 'projectile-rails-goto-routes)
+              (evil-leader/set-key "mrd" 'projectile-rails-goto-schema)
+              (evil-leader/set-key "mrs" 'projectile-rails-goto-seeds)
+              (evil-leader/set-key "mrh" 'projectile-rails-goto-spec-helper)
+              ;; Find commands
+              (evil-leader/set-key "mrm" 'projectile-rails-find-model)
+              (evil-leader/set-key "mrM" 'projectile-rails-find-current-model)
+
+              (evil-leader/set-key "mrc" 'projectile-rails-find-controller)
+              (evil-leader/set-key "mrC" 'projectile-rails-find-current-controller)
+
+              (evil-leader/set-key "mrv" 'projectile-rails-find-view)
+              (evil-leader/set-key "mrV" 'projectile-rails-find-current-view)
+
+              (evil-leader/set-key "mrj" 'projectile-rails-find-javascript)
+              (evil-leader/set-key "mrJ" 'projectile-rails-find-current-javascript)
+
+              (evil-leader/set-key "mrs" 'projectile-rails-find-stylesheet)
+              (evil-leader/set-key "mrS" 'projectile-rails-find-current-stylesheet)
+
+              (evil-leader/set-key "mrh" 'projectile-rails-find-helper)
+              (evil-leader/set-key "mrH" 'projectile-rails-find-current-helper)
+
+              (evil-leader/set-key "mrp" 'projectile-rails-find-spec)
+              (evil-leader/set-key "mrP" 'projectile-rails-find-current-spec)
+
+              (evil-leader/set-key "mrt" 'projectile-rails-find-test)
+              (evil-leader/set-key "mrT" 'projectile-rails-find-current-test)
+
+              (evil-leader/set-key "mrn" 'projectile-rails-find-migration)
+              (evil-leader/set-key "mrN" 'projectile-rails-find-current-migration)
+
+              (evil-leader/set-key "mru" 'projectile-rails-find-fixture)
+              (evil-leader/set-key "mrU" 'projectile-rails-find-current-fixture)
+
+              (evil-leader/set-key "mrl" 'projectile-rails-find-lib)
+              (evil-leader/set-key "mrf" 'projectile-rails-find-feature)
+              (evil-leader/set-key "mri" 'projectile-rails-find-initializer)
+              (evil-leader/set-key "mro" 'projectile-rails-find-log)
+              (evil-leader/set-key "mre" 'projectile-rails-find-environment)
+              (evil-leader/set-key "mra" 'projectile-rails-find-locale)
+              (evil-leader/set-key "mr@" 'projectile-rails-find-mailer)
+              (evil-leader/set-key "mry" 'projectile-rails-find-layout)
+              (evil-leader/set-key "mrk" 'projectile-rails-find-rake-task)
+
+              (evil-leader/set-key "mrx" 'projectile-rails-extract-region)
+
+              ;; RUN commands
+              (evil-leader/set-key "mrc" 'projectile-rails-console)
+              (evil-leader/set-key "mrs" 'projectile-rails-server)
+              (evil-leader/set-key "mrr" 'projectile-rails-rake)
+              (evil-leader/set-key "mrg" 'projectile-rails-generate)
+   )
+  ))
+
 (defun ruby/init-robe ()
   "Initialize Robe mode"
   (use-package robe
     :defer t
     :init (add-hook 'enh-ruby-mode-hook 'robe-mode)
-    :config (progn (evil-leader/set-key-for-mode 'enh-ruby-mode "mgg" 'robe-jump)
-                   (evil-leader/set-key-for-mode 'enh-ruby-mode "mhd" 'robe-doc)
-                   (evil-leader/set-key-for-mode 'enh-ruby-mode "mR" 'robe-rails-refresh)
-                   (evil-leader/set-key-for-mode 'enh-ruby-mode "mi" 'robe-start))))
+    :config (progn
+              (spacemacs|diminish robe-mode " ♦" " r")
+              (evil-leader/set-key-for-mode 'enh-ruby-mode "mgg" 'robe-jump)
+              (evil-leader/set-key-for-mode 'enh-ruby-mode "mhd" 'robe-doc)
+              (evil-leader/set-key-for-mode 'enh-ruby-mode "mR" 'robe-rails-refresh)
+              (evil-leader/set-key-for-mode 'enh-ruby-mode "mi" 'robe-start))))
 
 (defun ruby/init-yaml-mode ()
   "Initialize YAML mode"
@@ -61,6 +134,11 @@ which require an initialization must be listed explicitly in the list.")
     :mode (("\\.\\(yml\\|yaml\\)\\'" . yaml-mode)
            ("Procfile\\'" . yaml-mode))))
 
+(defun ruby/init-feature-mode ()
+  "Initialize Cucumber feature mode"
+  (use-package feature-mode
+    :defer t
+    :mode (("\\.feature\\'" . feature-mode))))
 
 
 (defun ruby/init-ruby-test-mode ()
