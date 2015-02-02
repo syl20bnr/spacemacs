6c63207a4dcfb31232e6d7894742aa719aadfb19
Brian Hicks <brian@brianthicks.com>
2015-02-02 00:31:11 -0600

    contrib(yaml): add yaml layer
    This is pretty directly extracted from layers that already include
    `yaml-mode`, but there are situations in which one would want to use
    yaml directly, instead of an accessory language, such as editing
    configuration files or writing Salt states or Ansible roles.

---
+++ contrib/lang/yaml/packages.el
@@ -0,0 +1,16 @@
+;;; extensions.el --- YAML Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+(defvar yaml-packages '(yaml-mode))
+
+(defun yaml/yaml-mode-init ()
+  (use-package yaml-mode
+    :defer t))
