6a9304b83d82a71297987b36f2d8649d49d8fbd1
syl20bnr <sylvain.benner@gmail.com>
2015-02-03 23:56:38 -0500

    Add rollback support

--- core/core-funcs.el
+++ core/core-funcs.el
@@ -46,4 +46,23 @@ and its values are removed."
       (push (pop tail) result))
     (nreverse result)))
 
+;; From http://stackoverflow.com/questions/2321904/elisp-how-to-save-data-in-a-file
+(defun spacemacs/dump-vars-to-file (varlist filename)
+  "simplistic dumping of variables in VARLIST to a file FILENAME"
+  (save-excursion
+    (let ((buf (find-file-noselect filename)))
+      (set-buffer buf)
+      (erase-buffer)
+      (spacemacs/dump varlist buf)
+      (make-directory (file-name-directory filename) t)
+      (save-buffer)
+      (kill-buffer))))
+
+;; From http://stackoverflow.com/questions/2321904/elisp-how-to-save-data-in-a-file
+(defun spacemacs/dump (varlist buffer)
+  "insert into buffer the setq statement to recreate the variables in VARLIST"
+  (loop for var in varlist do
+        (print (list 'setq var (list 'quote (symbol-value var)))
+               buffer)))
+
 (provide 'core-funcs)
