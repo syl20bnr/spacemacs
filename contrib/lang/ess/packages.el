638d969a19ba7bf378e4cbec423b49ce2068e3da
sooheon <tngjs0@gmail.com>
2015-02-05 09:36:08 +0900

    Add rainbow-delimiters hook, add R-mode to auto-mode-alist, add convenience keybinding, and add edit default offsets.

--- contrib/lang/ess/packages.el
+++ contrib/lang/ess/packages.el
@@ -27,8 +27,16 @@ which require an initialization must be listed explicitly in the list.")
   ;; R --------------------------------------------------------------------------
   (eval-after-load "ess-site"
     '(progn
+       (add-to-list 'auto-mode-alist '("\\.R$" . R-mode))
+       (add-hook 'ess-mode-hook #'rainbow-delimiters-mode)
        (add-hook 'ess-mode-hook 'ess-smart-equals-mode)
        (add-hook 'inferior-ess-mode-hook 'ess-smart-equals-mode)
+       ;; Follow Hadley Wickham's R style guide
+       (setq ess-first-continued-statement-offset 2
+             ess-continued-statement-offset 0
+             ess-expression-offset 2
+             ess-nuke-trailing-whitespace-p t
+             ess-default-style 'DEFAULT)
        (evil-leader/set-key-for-mode 'ess-mode
          "mi" 'R
          "mp" 'ess-R-object-popup
@@ -45,5 +53,6 @@ which require an initialization must be listed explicitly in the list.")
          "mvp" 'ess-R-dv-pprint
          "mvt" 'ess-R-dv-ctable
          )
+       (define-key ess-mode-map (kbd "<s-return>") 'ess-eval-line)
        (define-key inferior-ess-mode-map (kbd "C-j") 'comint-next-input)
        (define-key inferior-ess-mode-map (kbd "C-k") 'comint-previous-input))))
