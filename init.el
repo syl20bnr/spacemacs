570f5e5e05d6f83c54188b92fdfdf91859153993
syl20bnr <sylvain.benner@gmail.com>
2015-02-11 17:00:22 -0500

    Move dotspacemacs/config to after-init-hook
    Add log in `*Messages*` when executing `init` and `config` dotspacemacs
    functions
    
    resolves #588

--- init.el
+++ init.el
@@ -22,14 +22,13 @@
   (dotspacemacs/load)
   (spacemacs/initialize)
   ;; Initializing configuration from ~/.spacemacs
-  (dotspacemacs|call-func dotspacemacs/init)
+  (dotspacemacs|call-func dotspacemacs/init "Executing user init...")
   ;; synchronize and load configuration layers
   (configuration-layer/declare-all-layers)
   (configuration-layer/load-layers)
   (configuration-layer/delete-orphan-packages)
   ;; Ultimate configuration decisions are given to the user who can defined
   ;; them in his/her ~/.spacemacs file
-  (dotspacemacs|call-func dotspacemacs/config)
   (configuration-layer/setup-after-init-hook)
   ;; start a server for subsequent emacs clients
   (require 'server)
