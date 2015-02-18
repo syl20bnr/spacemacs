d42c350b9c25d24ec8cb9a613f32ecb6810269ee
syl20bnr <sylvain.benner@gmail.com>
2015-02-17 22:22:53 -0500

    Minor fixes to ansible layer

--- contrib/ansible/packages.el
+++ contrib/ansible/packages.el
@@ -1,4 +1,4 @@
-;;; extensions.el --- Ansible Layer extensions File for Spacemacs
+;;; packages.el --- Ansible Layer extensions File for Spacemacs
 ;;
 ;; Copyright (c) 2012-2014 Sylvain Benner
 ;; Copyright (c) 2015 Brian Hicks & Contributors
