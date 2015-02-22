27513562c4e3610c54bf46b6765542293b071275
Bjarke Vad <bjarke.vad90@gmail.com>
2015-02-22 12:03:46 +0100

    Added ghci-ng support to haskell layer
    - uses the variable `haskell-ghci-ng-support` to enable support

---
+++ contrib/lang/haskell/config.el
@@ -0,0 +1,14 @@
+;;; config.el --- Haskell Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2015 Bjarke Vad Andersen
+;;
+;; Author: Bjarke Vad Andersen <bjarke.vad90@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+(defvar haskell-ghci-ng-support nil
+  "If non nil ghci-ng support is enabled")
