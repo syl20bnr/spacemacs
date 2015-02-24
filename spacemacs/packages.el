1b7777be360bcc708cf27016d5559ecaba3a1c51
syl20bnr <sylvain.benner@gmail.com>
2015-02-23 23:47:05 -0500

    Move hippie-expand configuration in spacemacs/init-hippie-exp

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -74,6 +74,7 @@
     helm-swoop
     helm-themes
     highlight
+    hippie-exp
     hl-anything
     hungry-delete
     ido-vertical-mode
@@ -1378,6 +1379,32 @@ which require an initialization must be listed explicitly in the list.")
     :defer t
     :init
     (evil-leader/set-key "Th" 'helm-themes)))
+
+(defun spacemacs/init-hippie-exp ()
+  (global-set-key (kbd "M-/") 'hippie-expand) ;; replace dabbrev-expand
+  (setq hippie-expand-try-functions-list
+        '(
+          ;; Try to expand word "dynamically", searching the current buffer.
+          try-expand-dabbrev
+          ;; Try to expand word "dynamically", searching all other buffers.
+          try-expand-dabbrev-all-buffers
+          ;; Try to expand word "dynamically", searching the kill ring.
+          try-expand-dabbrev-from-kill
+          ;; Try to complete text as a file name, as many characters as unique.
+          try-complete-file-name-partially
+          ;; Try to complete text as a file name.
+          try-complete-file-name
+          ;; Try to expand word before point according to all abbrev tables.
+          try-expand-all-abbrevs
+          ;; Try to complete the current line to an entire line in the buffer.
+          try-expand-list
+          ;; Try to complete the current line to an entire line in the buffer.
+          try-expand-line
+          ;; Try to complete as an Emacs Lisp symbol, as many characters as
+          ;; unique.
+          try-complete-lisp-symbol-partially
+          ;; Try to complete word as an Emacs Lisp symbol.
+          try-complete-lisp-symbol)))
 
 (defun spacemacs/init-hl-anything ()
   (use-package hl-anything
