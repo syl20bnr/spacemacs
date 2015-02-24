97302118e0debdc4f722184b1413756e73cccd76
Christoph Paulik <cpaulik@gmail.com>
2015-02-24 16:42:22 +0100

    added keybindings for visual line movement in visual mode

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -604,6 +604,9 @@ which require an initialization must be listed explicitly in the list.")
       ;; Make evil-mode up/down operate in screen lines instead of logical lines
       (define-key evil-normal-state-map "j" 'evil-next-visual-line)
       (define-key evil-normal-state-map "k" 'evil-previous-visual-line)
+      ;; Also in visual mode
+      (define-key evil-visual-state-map "j" 'evil-next-visual-line)
+      (define-key evil-visual-state-map "k" 'evil-previous-visual-line)
       ;; Make the current definition and/or comment visible.
       (define-key evil-normal-state-map "zf" 'reposition-window)
       ;; quick navigation
