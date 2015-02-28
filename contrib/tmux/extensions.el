acfa23f20c48e41526d2fd65dc4350f0aed4e514
Ryan Phillips <ryan.phillips@rackspace.com>
2015-02-28 00:02:45 -0600

    add tmux contrib layer

---
+++ contrib/tmux/extensions.el
@@ -0,0 +1,5 @@
+(defvar evil-tmux-navigator-post-extensions '(evil-tmux-navigator))
+
+(defun evil-tmux-navigator/init-evil-tmux-navigator ()
+  "Initialize evil-tmux-navigator"
+  (use-package evil-tmux-navigator))
