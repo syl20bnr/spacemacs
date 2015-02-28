da719adf13535c1684342c52fd7c1c0230a8dc81
syl20bnr <sylvain.benner@gmail.com>
2015-02-27 22:06:52 -0500

    Add evil-snipe layer variable evil-snipe-enable-alternate-f-and-t-behaviors

---
+++ contrib/evil-snipe/config.el
@@ -0,0 +1,16 @@
+;;; config.el --- evil-snipe Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+
+(defvar evil-snipe-enable-alternate-f-and-t-behaviors nil
+  "if non nil f/F/t/T behaviors are replaced by evil-snipe behavior.")
