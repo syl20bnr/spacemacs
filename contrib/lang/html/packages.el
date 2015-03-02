e0911e7d200f594c1a5a44e6bd193d07e949cbde
Emmanuel Touzery <etouzery@gmail.com>
2015-03-02 21:52:30 +0100

    web-mode shortcuts.

--- contrib/lang/html/packages.el
+++ contrib/lang/html/packages.el
@@ -96,3 +96,31 @@ which require an initialization must be listed explicitly in the list.")
 (defun html/init-slim-mode ()
   (use-package slim-mode
     :defer t))
+
+(evil-leader/set-key-for-mode 'web-mode
+  "mgp" 'web-mode-element-parent
+  "mgc" 'web-mode-element-child
+  "mgb" 'web-mode-element-beginning
+  "mgn" 'spacemacs/html-navigation-micro-state
+  "mgs" 'web-mode-element-sibling-next
+  ;; v like 'View'
+  "mvx" 'web-mode-dom-xpath
+  "mve" 'web-mode-dom-errors-show
+  ;; i like 'Item'
+  "mir" 'web-mode-element-rename
+  "miw" 'web-mode-element-wrap
+  "mic" 'web-mode-element-clone
+  "mf" 'web-mode-fold-or-unfold
+  ;; TODO element close would be nice but broken with evil.
+  )
+
+(spacemacs|define-micro-state html-navigation
+  :doc "[N] previous [n] next [s] sibling [p] parent [c] child [q] quit"
+  :persistent t
+  :bindings
+  ("N" web-mode-element-previous)
+  ("n" web-mode-element-next)
+  ("s" web-mode-element-sibling-next)
+  ("p" web-mode-element-parent)
+  ("c" web-mode-element-child)
+  ("q" nil :exit t))
