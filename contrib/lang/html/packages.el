dcc1577f1d63887df3ba7e5d6d02826b3fb80603
Emmanuel Touzery <etouzery@gmail.com>
2015-03-04 16:50:08 +0100

    update the wweb-mode shortcuts based on feedback

--- contrib/lang/html/packages.el
+++ contrib/lang/html/packages.el
@@ -37,6 +37,35 @@ which require an initialization must be listed explicitly in the list.")
 (defun html/init-web-mode ()
   (use-package web-mode
     :defer t
+    :config
+    (progn
+      (evil-leader/set-key-for-mode 'web-mode
+        "mgp" 'web-mode-element-parent
+        "mgc" 'web-mode-element-child
+        "mgb" 'web-mode-element-beginning
+        "mgn" 'spacemacs/html-navigation-micro-state
+        "mgs" 'web-mode-element-sibling-next
+        "mhp" 'web-mode-dom-xpath
+        "meu" 'web-mode-dom-errors-show
+        "mrr" 'web-mode-element-rename
+        "mrd" 'web-mode-element-vanish
+        "mrw" 'web-mode-element-wrap
+        "mrc" 'web-mode-element-clone
+        "mf" 'web-mode-fold-or-unfold
+        ;; TODO element close would be nice but broken with evil.
+        )
+
+      (spacemacs|define-micro-state html-navigation
+        :doc "[h] previous [l] next [L] sibling [k] parent [j] child [q] quit"
+        :persistent t
+        :bindings
+        ("h" web-mode-element-previous)
+        ("l" web-mode-element-next)
+        ("L" web-mode-element-sibling-next)
+        ("k" web-mode-element-parent)
+        ("j" web-mode-element-child)
+        ("q" nil :exit t))
+      )
     :mode (("\\.phtml\\'"      . web-mode)
            ("\\.tpl\\.php\\'"  . web-mode)
            ("\\.html\\'"       . web-mode)
@@ -96,31 +125,3 @@ which require an initialization must be listed explicitly in the list.")
 (defun html/init-slim-mode ()
   (use-package slim-mode
     :defer t))
-
-(evil-leader/set-key-for-mode 'web-mode
-  "mgp" 'web-mode-element-parent
-  "mgc" 'web-mode-element-child
-  "mgb" 'web-mode-element-beginning
-  "mgn" 'spacemacs/html-navigation-micro-state
-  "mgs" 'web-mode-element-sibling-next
-  ;; v like 'View'
-  "mvx" 'web-mode-dom-xpath
-  "mve" 'web-mode-dom-errors-show
-  ;; i like 'Item'
-  "mir" 'web-mode-element-rename
-  "miw" 'web-mode-element-wrap
-  "mic" 'web-mode-element-clone
-  "mf" 'web-mode-fold-or-unfold
-  ;; TODO element close would be nice but broken with evil.
-  )
-
-(spacemacs|define-micro-state html-navigation
-  :doc "[N] previous [n] next [s] sibling [p] parent [c] child [q] quit"
-  :persistent t
-  :bindings
-  ("N" web-mode-element-previous)
-  ("n" web-mode-element-next)
-  ("s" web-mode-element-sibling-next)
-  ("p" web-mode-element-parent)
-  ("c" web-mode-element-child)
-  ("q" nil :exit t))
