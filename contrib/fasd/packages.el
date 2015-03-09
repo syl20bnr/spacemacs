ab956fd1fd7eb9078150a8ca55ad6e7388a4a7ce
Michael Hauser-Raspe <hauserra@amazon.com>
2015-03-09 14:34:37 +0000

    fasd layer: add file-only, directory-only functions and helm-completion

--- contrib/fasd/packages.el
+++ contrib/fasd/packages.el
@@ -1,7 +1,6 @@
 (defvar fasd-packages
   '(
     fasd
-    ;; package fasds go here
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
@@ -9,19 +8,27 @@ which require an initialization must be listed explicitly in the list.")
 (defvar fasd-excluded-packages '()
   "List of packages to exclude.")
 
+(defun fasd-find-file-only ()
+  (interactive)
+  (fasd-find-file -1))
+
+(defun fasd-find-directory-only ()
+  (interactive)
+  (fasd-find-file 1))
+
 (defun fasd/init-fasd ()
   "initializes fasd-emacs and adds a key binding to <SPC f z>"
   (use-package fasd
     :init
     (progn
       (global-fasd-mode 1)
-      (evil-leader/set-key "fz" 'fasd-find-file))))
-;; For each package, define a function fasd/init-<package-fasd>
-;;
-;; (defun fasd/init-my-package ()
-;;   "Initialize my package"
-;;   )
-;;
-;; Often the body of an initialize function uses `use-package'
-;; For more info on `use-package', see readme:
-;; https://github.com/jwiegley/use-package
+      (spacemacs/declare-prefix "fa" "fasd-find")
+      (evil-leader/set-key "fad" 'fasd-find-directory-only)
+      (evil-leader/set-key "faf" 'fasd-find-file-only)
+      (evil-leader/set-key "fas" 'fasd-find-file)
+
+      ;; we will fall back to using the default completing-read function, which is helm once helm is loaded.
+      (setq fasd-completing-read-function 'nil)
+      )
+    )
+  )
