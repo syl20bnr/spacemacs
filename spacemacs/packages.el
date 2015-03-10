068426dd9cd1640e42ebd83bba13660be6e5b540
Simon Vandel Sillesen <simon.vandel@gmail.com>
2015-03-10 20:32:19 +0100

    Add keybinding for doc-view-goto-page (gt) in DocView mode

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -500,6 +500,7 @@ which require an initialization must be listed explicitly in the list.")
                          "?"  'spacemacs/doc-view-search-new-query-backward
                          "gg" 'doc-view-first-page
                          "G"  'doc-view-last-page
+                         "gt" 'doc-view-goto-page
                          "h"  'doc-view-previous-page
                          "j"  'doc-view-next-line-or-next-page
                          "k"  'doc-view-previous-line-or-previous-page
