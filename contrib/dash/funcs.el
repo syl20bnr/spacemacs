67c650807ecca9b4e1f4be81611065a2078e227d
syl20bnr <sylvain.benner@gmail.com>
2015-03-13 23:16:40 -0400

    Refactor dash layer
    Rename variable dash/helm-dash-docset-newpath to
    dash-helm-dash-docset-path

--- contrib/dash/funcs.el
+++
@@ -1,9 +0,0 @@
-(defun dash/activate-package-docsets (root)
-  (progn
-    (setq helm-dash-docsets-path root)
-    (setq helm-dash-common-docsets (helm-dash-installed-docsets))
-
-    (message
-     (format "activated %d docsets from: %s"
-             (length helm-dash-common-docsets) root))
-))
