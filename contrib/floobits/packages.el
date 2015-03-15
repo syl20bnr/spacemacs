e4101c84cba3ea75f200d23da737372097f05fe2
syl20bnr <sylvain.benner@gmail.com>
2015-03-15 00:59:27 -0400

    Edit floobits layer README

--- contrib/floobits/packages.el
+++ contrib/floobits/packages.el
@@ -16,29 +16,28 @@
   "List of all packages to instal and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
 
-(defun floobits/rclocation ()
-  "Return the absolute path to the floobits dotfile."
-  (concat user-home-directory ".floorc.json"))
-
-
-(defun floobits/load-rcfile ()
-  "Load ~/.floobitsrc if it exists."
-  (let ((floobitsrc (floobits/rclocation)))
-    (if (file-exists-p floobitsrc) (load floobitsrc))))
-
 (defun floobits/init-floobits ()
   (use-package floobits
     :defer t
     :init
-    (evil-leader/set-key
-      "Pc" 'floobits-clear-highlights
-      "Pd" 'floobits/load-rcfile
-      "Pf" 'floobits-follow-user
-      "Pj" 'floobits-join-workspace
-      "Pl" 'floobits-leave-workspace
-      "Ps" 'floobits-summon
-      "Pt" 'floobits-follow-mode-toggle
-      "PR" 'floobits-share-dir-private
-      "PU" 'floobits-share-dir-public)
-    )
-  )
+    (progn
+
+      (defun spacemacs/floobits-rclocation ()
+        "Return the absolute path to the floobits dotfile."
+        (concat user-home-directory ".floorc.json"))
+
+      (defun spacemacs/floobits-load-rcfile ()
+        "Load ~/.floobitsrc if it exists."
+        (let ((floobitsrc (spacemacs/floobits-rclocation)))
+          (if (file-exists-p floobitsrc) (load floobitsrc))))
+
+      (evil-leader/set-key
+        "Pc" 'floobits-clear-highlights
+        "Pd" 'spacemacs/floobits-load-rcfile
+        "Pf" 'floobits-follow-user
+        "Pj" 'floobits-join-workspace
+        "Pl" 'floobits-leave-workspace
+        "PR" 'floobits-share-dir-private
+        "Ps" 'floobits-summon
+        "Pt" 'floobits-follow-mode-toggle
+        "PU" 'floobits-share-dir-public))))
