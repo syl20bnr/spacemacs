29203af36ec99d6a3baa257b40639b0d0633c269
syl20bnr <sylvain.benner@gmail.com>
2015-03-15 23:48:31 -0400

    Fix bookmark lazy-loading

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -422,14 +422,10 @@ which require an initialization must be listed explicitly in the list.")
 
 (defun spacemacs/init-bookmark ()
   (use-package bookmark
-    :commands (bookmark-delete
-               bookmark-jump
-               bookmark-rename
-               bookmark-set)
-    :config
-    (setq
-     bookmark-default-file "~/.emacs.d/bookmarks" ; keep my ~/ clean
-     bookmark-save-flag 1)))                      ; autosave each change
+    :defer t
+    :init
+    (setq bookmark-default-file "~/.emacs.d/bookmarks" ; keep my ~/ clean
+          bookmark-save-flag 1)))                      ; autosave each change
 
 (defun spacemacs/init-buffer-move ()
   (use-package buffer-move
