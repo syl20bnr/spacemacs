f657bab7cd8475d878d1befcc297d4e2d10bff23
Tu Do <tuhdo1710@gmail.com>
2015-03-22 20:22:58 +0700

    [Fix #890] Improve editing functionality for holy-mode
    Add:
    
    - Key binding for evil-join for both evil/holy-mode.
    - A command for indenting whole buffer and a key binding in both
    evil/holy-mode.
    - A smart beginning of line for holy-mode.
    
    Conflicts:
    	spacemacs/extensions/holy-mode/holy-mode.el

--- spacemacs/keybindings.el
+++ spacemacs/keybindings.el
@@ -116,8 +116,10 @@ Ensure that helm is required before calling FUNC."
 ;; <SPC> J split the current line at point and indent it
 (evil-leader/set-key
   "J"  'sp-split-sexp
+  "jj" 'evil-join
   "jJ" 'spacemacs/split-and-new-line
-  "jj" 'sp-newline
+  "jn" 'sp-newline
+  "ji" 'spacemacs/indent-region-or-buffer
   "jk" 'evil-goto-next-line-and-indent)
 ;; navigation -----------------------------------------------------------------
 (evil-leader/set-key
