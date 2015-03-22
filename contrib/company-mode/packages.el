8d66f55d05bbf031200e9e68e1995956b90ea4b0
syl20bnr <sylvain.benner@gmail.com>
2015-03-22 02:28:10 -0400

    Set company-delay to 0.5 as default
    0.5 is a sane default, users can easily tweak it in the dotfile.

--- contrib/company-mode/packages.el
+++ contrib/company-mode/packages.el
@@ -12,7 +12,7 @@ conflict.")
     :defer t
     :init
     (progn
-      (setq company-idle-delay 0.0
+      (setq company-idle-delay 0.5
             company-minimum-prefix-length 2
             company-require-match nil
             company-dabbrev-ignore-case nil
