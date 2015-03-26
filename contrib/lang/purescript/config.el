44f1a5fadea739ba8425e41ad7b578ed0c5825de
Ryan L. Bell <ryan.l.bell@gmail.com>
2015-03-26 13:37:55 -0400

    Creates a really basic, early stages purescript layer.
    Leverages:
    - purescript-mode
    - psci
    
    Purescript README.md
    
    Fixed Purescript logo

---
+++ contrib/lang/purescript/config.el
@@ -0,0 +1,14 @@
+;;; config.el --- Purescript Layer config File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2015 Ryan L. Bell & Contributors
+;;
+;; Author: Ryan L. Bell
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+
+;; Nothing here yet
