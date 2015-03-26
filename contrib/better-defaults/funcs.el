0e2854de37e8b873528ef2cac4d063ca829b6663
syl20bnr <sylvain.benner@gmail.com>
2015-03-26 12:26:09 -0400

    Uncomment spacemacs/indent-region-or-buffer and move it
    to spacemacs/funcs.el

--- contrib/better-defaults/funcs.el
+++ contrib/better-defaults/funcs.el
@@ -28,30 +28,3 @@ point reaches the beginning or end of the buffer, stop there."
     (back-to-indentation)
     (when (= orig-point (point))
       (move-beginning-of-line 1))))
-
-;; from Prelude
-;; TODO To adapt for `=' of evil
-;; (defvar spacemacs-indent-sensitive-modes
-;;   '(coffee-mode
-;;     python-mode
-;;     slim-mode
-;;     haml-mode
-;;     yaml-mode
-;;     makefile-mode
-;;     makefile-gmake-mode
-;;     makefile-imake-mode
-;;     makefile-bsdmake-mode)
-;;   "Modes for which auto-indenting is suppressed.")
-;; (defun spacemacs/indent-region-or-buffer ()
-;;   "Indent a region if selected, otherwise the whole buffer."
-;;   (interactive)
-;;   (unless (member major-mode spacemacs-indent-sensitive-modes)
-;;     (save-excursion
-;;       (if (region-active-p)
-;;           (progn
-;;             (indent-region (region-beginning) (region-end))
-;;             (message "Indented selected region."))
-;;         (progn
-;;           (evil-indent (point-min) (point-max))
-;;           (message "Indented buffer.")))
-;;       (whitespace-cleanup))))
