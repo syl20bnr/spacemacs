02085bef401b4b69fb107aaf6fee898308815be6
Tristan Hume <tris.hume@gmail.com>
2015-03-26 20:24:14 -0400

    Make tweaks to Racket layer

--- contrib/lang/racket/packages.el
+++ contrib/lang/racket/packages.el
@@ -16,11 +16,9 @@
         "mr" 'racket-run
         "mg" 'racket-visit-definition
         "mhd" 'racket-doc)
+      (define-key racket-mode-map (kbd "H-r") 'racket-run)
       ;; Bug exists in Racket company backend that opens docs in new window when
       ;; company-quickhelp calls it. Note hook is appendended for proper ordering.
       (when (configuration-layer/package-declaredp 'company-quickhelp)
         (add-hook 'company-mode-hook
-                  '(lambda () (when (equal major-mode 'racket-mode) (company-quickhelp-mode -1))) t))
-      (add-hook 'racket-mode-hook
-                '(lambda ()
-                   (define-key racket-mode-map (kbd "H-r") 'racket-run))))))
+                  '(lambda () (when (equal major-mode 'racket-mode) (company-quickhelp-mode -1))) t)))))
