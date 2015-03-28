f3be6384664795b33e9d59e06f79a09bd7834417
Carl Lange <carl@flax.ie>
2015-03-28 22:17:44 +0200

    use boundp (because this actually works)

--- core/core-spacemacs.el
+++ core/core-spacemacs.el
@@ -322,7 +322,7 @@ version and the NEW version."
      ;; Ultimate configuration decisions are given to the user who can defined
      ;; them in his/her ~/.spacemacs file
      (dotspacemacs|call-func dotspacemacs/config "Calling dotfile config...")
-     (unless (equal 'dotspacemacs-startup-lists 'nil)
+     (when (boundp 'dotspacemacs-startup-lists)
        (spacemacs/insert-startupify-lists))
      ;; from jwiegley
      ;; https://github.com/jwiegley/dot-emacs/blob/master/init.el
