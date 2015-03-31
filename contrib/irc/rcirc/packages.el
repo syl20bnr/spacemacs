a667c918af23b3188176b138da38a342b4a60ae8
luxbock <opieppo@gmail.com>
2015-03-31 19:23:18 +0700

    Add `rcirc-track-minor-mode` to the rcirc-mode-hooks

--- contrib/irc/rcirc/packages.el
+++ contrib/irc/rcirc/packages.el
@@ -10,7 +10,9 @@
     :defer t
     :init
     (progn
-      (add-to-hook 'rcirc-mode-hook '(flyspell-mode rcirc-omit-mode))
+      (add-to-hook 'rcirc-mode-hook '(flyspell-mode
+                                      rcirc-omit-mode
+                                      rcirc-track-minor-mode))
 
       (defun spacemacs//rcirc-with-authinfo (arg)
         "Fire rcirc with support for authinfo."
