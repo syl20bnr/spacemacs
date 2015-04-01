334d703e496c59d423e9356f803362887684d51c
Rob Merrell <rmerrell@partnerfusion.com>
2015-04-01 13:39:01 -0600

    Added the go/run-package-tests command along with the 'mtp' key combo to invoke it

--- contrib/lang/go/packages.el
+++ contrib/lang/go/packages.el
@@ -16,20 +16,25 @@ which require an initialization must be listed explicitly in the list.")
 (defun go/init-flycheck ()
     (add-hook 'go-mode-hook 'flycheck-mode))
 
+(defun go/run-package-tests ()
+  (interactive)
+  (shell-command "go test"))
+
 (defun go/init-go-mode()
   (use-package go-mode
     :defer t
     :config
       (add-hook 'before-save-hook 'gofmt-before-save)
       (evil-leader/set-key-for-mode 'go-mode
-        "mdp"  'godoc-at-point
-        "mig"  'go-goto-imports
-        "mia"  'go-import-add
-        "mir"  'go-remove-unused-imports
-        "mpb"  'go-play-buffer
-        "mpr"  'go-play-region
-        "mpd"  'go-download-play
-        "mgg"   'godef-jump)))
+        "mdp" 'godoc-at-point
+        "mig" 'go-goto-imports
+        "mia" 'go-import-add
+        "mir" 'go-remove-unused-imports
+        "mpb" 'go-play-buffer
+        "mpr" 'go-play-region
+        "mpd" 'go-download-play
+        "mgg" 'godef-jump
+        "mtp" 'go/run-package-tests)))
 
 (defun go/init-go-eldoc()
     (add-hook 'go-mode-hook 'go-eldoc-setup))
