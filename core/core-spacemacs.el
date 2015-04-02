fae7b4f10a7a060a25493560590e87afe39d6c5e
syl20bnr <sylvain.benner@gmail.com>
2015-04-02 00:12:19 -0400

    Fix and tweaks for startup lists

--- core/core-spacemacs.el
+++ core/core-spacemacs.el
@@ -332,7 +332,8 @@ version and the NEW version."
      ;; Ultimate configuration decisions are given to the user who can defined
      ;; them in his/her ~/.spacemacs file
      (dotspacemacs|call-func dotspacemacs/config "Calling dotfile config...")
-     (when 'dotspacemacs-startup-lists
+     ;; Display useful lists of items
+     (when dotspacemacs-startup-lists
        (spacemacs/insert-startupify-lists))
      ;; from jwiegley
      ;; https://github.com/jwiegley/dot-emacs/blob/master/init.el
