6d769705369d19e13e881314a8d94403c9a2587d
syl20bnr <sylvain.benner@gmail.com>
2015-04-04 01:05:08 -0400

    evil-snipe layer: fix the use-package configuration

--- contrib/evil-snipe/packages.el
+++ contrib/evil-snipe/packages.el
@@ -9,14 +9,14 @@ which require an initialization must be listed explicitly in the list.")
   (use-package evil-snipe
     :diminish evil-snipe-mode
     :init
-    (setq evil-snipe-scope 'whole-buffer
-          evil-snipe-enable-highlight t
-          evil-snipe-enable-incremental-highlight t
-          evil-snipe-enable-half-cursor nil
-          evil-snipe-show-prompt nil
-          evil-snipe-smart-case t)
-    (when evil-snipe-enable-alternate-f-and-t-behaviors
-      (setq evil-snipe-repeat-scope 'whole-buffer
-            evil-snipe-override-evil t))
-    :config
-    (global-evil-snipe-mode t)))
+    (progn
+      (setq evil-snipe-scope 'whole-buffer
+            evil-snipe-enable-highlight t
+            evil-snipe-enable-incremental-highlight t
+            evil-snipe-enable-half-cursor nil
+            evil-snipe-show-prompt nil
+            evil-snipe-smart-case t)
+      (when evil-snipe-enable-alternate-f-and-t-behaviors
+        (setq evil-snipe-repeat-scope 'whole-buffer
+              evil-snipe-override-evil t))
+      (global-evil-snipe-mode))))
