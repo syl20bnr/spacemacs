bdf370c05000dc5698320e139494699a60932231
syl20bnr <sylvain.benner@gmail.com>
2015-04-04 01:02:37 -0400

    lua layer: move flycheck config in a post-init function

--- contrib/lang/lua/packages.el
+++ contrib/lang/lua/packages.el
@@ -1,9 +1,13 @@
 (defvar lua-packages
   '(
+    flycheck
     lua-mode
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
+
+(defun lua/post-init-flycheck ()
+  (add-hook 'lua-mode-hook 'flycheck-mode))
 
 (defun lua/init-lua-mode ()
   (use-package lua-mode
@@ -14,7 +18,6 @@ which require an initialization must be listed explicitly in the list.")
     (progn
       (setq lua-indent-level 2
             lua-indent-string-contents t)
-      (add-hook 'lua-mode-hook 'flycheck-mode)
       (evil-leader/set-key-for-mode 'lua-mode "md" 'lua-search-documentation)
       (evil-leader/set-key-for-mode 'lua-mode "msb" 'lua-send-buffer)
       (evil-leader/set-key-for-mode 'lua-mode "msf" 'lua-send-defun)
