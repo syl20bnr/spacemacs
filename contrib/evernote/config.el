aabdd84acb8ca12a361039f77c0639fd4d29da28
syl20bnr <sylvain.benner@gmail.com>
2015-04-09 01:28:43 -0400

    evernote layer: Add prefix command name for `ae`

---
+++ contrib/evernote/config.el
@@ -0,0 +1,25 @@
+;;; config.el --- Evernote Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+
+(defvar colors-enable-rainbow-identifiers nil
+  "if non nil the `rainbow-identifers' package is enabled.")
+
+(defvar colors-enable-nyan-cat-progress-bar nil
+  "if non nil all nyan cat packges are enabled (for now only `nyan-mode').")
+
+;; Command prefixes
+
+(setq evernote/key-binding-prefixes '(("ae" . "applications-evernote")))
+(mapc (lambda (x) (spacemacs/declare-prefix (car x) (cdr x)))
+      evernote/key-binding-prefixes)
