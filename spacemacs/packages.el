b5930e91aa4e93fe644941b5647e39b0e9a4daa5
Allen Li <darkfeline@felesatra.moe>
2015-04-15 17:44:17 -0400

    Add binding for EOF (^D) in term
    This is a useful binding to have, and is bound similarly to
    term-send-esc (`C-c C-e`) and term-interrupt-subjob (`C-c C-c`).

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -1946,7 +1946,8 @@ Put (global-hungry-delete-mode) in dotspacemacs/config to enable by default."
         (interactive)
         (term-send-raw-string "\t"))
 
-      (add-to-list 'term-bind-key-alist '("<tab>" . term-send-tab)))))
+      (add-to-list 'term-bind-key-alist '("<tab>" . term-send-tab))
+      (add-to-list 'term-bind-key-alist `(,(kbd "C-c C-d") . term-send-eof)))))
 
 (defun spacemacs/init-neotree ()
   (use-package neotree
