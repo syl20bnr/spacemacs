ef08d06ba979b3ec5d967acc0d8d685be7279d99
Tu Do <tu.h.do@dektech.com.au>
2015-04-16 11:34:40 +0700

    M-* should use helm-gtags-pop-stack

--- contrib/gtags/packages.el
+++ contrib/gtags/packages.el
@@ -78,7 +78,7 @@ which require an initialization must be listed explicitly in the list.")
       (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
       (define-key helm-gtags-mode-map (kbd "C-x 4 .") 'helm-gtags-find-tag-other-window)
       (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-      (define-key helm-gtags-mode-map (kbd "M-*") 'pop-tag-mark)
+      (define-key helm-gtags-mode-map (kbd "M-*") 'helm-gtags-pop-stack)
 
       ;; modes that do not have a layer, define here
       (spacemacs/gtags-define-keys-for-mode 'tcl-mode)
