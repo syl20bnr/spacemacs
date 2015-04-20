6267d1a893fe32a06d6e76e4eded560b6d0f6b4f
hujianxin <hujianxincn@163.com>
2015-04-20 10:46:27 +0800

    add surport to elpy and cancel it

--- contrib/lang/python/packages.el
+++ contrib/lang/python/packages.el
@@ -27,6 +27,7 @@
     python
     semantic
     smartparens
+;;    elpy
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
@@ -249,6 +250,18 @@ which require an initialization must be listed explicitly in the list.")
       (if pythonp
           ad-do-it
         (call-interactively 'sp-backward-delete-char)))))
+
+;; (defun python/init-elpy ()
+;;   (use-package elpy
+;;     :config
+;;     (elpy-enable t)
+;;     (setq elpy-rpc-backend "jedi")
+;;     (when (executable-find "ipython")
+;;       (elpy-use-ipython))
+;;     (define-key python-mode-map (kbd "RET")
+;;       'newline-and-indent)
+;;     (add-hook 'python-mode-hook
+;;               (lambda () (set (make-local-variable 'comment-inline-offset) 2)))))
 
 (when (configuration-layer/layer-usedp 'auto-completion)
   (defun python/post-init-company ()
