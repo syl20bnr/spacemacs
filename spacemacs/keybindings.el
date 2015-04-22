6ab92871cb3147c4b9dda3adc0054652bf2df37e
hujianxin <hujianxincn@163.com>
2015-04-22 11:52:39 +0800

    add keybinding for quickrun

--- spacemacs/keybindings.el
+++ spacemacs/keybindings.el
@@ -50,7 +50,8 @@
   "ap"  'proced
   "ase" 'eshell
   "asi" 'shell
-  "au"  'undo-tree-visualize)
+  "au"  'undo-tree-visualize
+  "aq"  'quick-run)
 ;; buffers --------------------------------------------------------------------
 (evil-leader/set-key
   "b0"  'beginning-of-buffer
