483376cf1d28a49a42dad5f85abd68ac74694ace
syl20bnr <sylvain.benner@gmail.com>
2015-04-21 23:38:42 -0400

    git layer: add lazy loading of magit-gh-pulls on `#gf`

--- contrib/git/packages.el
+++ contrib/git/packages.el
@@ -310,8 +310,15 @@
         (interactive)
         (magit-gh-pulls-mode)
         (magit-gh-pulls-reload))
+      (defun spacemacs/fetch-gh-pulls-mode ()
+        "Start `magit-gh-pulls-mode' only after a manual request."
+        (interactive)
+        (magit-gh-pulls-mode)
+        (magit-gh-pulls-fetch-commits))
       (eval-after-load 'magit
-          '(define-key magit-mode-map "#gg" 'spacemacs/load-gh-pulls-mode)))
+        '(progn
+           (define-key magit-mode-map "#gg" 'spacemacs/load-gh-pulls-mode)
+           (define-key magit-mode-map "#gf" 'spacemacs/fetch-gh-pulls-mode))))
     :config
     (spacemacs|diminish magit-gh-pulls-mode "Github-PR")))
 
