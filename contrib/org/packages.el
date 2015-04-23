ee4d8f34ce1e88e564968742f600a84c3b6cec38
hujianxin <hujianxincn@163.com>
2015-04-23 20:22:20 +0800

    Move org2blog configurartion to org2blog layer.

--- contrib/org/packages.el
+++ contrib/org/packages.el
@@ -18,7 +18,6 @@
     org-pomodoro
     org-repo-todo
     ox-gfm
-    org2blog
     )
   "List of all packages to install and/or initialize. Built-in packages
 which require an initialization must be listed explicitly in the list.")
@@ -112,40 +111,3 @@ which require an initialization must be listed explicitly in the list.")
 (defun org/init-ox-gfm ()
   (use-package ox-gfm
     :defer t))
-
-(defun org/init-org2blog()
-  (use-package org2blog
-    :init
-    (progn
-      (evil-leader/set-key
-        "aoi"   'org2blog/wp-login
-        "aob"   'org2blog/wp-post-buffer
-        "aop"   'org2blog/wp-post-buffer-as-page
-        "aoo"   'org2blog/wp-logout
-        ))
-    :config
-    (progn
-      (require 'org2blog-autoloads)
-      (setq org2blog/wp-blog-alist
-            '(("wordpress"
-               :url "http://hujianxin.com/xmlrpc.php"
-               :username "hujianxin"
-               :default-title "Emacs Post"
-               :default-categories ("Tecnology")
-               :tags-as-categories nil)
-              ("localhost"
-               :url "http://localhost/wordpress/xmlrpc.php")))
-      (setq org2blog/wp-use-sourcecode-shortcode 't)
-      ;; removed light="true"
-      (setq org2blog/wp-sourcecode-default-params nil)
-      ;; target language needs to be in here
-      (setq org2blog/wp-sourcecode-langs
-            '("actionscript3" "bash" "coldfusion" "cpp" "csharp" "css" "delphi"
-              "erlang" "fsharp" "diff" "groovy" "javascript" "java" "javafx" "matlab"
-              "objc" "perl" "php" "text" "powershell" "python" "ruby" "scala" "sql"
-              "vb" "xml" "sh" "emacs-lisp" "lisp" "lua"))
-      ;; this will use emacs syntax higlighting in your #+BEGIN_SRC
-      ;; <language> <your-code> #+END_SRC code blocks.
-    (setq org-src-fontify-natively t))
-    )
-)
