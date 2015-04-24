2eb6c93f96346c08773f6234a2e0327091ee1e45
Ben Hayden <ben@britecore.com>
2015-04-23 23:31:08 -0500

    Adding pudb as a python breakpoint

--- contrib/lang/python/funcs.el
+++ contrib/lang/python/funcs.el
@@ -14,15 +14,15 @@
 (defun annotate-pdb ()
   "Highlight break point lines."
   (interactive)
-  (highlight-lines-matching-regexp "import i?pdb")
-  (highlight-lines-matching-regexp "i?pdb.set_trace()"))
+  (highlight-lines-matching-regexp "import i?pu?db")
+  (highlight-lines-matching-regexp "i?pu?db.set_trace()"))
 
 (defun python-toggle-breakpoint ()
   "Add a break point, highlight it."
   (interactive)
-  (let ((trace (if (executable-find "ipdb")
-                   "import ipdb; ipdb.set_trace()"
-                 "import pdb; pdb.set_trace()"))
+  (let ((trace (cond ((executable-find "ipdb") "import ipdb; ipdb.set_trace()")
+                     ((executable-find "pudb") "import pudb; pudb.set_trace()")
+                     ((t "import pdb; pdb.set_trace()"))))
         (line (thing-at-point 'line)))
     (if (and line (string-match trace line))
         (kill-whole-line)
