81706ba6c5f6e1af086c763c48c208fcd02c5dd3
syl20bnr <sylvain.benner@gmail.com>
2015-04-24 01:05:06 -0400

    sql layer: update to match conventions

--- contrib/lang/sql/packages.el
+++ contrib/lang/sql/packages.el
@@ -19,13 +19,70 @@
     :defer t
     :init
     (progn
+      ;; should not set this to anything else than nil
+      ;; the focus of SQLi is handled by spacemacs conventions
+      (setq sql-pop-to-buffer-after-send-region nil)
+
+      (defmacro sql-client (name)
+        "Define a function to set syntax highlighting and start a SQLi buffer."
+        (let
+            ((funcname (intern (concat "sql/" name "-client")))
+             (highlight (intern (concat "sql-highlight-" name "-keywords")))
+             (client (intern (concat "sql-" name))))
+          `(defun ,funcname ()
+             (interactive)
+             (,highlight)
+             (,client))))
+      (sql-client "db2")
+      (sql-client "mysql")
+      (sql-client "postgres")
+      (sql-client "sqlite")
+      (sql-client "ms")
+      (sql-client "informix")
+      (sql-client "ingrex")
+      (sql-client "interbase")
+      (sql-client "linter")
+      (sql-client "oracle")
+      (sql-client "solid")
+      (sql-client "sybase")
+      (sql-client "vertica")
+
+      (defun spacemacs/sql-send-string-and-focus ()
+        "Send a string to SQLi and switch to SQLi in `insert state'."
+        (interactive)
+        (let ((sql-pop-to-buffer-after-send-region t))
+          (call-interactively 'sql-send-string)
+          (evil-insert-state)))
+
+      (defun spacemacs/sql-send-buffer-and-focus ()
+        "Send the buffer to SQLi and switch to SQLi in `insert state'."
+        (interactive)
+        (let ((sql-pop-to-buffer-after-send-region t))
+          (sql-send-buffer)
+          (evil-insert-state)))
+
+      (defun spacemacs/sql-send-paragraph-and-focus ()
+        "Send the paragraph to SQLi and switch to SQLi in `insert state'."
+        (interactive)
+        (let ((sql-pop-to-buffer-after-send-region t))
+          (sql-send-paragraph)
+          (evil-insert-state)))
+
+      (defun spacemacs/sql-send-region-and-focus (start end)
+        "Send region to SQLi and switch to SQLi in `insert state'."
+        (interactive "r")
+        (let ((sql-pop-to-buffer-after-send-region t))
+          (sql-send-region start end)
+          (evil-insert-state)))
+
       (evil-leader/set-key-for-mode 'sql-mode
         ;; sqli buffer
         "mbb" 'sql-show-sqli-buffer
         "mbs" 'sql-set-sqli-buffer
-        "mbp" 'sql-toggle-pop-to-buffer-after-send-region
 
         ;; clients
+        ;; TODO: change the key binding to `msi' and use a helm buffer
+        ;; to choose the client type
         "mcb" 'sql/sybase-client
         "mcd" 'sql/db2-client
         "mci" 'sql/informix-client
@@ -41,29 +98,34 @@
         "mcv" 'sql/vertica-client
 
         ;; dialects
-        "mda" 'sql-highlight-ansi-keywords
-        "mdb" 'sql-highlight-sybase-keywords
-        "mdd" 'sql-highlight-db2-keywords
-        "mdi" 'sql-highlight-informix-keywords
-        "mdl" 'sql-highlight-linter-keywords
-        "mdm" 'sql-highlight-mysql-keywords
-        "mdn" 'sql-highlight-ingrex-keywords
-        "mdo" 'sql-highlight-oracle-keywords
-        "mdp" 'sql-highlight-postgres-keywords
-        "mdq" 'sql-highlight-sqlite-keywords
-        "mds" 'sql-highlight-ms-keywords
-        "mdS" 'sql-highlight-solid-keywords
-        "mdt" 'sql-highlight-interbase-keywords
-        "mdv" 'sql-highlight-vertica-keywords
+        ;; TODO: use `mhk' and use a helm buffer to choose the dialect
+        "mhka" 'sql-highlight-ansi-keywords
+        "mhkb" 'sql-highlight-sybase-keywords
+        "mhkd" 'sql-highlight-db2-keywords
+        "mhki" 'sql-highlight-informix-keywords
+        "mhkl" 'sql-highlight-linter-keywords
+        "mhkm" 'sql-highlight-mysql-keywords
+        "mhkn" 'sql-highlight-ingrex-keywords
+        "mhko" 'sql-highlight-oracle-keywords
+        "mhkp" 'sql-highlight-postgres-keywords
+        "mhkq" 'sql-highlight-sqlite-keywords
+        "mhks" 'sql-highlight-ms-keywords
+        "mhkS" 'sql-highlight-solid-keywords
+        "mhkt" 'sql-highlight-interbase-keywords
+        "mhkv" 'sql-highlight-vertica-keywords
 
         ;; interactivity
         "msb" 'sql-send-buffer
+        "msB" 'spacemacs/sql-send-buffer-and-focus
+        ;; paragraph gets "f" here because they can be assimilated to functions.
+        ;; If you separate your commands in a SQL file, double-tapping the key
+        ;; will send the command under the point, which is what you probably want.
+        "msf" 'sql-send-paragraph
+        "msF" 'spacemacs/sql-send-paragraph-and-focus
+        "msq" 'sql-send-string
+        "msQ" 'spacemacs/sql-send-string-and-focus
         "msr" 'sql-send-region
-        ; paragraph gets "s" here because it's probably the most handy. If you
-        ; separate your commands in a SQL file, double-tapping the key will
-        ; send the command under the point, which is what you probably want.
-        "mss" 'sql-send-paragraph
-        "mst" 'sql-send-string
+        "msR" 'spacemacs/sql-send-region-and-focus
 
         ;; listing
         "mla" 'sql-list-all
