6bfb45e3a57f5d985cbaeb599fbcc61256ede90d
Brian Hicks <brian@brianthicks.com>
2015-04-24 23:26:44 -0500

    sql: use Helm to select dialect/client

--- contrib/lang/sql/packages.el
+++ contrib/lang/sql/packages.el
@@ -17,35 +17,40 @@
 (defun sql/init-sql ()
   (use-package sql
     :defer t
-    :init
+    :config
     (progn
-      ;; should not set this to anything else than nil
-      ;; the focus of SQLi is handled by spacemacs conventions
-      (setq sql-pop-to-buffer-after-send-region nil)
+      (setq spacemacs-sql-highlightable sql-product-alist
+            spacemacs-sql-startable (remove-if-not
+                                (lambda (product) (sql-get-product-feature (car product) :sqli-program))
+                                sql-product-alist)
 
-      (defmacro sql-client (name)
-        "Define a function to set syntax highlighting and start a SQLi buffer."
-        (let
-            ((funcname (intern (concat "sql/" name "-client")))
-             (highlight (intern (concat "sql-highlight-" name "-keywords")))
-             (client (intern (concat "sql-" name))))
-          `(defun ,funcname ()
-             (interactive)
-             (,highlight)
-             (,client))))
-      (sql-client "db2")
-      (sql-client "mysql")
-      (sql-client "postgres")
-      (sql-client "sqlite")
-      (sql-client "ms")
-      (sql-client "informix")
-      (sql-client "ingrex")
-      (sql-client "interbase")
-      (sql-client "linter")
-      (sql-client "oracle")
-      (sql-client "solid")
-      (sql-client "sybase")
-      (sql-client "vertica")
+            ;; should not set this to anything else than nil
+            ;; the focus of SQLi is handled by spacemacs conventions
+            sql-pop-to-buffer-after-send-region nil)
+
+      (defun spacemacs//sql-source (products)
+        "return a source for helm selection"
+        `((name . "SQL Products")
+          (candidates . ,(mapcar (lambda (product)
+                                   (cons (sql-get-product-feature (car product) :name)
+                                         (car product)))
+                                 products))
+          (action . (lambda (candidate) (helm-marked-candidates)))))
+
+      (defun spacemacs/sql-highlight ()
+        "set SQL dialect-specific highlighting"
+        (interactive)
+        (let ((product (car (helm
+                             :sources (list (spacemacs//sql-source spacemacs-sql-highlightable))))))
+          (sql-set-product product)))
+
+      (defun spacemacs/sql-start ()
+        "set SQL dialect-specific highlighting and start inferior SQLi process"
+        (interactive)
+        (let ((product (car (helm
+                             :sources (list (spacemacs//sql-source spacemacs-sql-startable))))))
+          (sql-set-product product)
+          (sql-product-interactive product)))
 
       (defun spacemacs/sql-send-string-and-focus ()
         "Send a string to SQLi and switch to SQLi in `insert state'."
@@ -80,46 +85,16 @@
         "mbb" 'sql-show-sqli-buffer
         "mbs" 'sql-set-sqli-buffer
 
-        ;; clients
-        ;; TODO: change the key binding to `msi' and use a helm buffer
-        ;; to choose the client type
-        "mcb" 'sql/sybase-client
-        "mcd" 'sql/db2-client
-        "mci" 'sql/informix-client
-        "mcl" 'sql/linter-client
-        "mcm" 'sql/mysql-client
-        "mcn" 'sql/ingrex-client
-        "mco" 'sql/oracle-client
-        "mcp" 'sql/postgres-client
-        "mcq" 'sql/sqlite-client
-        "mcs" 'sql/ms-client
-        "mcS" 'sql/solid-client
-        "mct" 'sql/interbase-client
-        "mcv" 'sql/vertica-client
-
         ;; dialects
-        ;; TODO: use `mhk' and use a helm buffer to choose the dialect
-        "mhka" 'sql-highlight-ansi-keywords
-        "mhkb" 'sql-highlight-sybase-keywords
-        "mhkd" 'sql-highlight-db2-keywords
-        "mhki" 'sql-highlight-informix-keywords
-        "mhkl" 'sql-highlight-linter-keywords
-        "mhkm" 'sql-highlight-mysql-keywords
-        "mhkn" 'sql-highlight-ingrex-keywords
-        "mhko" 'sql-highlight-oracle-keywords
-        "mhkp" 'sql-highlight-postgres-keywords
-        "mhkq" 'sql-highlight-sqlite-keywords
-        "mhks" 'sql-highlight-ms-keywords
-        "mhkS" 'sql-highlight-solid-keywords
-        "mhkt" 'sql-highlight-interbase-keywords
-        "mhkv" 'sql-highlight-vertica-keywords
+        "mhk" 'spacemacs/sql-highlight
 
         ;; interactivity
         "msb" 'sql-send-buffer
         "msB" 'spacemacs/sql-send-buffer-and-focus
+        "msi" 'spacemacs/sql-start
         ;; paragraph gets "f" here because they can be assimilated to functions.
-        ;; If you separate your commands in a SQL file, double-tapping the key
-        ;; will send the command under the point, which is what you probably want.
+        ;; If you separate your commands in a SQL file, this key will send the
+        ;; command under the point, which is what you probably want.
         "msf" 'sql-send-paragraph
         "msF" 'spacemacs/sql-send-paragraph-and-focus
         "msq" 'sql-send-string
