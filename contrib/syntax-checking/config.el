9bc2a67e7f467a1f1ebf7e27e315e52e5f7bb68c
Ben Hayden <ben@britecore.com>
2015-04-26 23:10:14 -0500

    Add ability to disable `flycheck-pos-tip`
    Some people don't like pop-up messages in their editing window and
    prefer to keep it located in the echo area.

---
+++ contrib/syntax-checking/config.el
@@ -0,0 +1,16 @@
+;;; config.el --- Syntax Checking Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+
+(defvar syntax-checking-flycheck-pos-tip t
+  "If non nil the `flycheck-pos-tip` package is is enabled.")
