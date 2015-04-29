3e980b163ae2158208fd2227b965dcbbf504112d
Tu Do <tuhdo1710@gmail.com>
2015-04-29 16:58:22 +0700

    Remove visual noise in Helm window
    In a Helm window, the top header line displays a key binding to execute
    persistent action. The key binding is C-j. The problem, not all sources
    are applicable with C-j, i.e. helm-projectile-switch-project.
    
    Removing the header line also makes Helm look cleaner.

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -1307,6 +1307,7 @@ Example: (evil-map visual \"<\" \"<gv\")"
     (progn
       (setq helm-prevent-escaping-from-minibuffer t
             helm-bookmark-show-location t
+            helm-display-header-line nil
             helm-split-window-in-side-p t
             helm-always-two-windows t)
 
