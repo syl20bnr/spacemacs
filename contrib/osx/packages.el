9a6af7b56556a1b49e8bd9ca111165184daecb5c
Uri Sharf <uri.sharf@gmail.com>
2015-05-04 19:36:02 +0200

    Make dired use coreutils' gls, if installed

--- contrib/osx/packages.el
+++ contrib/osx/packages.el
@@ -3,6 +3,13 @@
     pbcopy
     ))
 
+(if (executable-find "gls")
+    ; maybe absolute or relative name of the `ls' program used by `insert-directory'.
+    ; brew info coreutils
+    (setq insert-directory-program "gls"
+          dired-listing-switches "-aBhl --group-directories-first")
+  (setq dired-use-ls-dired nil))
+
 (defun osx/init-pbcopy ()
   (use-package pbcopy
     :if (not (display-graphic-p))
