07e35ff36675b669235c05c31f48504c66dfb118
JP-Ellis <coujellis@gmail.com>
2015-05-04 16:29:34 +1000

    A few improvements to AucTeX:
    - Move function definitions from inside package.el to funcs.el;
    - Add a few variables to allow some configuration with .spacemacs;
    - Enable RefTeX support;
    - Move key-binding-prefixes to config.el (similar to git layer);
    - Use auto-fill-mode, with a environment-aware auto-fill function;
    - Provide a few additional sane defaults (some of the more "annoying"
      ones are configurable from .spacemacs)
    - Remove `build-view` in favour of just `build` as it seemed to be
      broken and introduce `SPC m v` to view;
    - Add support to LatexMk.
    
    !squash Added auctex-reftex-plugin variable.
    
    Signed-off-by: JP-Ellis <coujellis@gmail.com>

--- contrib/auctex/packages.el
+++ contrib/auctex/packages.el
@@ -1,6 +1,7 @@
 (setq auctex-packages
   '(
     auctex
+    auctex-latexmk
     company
     company-auctex
     evil-matchit
@@ -12,36 +13,36 @@
     :defer t
     :config
     (progn
-      (defun auctex/build-view ()
-        (interactive)
-        (progn
-          (let ((TeX-save-query nil))
-            (TeX-save-document (TeX-master-file)))
-          (setq build-proc (TeX-command "LaTeX" 'TeX-master-file -1))
-          (set-process-sentinel  build-proc  'auctex/build-sentinel))
-        (TeX-view))
+      (add-hook 'TeX-mode-hook 'flyspell-mode)
+      (add-hook 'TeX-mode-hook 'company-mode)
+      (add-hook 'TeX-mode-hook 'spacemacs/load-yasnippet)
+      (when auctex-auto-fill (add-hook 'TeX-mode-hook 'auctex/auto-fill-mode))
+      (add-hook 'TeX-mode-hook 'smartparens-mode)
+      (add-hook 'LaTeX-mode-hook 'latex-math-mode)
 
-      (defun auctex/build-sentinel (process event)
-        (if (string= event "finished\n")
-            (TeX-view)
-          (message "Errors! Check with C-`")))
+      ;; (setq spacemacs/key-binding-prefixes '(("mp" . "LaTeX Preview")))
 
-      (add-hook 'LaTeX-mode-hook '(lambda () (local-set-key (kbd "H-r") 'auctex/build-view)))
-      (add-hook 'LaTeX-mode-hook 'flyspell-mode)
-      (add-hook 'LaTeX-mode-hook 'company-mode)
-      (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-      (add-hook 'LaTeX-mode-hook 'spacemacs/load-yasnippet)
+      ;; Key bindings for plain TeX
+      (evil-leader/set-key-for-mode 'tex-mode
+        "mC" 'TeX-command-master
+        "mb" 'auctex/build
+        "mv" 'TeX-view
+        "m\\" 'TeX-insert-macro
+        "mf" 'TeX-font) ;; Find a way to rebind tex-fonts
 
-      (setq spacemacs/key-binding-prefixes '(("mp" . "LaTeX Preview")))
+      ;; Key bindings for LaTeX
       (evil-leader/set-key-for-mode 'latex-mode
-        "mb" 'auctex/build-view
+        "mC" 'TeX-command-master
+        "mb" 'auctex/build
+        "mv" 'TeX-view
+        "m\\" 'TeX-insert-macro
+        "mf" 'TeX-font ;; Find a way to rebind tex-fonts
         "me" 'LaTeX-environment
         "mc" 'LaTeX-close-environment
-        "mi" 'LaTeX-insert-item
-        "mf" 'TeX-font ;; Find a way to rebind tex-fonts
+        "mi" 'LaTeX-insert-item)
 
-        "mC" 'TeX-command-master
-
+      ;; Preview key bindings
+      (evil-leader/set-key-for-mode 'latex-mode
         "mpr" 'preview-region
         "mpb" 'preview-buffer
         "mpd" 'preview-document
@@ -54,10 +55,36 @@
         "mhd" 'TeX-doc ;; TeX-doc is a very slow function
         )
 
+      (setq-default TeX-command-default auctex-build-command)
+
       (setq-default TeX-auto-save t)
       (setq-default TeX-parse-self t)
-      (setq-default TeX-PDF-mode t))))
+      (setq-default TeX-syntactic-comment t)
+      (setq-default TeX-PDF-mode t)
 
+      (when auctex-electric-sub-and-superscript
+        (setq-default TeX-electric-sub-and-superscript t))
+      (when auctex-electric-escape
+        (setq-default TeX-electric-escape t))
+      (setq-default TeX-electric-macro)
+
+      ;; Synctex support
+      (setq-default TeX-source-correlate-mode t)
+      (setq-default TeX-source-correlate-start-server nil)
+
+      ;; Setup reftex style (RefTeX is supported through extension)
+      (setq-default reftex-use-fonts t)
+
+      ;; Don't insert line-break at inline math
+      (setq-default LaTeX-fill-break-at-separators '()))))
+
+(when (string= auctex-build-command "LatexMk")
+  (defun auctex/init-auctex-latexmk ()
+    (use-package auctex-latexmk
+      :defer t
+      :init
+      (progn
+        (auctex-latexmk-setup)))))
 
 (defun auctex/post-init-evil-matchit ()
   (add-hook 'LaTeX-mode-hook 'evil-matchit-mode))
