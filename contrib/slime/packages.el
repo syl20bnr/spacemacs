87ab945a980e75a1f7df8a70a4f3e761ae7162da
Tu Do <tuhdo1710@gmail.com>
2015-05-04 20:12:25 +0700

    Disable smartparens in SLIME REPL

--- contrib/slime/packages.el
+++ contrib/slime/packages.el
@@ -29,7 +29,12 @@
       (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
 
       ;; enabel smartparen in code buffer and SLIME REPL
-      (add-hook 'slime-repl-mode-hook #'smartparens-strict-mode)
+      ;; (add-hook 'slime-repl-mode-hook #'smartparens-strict-mode)
+      (defun slime/disable-smartparens ()
+        (smartparens-strict-mode -1)
+        (turn-off-smartparens-mode))
+
+      (add-hook 'slime-repl-mode-hook #'slime/disable-smartparens)
 
       (add-to-hooks 'slime-mode '(lisp-mode-hook scheme-mode-hook)))
     :config
