e8758edcab1c811e1d0fa6d3495475d68a0281f1
Tony Lotts <tonylotts@users.noreply.github.com>
2015-05-08 17:47:16 -0400

    Add 'SPC p $' to run multi-term at project root

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -2504,6 +2504,7 @@ It is a string holding:
           "ps" 'projectile-switch-project))
       (evil-leader/set-key
         "p!" 'projectile-run-shell-command-in-root
+        "p$" 'projectile-multi-term-in-root
         "p&" 'projectile-run-async-shell-command-in-root
         "pc" 'projectile-compile-project
         "pD" 'projectile-dired
