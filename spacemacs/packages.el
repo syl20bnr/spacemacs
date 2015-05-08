727a41b0868afd4c51db90b2401c8ff4df754b6e
syl20bnr <sylvain.benner@gmail.com>
2015-05-08 00:33:30 -0400

    Add powerline face tweak to spacemacs/post-theme-init

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -2209,9 +2209,12 @@ displayed in the mode-line.")
           (setq-default powerline-default-separator 'wave)
         (setq-default powerline-default-separator 'utf-8))
 
-      ;; change powerline-inactive2 face to make it usable across themes
-      (set-face-attribute 'powerline-inactive2 nil
-                          :inherit 'font-lock-preprocessor-face)
+      (defun spacemacs//customize-powerline-faces ()
+        "Alter powerline face to make them work with more themes."
+        (set-face-attribute 'powerline-inactive2 nil
+                            :inherit 'font-lock-preprocessor-face))
+      (spacemacs//customize-powerline-faces)
+
 
       (defun spacemacs/mode-line-prepare-left ()
         (let* ((active (powerline-selected-window-active))
