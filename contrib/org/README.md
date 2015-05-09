ff57c1b55afd2f4476c58873fd6cc148d309bf38
hujianxin <hujianxincn@163.com>
2015-05-09 13:11:31 +0800

    Add support for Chinese.

--- contrib/org/README.md
+++ contrib/org/README.md
@@ -10,6 +10,7 @@
     - [Install](#install)
         - [Layer](#layer)
         - [Different bullets](#different-bullets)
+        - [Support Chinese](#support-chinese)
     - [Key bindings](#key-bindings)
         - [Org with evil-org-mode](#org-with-evil-org-mode)
         - [Pomodoro](#pomodoro)
@@ -46,6 +47,11 @@ You can tweak the bullets displayed in the org buffer in the function
 ```elisp
 (setq org-bullets-bullet-list '("■" "◆" "▲" "▶"))
 ```
+
+### Support Chinese
+
+For supporting chinese, you need install some chinese fonts.
+For example, in debian or ubuntu, you can run `sudo apt-get install fonts-wqy-microhei fonts-wqy-zenhei` .
 
 ## Key bindings
 
--- contrib/org/packages.el
+++ contrib/org/packages.el
@@ -74,12 +74,93 @@ which require an initialization must be listed explicitly in the list.")
            (define-key org-agenda-mode-map "j" 'org-agenda-next-line)
            (define-key org-agenda-mode-map "k" 'org-agenda-previous-line)
            (define-key org-agenda-mode-map
-             (kbd "SPC") evil-leader--default-map))))
+             (kbd "SPC") evil-leader--default-map)))
+
+      (when (require 'ox-latex nil 'noerror)
+        (message "in debug")
+        ;; You need to install pygments to use minted
+        (when (executable-find "pygmentize")
+          ;; Add minted to the defaults packages to include when exporting.
+          (add-to-list 'org-latex-packages-alist '("" "minted"))
+          ;; Tell the latex export to use the minted package for source
+          ;; code coloration.
+          (setq org-latex-listings 'minted)
+          ;; Let the exporter use the -shell-escape option to let latex
+          ;; execute external programs.
+          ;; This obviously and can be dangerous to activate!
+          (setq org-latex-minted-options
+                '(("mathescape" "true")
+                  ("linenos" "true")
+                  ("numbersep" "5pt")
+                  ("frame" "lines")
+                  ("framesep" "2mm")))
+          (setq org-latex-default-packages-alist
+                '(("" "hyperref" nil)
+                  ("AUTO" "inputenc" t)
+                  ("" "fixltx2e" nil)
+                  ("" "graphicx" t)
+                  ("" "longtable" nil)
+                  ("" "float" nil)
+                  ("" "wrapfig" nil)
+                  ("" "rotating" nil)
+                  ("normalem" "ulem" t)
+                  ("" "amsmath" t)
+                  ("" "textcomp" t)
+                  ("" "marvosym" t)
+                  ("" "wasysym" t)
+                  ("" "multicol" t) 
+                  ("" "amssymb" t)
+                  "\\tolerance=1000"))
+          (setq org-latex-pdf-process
+                '("xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+                  "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f"
+                  "xelatex -shell-escape -interaction nonstopmode -output-directory %o %f")))))
     :config
     (progn
       (require 'org-indent)
       (define-key global-map "\C-cl" 'org-store-link)
-      (define-key global-map "\C-ca" 'org-agenda))))
+      (define-key global-map "\C-ca" 'org-agenda)
+      (setq org-latex-classes
+            '(("article"
+               "
+ 		\\documentclass[12pt,a4paper]{article}
+		\\usepackage[margin=2cm]{geometry}
+		\\usepackage{fontspec}
+		\\setromanfont{文泉驛正黑}
+
+		\\usepackage{etoolbox}  % Quote 部份的字型設定
+		\\newfontfamily\\quotefont{文泉驛等寬正黑}
+		\\AtBeginEnvironment{quote}{\\quotefont\\small}
+
+		\\setmonofont[Scale=0.9]{Courier} % 等寬字型 [FIXME] Courier 中文會爛掉！
+		\\font\\wqyHei=''文泉驛正黑'' at 10pt
+		\\font\\wqyHeiMono=''文泉驛等寬正黑'' at 10pt
+		\\font\\wqyHeiMicro=''文泉驛微米黑'' at 10pt
+		\\XeTeXlinebreaklocale ``zh''
+		\\XeTeXlinebreakskip = 0pt plus 1pt
+		\\linespread{1.36}
+
+		% [FIXME] ox-latex 的設計不良導致 hypersetup 必須在這裡插入
+		\\usepackage{hyperref}
+		\\hypersetup{
+		colorlinks=true, %把紅框框移掉改用字體顏色不同來顯示連結
+		linkcolor=[rgb]{0,0.37,0.53},
+		citecolor=[rgb]{0,0.47,0.68},
+		filecolor=[rgb]{0,0.37,0.53},
+		urlcolor=[rgb]{0,0.37,0.53},
+		pagebackref=true,
+		linktoc=all,}
+               "
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
+      ;; [FIXME]
+      ;; 原本是不要讓 org 插入 hypersetup（因為 org-mode 這部份設計成沒辦法自訂，或許可以去 report 一下？）
+      ;; 改成自行插入，但這樣 pdfcreator 沒辦法根據 Emacs 版本插入，pdfkeyword 也會無效...幹。
+      (setq org-latex-with-hyperref t)
+      )))
 
 (defun org/init-org-bullets ()
   (use-package org-bullets
