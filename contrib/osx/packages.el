f4670d6c7acede9ac18e7e0cb065224877b7140c
syl20bnr <sylvain.benner@gmail.com>
2015-05-10 01:15:00 -0400

    osx layer: add documentation for coreutils' gls

--- contrib/osx/packages.el
+++ contrib/osx/packages.el
@@ -4,8 +4,9 @@
     ))
 
 (if (executable-find "gls")
-    ; maybe absolute or relative name of the `ls' program used by `insert-directory'.
-    ; brew info coreutils
+    ;; maybe absolute or relative name of the `ls' program used by
+    ;; `insert-directory'.
+    ;; brew info coreutils
     (setq insert-directory-program "gls"
           dired-listing-switches "-aBhl --group-directories-first")
   (setq dired-use-ls-dired nil))
