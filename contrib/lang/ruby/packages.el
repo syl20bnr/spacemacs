d7cc695ad96028b67103077566e130131a065784
syl20bnr <sylvain.benner@gmail.com>
2015-05-13 23:25:36 -0400

    ruby layer: simplify declaration of rails key bindings

--- contrib/lang/ruby/packages.el
+++ contrib/lang/ruby/packages.el
@@ -72,49 +72,50 @@
       (spacemacs|diminish projectile-rails-mode " ⇋" " RoR")
 
       ;; Find files
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfa" 'projectile-rails-find-locale)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfc" 'projectile-rails-find-controller)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfe" 'projectile-rails-find-environment)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrff" 'projectile-rails-find-feature)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfh" 'projectile-rails-find-helper)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfi" 'projectile-rails-find-initializer)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfj" 'projectile-rails-find-javascript)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfl" 'projectile-rails-find-lib)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfm" 'projectile-rails-find-model)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfn" 'projectile-rails-find-migration)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfo" 'projectile-rails-find-log)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfp" 'projectile-rails-find-spec)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfr" 'projectile-rails-find-rake-task)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfs" 'projectile-rails-find-stylesheet)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrft" 'projectile-rails-find-test)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfu" 'projectile-rails-find-fixture)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfv" 'projectile-rails-find-view)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrfy" 'projectile-rails-find-layout)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrf@" 'projectile-rails-find-mailer)
-      ;; Goto file
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgc" 'projectile-rails-find-current-controller)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgd" 'projectile-rails-goto-schema)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrge" 'projectile-rails-goto-seeds)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgh" 'projectile-rails-find-current-helper)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgj" 'projectile-rails-find-current-javascript)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgg" 'projectile-rails-goto-gemfile)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgm" 'projectile-rails-find-current-model)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgn" 'projectile-rails-find-current-migration)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgp" 'projectile-rails-find-current-spec)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgr" 'projectile-rails-goto-routes)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgs" 'projectile-rails-find-current-stylesheet)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgt" 'projectile-rails-find-current-test)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgu" 'projectile-rails-find-current-fixture)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgv" 'projectile-rails-find-current-view)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrgz" 'projectile-rails-goto-spec-helper)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrg." 'projectile-rails-goto-file-at-point)
-      ;; Rails external commands
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrcc" 'projectile-rails-generate)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mri" 'projectile-rails-console)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrr:" 'projectile-rails-rake)
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrxs" 'projectile-rails-server)
-      ;; Refactoring 'projectile-rails-mode
-      (evil-leader/set-key-for-mode 'enh-ruby-mode "mrRx" 'projectile-rails-extract-region)
+      (evil-leader/set-key-for-mode 'enh-ruby-mode
+        "mrfa" 'projectile-rails-find-locale
+        "mrfc" 'projectile-rails-find-controller
+        "mrfe" 'projectile-rails-find-environment
+        "mrff" 'projectile-rails-find-feature
+        "mrfh" 'projectile-rails-find-helper
+        "mrfi" 'projectile-rails-find-initializer
+        "mrfj" 'projectile-rails-find-javascript
+        "mrfl" 'projectile-rails-find-lib
+        "mrfm" 'projectile-rails-find-model
+        "mrfn" 'projectile-rails-find-migration
+        "mrfo" 'projectile-rails-find-log
+        "mrfp" 'projectile-rails-find-spec
+        "mrfr" 'projectile-rails-find-rake-task
+        "mrfs" 'projectile-rails-find-stylesheet
+        "mrft" 'projectile-rails-find-test
+        "mrfu" 'projectile-rails-find-fixture
+        "mrfv" 'projectile-rails-find-view
+        "mrfy" 'projectile-rails-find-layout
+        "mrf@" 'projectile-rails-find-mailer
+        ;; Goto file
+        "mrgc" 'projectile-rails-find-current-controller
+        "mrgd" 'projectile-rails-goto-schema
+        "mrge" 'projectile-rails-goto-seeds
+        "mrgh" 'projectile-rails-find-current-helper
+        "mrgj" 'projectile-rails-find-current-javascript
+        "mrgg" 'projectile-rails-goto-gemfile
+        "mrgm" 'projectile-rails-find-current-model
+        "mrgn" 'projectile-rails-find-current-migration
+        "mrgp" 'projectile-rails-find-current-spec
+        "mrgr" 'projectile-rails-goto-routes
+        "mrgs" 'projectile-rails-find-current-stylesheet
+        "mrgt" 'projectile-rails-find-current-test
+        "mrgu" 'projectile-rails-find-current-fixture
+        "mrgv" 'projectile-rails-find-current-view
+        "mrgz" 'projectile-rails-goto-spec-helper
+        "mrg." 'projectile-rails-goto-file-at-point
+        ;; Rails external commands
+        "mrcc" 'projectile-rails-generate
+        "mri" 'projectile-rails-console
+        "mrr:" 'projectile-rails-rake
+        "mrxs" 'projectile-rails-server
+        ;; Refactoring 'projectile-rails-mode
+        "mrRx" 'projectile-rails-extract-region)
       ;; Ex-commands
       (evil-ex-define-cmd "A" 'projectile-toggle-between-implementation-and-test))))
 
