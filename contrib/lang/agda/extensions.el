9e13aed949d87b16ce4044bb5b2d4b4c8344eaed
syl20bnr <sylvain.benner@gmail.com>
2015-05-18 01:51:03 -0400

    agda layer: refactor layer to use `use-package`
    - display a warning if agda has not been found
    - add a micro-state to easily navigate between the goals
    - use face-remapping-alist to redefine the faces
    - add some missing key bindings
    - add support for golden-ratio
    - add quick cabal instruction to install agda
    - sort key bindings alphabetically

--- contrib/lang/agda/extensions.el
+++ contrib/lang/agda/extensions.el
@@ -10,39 +10,70 @@
 ;;
 ;;; License: GPLv3
 
-(defvar agda-pre-extensions '(agda))
+(defvar agda-post-extensions '(agda))
 
 (defun agda/init-agda ()
-  (custom-set-faces
-   '(agda2-highlight-datatype-face ((t (:inherit font-lock-type-face))))
-   '(agda2-highlight-function-face ((t (:inherit font-lock-type-face))))
-   '(agda2-highlight-inductive-constructor-face ((t (:inherit font-lock-function-name-face))))
-   '(agda2-highlight-keyword-face ((t (:inherit font-lock-keyword-face))))
-   '(agda2-highlight-module-face ((t (:inherit font-lock-constant-face))))
-   '(agda2-highlight-number-face ((t nil)))
-   '(agda2-highlight-postulate-face ((t (:inherit font-lock-type-face))))
-   '(agda2-highlight-primitive-type-face ((t (:inherit font-lock-type-face))))
-   '(agda2-highlight-record-face ((t (:inherit font-lock-type-face)))))
+  (unless (executable-find "agda-mode")
+    (spacemacs-buffer/warning
+     (concat "Agda not detected, be sure that Agda binaries are "
+             "available in your PATH or check the installation "
+             "instructions in the README file.")))
 
-  (load-file (let ((coding-system-for-read 'utf-8))
-               (shell-command-to-string "agda-mode locate")))
+  (use-package agda2-mode
+    :if (executable-find "agda-mode")
+    :defer t
+    :init (load-file (let ((coding-system-for-read 'utf-8))
+                       (shell-command-to-string "agda-mode locate")))
+    (mapc
+     (lambda (x) (add-to-list 'face-remapping-alist x))
+     '((agda2-highlight-datatype-face              . font-lock-type-face)
+       (agda2-highlight-function-face              . font-lock-type-face)
+       (agda2-highlight-inductive-constructor-face . font-lock-function-name-face)
+       (agda2-highlight-keyword-face               . font-lock-keyword-face)
+       (agda2-highlight-module-face                . font-lock-constant-face)
+       (agda2-highlight-number-face                . nil)
+       (agda2-highlight-postulate-face             . font-lock-type-face)
+       (agda2-highlight-primitive-type-face        . font-lock-type-face)
+       (agda2-highlight-record-face                . font-lock-type-face)))
+    :config
+    (progn
+      (spacemacs|define-micro-state goal-navigation
+        :doc "[f] next [b] previous [q]uit"
+        :execute-binding-on-enter t
+        :evil-leader-for-mode
+        (agda2-mode . "mf")
+        (agda2-mode . "mb")
+        :bindings
+        ("f" agda2-next-goal)
+        ("b" agda2-previous-goal)
+        ("q" nil :exit t))
 
-  (evil-leader/set-key-for-mode 'agda2-mode
-    "ml" 'agda2-load
-    "ma" 'agda2-auto
-    "mr" 'agda2-refine
-    "ms" 'agda2-solveAll
-    "mt" 'agda2-goal-type
-    "mc" 'agda2-make-case
-    "mf" 'agda2-next-goal
-    "m?" 'agda2-show-goals
-    "me" 'agda2-show-context
-    "mb" 'agda2-previous-goal
-    "m," 'agda2-goal-and-context
-    "m=" 'agda2-show-constraints
-    "mh" 'agda2-helper-function-type
-    "md" 'agda2-infer-type-maybe-toplevel
-    "mw" 'agda2-why-in-scope-maybe-toplevel
-    "m." 'agda2-goal-and-context-and-inferred
-    "mo" 'agda2-module-contents-maybe-toplevel
-    "mn" 'agda2-compute-normalised-maybe-toplevel))
+      (evil-leader/set-key-for-mode 'agda2-mode
+        "m?" 'agda2-show-goals
+        "m." 'agda2-goal-and-context-and-inferred
+        "m," 'agda2-goal-and-context
+        "m=" 'agda2-show-constraints
+        "m <SPC>" 'agda2-give
+        "ma" 'agda2-auto
+        "mc" 'agda2-make-case
+        "md" 'agda2-infer-type-maybe-toplevel
+        "me" 'agda2-show-context
+        "mgg" 'agda2-goto-definition-keyboard
+        "mgG" 'agda2-go-back
+        "mh" 'agda2-helper-function-type
+        "ml" 'agda2-load
+        "mn" 'agda2-compute-normalised-maybe-toplevel
+        "mo" 'agda2-module-contents-maybe-toplevel
+        "mr" 'agda2-refine
+        "ms" 'agda2-solveAll
+        "mt" 'agda2-goal-type
+        "mw" 'agda2-why-in-scope-maybe-toplevel
+        "mxc" 'agda2-compile
+        "mxd" 'agda2-remove-annotations
+        "mxh" 'agda2-display-implicit-arguments
+        "mxq" 'agda2-quit
+        "mxr" 'agda2-restart)
+
+      (eval-after-load 'golden-ratio
+        '(add-to-list 'golden-ratio-exclude-buffer-names
+                      "*Agda information*")))))
