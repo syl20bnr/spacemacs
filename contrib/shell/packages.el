d681234a718640e4104ceb3defe30f43b066788e
syl20bnr <sylvain.benner@gmail.com>
2015-05-20 22:39:14 -0400

    new layer: shell

---
+++ contrib/shell/packages.el
@@ -0,0 +1,122 @@
+;;; packages.el --- shell packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq shell-packages
+      '(
+        helm
+        shell
+        shell-pop
+        term
+        ))
+
+(defun shell/post-init-helm ()
+  (spacemacs|use-package-add-hook helm
+    :post-config
+    (progn
+      (defun spacemacs//shell-helm-post-:config ()
+        "Configuration to append to helm package `:config' block."
+        ;; eshell
+        (defun spacemacs/helm-eshell-history ()
+          "Correctly revert to insert state after selection."
+          (interactive)
+          (helm-eshell-history)
+          (evil-insert-state))
+        (defun spacemacs/helm-shell-history ()
+          "Correctly revert to insert state after selection."
+          (interactive)
+          (helm-comint-input-ring)
+          (evil-insert-state))
+        (defun spacemacs/init-helm-eshell ()
+          "Initialize helm-eshell."
+          ;; this is buggy for now
+          ;; (define-key eshell-mode-map (kbd "<tab>") 'helm-esh-pcomplete)
+          (evil-leader/set-key-for-mode 'eshell-mode
+            "mH" 'spacemacs/helm-eshell-history))
+        (add-hook 'eshell-mode-hook 'spacemacs/init-helm-eshell)
+        ;;shell
+        (evil-leader/set-key-for-mode 'shell-mode
+          "mH" 'spacemacs/helm-shell-history))))
+  )
+
+(defun spacemacs/init-shell ()
+  (defun shell-comint-input-sender-hook ()
+    "Check certain shell commands.
+ Executes the appropriate behavior for certain commands."
+    (setq comint-input-sender
+          (lambda (proc command)
+            (cond
+             ;; Check for clear command and execute it.
+             ((string-match "^[ \t]*clear[ \t]*$" command)
+              (comint-send-string proc "\n")
+              (erase-buffer))
+             ;; Check for man command and execute it.
+             ((string-match "^[ \t]*man[ \t]*" command)
+              (comint-send-string proc "\n")
+              (setq command (replace-regexp-in-string "^[ \t]*man[ \t]*" "" command))
+              (setq command (replace-regexp-in-string "[ \t]+$" "" command))
+              (funcall 'man command))
+             ;; Send other commands to the default handler.
+             (t (comint-simple-send proc command))))))
+  (defun eshell/clear ()
+    "Clear contents in eshell."
+    (interactive)
+    (let ((inhibit-read-only t))
+      (erase-buffer)))
+  (add-hook 'shell-mode-hook 'shell-comint-input-sender-hook)
+  (add-hook 'eshell-mode-hook (lambda ()
+                                (setq pcomplete-cycle-completions nil))))
+
+(defun shell/init-shell-pop ()
+  (use-package shell-pop
+    :defer t
+    :init
+    (progn
+      (setq shell-pop-window-position shell-default-position
+            shell-pop-window-height   shell-default-height
+            shell-pop-term-shell      shell-default-term-shell)
+      (defmacro make-shell-pop-command (type &optional shell)
+        (let* ((name (symbol-name type)))
+          `(defun ,(intern (concat "shell-pop-" name)) (index)
+             (interactive "P")
+             (require 'shell-pop)
+             (shell-pop--set-shell-type
+              'shell-pop-shell-type
+              (backquote (,name
+                          ,(concat "*" name "*")
+                          (lambda nil (funcall ',type ,shell)))))
+             (shell-pop index))))
+      (make-shell-pop-command eshell)
+      (make-shell-pop-command shell)
+      (make-shell-pop-command term shell-pop-term-shell)
+      (make-shell-pop-command ansi-term shell-pop-term-shell)
+
+      (defun spacemacs/default-pop-shell ()
+        "Open the default shell in a popup."
+        (interactive)
+        (call-interactively (intern (format "shell-pop-%S" shell-default-shell))))
+      (evil-leader/set-key
+        ";"   'spacemacs/default-pop-shell
+        "ase" 'shell-pop-eshell
+        "asi" 'shell-pop-shell
+        "ast" 'shell-pop-term
+        "asT" 'shell-pop-ansi-term))))
+
+(defun shell/init-term ()
+  (defun term-send-tab ()
+    "Send tab in term mode."
+    (interactive)
+    (term-send-raw-string "\t"))
+  ;; hack to fix pasting issue, the paste micro-state won't
+  ;; work in term
+  (evil-define-key 'normal term-raw-map "p" 'term-paste)
+  (evil-define-key 'insert term-raw-map (kbd "C-c C-d") 'term-send-eof)
+  (evil-define-key 'insert term-raw-map (kbd "<tab>") 'term-send-tab))
--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -79,7 +79,6 @@
     leuven-theme
     linum-relative
     move-text
-    multi-term
     neotree
     page-break-lines
     popup
@@ -89,8 +88,6 @@
     rainbow-delimiters
     recentf
     rfringe
-    shell
-    shell-pop
     smartparens
     smooth-scrolling
     subword
@@ -1468,26 +1465,6 @@ ARG non nil means that the editing style is `vim'."
           (define-key helm-map (kbd "C-l") 'helm-recenter-top-bottom-other-window))))
       (spacemacs//helm-hjkl-navigation (eq 'vim dotspacemacs-editing-style))
 
-      ;; eshell
-      (defun spacemacs/helm-eshell-history ()
-        "Correctly revert to insert state after selection."
-        (interactive)
-        (helm-eshell-history)
-        (evil-insert-state))
-      (defun spacemacs/helm-shell-history ()
-        "Correctly revert to insert state after selection."
-        (interactive)
-        (helm-comint-input-ring)
-        (evil-insert-state))
-      (defun spacemacs/init-helm-eshell ()
-        "Initialize helm-eshell."
-        ;; this is buggy for now
-        ;; (define-key eshell-mode-map (kbd "<tab>") 'helm-esh-pcomplete)
-        (evil-leader/set-key-for-mode 'eshell-mode "mH" 'spacemacs/helm-eshell-history))
-      (add-hook 'eshell-mode-hook 'spacemacs/init-helm-eshell)
-      ;;shell
-      (evil-leader/set-key-for-mode 'shell-mode "mH" 'spacemacs/helm-shell-history)
-
       (defun spacemacs/helm-edit ()
         "Switch in edit mode depending on the current helm buffer."
         (interactive)
@@ -1980,24 +1957,6 @@ Put (global-hungry-delete-mode) in dotspacemacs/config to enable by default."
       :bindings
       ("J" move-text-down)
       ("K" move-text-up))))
-
-(defun spacemacs/init-multi-term ()
-  (use-package multi-term
-    :defer t
-    :init
-    (evil-leader/set-key "ast" 'multi-term)
-    :config
-    (progn
-      (defun term-send-tab ()
-        "Send tab in term mode."
-        (interactive)
-        (term-send-raw-string "\t"))
-
-      ;; hack to fix pasting issue, the paste micro-state won't
-      ;; work in multi-term
-      (evil-define-key 'normal term-raw-map "p" 'term-paste)
-      (evil-define-key 'insert term-raw-map (kbd "C-c C-d") 'term-send-eof)
-      (evil-define-key 'insert term-raw-map (kbd "<tab>") 'term-send-tab))))
 
 (defun spacemacs/init-neotree ()
   (use-package neotree
@@ -2575,51 +2534,6 @@ It is a string holding:
 (defun spacemacs/init-rfringe ()
   (use-package rfringe
     :defer t))
-
-(defun spacemacs/init-shell ()
-  (defun shell-comint-input-sender-hook ()
-    "Check certain shell commands.
- Executes the appropriate behavior for certain commands."
-    (setq comint-input-sender
-          (lambda (proc command)
-            (cond
-             ;; Check for clear command and execute it.
-             ((string-match "^[ \t]*clear[ \t]*$" command)
-              (comint-send-string proc "\n")
-              (erase-buffer))
-             ;; Check for man command and execute it.
-             ((string-match "^[ \t]*man[ \t]*" command)
-              (comint-send-string proc "\n")
-              (setq command (replace-regexp-in-string "^[ \t]*man[ \t]*" "" command))
-              (setq command (replace-regexp-in-string "[ \t]+$" "" command))
-              (funcall 'man command))
-             ;; Send other commands to the default handler.
-             (t (comint-simple-send proc command))))))
-  (defun eshell/clear ()
-    "Clear contents in eshell."
-    (interactive)
-    (let ((inhibit-read-only t))
-      (erase-buffer)))
-  (add-hook 'shell-mode-hook 'shell-comint-input-sender-hook)
-  (add-hook 'eshell-mode-hook (lambda ()
-                                (setq pcomplete-cycle-completions nil))))
-
-(defun spacemacs/init-shell-pop ()
-  (use-package shell-pop
-    :defer t
-    :init
-    (defmacro make-shell-pop-command (type &optional shell)
-      (let* ((name (symbol-name type)))
-        `(defun ,(intern (concat "shell-pop-" name)) (index)
-           (interactive "P")
-           (require 'shell-pop)
-           (shell-pop--set-shell-type 'shell-pop-shell-type (backquote (,name
-                                                                        ,(concat "*" name "*")
-                                                                        (lambda nil (funcall ',type ,shell)))))
-           (shell-pop index))))
-    (make-shell-pop-command eshell)
-    (make-shell-pop-command shell)
-    (make-shell-pop-command term shell-pop-term-shell)))
 
 (defun spacemacs/init-smartparens ()
   (use-package smartparens
