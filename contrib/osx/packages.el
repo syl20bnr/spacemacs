ad799003cb0de7fbe0aa1f3138415526d4dd0491
Uri Sharf <uri.sharf@gmail.com>
2015-05-21 18:48:14 +0200

    Refactor `system-move-to-file-trash` declaration to the osx layer

--- contrib/osx/packages.el
+++ contrib/osx/packages.el
@@ -11,6 +11,15 @@
           dired-listing-switches "-aBhl --group-directories-first")
   (setq dired-use-ls-dired nil))
 
+;; use trash if installed
+(if (executable-find "trash")
+    (defun system-move-file-to-trash (file)
+      "Use `trash' to move FILE to the system trash.
+Can be installed with `brew install trash', or `brew install osxutils`''."
+      (call-process (executable-find "trash") nil 0 nil file))
+  ;; regular move to trash directory
+  (setq trash-directory "~/.Trash/emacs"))
+
 (defun osx/init-pbcopy ()
   (use-package pbcopy
     :if (not (display-graphic-p))
