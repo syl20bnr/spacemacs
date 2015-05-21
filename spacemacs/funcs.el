91462a6d4133200aea4317d58555d1bb9fecb07f
Eivind Fonn <evfonn@gmail.com>
2015-05-21 12:14:06 +0200

    Don't close compilation buffer after success

--- spacemacs/funcs.el
+++ spacemacs/funcs.el
@@ -536,20 +536,19 @@ argument takes the kindows rotate backwards."
 
 ;; From http://xugx2007.blogspot.ca/2007/06/benjamin-rutts-emacs-c-development-tips.html
 (setq compilation-finish-function
-   (lambda (buf str)
+      (lambda (buf str)
 
-     (if (or (string-match "exited abnormally" str)
-            (string-match "FAILED" (buffer-string)))
+        (if (or (string-match "exited abnormally" str)
+                (string-match "FAILED" (buffer-string)))
 
-         ;;there were errors
-         (message "There were errors. SPC-e-n to visit.")
-       (unless (or (string-match "Grep finished" (buffer-string))
-                  (string-match "Ag finished" (buffer-string))
-                  (string-match "nosetests" (buffer-name)))
+            ;; there were errors
+            (message "There were errors. SPC-e-n to visit.")
+          (unless (or (string-match "Grep finished" (buffer-string))
+                      (string-match "Ag finished" (buffer-string))
+                      (string-match "nosetests" (buffer-name)))
 
-         ;;no errors, make the compilation window go away in 0.5 seconds
-         (delete-windows-on buf)
-         (message "compilation ok.")))))
+            ;; no errors
+            (message "compilation ok.")))))
 
 ;; from https://gist.github.com/timcharper/493269
 (defun split-window-vertically-and-switch ()
