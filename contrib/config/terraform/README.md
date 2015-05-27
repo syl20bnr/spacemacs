812bdc6a12f234565b707767abe7b120093092ec
Brian Hicks <brian@brianthicks.com>
2015-05-27 13:04:52 -0500

    terraform: add layer

---
+++ contrib/config/terraform/README.md
@@ -0,0 +1,25 @@
+# Terraform contribution layer for Spacemacs
+
+![Terraform logo](img/terraform.png)
+
+<!-- markdown-toc start - Don't edit this section. Run M-x markdown-toc/generate-toc again -->
+**Table of Contents**
+
+- [Terraform contribution layer for Spacemacs](#terraform-contribution-layer-for-spacemacs)
+    - [Description](#description)
+    - [Install](#install)
+
+<!-- markdown-toc end -->
+
+## Description
+
+This layer provides syntax support for Terraform `.tf` files using
+[terraform-mode](https://github.com/syohex/emacs-terraform-mode).
+
+## Install
+
+To use this layer add it to your `~/.spacemacs`
+
+```elisp
+(setq-default dotspacemacs-configuration-layers '(terraform)')
+```
---
+++ contrib/config/terraform/packages.el
@@ -0,0 +1,23 @@
+;;; packages.el --- terraform Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Brian Hicks & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; List of all packages to install and/or initialize. Built-in packages
+;; which require an initialization must be listed explicitly in the list.
+(defvar terraform-packages
+  '(terraform-mode))
+
+;; List of packages to exclude.
+(defvar terraform-excluded-packages '())
+
+(defun terraform/init-terraform-mode ()
+  (use-package terraform-mode
+    :defer t))
