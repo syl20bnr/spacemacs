5f62f0ed40dee01b6e1cab3ad7340f92579cb82b
Kirill Kulikov <kirill.kulikov@gmail.com>
2015-05-31 22:15:37 +0100

    ensime configured to store files in .cache/

--- contrib/lang/scala/packages.el
+++ contrib/lang/scala/packages.el
@@ -36,6 +36,8 @@ which require an initialization must be listed explicitly in the list.")
       (add-hook 'scala-mode-hook 'scala/maybe-start-ensime))
     :config
     (progn
+      (setq user-emacs-ensime-directory ".cache/ensime")
+
       (evil-define-key 'insert ensime-mode-map
         (kbd ".") 'scala/completing-dot
         (kbd "M-.") 'ensime-edit-definition
