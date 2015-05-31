4b518018c2b34de3d4309149ea535cfccd25665d
ralesi <scio62@gmail.com>
2015-05-30 19:01:28 -0700

    Unimpaired contribution layer

---
+++ contrib/!vim/unimpaired/packages.el
@@ -0,0 +1,28 @@
+;;; packages.el --- unimpaired Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq unimpaired-packages
+  '(
+    ;; package unimpaireds go here
+    ))
+
+(setq unimpaired-excluded-packages '())
+
+;; For each package, define a function unimpaired/init-<package-unimpaired>
+;;
+;; (defun unimpaired/init-my-package ()
+;;   "Initialize my package"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
