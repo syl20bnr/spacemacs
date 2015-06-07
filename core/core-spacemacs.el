5543f3a19652c1c068780ce76287bab5244a28e1
syl20bnr <sylvain.benner@gmail.com>
2015-06-06 23:26:14 -0400

    core: add support for spacemacs update
    Move release management suff to newly created core-release-management.el

--- core/core-spacemacs.el
+++ core/core-spacemacs.el
@@ -14,6 +14,7 @@
 
 (require 'subr-x nil 'noerror)
 (require 'core-emacs-backports)
+(require 'core-release-management)
 (require 'core-auto-completion)
 (require 'core-themes-support)
 (require 'core-fonts-support)
@@ -22,29 +23,10 @@
 (require 'core-micro-state)
 (require 'core-use-package-ext)
 
-(defconst spacemacs-repository "spacemacs"
-  "Name of the Spacemacs remote repository.")
-(defconst spacemacs-repository-owner "syl20bnr"
-  "Name of the Spacemacs remote repository owner.")
-(defconst spacemacs-checkversion-remote "checkversion"
-  "Name of the remote repository used to check for new version.")
-(defconst spacemacs-checkversion-branch "master"
-  "Name of the branch used to check for new version.")
-
 (defgroup spacemacs nil
   "Spacemacs customizations."
   :group 'starter-kit
   :prefix 'spacemacs-)
-
-;; new version variables
-(defvar spacemacs-new-version nil
-  "If non-nil a new Spacemacs version is available.")
-(defvar spacemacs-version-check-timer nil
-  "The current timer for new version check.")
-(defvar spacemacs-version-check-interval "6 hours"
-  "Time between two version checks.")
-(defvar spacemacs-version-check-lighter "[+]"
-  "Text displayed in the mode-line when a new version is available.")
 
 ;; loading progress bar variables
 (defvar spacemacs-loading-char ?█)
@@ -199,150 +181,6 @@ FILE-TO-LOAD is an explicit file to load after the installation."
 (defun display-startup-echo-area-message ()
   "Change the default welcome message of minibuffer to another one."
   (message "Spacemacs is ready."))
-
-(defun spacemacs/get-last-version (repo owner remote branch)
-  "Return the last tagged version of BRANCH on REMOTE repository from
-OWNER REPO."
-  (let ((url (format "https://github.com/%s/%s" owner repo)))
-    (spacemacs/git-declare-remote remote url))
-  (spacemacs/git-fetch-tags remote branch)
-  (let ((version (spacemacs/git-latest-tag remote branch)))
-    (when version
-      (save-match-data
-        (string-match "^.*\\([0-9]+\\.[0-9]+\\.[0-9]+\\)$" version)
-        (match-string 1 version)))))
-
-(defun spacemacs/check-for-new-version (&optional interval)
-  "Periodicly check for new for new Spacemacs version.
-Update `spacemacs-new-version' variable if any new version has been
-found."
-  (message "Start checking for new version...")
-  (async-start
-   (lambda ()
-     (load-file (concat user-emacs-directory "core/core-load-paths.el"))
-     (require 'core-spacemacs)
-     (spacemacs/get-last-version spacemacs-repository
-                                 spacemacs-repository-owner
-                                 spacemacs-checkversion-remote
-                                 spacemacs-checkversion-branch))
-   (lambda (result)
-     (if result
-         (if (or (version< result spacemacs-version)
-                 (string= result spacemacs-version)
-                 (if spacemacs-new-version
-                     (string= result spacemacs-new-version)))
-             (message "Spacemacs is up to date.")
-           (message "New version of Spacemacs available: %s" result)
-           (setq spacemacs-new-version result))
-       (message "Unable to check for new version."))))
-  (when interval
-    (setq spacemacs-version-check-timer
-          (run-at-time t (timer-duration interval)
-                       'spacemacs/check-for-new-version))))
-
-(defun spacemacs/git-has-remote (remote)
-  "Return non nil if REMOTE is declared."
-  (let((proc-buffer "git-has-remote")
-       (default-directory (file-truename user-emacs-directory)))
-    (when (eq 0 (process-file "git" nil proc-buffer nil "remote"))
-        (with-current-buffer proc-buffer
-          (prog2
-              (goto-char (point-min))
-              (re-search-forward (format "^%s$" remote) nil 'noerror)
-            (kill-buffer proc-buffer))))))
-
-(defun spacemacs/git-declare-remote (remote url)
-  "Declare a new REMOTE pointing to URL, return t if no error."
-  (let((proc-buffer "git-declare-remote")
-       (default-directory (file-truename user-emacs-directory)))
-    (prog2
-        (progn
-          (eq 0 (process-file "git" nil proc-buffer nil
-                              "remote" "remove" remote))
-          (eq 0 (process-file "git" nil proc-buffer nil
-                              "remote" "add" remote url)))
-        (eq 0 (process-file "git" nil proc-buffer nil
-                            "fetch" remote))
-      (kill-buffer proc-buffer))))
-
-(defun spacemacs/git-fetch-tags (remote branch)
-  "Fetch the tags for BRANCH in REMOTE repository."
-  (let((proc-buffer "git-fetch-tags")
-       (default-directory (file-truename user-emacs-directory)))
-    (prog2
-        (eq 0 (process-file "git" nil proc-buffer nil
-                            "fetch" remote branch))
-        ;; explicitly fetch the new tags
-        (eq 0 (process-file "git" nil proc-buffer nil
-                            "fetch" "--tags" remote branch))
-      (kill-buffer proc-buffer))))
-
-(defun spacemacs/git-latest-tag (remote branch)
-  "Returns the latest tag on REMOTE/BRANCH."
-  (let((proc-buffer "git-latest-tag")
-       (default-directory (file-truename user-emacs-directory))
-       (where (format "%s/%s" remote branch)))
-    (when (eq 0 (process-file "git" nil proc-buffer nil
-                              "describe" "--tags" "--abbrev=0"
-                              "--match=v*" where "FETCH_HEAD"))
-      (with-current-buffer proc-buffer
-        (prog1
-            (when (buffer-string)
-                (end-of-buffer)
-                (forward-line -1)
-                (replace-regexp-in-string
-                 "\n$" ""
-                 (buffer-substring (line-beginning-position)
-                                   (line-end-position))))
-          (kill-buffer proc-buffer))))))
-
-(defun spacemacs//deffaces-new-version-lighter (state)
-  "Define a new version lighter face for the given STATE."
-  (let* ((fname (intern (format "spacemacs-mode-line-new-version-lighter-%s-face"
-                                (symbol-name state))))
-         (foreground (face-foreground state)))
-    (eval `(defface ,fname '((t ()))
-             ,(format "Color for new version lighter in mode line (%s)."
-                      (symbol-name state))
-             :group 'spacemacs))
-    (set-face-attribute fname nil
-                        :foreground foreground
-                        :box (face-attribute 'mode-line :box))))
-
-(defun spacemacs/set-new-version-lighter-mode-line-faces ()
-  "Define or set the new version lighter mode-line faces."
-  (mapcar 'spacemacs//deffaces-new-version-lighter
-          '(error warning success)))
-(spacemacs/set-new-version-lighter-mode-line-faces)
-
-(defun spacemacs//compute-version-score (version)
-  "Returns an integer from the version list.
-Example: (1 42 3) = 1 042 003"
-  (let ((result 0)
-        (rev (reverse version)))
-    (dotimes (i 3)
-      (setq result (+ result (* (nth i rev) (expt 10 (* i 3))))))
-    result))
-
-(defun spacemacs//compute-version-score (version)
-  "Returns an integer from the version list.
-Example: (1 42 3) = 1 042 003"
-  (let ((i -1))
-    (reduce '+ (mapcar (lambda (n) (setq i (1+ i)) (* n (expt 10 (* i 3))))
-                       (reverse version)))))
-
-(defun spacemacs/get-new-version-lighter-face (current new)
-  "Return the new version lighter face given the difference between the CURRENT
-version and the NEW version."
-  (let* ((lcur (version-to-list current))
-         (lnew (version-to-list new))
-         (scur (spacemacs//compute-version-score lcur))
-         (snew (spacemacs//compute-version-score lnew))
-         (diff (- snew scur)))
-    (cond
-     ((< diff 3000) 'spacemacs-mode-line-new-version-lighter-success-face)
-     ((< diff 5000) 'spacemacs-mode-line-new-version-lighter-warning-face)
-     (t 'spacemacs-mode-line-new-version-lighter-error-face))))
 
 (defun spacemacs/setup-after-init-hook ()
   "Add post init processing."
