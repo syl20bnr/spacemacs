31556a6e1456559123ff40a1c95245e82c830617
ralesi <scio62@gmail.com>
2015-06-08 03:07:50 -0700

    Vinegar contribution layer

---
+++ contrib/!vim/vinegar/keybindings.el
@@ -0,0 +1,27 @@
+(require 'dired-x)
+(define-key evil-normal-state-map (kbd "-") 'dired-jump)
+
+(add-hook 'dired-mode-hook 'vinegar/dired-setup)
+
+(eval-after-load "dired-mode"
+  (evilify dired-mode dired-mode-map
+           "j"         'vinegar/move-down
+           "k"         'vinegar/move-up
+           "-"         'vinegar/up-directory
+           "0"         'dired-back-to-start-of-files
+           "="         'vinegar/dired-diff
+           (kbd "C-j") 'dired-next-subdir
+           (kbd "C-k") 'dired-prev-subdir
+           "I"         'vinegar/dotfiles-toggle
+           (kbd "~")   '(lambda ()(interactive) (find-alternate-file "~/"))
+           (kbd "RET") 'dired-find-alternate-file
+           "f"         'helm-find-files
+           (kbd "C-f") 'find-name-dired
+           "H"         'diredp-dired-recent-dirs
+           "T"         'dired-tree-down
+           "K"         'dired-do-kill-lines
+           "r"         'dired-do-redisplay
+           (kbd "C-r") 'revert-buffer
+           "gg"        'vinegar/back-to-top
+           "G"         'vinegar/jump-to-bottom
+           ))
