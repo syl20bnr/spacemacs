71cb60fd7d1c6ec615eac373dedb6040019e3cce
sooheon <tngjs0@gmail.com>
2015-06-13 03:40:58 +0900

    Change s-v binding from evil-paste-after to evil-paste-before

--- contrib/osx/keybindings.el
+++ contrib/osx/keybindings.el
@@ -14,7 +14,7 @@
     (global-set-key (kbd "s--") 'spacemacs/scale-down-font)
     (global-set-key (kbd "s-0") 'spacemacs/reset-font-size)
     (global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
-    (global-set-key (kbd "s-v") 'evil-paste-after)
+    (global-set-key (kbd "s-v") 'evil-paste-before)
     (global-set-key (kbd "s-c") 'evil-yank)
     (global-set-key (kbd "s-a") 'mark-whole-buffer)
     (global-set-key (kbd "s-x") 'kill-region)
