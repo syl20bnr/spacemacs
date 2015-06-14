3549fd37d6b9badfee36e9eadde132b6cd63fbee
sooheon <tngjs0@gmail.com>
2015-06-14 16:54:51 +0900

    Correct s-v keybinding with `yank`

--- contrib/osx/keybindings.el
+++ contrib/osx/keybindings.el
@@ -17,7 +17,7 @@
     (global-set-key (kbd "s--") 'spacemacs/scale-down-font)
     (global-set-key (kbd "s-0") 'spacemacs/reset-font-size)
     (global-set-key (kbd "s-q") 'save-buffers-kill-terminal)
-    (global-set-key (kbd "s-v") 'evil-paste-before)
+    (global-set-key (kbd "s-v") 'yank)
     (global-set-key (kbd "s-c") 'evil-yank)
     (global-set-key (kbd "s-a") 'mark-whole-buffer)
     (global-set-key (kbd "s-x") 'kill-region)
