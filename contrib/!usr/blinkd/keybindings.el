f27de3d8b03482afcf0e0a831e059e7cd00479b2
wsk <tshemeng@gmail.com>
2015-06-15 17:10:02 +0800

    winner-mode

--- contrib/!usr/blinkd/keybindings.el
+++ contrib/!usr/blinkd/keybindings.el
@@ -1,5 +1,8 @@
 (global-set-key (kbd "C-x x") 'call-last-kbd-macro)
 
+;; (global-set-key (kbd "C-x 4 u") 'winner-undo)
+;; (global-set-key (kbd "C-x 4 r") 'winner-redo)
+
 (cond ((system-is-linux)
        (global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
        (global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease))
