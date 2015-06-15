912eecbe520cf9db432691fae041d46604eacf06
wsk <tshemeng@gmail.com>
2015-06-15 16:59:34 +0800

    create layer blinkd

---
+++ contrib/!usr/blinkd/extensions.el
@@ -0,0 +1,31 @@
+;;; extensions.el --- blinkd Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq blinkd-pre-extensions
+      '(
+        ;; pre extension names go here
+        ))
+
+(setq blinkd-post-extensions
+      '(
+        ;; post extension names go here
+        ))
+
+;; For each extension, define a function blinkd/init-<extension-name>
+;;
+;; (defun blinkd/init-my-extension ()
+;;   "Initialize my extension"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
