e34e5507043f369bd4b9ff32b417dad1874a66b4
wsk <tshemeng@gmail.com>
2015-06-15 17:06:50 +0800

    keybindings:mouse wheel uo/down for text increase/decrease

---
+++ contrib/!usr/blinkd/keybindings.el
@@ -0,0 +1,6 @@
+(cond ((system-is-linux)
+       (global-set-key (kbd "<C-mouse-4>") 'text-scale-increase)
+       (global-set-key (kbd "<C-mouse-5>") 'text-scale-decrease))
+      ((system-is-mswindows)
+       (global-set-key (kbd "<C-wheel-up>") 'text-scale-increase)
+       (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)))
