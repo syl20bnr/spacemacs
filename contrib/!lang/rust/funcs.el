106d7f071f0a89853cb94c1bc9b231b07c019663
JP-Ellis <coujellis@gmail.com>
2015-06-16 18:27:54 +1000

    Rust improvements:
    - Use compile instead of shell for cargo. This allow Emacs to parse the
      output, including all the errors :)
    - Added support for Racer (it works for me at least... hopefully for
      others too)
    
    Signed-off-by: JP-Ellis <coujellis@gmail.com>

---
+++ contrib/!lang/rust/funcs.el
@@ -0,0 +1,28 @@
+;;; funcs.el --- Rust Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2015 Chris Hoeppner & Contributors
+;;
+;; Author: Chris Hoeppner <me@mkaito.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; http://doc.crates.io/guide.html
+(defun spacemacs/rust-cargo-build ()
+  (interactive)
+  (compile "cargo build"))
+
+(defun spacemacs/rust-cargo-run ()
+  (interactive)
+  (compile "cargo run"))
+
+(defun spacemacs/rust-cargo-test ()
+  (interactive)
+  (compile "cargo test"))
+
+(defun spacemacs/rust-cargo-doc ()
+  (interactive)
+  (compile "cargo doc"))
