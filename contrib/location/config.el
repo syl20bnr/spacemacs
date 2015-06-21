8c8778bbd7a18f8254df5d1e16128704e1e302b2
Gogs <gogitservice@gmail.com>
2015-06-21 16:42:44 +0200

    Add location contrib layer

---
+++ contrib/location/config.el
@@ -0,0 +1,14 @@
+(defvar enable-osx-location-service-support nil
+  "")
+
+(defvar enable-weather-forecast nil
+  "")
+
+(defvar enable-automatic-theme-managment nil
+  "")
+
+(defvar spacemacs/day-theme 'spacemacs-light
+  "")
+
+(defvar spacemacs/night-theme 'spacemacs-dark
+  "")
