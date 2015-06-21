9a974ebd7f7991841dec072c136a90db52c4f351
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2015-06-21 01:14:55 -0400

    Change `cider-inspect` key-binding to `m d i`
    Reading the CONVENTIONS.org doc I realized that `cider-inspect` does the
    same action as 'inspect value at point'.

--- contrib/!lang/clojure/packages.el
+++ contrib/!lang/clojure/packages.el
@@ -173,7 +173,7 @@ the focus."
         "mtr" 'spacemacs/cider-test-rerun-tests
         "mtt" 'spacemacs/cider-test-run-focused-test
 
-        "mi" 'cider-inspect)
+        "mdi" 'cider-inspect)
       (when clojure-enable-fancify-symbols
         (clojure/fancify-symbols 'cider-repl-mode)))
 
