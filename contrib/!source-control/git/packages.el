f2edee90b751c957d5f73f82901a7bfa57c5dd88
Tommi Komulainen <tommi.komulainen@iki.fi>
2015-06-21 09:50:00 +0200

    git: add `SPC g L` keybinding for magit-file-log
    Display the log for the currently visited file or another one.

--- contrib/!source-control/git/packages.el
+++ contrib/!source-control/git/packages.el
@@ -130,6 +130,7 @@
       (evil-leader/set-key
         "gb" 'magit-blame-mode
         "gl" 'magit-log
+        "gL" 'magit-file-log
         "gs" 'magit-status
         "gd" 'spacemacs/magit-diff-head
         "gC" 'magit-commit)
