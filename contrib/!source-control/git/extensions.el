7876176d3895dfb6c3f757569f22223a9458be01
syl20bnr <sylvain.benner@gmail.com>
2015-06-23 23:52:24 -0400

    Add magit-next as an extension (submodule)
    Set git-use-magit-next to t in the layer variable to use it.
    For now magit-ghpull from github layer is disabled.
    A LOT of issues with next magit and evil remain to be fixed

--- contrib/!source-control/git/extensions.el
+++ contrib/!source-control/git/extensions.el
@@ -1,4 +1,4 @@
-;;; packages.el --- Git Layer packages File for Spacemacs
+;;; extensions.el --- Git Layer Extensions File for Spacemacs
 ;;
 ;; Copyright (c) 2012-2014 Sylvain Benner
 ;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
@@ -10,108 +10,23 @@
 ;;
 ;;; License: GPLv3
 
-(setq git-packages
-      '(
-        gitattributes-mode
-        gitconfig-mode
-        gitignore-mode
-        git-messenger
-        git-timemachine
-        helm-gitignore
-        smeargle))
+(setq git-post-extensions '())
 
-(unless git-use-magit-next
-  (push 'magit git-packages)
-  ;; not compatible with magit-next
-  (push 'magit-svn git-packages)
-  (push 'git-commit-mode git-packages)
-  (push 'git-rebase-mode git-packages)
-  (push 'magit-gitflow git-packages))
+(when git-use-magit-next
+  (setq git-post-extensions '(magit-next)))
 
-(defun git/init-helm-gitignore ()
-  (use-package helm-gitignore
-    :defer t
-    :init (evil-leader/set-key "gI" 'helm-gitignore)))
-
-(defun git/init-git-commit-mode ()
-  (use-package git-commit-mode
-    :defer t
-    :config
-    (evil-leader/set-key-for-mode 'git-commit-mode
-      "mcc" 'git-commit-commit
-      "mk" 'git-commit-abort)))
-
-(defun git/init-git-messenger ()
-  (use-package git-messenger
-    :defer t
-    :init
-    (evil-leader/set-key
-      "gm" 'git-messenger:popup-message)))
-
-(defun git/init-git-rebase-mode ()
-  (use-package git-rebase-mode
-    :defer t
-    :config
-    (progn
-      (evilify git-rebase-mode git-rebase-mode-map
-               "J" 'git-rebase-move-line-down
-               "K" 'git-rebase-move-line-up
-               "u" 'git-rebase-undo
-               "y" 'git-rebase-insert)
-      (evil-leader/set-key-for-mode 'git-rebase-mode
-        "mcc" 'git-rebase-server-edit
-        "mk" 'git-rebase-abort))))
-
-(defun git/init-git-timemachine ()
-  (use-package git-timemachine
-    :defer t
-    :commands spacemacs/time-machine-micro-state
-    :init
-    (evil-leader/set-key
-      "gt" 'spacemacs/time-machine-micro-state)
-
-    :config
-    (progn
-
-      (defun spacemacs//time-machine-ms-on-enter ()
-        "Initiate git-timemachine properly with goden-ratio support."
-        (let ((golden-ratio (when (boundp 'golden-ratio-mode)
-                              golden-ratio-mode)))
-          (when (bound-and-true-p golden-ratio-mode) (golden-ratio-mode -1))
-          (git-timemachine)
-          (when golden-ratio (golden-ratio-mode))))
-
-      (spacemacs|define-micro-state time-machine
-        :doc "[p] [N] previous [n] next [c] current [Y] copy hash [q] quit"
-        :on-enter (spacemacs//time-machine-ms-on-enter)
-        :on-exit (git-timemachine-quit)
-        :persistent t
-        :bindings
-        ("c" git-timemachine-show-current-revision)
-        ("p" git-timemachine-show-previous-revision)
-        ("n" git-timemachine-show-next-revision)
-        ("N" git-timemachine-show-previous-revision)
-        ("Y" git-timemachine-kill-revision)
-        ("q" nil :exit t)))))
-
-(defun git/init-gitattributes-mode ()
-  (use-package gitattributes-mode
-    :defer t))
-
-(defun git/init-gitconfig-mode ()
-  (use-package gitconfig-mode
-    :defer t))
-
-(defun git/init-gitignore-mode ()
-  (use-package gitignore-mode
-    :defer t))
-
-(defun git/init-magit ()
+(defun git/init-magit-next ()
   (use-package magit
-    :if (not git-use-magit-next)
-    :defer t
+    :if git-use-magit-next
+    :commands (magit-status
+               magit-blame-mode
+               magit-log
+               magit-commit)
     :init
     (progn
+      (add-to-list 'load-path (format "%smagit-next/lisp/"
+                                      (configuration-layer/get-layer-property
+                                       'git :ext-dir)))
       (setq magit-last-seen-setup-instructions "1.4.0"
             magit-completing-read-function 'magit-ido-completing-read)
       (add-hook 'git-commit-mode-hook 'fci-mode)
@@ -179,7 +94,10 @@
                (kbd "C-v") 'magit-revert-item))
     :config
     (progn
-      (spacemacs|hide-lighter magit-auto-revert-mode)
+      ;; (spacemacs|hide-lighter magit-auto-revert-mode)
+      (define-key magit-staged-section-map "k" 'evil-previous-visual-line)
+      (define-key magit-hunk-section-map "k" 'evil-previous-visual-line)
+      (define-key magit-file-section-map "k" 'evil-previous-visual-line)
       ;; full screen magit-status
       (when git-magit-status-fullscreen
         (defadvice magit-status (around magit-fullscreen activate)
@@ -210,28 +128,3 @@
         (setq magit-diff-options (remove "-w" magit-diff-options))
         (magit-refresh))
       (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace))))
-
-(defun git/init-magit-gitflow ()
-  (use-package magit-gitflow
-    :commands turn-on-magit-gitflow
-    :init (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
-    :config (spacemacs|diminish magit-gitflow-mode "Flow")))
-
-(defun git/init-magit-svn ()
-  (use-package magit-svn
-    :if git-enable-magit-svn-plugin
-    :commands turn-on-magit-svn
-    :init (add-hook 'magit-mode-hook 'turn-on-magit-svn)
-    :config
-    (progn
-      (evil-define-key 'emacs magit-status-mode-map
-        "N" 'magit-key-mode-popup-svn))))
-
-(defun git/init-smeargle ()
-  (use-package smeargle
-    :defer t
-    :init
-    (evil-leader/set-key
-      "ghc" 'smeargle-clear
-      "ghh" 'smeargle-commits
-      "ght" 'smeargle)))
