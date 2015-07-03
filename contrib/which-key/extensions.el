960ee463a14d03aa95c65230b7142805f530db6a
syl20bnr <sylvain.benner@gmail.com>
2015-07-02 22:46:34 -0400

    New WIP layer: which-key

---
+++ contrib/which-key/extensions.el
@@ -0,0 +1,16 @@
+;;; extensions.el --- which-key Layer Extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq which-key-post-extensions '(which-key))
+
+(defun which-key/init-which-key ()
+  (use-package which-key)) 
