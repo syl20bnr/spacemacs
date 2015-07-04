dca6eca3c083281ab827dd250aa8bf6908984e99
syl20bnr <sylvain.benner@gmail.com>
2015-07-03 23:33:13 -0400

    Split ruby layer into !lang/ruby and !framework/ruby-on-rails

--- contrib/!lang/ruby/config.el
+++ contrib/!lang/ruby/config.el
@@ -16,25 +16,3 @@
 
 (defvar ruby-version-manager nil
   "If non nil defines the Ruby version manager (i.e. rbenv, rvm)")
-
-(defvar ruby-enable-ruby-on-rails-support nil
-  "If non nil we'll load support for Rails (haml and/or slim, features, navigation)")
-
-(defvar ruby-enable-haml-mode t
-  "When non-nil, well load haml-mode. By default this is enabled.")
-
-(defvar ruby-enable-slim-mode nil
-  "When non-nil, will load slim-mode")
-
-;; not supported for now
-;; (setq ruby/key-binding-prefixes
-;;       '(("mr" . "RoR")
-;;         ("mrc" . "RoR-compile/generate")
-;;         ("mrg" . "RoR-goto")
-;;         ("mrf" . "RoR-find")
-;;         ("mrr" . "RoR-rake")
-;;         ("mrR" . "RoR-refactoring")
-;;         ("mrs" . "RoR-REPL")
-;;         ("mrx" . "RoR-run")))
-;; (mapc (lambda (x) (spacemacs/declare-prefix (car x) (cdr x)))
-;;       ruby/key-binding-prefixes)
--- contrib/!lang/ruby/packages.el
+++ contrib/!lang/ruby/packages.el
@@ -1,3 +1,15 @@
+;;; packages.el --- Ruby Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
 (setq ruby-packages
   '(
     bundler
@@ -11,15 +23,6 @@
 
 (when ruby-version-manager
   (add-to-list 'ruby-packages ruby-version-manager))
-
-(when ruby-enable-ruby-on-rails-support
-  (when ruby-enable-slim-mode
-    (add-to-list 'ruby-packages `slim-mode))
-  (when ruby-enable-haml-mode
-    (add-to-list `ruby-packages `haml-mode))
-
-  (add-to-list 'ruby-packages 'feature-mode)
-  (add-to-list 'ruby-packages 'projectile-rails))
 
 (defun ruby/init-rbenv ()
   "Initialize RBENV mode"
@@ -74,64 +77,6 @@
       (evil-leader/set-key-for-mode 'enh-ruby-mode "mbu" 'bundle-update)
       (evil-leader/set-key-for-mode 'enh-ruby-mode "mbx" 'bundle-exec))))
 
-(defun ruby/init-projectile-rails ()
-  (use-package projectile-rails
-    :defer t
-    :init
-    (progn
-      (add-hook 'enh-ruby-mode-hook 'projectile-rails-on))
-    :config
-    (progn
-      (spacemacs|diminish projectile-rails-mode " ⇋" " RoR")
-
-      ;; Find files
-      (evil-leader/set-key-for-mode 'enh-ruby-mode
-        "mrfa" 'projectile-rails-find-locale
-        "mrfc" 'projectile-rails-find-controller
-        "mrfe" 'projectile-rails-find-environment
-        "mrff" 'projectile-rails-find-feature
-        "mrfh" 'projectile-rails-find-helper
-        "mrfi" 'projectile-rails-find-initializer
-        "mrfj" 'projectile-rails-find-javascript
-        "mrfl" 'projectile-rails-find-lib
-        "mrfm" 'projectile-rails-find-model
-        "mrfn" 'projectile-rails-find-migration
-        "mrfo" 'projectile-rails-find-log
-        "mrfp" 'projectile-rails-find-spec
-        "mrfr" 'projectile-rails-find-rake-task
-        "mrfs" 'projectile-rails-find-stylesheet
-        "mrft" 'projectile-rails-find-test
-        "mrfu" 'projectile-rails-find-fixture
-        "mrfv" 'projectile-rails-find-view
-        "mrfy" 'projectile-rails-find-layout
-        "mrf@" 'projectile-rails-find-mailer
-        ;; Goto file
-        "mrgc" 'projectile-rails-find-current-controller
-        "mrgd" 'projectile-rails-goto-schema
-        "mrge" 'projectile-rails-goto-seeds
-        "mrgh" 'projectile-rails-find-current-helper
-        "mrgj" 'projectile-rails-find-current-javascript
-        "mrgg" 'projectile-rails-goto-gemfile
-        "mrgm" 'projectile-rails-find-current-model
-        "mrgn" 'projectile-rails-find-current-migration
-        "mrgp" 'projectile-rails-find-current-spec
-        "mrgr" 'projectile-rails-goto-routes
-        "mrgs" 'projectile-rails-find-current-stylesheet
-        "mrgt" 'projectile-rails-find-current-test
-        "mrgu" 'projectile-rails-find-current-fixture
-        "mrgv" 'projectile-rails-find-current-view
-        "mrgz" 'projectile-rails-goto-spec-helper
-        "mrg." 'projectile-rails-goto-file-at-point
-        ;; Rails external commands
-        "mrcc" 'projectile-rails-generate
-        "mri" 'projectile-rails-console
-        "mrr:" 'projectile-rails-rake
-        "mrxs" 'projectile-rails-server
-        ;; Refactoring 'projectile-rails-mode
-        "mrRx" 'projectile-rails-extract-region)
-      ;; Ex-commands
-      (evil-ex-define-cmd "A" 'projectile-toggle-between-implementation-and-test))))
-
 (defun ruby/init-robe ()
   "Initialize Robe mode"
   (use-package robe
@@ -164,19 +109,6 @@
     :config (add-hook 'yaml-mode-hook
                       '(lambda ()
                          (define-key yaml-mode-map "\C-m" 'newline-and-indent)))))
-
-(defun ruby/init-feature-mode ()
-  "Initialize Cucumber feature mode"
-  (use-package feature-mode
-    :mode (("\\.feature\\'" . feature-mode))))
-
-(defun ruby/init-haml-mode ()
-  (use-package haml-mode
-    :defer t))
-
-(defun ruby/init-slim-mode ()
-  (use-package slim-mode
-    :defer t))
 
 (defun ruby/init-ruby-test-mode ()
   "Define keybindings for ruby test mode"
