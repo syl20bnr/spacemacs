c7ed7061d6803c174df45e37be9903ec3a4f6aaf
syl20bnr <sylvain.benner@gmail.com>
2015-07-05 00:34:27 -0400

    chinese layer: minor formatting of documentation and code

--- contrib/chinese/packages.el
+++ contrib/chinese/packages.el
@@ -27,49 +27,44 @@
     :if (eq 'wubi chinese-default-input-method)
     :init
     (progn
-      (autoload 'chinese-wbim-use-package "chinese-wubi" "Another emacs input method")
+      (autoload 'chinese-wbim-use-package "chinese-wubi"
+        "Another emacs input method")
       ;; Tooptip is not good enough, so disable it here.
       (setq chinese-wbim-use-tooltip nil)
-
       (register-input-method
        "chinese-wubi" "euc-cn" 'chinese-wbim-use-package
        "五笔" "汉字五笔输入法" "wb.txt")
       (require 'chinese-wbim-extra)
       (global-set-key ";" 'chinese-wbim-insert-ascii)
-      (setq default-input-method 'chinese-wubi)
-      )))
+      (setq default-input-method 'chinese-wubi))))
 
 (defun chinese/init-youdao-dictionary ()
   (use-package youdao-dictionary
     :config
     (progn
       ;; Enable Cache
-      (setq url-automatic-caching t)
-
-      ;; Set file path for saving search history
-      (setq youdao-dictionary-search-history-file (concat spacemacs-cache-directory ".youdao"))
-
-      ;; Enable Chinese word segmentation support 
-      (setq youdao-dictionary-use-chinese-word-segmentation t)
-      )))
+      (setq url-automatic-caching t
+            ;; Set file path for saving search history
+            youdao-dictionary-search-history-file
+            (concat spacemacs-cache-directory ".youdao")
+            ;; Enable Chinese word segmentation support
+            youdao-dictionary-use-chinese-word-segmentation t))))
 
 (defun chinese/init-chinese-pyim ()
   (use-package chinese-pyim
     :if (eq 'pinyin chinese-default-input-method)
     :init
     (progn
-      (setq pyim-use-tooltip t)
-      (setq pyim-dicts-directory spacemacs-cache-directory)
-      (setq pyim-personal-file (concat spacemacs-cache-directory "pyim-personal.txt"))
-      (setq default-input-method "chinese-pyim")
-      (evilify pyim-dicts-manager-mode pyim-dicts-manager-mode-map)
-      )))
-
+      (setq pyim-use-tooltip t
+            pyim-dicts-directory spacemacs-cache-directory
+            pyim-personal-file (concat spacemacs-cache-directory
+                                       "pyim-personal.txt")
+            default-input-method "chinese-pyim")
+      (evilify pyim-dicts-manager-mode pyim-dicts-manager-mode-map))))
 
 (defun chinese/init-find-by-pinyin-dired ()
   (use-package find-by-pinyin-dired
-    :defer t
-    ))
+    :defer t))
 
 (defun chinese/init-ace-pinyin ()
   (use-package ace-pinyin
