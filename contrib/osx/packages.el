d93da90ad7e1f4855e80b2d7151d5698dee3ca32
Uri Sharf <uri.sharf@gmail.com>
2015-07-06 14:17:24 +0200

    Add support for launchctld on OS X

--- contrib/osx/packages.el
+++ contrib/osx/packages.el
@@ -1,6 +1,7 @@
 (setq osx-packages
   '(
     pbcopy
+    launchctl
     reveal-in-finder
     ))
 
@@ -35,6 +36,38 @@ Can be installed with `brew install trash'."
   (use-package pbcopy
     :if (and (system-is-mac)(not (display-graphic-p))) 
     :init (turn-on-pbcopy)))
+
+(defun osx/init-launchctl ()
+  (use-package launchctl
+    :if (system-is-mac)
+    :defer t
+    :init
+    (progn
+      (add-to-list 'auto-mode-alist '("\\.plist$" . nxml-mode))
+      (evil-leader/set-key "al" 'launchctl))
+    :config
+    (progn
+      (evilify launchctl-mode launchctl-mode-map
+               (kbd "q") 'quit-window
+               (kbd "s") 'tabulated-list-sort
+               (kbd "g") 'launchctl-refresh
+               (kbd "n") 'launchctl-new
+               (kbd "e") 'launchctl-edit
+               (kbd "v") 'launchctl-view
+               (kbd "l") 'launchctl-load
+               (kbd "u") 'launchctl-unload
+               (kbd "r") 'launchctl-reload
+               (kbd "S") 'launchctl-start
+               (kbd "K") 'launchctl-stop
+               (kbd "R") 'launchctl-restart
+               (kbd "D") 'launchctl-remove
+               (kbd "d") 'launchctl-disable
+               (kbd "E") 'launchctl-enable
+               (kbd "i") 'launchctl-info
+               (kbd "f") 'launchctl-filter
+               (kbd "=") 'launchctl-setenv
+               (kbd "#") 'launchctl-unsetenv
+               (kbd "h") 'launchctl-help))))
 
 (defun osx/init-reveal-in-finder ()
   (use-package reveal-in-finder
