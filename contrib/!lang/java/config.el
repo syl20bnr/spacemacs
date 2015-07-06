2cf6854628327bb97232c1b6d2ec6dc5ce1d778a
Martin Martinez Rivera <martinmr@mit.edu>
2015-07-06 13:49:47 -0400

    Using declare-prefix-for-mode instead.

--- contrib/!lang/java/config.el
+++ contrib/!lang/java/config.el
@@ -10,12 +10,19 @@
 
 ;; Define the buffer local company backend variable
 (spacemacs|defvar-company-backends java-mode)
-(spacemacs/declare-prefix "me" "errors")
-(spacemacs/declare-prefix "mf" "find")
-(spacemacs/declare-prefix "mg" "goto")
-(spacemacs/declare-prefix "mr" "refactor")
-(spacemacs/declare-prefix "mh" "documentation")
-(spacemacs/declare-prefix "mm" "maven")
-(spacemacs/declare-prefix "ma" "ant")
-(spacemacs/declare-prefix "mp" "project")
-(spacemacs/declare-prefix "mt" "test")
+
+;; Command prefixes
+(setq java/key-binding-prefixes '(("me" . "errors")
+                                  ("mf" . "find")
+                                  ("mg" . "goto")
+                                  ("mr" . "refactor")
+                                  ("mh" . "documentation")
+                                  ("mm" . "maven")
+                                  ("ma" . "ant")
+                                  ("mp" . "project")
+                                  ("mt" . "test")))
+
+(mapc (lambda(x) (spacemacs/declare-prefix-for-mode 'java-mode (car x) (cdr x)))
+      java/key-binding-prefixes)
+
+
