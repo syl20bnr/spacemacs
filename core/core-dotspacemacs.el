6b9ded451370bb06a22e63d7356d2b4cb66e3b72
syl20bnr <sylvain.benner@gmail.com>
2015-07-09 01:01:20 -0400

    core: remove monokai and zenburn from dotfile template
    They are not part of spacemacs core since several releases so
    they should not be treated differently than other themes.

--- core/core-dotspacemacs.el
+++ core/core-dotspacemacs.el
@@ -49,9 +49,7 @@ of a list then all discovered layers will be installed.")
                               spacemacs-light
                               solarized-dark
                               solarized-light
-                              leuven
-                              monokai
-                              zenburn)
+                              leuven)
   "List of themes, the first of the list is loaded when spacemacs starts.
 Press <SPC> T n to cycle to the next theme in the list (works great
 with 2 themes variants, one dark and one light")
