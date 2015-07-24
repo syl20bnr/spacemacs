6bc203c1c24b9120a24164fe849c5e587248e417
syl20bnr <sylvain.benner@gmail.com>
2015-07-23 21:38:52 -0400

    Convert wakatime readme to org

--- contrib/!tools/wakatime/packages.el
+++ contrib/!tools/wakatime/packages.el
@@ -1,9 +1,6 @@
-(setq wakatime-packages
-  '(
-    wakatime-mode
-    ))
+(setq wakatime-packages '(wakatime-mode))
 
 (defun wakatime/init-wakatime-mode ()
   (use-package wakatime-mode
-    :init
-    (add-hook 'prog-mode-hook 'wakatime-mode)))
+    :defer t
+    :init (add-hook 'prog-mode-hook 'wakatime-mode)))
