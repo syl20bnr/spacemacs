bee3eafbe0a86c071c38ef46aa78c5da178a4ec2
syl20bnr <sylvain.benner@gmail.com>
2015-07-26 01:28:20 -0400

    Delete deprecated !usr contrib directory

--- contrib/!usr/syl20bnr/packages.el
+++
@@ -1,2 +0,0 @@
-(setq syl20bnr-packages '())
-
--- contrib/!usr/trishume/config.el
+++
@@ -1,8 +0,0 @@
-(dolist (mode '(c++
-                json
-                racket
-                elisp
-                LaTeX
-                yaml))
-  (add-hook (intern (concat (symbol-name mode) "-mode-hook"))
-            'flycheck-mode))
