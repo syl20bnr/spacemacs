96483c7f7de5236e114401b2e451cc2f9562c808
Andrea Moretti <axyzxp@gmail.com>
2015-07-29 00:05:16 +0200

    Convert to a mayor mode

---
+++ contrib/!frameworks/react/config.el
@@ -0,0 +1,15 @@
+;;; packages.el --- react Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2015 Sylvain Benner
+;; Copyright (c) 2014-2015 Andrea Moretti & Contributors
+;;
+;; Author: Andrea Moretti <axyzxp@gmail.com>
+;; URL: https://github.com/axyz
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+
+(spacemacs|defvar-company-backends react-mode)
