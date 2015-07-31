061c6dd13a67287c7024c5e64bb45d7a466d6937
syl20bnr <sylvain.benner@gmail.com>
2015-07-30 21:33:28 -0400

    core: fix tests

--- core/tests/core-spacemacs-buffer-ftest.el
+++ core/tests/core-spacemacs-buffer-ftest.el
@@ -21,39 +21,44 @@
 
 (ert-deftest test-render-framed-text--msg-width-caption-and-padding ()
   (should (equal (spacemacs//render-framed-text test-text 32 "Caption" 4)
-                 "╭─Caption────────────────────────────╮
-│                                    │
-│    Lorem ipsum  dolor sit amet,    │
-│    consectetur adipiscing elit,    │
-│    sed   do    eiusmod   tempor    │
-│    incididunt   ut  labore   et    │
-│    dolore magna aliqua.            │
-│                                    │
-╰────────────────────────────────────╯")))
+                 "╭─Caption──────────────────────╮
+│                              │
+│    Lorem ipsum  dolor sit    │
+│    amet,      consectetur    │
+│    adipiscing  elit,  sed    │
+│    do    eiusmod   tempor    │
+│    incididunt  ut  labore    │
+│    et     dolore    magna    │
+│    aliqua.                   │
+│                              │
+╰──────────────────────────────╯"
+                 )))
 
 (ert-deftest test-render-framed-text--msg-width-caption-no-padding ()
   (should (equal (spacemacs//render-framed-text test-text 32 "Caption")
-                 "╭─Caption─────────────────────────╮
-│                                 │
-│ Lorem  ipsum  dolor  sit  amet, │
-│ consectetur   adipiscing  elit, │
-│ sed    do     eiusmod    tempor │
-│ incididunt ut  labore et dolore │
-│ magna aliqua.                   │
-│                                 │
-╰─────────────────────────────────╯")))
+                 "╭─Caption──────────────────────╮
+│                              │
+│ Lorem ipsum  dolor sit amet, │
+│ consectetur adipiscing elit, │
+│ sed   do    eiusmod   tempor │
+│ incididunt   ut  labore   et │
+│ dolore magna aliqua.         │
+│                              │
+╰──────────────────────────────╯"
+                 )))
 
 (ert-deftest test-render-framed-text--msg-width-no-caption-no-padding ()
   (should (equal (spacemacs//render-framed-text test-text 32)
-                 "╭─────────────────────────────────╮
-│                                 │
-│ Lorem  ipsum  dolor  sit  amet, │
-│ consectetur   adipiscing  elit, │
-│ sed    do     eiusmod    tempor │
-│ incididunt ut  labore et dolore │
-│ magna aliqua.                   │
-│                                 │
-╰─────────────────────────────────╯")))
+                 "╭──────────────────────────────╮
+│                              │
+│ Lorem ipsum  dolor sit amet, │
+│ consectetur adipiscing elit, │
+│ sed   do    eiusmod   tempor │
+│ incididunt   ut  labore   et │
+│ dolore magna aliqua.         │
+│                              │
+╰──────────────────────────────╯"
+                 )))
 
 (ert-deftest test-render-framed-text--msg-no-width-no-caption-no-padding ()
   (should (equal (spacemacs//render-framed-text test-text)
@@ -62,7 +67,8 @@
 │ Lorem ipsum dolor  sit amet, consectetur adipiscing elit, sed  do eiusmod tempor │
 │ incididunt ut labore et dolore magna aliqua.                                     │
 │                                                                                  │
-╰──────────────────────────────────────────────────────────────────────────────────╯")))
+╰──────────────────────────────────────────────────────────────────────────────────╯"
+                 )))
 
 (ert-deftest test-render-framed-text--msg-short-text ()
   (should (equal (spacemacs//render-framed-text "Short content.")
@@ -70,7 +76,8 @@
 │                                                                                  │
 │ Short content.                                                                   │
 │                                                                                  │
-╰──────────────────────────────────────────────────────────────────────────────────╯")))
+╰──────────────────────────────────────────────────────────────────────────────────╯"
+                 )))
 
 (ert-deftest test-render-framed-text--msg-several-paragraphs ()
   (should (equal (spacemacs//render-framed-text
@@ -92,22 +99,24 @@
 │ incididunt ut labore et dolore magna aliqua.                                     │
 │                                                                                  │
 │                                                                                  │
-╰──────────────────────────────────────────────────────────────────────────────────╯")))
-
+╰──────────────────────────────────────────────────────────────────────────────────╯"
+                 )))
 
 (ert-deftest test-render-framed-text--file-caption-and-padding ()
   (should (equal (spacemacs//render-framed-text
                   (concat spacemacs-core-directory "tests/data/framed-text.txt")
                   62 "Caption" 4)
-                 "╭─Caption──────────────────────────────────────────────────────────╮
-│                                                                  │
-│    Lorem ipsum  dolor sit amet, consectetur  adipiscing elit,    │
-│    sed do eiusmod tempor incididunt ut labore et dolore magna    │
-│    aliqua. Ut enim ad minim veniam, quis nostrud exercitation    │
-│    ullamco laboris  nisi ut aliquip ex  ea commodo consequat.    │
-│    Duis aute irure dolor  in reprehenderit in voluptate velit    │
-│    esse  cillum dolore  eu fugiat  nulla pariatur.  Excepteur    │
-│    sint occaecat  cupidatat non  proident, sunt in  culpa qui    │
-│    officia deserunt mollit anim id est laborum.                  │
-│                                                                  │
-╰──────────────────────────────────────────────────────────────────╯")))
+                 "╭─Caption────────────────────────────────────────────────────╮
+│                                                            │
+│    Lorem ipsum  dolor sit amet,  consectetur adipiscing    │
+│    elit, sed do eiusmod  tempor incididunt ut labore et    │
+│    dolore magna  aliqua. Ut enim ad  minim veniam, quis    │
+│    nostrud exercitation ullamco laboris nisi ut aliquip    │
+│    ex ea  commodo consequat.  Duis aute irure  dolor in    │
+│    reprehenderit in voluptate  velit esse cillum dolore    │
+│    eu  fugiat nulla  pariatur. Excepteur  sint occaecat    │
+│    cupidatat non  proident, sunt  in culpa  qui officia    │
+│    deserunt mollit anim id est laborum.                    │
+│                                                            │
+╰────────────────────────────────────────────────────────────╯"
+                 )))
