706115fc0e02c8c2d7c052bce2b17a927ef9f5a7
Xiang Ji <hi@xiangji.me>
2015-08-01 22:58:51 +0800

    Fixed various naming errors in package file descriptions
    Well, probably mostly aesthetic, but still they just look a bit jarring out there.

--- contrib/!irc/erc/config.el
+++ contrib/!irc/erc/config.el
@@ -1,4 +1,4 @@
-;;; config.el --- rcirc Layer configuration File for Spacemacs
+;;; config.el --- erc Layer configuration File for Spacemacs
 ;;
 ;; Copyright (c) 2012-2014 Sylvain Benner
 ;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
