774275495e8c9a7e343072fe71255a0833ffe19e
Robert O'Connor <rob@oconnor.ninja>
2015-08-05 04:32:36 -0400

    Use melpa version of magit-gh-pulls

--- contrib/!source-control/github/packages.el
+++ contrib/!source-control/github/packages.el
@@ -15,6 +15,7 @@
         gist
         git-link
         github-browse-file
+        magit-gh-pulls
         ;; not up to date
         ;; helm-gist
         ))
@@ -86,3 +87,25 @@
         "gfC" 'spacemacs/git-link-commit-copy-url-only)
       ;; default is to open the generated link
       (setq git-link-open-in-browser t))))
+
+(defun github/init-magit-gh-pulls ()
+  (use-package magit-gh-pulls
+    :defer t
+    :init
+    (progn
+      (defun spacemacs/load-gh-pulls-mode ()
+        "Start `magit-gh-pulls-mode' only after a manual request."
+        (interactive)
+        (magit-gh-pulls-mode)
+        (magit-gh-pulls-reload))
+      (defun spacemacs/fetch-gh-pulls-mode ()
+        "Start `magit-gh-pulls-mode' only after a manual request."
+        (interactive)
+        (magit-gh-pulls-mode)
+        (magit-gh-pulls-fetch-commits))
+      (eval-after-load 'magit
+        '(progn
+           (define-key magit-mode-map "#gg" 'spacemacs/load-gh-pulls-mode)
+           (define-key magit-mode-map "#gf" 'spacemacs/fetch-gh-pulls-mode))))
+    :config
+    (spacemacs|diminish magit-gh-pulls-mode "Github-PR")))
