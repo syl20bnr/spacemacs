f9b8bf02fccd9dc140131f5253ce146ff60719f1
syl20bnr <sylvain.benner@gmail.com>
2015-08-09 15:17:56 -0400

    spacemacs layer: chocolate-coated cursor in replace state

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -710,6 +710,10 @@
         (let ((c (when dotspacemacs-colorize-cursor-according-to-state
                    (spacemacs/state-color 'iedit-insert))))
           (setq evil-iedit-insert-state-cursor `(,c (bar . 2)))))
+      (defun set-default-evil-replace-state-cursor ()
+        (let ((c (when dotspacemacs-colorize-cursor-according-to-state
+                   (spacemacs/state-color 'replace))))
+          (setq evil-replace-state-cursor `(,c (hbar . 2)))))
       (defun evil-insert-state-cursor-hide ()
         (setq evil-insert-state-cursor '((hbar . 0))))
       (set-default-evil-emacs-state-cursor)
@@ -721,6 +725,7 @@
       (set-default-evil-lisp-state-cursor)
       (set-default-evil-iedit-state-cursor)
       (set-default-evil-iedit-insert-state-cursor)
+      (set-default-evil-replace-state-cursor)
 
       (evil-mode 1))
     :config
