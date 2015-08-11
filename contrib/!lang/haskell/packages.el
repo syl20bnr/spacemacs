2d83e92307abdf3374e062546b9a5ec3621582dc
Boris <d12frosted@icloud.com>
2015-08-11 09:54:11 +0300

    fix typo

--- contrib/!lang/haskell/packages.el
+++ contrib/!lang/haskell/packages.el
@@ -61,7 +61,7 @@
             (electric-indent-local-mode -1))
         (when haskell-enable-shm-support
           ;; in structured-haskell-mode line highlighting creates noise
-          (setq global-hl-line-mode nil))
+          (setq global-hl-line-mode nil)))
 
       ;; hooks
       (add-hook 'haskell-mode-hook 'spacemacs/init-haskell-mode)
