be4e4fb7059688cf4c60696c380981fbff9a49a9
Boris <d12frosted@icloud.com>
2015-08-18 22:49:22 +0300

    add csharp-mode specific prefix commands

--- contrib/!lang/csharp/packages.el
+++ contrib/!lang/csharp/packages.el
@@ -29,44 +29,53 @@
         ;; set back this variable to t.
         (setq omnisharp-auto-complete-want-documentation nil))
       (push 'company-omnisharp company-backends-csharp-mode))
-    :config (evil-leader/set-key-for-mode 'csharp-mode
-              ;; Compile
-              "mcc" 'omnisharp-build-in-emacs ;; Only one compile command so use top-level
-              ;; Solution/project manipulation
-              "mfa" 'omnisharp-add-to-solution-current-file
-              "mfA" 'omnisharp-add-to-solution-dired-selected-files
-              "mfr" 'omnisharp-remove-from-project-current-file
-              "mfR" 'omnisharp-remove-from-project-dired-selected-files
-              "mpl" 'omnisharp-add-reference
-              ;; Navigation
-              "mgg"   'omnisharp-go-to-definition
-              "mgG"   'omnisharp-go-to-definition-other-window
-              "mgu"   'omnisharp-helm-find-usages
-              "mgs"   'omnisharp-helm-find-symbols
-              "mgi"   'omnisharp-find-implementations
-              "mgr"   'omnisharp-navigate-to-region
-              "mgm"   'omnisharp-navigate-to-solution-member
-              "mgM"   'omnisharp-navigate-to-solution-member-other-window
-              "mgf"   'omnisharp-navigate-to-solution-file
-              "mgF"   'omnisharp-navigate-to-solution-file-then-file-member
-              ;; Help, documentation, info
-              "mht" 'omnisharp-current-type-information
-              "mhT" 'omnisharp-current-type-information-to-kill-ring
-              ;; Refactoring
-              "mrm" 'omnisharp-rename
-              "mrr" 'omnisharp-run-code-action-refactoring
-              ;; Server manipulation, inspired spacemacs REPL bindings since C# does not provice a REPL
-              "mss" 'omnisharp-start-omnisharp-server
-              "msS" 'omnisharp-stop-server
-              "msr" 'omnisharp-reload-solution
-              ;; Tests
-              "mta" 'omnisharp-unit-test-all
-              "mtb" 'omnisharp-unit-test-fixture
-              "mtt" 'omnisharp-unit-test-single
-              ;; Code manipulation
-              "mu" 'omnisharp-auto-complete-overrides
-              "mi" 'omnisharp-fix-usings
-              "m=" 'omnisharp-code-format)))
+    :config
+    (progn
+      (spacemacs/declare-prefix-for-mode 'csharp-mode "mc" "compile")
+      (spacemacs/declare-prefix-for-mode 'csharp-mode "mf" "file")
+      (spacemacs/declare-prefix-for-mode 'csharp-mode "mg" "navigation")
+      (spacemacs/declare-prefix-for-mode 'csharp-mode "mh" "documentation")
+      (spacemacs/declare-prefix-for-mode 'csharp-mode "mr" "refactoring")
+      (spacemacs/declare-prefix-for-mode 'csharp-mode "ms" "server")
+      (spacemacs/declare-prefix-for-mode 'csharp-mode "mt" "tests")
+      (evil-leader/set-key-for-mode 'csharp-mode
+        ;; Compile
+        "mcc" 'omnisharp-build-in-emacs ;; Only one compile command so use top-level
+        ;; Solution/project manipulation
+        "mfa" 'omnisharp-add-to-solution-current-file
+        "mfA" 'omnisharp-add-to-solution-dired-selected-files
+        "mfr" 'omnisharp-remove-from-project-current-file
+        "mfR" 'omnisharp-remove-from-project-dired-selected-files
+        "mpl" 'omnisharp-add-reference
+        ;; Navigation
+        "mgg"   'omnisharp-go-to-definition
+        "mgG"   'omnisharp-go-to-definition-other-window
+        "mgu"   'omnisharp-helm-find-usages
+        "mgs"   'omnisharp-helm-find-symbols
+        "mgi"   'omnisharp-find-implementations
+        "mgr"   'omnisharp-navigate-to-region
+        "mgm"   'omnisharp-navigate-to-solution-member
+        "mgM"   'omnisharp-navigate-to-solution-member-other-window
+        "mgf"   'omnisharp-navigate-to-solution-file
+        "mgF"   'omnisharp-navigate-to-solution-file-then-file-member
+        ;; Help, documentation, info
+        "mht" 'omnisharp-current-type-information
+        "mhT" 'omnisharp-current-type-information-to-kill-ring
+        ;; Refactoring
+        "mrm" 'omnisharp-rename
+        "mrr" 'omnisharp-run-code-action-refactoring
+        ;; Server manipulation, inspired spacemacs REPL bindings since C# does not provice a REPL
+        "mss" 'omnisharp-start-omnisharp-server
+        "msS" 'omnisharp-stop-server
+        "msr" 'omnisharp-reload-solution
+        ;; Tests
+        "mta" 'omnisharp-unit-test-all
+        "mtb" 'omnisharp-unit-test-fixture
+        "mtt" 'omnisharp-unit-test-single
+        ;; Code manipulation
+        "mu" 'omnisharp-auto-complete-overrides
+        "mi" 'omnisharp-fix-usings
+        "m=" 'omnisharp-code-format))))
 
 (when (configuration-layer/layer-usedp 'auto-completion)
   (defun csharp/post-init-company ()
