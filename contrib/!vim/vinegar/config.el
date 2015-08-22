658784d181332cd3a41dbbbf3b66e7694b862d53
sooheon <tngjs0@gmail.com>
2015-08-23 02:18:37 +0900

    Clean up vinegar layer
    Add keybinding for dired-goto-file at `J`, previously shadowed by `j`.
    This command take you *to* a file or folder but does not enter it. Add
    dotspacemacs variable to determine dired buffer reuse behavior (rather
    than forcing only reuse). Swap `C-r` and `r` bindings, as revert-buffer
    is far more generally useful.

---
+++ contrib/!vim/vinegar/config.el
@@ -0,0 +1,16 @@
+;;; config.el --- Vinegar Layer Configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+
+(defvar vinegar-reuse-dired-buffer nil
+  "If non-nil, reuses one dired buffer for navigation.")
