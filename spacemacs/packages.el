5fc80917be117039b620ed5e4e79aa1f62b9b4bf
person808 <kainaluh808@gmail.com>
2015-08-28 17:43:47 -1000

    Make Y equivalent to y$.
    This is much more consistent with C and D and is even recommended by vim
    itself.

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -712,6 +712,13 @@
       ;; toggle maximize buffer
       (define-key evil-window-map (kbd "o") 'spacemacs/toggle-maximize-buffer)
       (define-key evil-window-map (kbd "C-o") 'spacemacs/toggle-maximize-buffer)
+      ;; Make Y equivalent to y$
+      (defun spacemacs/evil-yank-to-end-of-line ()
+        "Yank from point to end of line."
+        (interactive)
+        (evil-yank (point) (point-at-eol)))
+      (define-key evil-normal-state-map (kbd "Y") 'spacemacs/evil-yank-to-end-of-line)
+      (define-key evil-motion-state-map (kbd "Y") 'spacemacs/evil-yank-to-end-of-line)
 
       (evil-leader/set-key "re" 'evil-show-registers)
 
@@ -858,14 +865,14 @@ Example: (evil-map visual \"<\" \"<gv\")"
 
       ;; support smart 1parens-strict-mode
       (when (configuration-layer/package-usedp 'smartparens)
-          (defadvice evil-delete-backward-char-and-join
-              (around spacemacs/evil-delete-backward-char-and-join activate)
-            (defvar smartparens-strict-mode)
-            ;; defadvice compiles this sexp generating a compiler warning for a
-            ;; free variable reference. The line above fixes this
-            (if smartparens-strict-mode
-                (call-interactively 'sp-backward-delete-char)
-              ad-do-it))))))
+        (defadvice evil-delete-backward-char-and-join
+            (around spacemacs/evil-delete-backward-char-and-join activate)
+          (defvar smartparens-strict-mode)
+          ;; defadvice compiles this sexp generating a compiler warning for a
+          ;; free variable reference. The line above fixes this
+          (if smartparens-strict-mode
+              (call-interactively 'sp-backward-delete-char)
+            ad-do-it))))))
 
 (defun spacemacs/init-evil-anzu ()
   (use-package evil-anzu
