537979e904ae88a9e2c06b010e9449e094a1cd8c
Hamish Hutchings <hamish@freeagent.com>
2015-09-03 17:19:47 +0100

    add hooks for flycheck while stripping out flymake.
    As all other packages are written to depend on flycheck, I thought I would strip it out of this package to let us have a consistent interface in all layers.

--- contrib/!lang/typescript/packages.el
+++ contrib/!lang/typescript/packages.el
@@ -10,14 +10,55 @@
 ;;
 ;;; License: GPLv3
 
-(setq typescript-packages '(tss))
+(setq typescript-private-packages '(
+                                    tss
+                                    flycheck))
 
-(defun typescript/init-tss ()
-  "Initialize my package"
+(setq test-layer-excluded-packages '(flymake))
+
+(defun typescript-private/post-init-flycheck ()
+  "Add hook for flycheck"
+  (use-package flycheck
+    :defer t
+    :config (progn
+              ;; ;;; remove hook for flymake, as it seems to conflict.
+              ;; (add-hook 'typescript-mode-hook 'flycheck-mode (flymake-mode nil))
+              ;; (add-hook 'typescript-mode-hook (lambda flymake-mode nil))
+              ;;; This Code Snippet was from [Here](https://github.com/caisah/flycheck-typescript) and is licensed under the GPL
+              (flycheck-define-checker typescript
+                "A TypeScript syntax checker using tsc command."
+                :command ("tsc" "--out" "/dev/null" source-inplace)
+                :error-patterns
+                ((error line-start (file-name) "(" line "," column "): error " (message) line-end))
+                :modes (typescript-mode))
+
+              (add-to-list 'flycheck-checkers 'typescript)
+              )
+    )
+  )
+
+
+(defun typescript-private/init-tss ()
+  "Initialize tss"
   (use-package tss
     :defer t
-    :mode ("\\.ts\\'" . typescript-mode)
-    :init (evil-leader/set-key-for-mode 'typescript-mode
-            "mgg" 'tss-jump-to-definition
-            "mhh" 'tss-popup-help)
-    :config (tss-config-default)))
+    :mode (
+           ("\\.ts\\'" . typescript-mode)
+           ))
+  :init (evil-leader/set-key-for-mode 'flycheck-mode
+          "mgg" 'tss-jump-to-definition
+          "mhh" 'tss-popup-help)
+
+  )
+
+(defun typescript-private/post-init-tss ()
+  "Initialize tss"
+  (use-package tss
+    :defer t
+    :config (progn
+              ;; Remove Flymake mode from hook.
+              (advice-add 'tss-setup-current-buffer :before (lambda flymake-mode nil))
+
+              (add-hook 'typescript-mode-hook 'tss-setup-current-buffer t)
+              (add-hook 'kill-buffer-hook 'tss--delete-process t)
+              )))
