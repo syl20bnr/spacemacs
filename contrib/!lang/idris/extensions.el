81c3de0a0d2a2507fdc806ab334fd6c4fbb422a1
Rodolfo Hansen <rhansen@kitsd.com>
2015-09-05 20:35:47 -0400

    Add an idris language layer

---
+++ contrib/!lang/idris/extensions.el
@@ -0,0 +1,33 @@
+;;; extensions.el --- idris Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar idris-pre-extensions
+  '(
+    ;; pre extension idriss go here
+    )
+  "List of all extensions to load before the packages.")
+
+(defvar idris-post-extensions
+  '(
+    ;; post extension idriss go here
+    )
+  "List of all extensions to load after the packages.")
+
+;; For each extension, define a function idris/init-<extension-idris>
+;;
+;; (defun idris/init-my-extension ()
+;;   "Initialize my extension"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
