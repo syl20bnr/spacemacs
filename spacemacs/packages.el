7313fdfe5522aa21c067858c496eb965e01cf211
syl20bnr <sylvain.benner@gmail.com>
2015-09-07 00:23:51 -0400

    spacemacs: move evilnc functions to evilnc init func
    as well as the key bindings
    also import "gc" and "gy" bindings from evil-commentary

--- spacemacs/packages.el
+++ spacemacs/packages.el
@@ -1028,12 +1028,65 @@ Example: (evil-map visual \"<\" \"<gv\")"
 ;; other commenting functions in funcs.el with keybinds in keybindings.el
 (defun spacemacs/init-evil-nerd-commenter ()
   (use-package evil-nerd-commenter
-    :commands (evilnc-comment-operator)
+    :commands evilnc-comment-operator
     :init
     (progn
-      (evil-leader/set-key
-        ";"  'evilnc-comment-operator))))
+      ;; double all the commenting functions so that the inverse operations
+      ;; can be called without setting a flag
+      (defun spacemacs/comment-or-uncomment-lines-inverse (&optional arg)
+        (interactive "p")
+        (let ((evilnc-invert-comment-line-by-line t))
+          (evilnc-comment-or-uncomment-lines arg)))
 
+      (defun spacemacs/comment-or-uncomment-lines (&optional arg)
+        (interactive "p")
+        (let ((evilnc-invert-comment-line-by-line nil))
+          (evilnc-comment-or-uncomment-lines arg)))
+
+      (defun spacemacs/copy-and-comment-lines-inverse (&optional arg)
+        (interactive "p")
+        (let ((evilnc-invert-comment-line-by-line t))
+          (evilnc-copy-and-comment-lines arg)))
+
+      (defun spacemacs/copy-and-comment-lines (&optional arg)
+        (interactive "p")
+        (let ((evilnc-invert-comment-line-by-line nil))
+          (evilnc-copy-and-comment-lines arg)))
+
+      (defun spacemacs/quick-comment-or-uncomment-to-the-line-inverse
+          (&optional arg)
+        (interactive "p")
+        (let ((evilnc-invert-comment-line-by-line t))
+          (evilnc-comment-or-uncomment-to-the-line arg)))
+
+      (defun spacemacs/quick-comment-or-uncomment-to-the-line (&optional arg)
+        (interactive "p")
+        (let ((evilnc-invert-comment-line-by-line nil))
+          (evilnc-comment-or-uncomment-to-the-line arg)))
+
+      (defun spacemacs/comment-or-uncomment-paragraphs-inverse (&optional arg)
+        (interactive "p")
+        (let ((evilnc-invert-comment-line-by-line t))
+          (evilnc-comment-or-uncomment-paragraphs arg)))
+
+      (defun spacemacs/comment-or-uncomment-paragraphs (&optional arg)
+        (interactive "p")
+        (let ((evilnc-invert-comment-line-by-line nil))
+          (evilnc-comment-or-uncomment-paragraphs arg)))
+
+      (define-key evil-normal-state-map "gc" 'evilnc-comment-operator)
+      (define-key evil-normal-state-map "gy" 'spacemacs/copy-and-comment-lines)
+
+      (evil-leader/set-key
+        ";"  'evilnc-comment-operator
+        "cl" 'spacemacs/comment-or-uncomment-lines
+        "cL" 'spacemacs/comment-or-uncomment-lines-inverse
+        "cp" 'spacemacs/comment-or-uncomment-paragraphs
+        "cP" 'spacemacs/comment-or-uncomment-paragraphs-inverse
+        "ct" 'spacemacs/quick-comment-or-uncomment-to-the-line
+        "cT" 'spacemacs/quick-comment-or-uncomment-to-the-line-inverse
+        "cy" 'spacemacs/copy-and-comment-lines
+        "cY" 'spacemacs/copy-and-comment-lines-inverse))))
 
 (defun spacemacs/init-evil-matchit ()
   (use-package evil-matchit
