b8f3d395fd2ac6b969d4b19cc47d37663056803a
Boris <d12frosted@icloud.com>
2015-09-08 15:16:23 +0300

    Update doc string for haskell-enable-hindent-style
    Info was updated in README file, but not in doc string. Following #1786.

--- layers/!lang/haskell/config.el
+++ layers/!lang/haskell/config.el
@@ -21,7 +21,7 @@
   "If non-nil structured-haskell-mode support is enabled")
 
 (defvar haskell-enable-hindent-style nil
-  "Style to use for formatting with hindent; available are: fundamental johan-tibell chris-done andrew-gibiansky. If nil hindent is disabled.")
+  "Style to use for formatting with hindent; available are: fundamental johan-tibell chris-done gibiansky. If nil hindent is disabled.")
 
 (defvar haskell-enable-ghc-mod-support t
   "If non-nil ghc-mod support is enabled")
