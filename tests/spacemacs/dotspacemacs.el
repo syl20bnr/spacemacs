666c202d2839b1cacec1c364730794b1be3c8a36
Eivind Fonn <evfonn@gmail.com>
2015-09-12 13:16:57 +0200

    Fix tests

--- tests/spacemacs/dotspacemacs.el
+++ tests/spacemacs/dotspacemacs.el
@@ -1,3 +1,7 @@
-(defun dotspacemacs/layers ())
+(defun dotspacemacs/layers ()
+  (setq-default
+   dotspacemacs-distribution 'spacemacs))
 (defun dotspacemacs/init ())
+(defun dotspacemacs/user-init ())
 (defun dotspacemacs/config ())
+(defun dotspacemacs/user-config ())
