a21e721eba99bb8693097287c7edc9c28d41e630
justbur <justin@burkett.cc>
2015-09-24 09:36:54 -0400

    core: Add with-eval-after-load backport

--- core/core-emacs-backports.el
+++ core/core-emacs-backports.el
@@ -36,4 +36,12 @@
       "Check whether STRING is empty."
       (string= string "")))
 
+(unless (fboundp 'with-eval-after-load)
+  (defmacro with-eval-after-load (file &rest body)
+    "Execute BODY after FILE is loaded.
+FILE is normally a feature name, but it can also be a file name,
+in case that file does not provide any feature."
+    (declare (indent 1) (debug t))
+    `(eval-after-load ,file (lambda () ,@body))))
+
 (provide 'core-emacs-backports)
