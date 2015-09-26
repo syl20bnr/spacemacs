23658b53ffade79a448ef4c8f3e613443274745b
syl20bnr <sylvain.benner@gmail.com>
2015-09-25 21:29:48 -0400

    evilify: new pre-command-hook to dispatch commands
    Use a pre-command-hook to introspect current keymap under point and
    dispatch the command to evilified-state keymap or mode keymap.

--- core/core-evilified-state.el
+++ core/core-evilified-state.el
@@ -52,14 +52,29 @@
   :cursor box)
 
 (add-hook 'evil-evilified-state-entry-hook 'spacemacs//evilified-state-on-entry)
+(add-hook 'evil-evilified-state-exit-hook 'spacemacs//evilified-state-on-exit)
+
+(defun spacemacs//evilify-pre-command-hook ()
+  (let ((map (get-char-property (point) 'keymap)))
+    (when (and map (assq 'evilified-state map))
+      (let* ((submap (cdr (assq 'evilified-state map)))
+             (command (when (and submap (eq 1 (length (this-command-keys))))
+                        (lookup-key submap (this-command-keys)))))
+        (when command
+          (setq this-command command))))))
 
 (defun spacemacs//evilified-state-on-entry ()
   "Setup evilified state."
+  (add-hook 'pre-command-hook 'spacemacs//evilify-pre-command-hook nil 'local)
   (when (bound-and-true-p evil-surround-mode)
     (make-local-variable 'evil-surround-mode)
     (evil-surround-mode -1))
   (setq-local evil-normal-state-map (cons 'keymap nil))
   (setq-local evil-visual-state-map (cons 'keymap (list (cons ?y 'evil-yank)))))
+
+(defun spacemacs//evilified-state-on-exit ()
+  "Clean evilified-state"
+  (remove-hook 'pre-command-hook 'spacemacs//evilify-pre-command-hook 'local))
 
 ;; default key bindings for all evilified buffers
 (define-key evil-evilified-state-map (kbd dotspacemacs-leader-key)
