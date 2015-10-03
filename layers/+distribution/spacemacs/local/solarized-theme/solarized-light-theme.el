3eddeeeb5461bf58489f8add0c707a8591011ff8
Eivind Fonn <evfonn@gmail.com>
2015-10-03 10:24:42 +0200

    core: remove special handling of spacemacs-theme and solarized theme
    Also remove the local solarized-theme package

--- layers/+distribution/spacemacs/local/solarized-theme/solarized-light-theme.el
+++
@@ -1,7 +0,0 @@
-(require 'solarized)
-
-(deftheme solarized-light "The light variant of the Solarized colour theme")
-
-(create-solarized-theme 'light 'solarized-light)
-
-(provide-theme 'solarized-light)
