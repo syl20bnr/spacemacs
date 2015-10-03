f84d3594f7aeb02a1817efe2be7a2fa7f0bd6e3f
Eivind Fonn <evfonn@gmail.com>
2015-10-03 10:24:42 +0200

    spacemacs: remove solarized

--- layers/+distribution/spacemacs/local/solarized-theme/solarized-dark-theme.el
+++
@@ -1,7 +0,0 @@
-(require 'solarized)
-
-(deftheme solarized-dark "The dark variant of the Solarized colour theme")
-
-(create-solarized-theme 'dark 'solarized-dark)
-
-(provide-theme 'solarized-dark)
