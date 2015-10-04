7dcb77d80afc07d058e4ab4e508bc93a95e7549a
Robert O'Connor <rob@oconnor.ninja>
2015-10-04 07:16:42 -0400

    Add Grails Layer

---
+++ layers/+frameworks/grails/packages.el
@@ -0,0 +1,31 @@
+;;; packages.el --- grails Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; List of all packages to install and/or initialize. Built-in packages
+;; which require an initialization must be listed explicitly in the list.
+(setq grails-packages
+    '(
+      ;; package names go here
+      ))
+
+;; List of packages to exclude.
+(setq grails-excluded-packages '())
+
+;; For each package, define a function grails/init-<package-name>
+;;
+;; (defun grails/init-my-package ()
+;;   "Initialize my package"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
