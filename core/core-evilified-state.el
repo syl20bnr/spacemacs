syl20bnr/spacemacs

commit df6b530f0f5ca95fdcae4e0c5ec94ebb5978fa17

core: remove unneeded parenthesis

core/core-evilified-state.el
@@ -175,7 +175,7 @@ Each pair KEYn FUNCTIONn is defined in MAP after the evilification of it."
                (spacemacs/evilify-configure-default-state ',mode)))))
     (if (null eval-after-load)
         `(,@body)
-      `(with-eval-after-load ',eval-after-load (,@body)))))
+      `(with-eval-after-load ',eval-after-load ,@body))))
 
 (defun spacemacs/evilify-configure-default-state (mode)
   "Configure default state for the passed mode."

commit 3a2bd94e05d7c07d82197cc1095af2fc51b4be40

Fix bug introduced in df6b530

core/core-evilified-state.el
@@ -175,7 +175,7 @@ Each pair KEYn FUNCTIONn is defined in MAP after the evilification of it."
                (spacemacs/evilify-configure-default-state ',mode)))))
     (if (null eval-after-load)
         `(,@body)
-      `(with-eval-after-load ',eval-after-load ,@body))))
+      `(with-eval-after-load ',eval-after-load ,body))))
 
 (defun spacemacs/evilify-configure-default-state (mode)
   "Configure default state for the passed mode."