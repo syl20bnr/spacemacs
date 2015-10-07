0cbe01433d69ab9122ca13ba6c7643ae683c6df6
syl20bnr <sylvain.benner@gmail.com>
2015-10-07 15:05:45 -0400

    Move (setq help-window-select 't) to spacemacs-base

--- core/core-spacemacs.el
+++ core/core-spacemacs.el
@@ -103,8 +103,6 @@ initialization."
     (tool-bar-mode -1))
   (when (and (fboundp 'scroll-bar-mode) (not (eq scroll-bar-mode -1)))
     (scroll-bar-mode -1))
-  ;; Keep focus while navigating help buffers
-  (setq help-window-select 't)
   ;; tooltips in echo-aera
   (when (and (fboundp 'tooltip-mode) (not (eq tooltip-mode -1)))
     (tooltip-mode -1))
