6edfdb550e0f3bf67216b783c817f65aec8dc755
syl20bnr <sylvain.benner@gmail.com>
2015-10-10 23:19:23 -0400

    nim: move local packages to local folder and simplify config

--- layers/+lang/nim/packages.el
+++ layers/+lang/nim/packages.el
@@ -1,26 +1,29 @@
 (setq nim-packages
       '(company
+        company-nim
         flycheck
+        (flycheck-nim :location local)
         nim-mode))
+
+(defun nim/post-init-company ()
+  (spacemacs|add-company-hook nim-mode))
+
+(defun nim/post-init-flycheck ()
+  (spacemacs/add-flycheck-hook 'nim-mode))
+
+(defun nim/init-flycheck-nim ()
+  (use-package flycheck-nim
+    :if (configuration-layer/package-usedp 'flycheck)))
 
 (defun nim/init-nim-mode ()
   (use-package nim-mode
     :defer t
+    :init (when (configuration-layer/package-usedp 'company)
+            (push 'company-nim company-backends-nim-mode))
     :config
     (progn
       (defun spacemacs/nim-compile-run ()
         (interactive)
         (shell-command "nim compile --run main.nim"))
-
       (evil-leader/set-key-for-mode 'nim-mode
         "mcr" 'spacemacs/nim-compile-run))))
-
-(when (configuration-layer/layer-usedp 'auto-completion)
-  (defun nim/post-init-company ()
-    (spacemacs|add-company-hook nim-mode))
-
-  (defun nim/init-company-nim ()
-    (use-package company-nim
-      :defer t
-      :init
-      (push 'company-nim company-backends-nim-mode))))
