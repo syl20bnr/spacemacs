a3e2fdbe013c3e13eb73ef64b061c8167e32385e
Diego Berrocal <cestdiego@gmail.com>
2015-10-13 13:54:12 -0400

    Add Pacmacs to games layer

--- layers/+fun/games/packages.el
+++ layers/+fun/games/packages.el
@@ -10,7 +10,10 @@
 ;;
 ;;; License: GPLv3
 
-(defvar games-packages '(2048-game))
+(defvar games-packages '(
+                         2048-game
+                         pacmacs
+                         ))
 
 (defun games/init-2048-game ()
   (use-package 2048-mode
@@ -24,3 +27,15 @@
                "k" '2048-up
                "h" '2048-left
                "l" '2048-right))))
+
+(defun games/init-pacmacs ()
+  (use-package pacmacs
+    :defer t
+    :init
+    (push '("pacmacs" . (pacmacs-start :quit (kill-buffer-ask (get-buffer "*Pacmacs*"))
+                                :reset pacmacs-start)) helm-games-list)
+    (evilify pacmacs-mode pacmacs-mode-map
+             "h" 'pacmacs-left
+             "j" 'pacmacs-down
+             "k" 'pacmacs-up
+             "l" 'pacmacs-right)))
