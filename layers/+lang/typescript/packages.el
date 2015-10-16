3ae5c355a7c5af42217f2d03ec51e6dfd375520c
reed1 <reed.topcoder@gmail.com>
2015-10-16 07:22:05 +0700

    Update packages.el

--- layers/+lang/typescript/packages.el
+++ layers/+lang/typescript/packages.el
@@ -1,23 +1,32 @@
 ;;; packages.el --- typescript Layer packages File for Spacemacs
 ;;
 ;; Copyright (c) 2012-2014 Sylvain Benner
-;; Copyright (c) 2014-2015 Chris Bowdon & Contributors
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
 ;;
-;; Author: Chris Bowdon <c.bowdon@bath.edu>
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
 
-(setq typescript-packages '(tss))
+(setq typetide-packages '(tide))
 
-(defun typescript/init-tss ()
-  "Initialize my package"
-  (use-package tss
+(defun typetide/init-tide ()
+  "Initialize tide package"
+  (use-package tide
     :defer t
-    :mode ("\\.ts\\'" . typescript-mode)
-    :init (evil-leader/set-key-for-mode 'typescript-mode
-            "mgg" 'tss-jump-to-definition
-            "mhh" 'tss-popup-help)
-    :config (tss-config-default)))
+    :init
+    (progn
+      (add-hook 'typescript-mode-hook
+                (lambda ()
+                  (evil-leader/set-key-for-mode 'typescript-mode
+                    "mgg" 'tide-jump-to-definition
+                    "mhh" 'tide-documentation-at-point
+                    "mrr" 'tide-rename-symbol)
+                  (tide-setup)
+                  (flycheck-mode t)
+                  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+                  (eldoc-mode t)
+                  (company-mode-on))))
+    :config (message "Tide mode loaded")))
