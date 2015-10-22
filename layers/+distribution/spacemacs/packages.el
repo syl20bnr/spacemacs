4b3fb464c5ee191e7fdb4e46cdb41d8f5b97ff4e
Igor Avdeev <elephant@lislon.ru>
2015-10-22 19:44:28 +0300

    Make google translate language code case-insensitive

--- layers/+distribution/spacemacs/packages.el
+++ layers/+distribution/spacemacs/packages.el
@@ -934,15 +934,15 @@
     (progn
       (defun spacemacs/set-google-translate-languages (source target)
         "Set source language for google translate.
-For instance pass En as source for english."
+For instance pass En as source for English."
         (interactive
-         "sEnter source language (ie. En): \nsEnter target language (ie. En): "
+         "sEnter source language (ie. en): \nsEnter target language (ie. en): "
          source target)
         (message
          (format "Set google translate source language to %s and target to %s"
                  source target))
-        (setq google-translate-default-source-language source)
-        (setq google-translate-default-target-language target))
+        (setq google-translate-default-source-language (downcase source))
+        (setq google-translate-default-target-language (downcase target)))
       (evil-leader/set-key
         "xgQ" 'google-translate-query-translate-reverse
         "xgq" 'google-translate-query-translate
@@ -953,8 +953,8 @@ For instance pass En as source for english."
       (require 'google-translate-default-ui)
       (setq google-translate-enable-ido-completion t)
       (setq google-translate-show-phonetic t)
-      (setq google-translate-default-source-language "En")
-      (setq google-translate-default-target-language "Fr"))))
+      (setq google-translate-default-source-language "en")
+      (setq google-translate-default-target-language "fr"))))
 
 (defun spacemacs/init-helm-ag ()
   (use-package helm-ag
