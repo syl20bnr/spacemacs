1477a6ff2d2d0b1daa652ffec0fb791e041efd71
Christoph Paulik <cpaulik@gmail.com>
2015-10-24 09:11:03 +0200

    Don't use ido for sudo-edit of new files and bind key

--- layers/+distribution/spacemacs-base/funcs.el
+++ layers/+distribution/spacemacs-base/funcs.el
@@ -313,7 +313,7 @@ argument takes the kindows rotate backwards."
 (defun spacemacs/sudo-edit (&optional arg)
   (interactive "p")
   (if (or arg (not buffer-file-name))
-      (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
+      (find-file (concat "/sudo:root@localhost:" (read-file-name "File: ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
 ;; found at http://emacswiki.org/emacs/KillingBuffers
