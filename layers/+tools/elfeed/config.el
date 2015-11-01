df2de47c56aa34e19634be751f0130a317544057
d12frosted <d12frosted@icloud.com>
2015-11-01 12:17:42 +0200

    add elfeed layer

---
+++ layers/+tools/elfeed/config.el
@@ -0,0 +1,16 @@
+;;; config.el --- elfeed Layer extensions File for Spacemacs
+;;
+;; Copyright (c) 2012-2015 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Variables
+
+(defvar elfeed-web-enabled-on-emacs-startup nil
+  "If true, serve web interface Elfeed with simpl-httpd.")
