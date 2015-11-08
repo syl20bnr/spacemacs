eb04406fd9d577df13519e7d1cd0604e137dfb21
Muneeb Shaikh <muneeb@reversehack.in>
2015-11-08 11:48:36 +0530

    Fix helm-faces to select face under point

--- layers/+distribution/spacemacs-base/packages.el
+++ layers/+distribution/spacemacs-base/packages.el
@@ -627,10 +627,11 @@ Removes the automatic guessing of the initial value based on thing at point. "
         "Describe face."
         (interactive)
         (require 'helm-elisp)
-        (let ((default (thing-at-point 'symbol)))
-          (helm :sources (list (helm-def-source--emacs-faces default))
-                :buffer "*helm faces*"
-                :preselect (and default (concat "\\_<" (regexp-quote default) "\\_>")))))
+        (let ((default (face-at-point)))
+          (helm :sources (helm-def-source--emacs-faces (if default
+                                                           (symbol-name default)
+                                                         "default"))
+                :buffer "*helm faces*")))
 
       ;; use helm by default for M-x
       (unless (configuration-layer/package-usedp 'smex)
