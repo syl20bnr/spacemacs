a57a4059b44b26beda1c4078ccf96cd2b8908e6a
Eivind Fonn <evfonn@gmail.com>
2015-11-11 21:45:30 +0100

    Clean up mu4e layer
    - Clarify installation instructions
    - Bring README in line with conventions
    - Create layer variable for installation path
    - Move mu4e-account-alist to config.el
    - Move extensions.el to packages.el
    - Define mu4e as built-in and not local
    - Add commands to ensure mu4e is deferred

---
+++ layers/+email/mu4e/config.el
@@ -0,0 +1,20 @@
+;;; config.el --- mu4e Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar mu4e-installation-path nil
+  "Installation path for mu4e.")
+
+(defvar mu4e-account-alist nil
+  "Account alist for custom multi-account compose.")
+
+(when mu4e-installation-path
+  (push mu4e-installation-path load-path))
