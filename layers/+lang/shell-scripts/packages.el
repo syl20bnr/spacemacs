752367b63ee1a7936b770f76365e5b61ed40d936
Eivind Fonn <evfonn@gmail.com>
2015-11-15 12:49:38 +0100

    Refactor zsh shell-script code
    - Move to packages.el
    - Give setup function an explicit name
    - Guard for non-file buffers

--- layers/+lang/shell-scripts/packages.el
+++ layers/+lang/shell-scripts/packages.el
@@ -11,10 +11,31 @@
 ;;; License: GPLv3
 
 (setq shell-scripts-packages
-  '(
-    fish-mode
+  '(fish-mode
+    (sh-script :location built-in)
     ))
 
 (defun shell-scripts/init-fish-mode ()
   (use-package fish-mode
     :defer t))
+
+(defun shell-scripts/init-sh-script ()
+  (use-package sh-script
+    :defer t
+    :init
+    (progn
+      ;; Use sh-mode when opening `.zsh' files, and when opening Prezto runcoms.
+      (dolist (pattern '("\\.zsh\\'"
+                         "zlogin\\'"
+                         "zlogout\\'"
+                         "zpreztorc\\'"
+                         "zprofile\\'"
+                         "zshenv\\'"
+                         "zshrc\\'"))
+        (add-to-list 'auto-mode-alist (cons pattern 'sh-mode)))
+
+      (defun spacemacs//setup-shell ()
+        (when (and buffer-file-name
+                   (string-match "\\.zsh\\'" buffer-file-name))
+          (sh-set-shell "zsh")))
+      (add-hook 'sh-mode-hook 'spacemacs//setup-shell))))
