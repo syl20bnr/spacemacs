33d3a9ecdd0a831f09fe93aa795382688faa6df1
Color Fuzzy <color.fuzzy@gmail.com>
2015-11-18 13:14:07 +0800

    helm-dash use default docsets path on macos
    If we don't use the default docksets, helm-dash will not work smoothly
    unless we set the `dash-helm-dash-docset-path' variable when we import
    the dash layer(actually, the default dash docset path is always
    "~/Library/Application Support/Dash/DocSets").

--- layers/+tools/dash/funcs.el
+++ layers/+tools/dash/funcs.el
@@ -10,7 +10,8 @@
 
 (defun dash//activate-package-docsets (path)
   "Add dash docsets from specified PATH."
-  (setq helm-dash-docsets-path path
-        helm-dash-common-docsets (helm-dash-installed-docsets))
+  (if (not (string-blank-p path))
+      (setq helm-dash-docsets-path path))
+  (setq helm-dash-common-docsets (helm-dash-installed-docsets))
   (message (format "activated %d docsets from: %s"
                    (length helm-dash-common-docsets) path)))
