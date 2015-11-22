4937a83900cf803c6b54b83f3f3fedea32c8271c
Eivind Fonn <evfonn@gmail.com>
2015-11-22 19:48:32 +0100

    Fix evilify bug in vinegar layer

--- layers/+vim/vinegar/keybindings.el
+++ layers/+vim/vinegar/keybindings.el
@@ -2,28 +2,3 @@
 (define-key evil-normal-state-map (kbd "-") 'dired-jump)
 
 (add-hook 'dired-mode-hook 'vinegar/dired-setup)
-
-(with-eval-after-load 'dired
-  (evilified-state-evilify dired-mode dired-mode-map
-    "j"         'vinegar/move-down
-    "k"         'vinegar/move-up
-    "-"         'vinegar/up-directory
-    "0"         'dired-back-to-start-of-files
-    "="         'vinegar/dired-diff
-    (kbd "C-j") 'dired-next-subdir
-    (kbd "C-k") 'dired-prev-subdir
-    "I"         'vinegar/dotfiles-toggle
-    (kbd "~")   '(lambda ()(interactive) (find-alternate-file "~/"))
-    (kbd "RET") (if vinegar-reuse-dired-buffer
-                    'dired-find-alternate-file
-                  'dired-find-file)
-    "f"         'helm-find-files
-    "J"         'dired-goto-file
-    (kbd "C-f") 'find-name-dired
-    "H"         'diredp-dired-recent-dirs
-    "T"         'dired-tree-down
-    "K"         'dired-do-kill-lines
-    "r"         'revert-buffer
-    (kbd "C-r") 'dired-do-redisplay
-    "gg"        'vinegar/back-to-top
-    "G"         'vinegar/jump-to-bottom))
