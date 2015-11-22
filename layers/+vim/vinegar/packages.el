1cba7ee18dac6b99034ea6e4a46e8751ff4839e3
Eivind Fonn <evfonn@gmail.com>
2015-11-22 19:48:32 +0100

    Fix evilify bug in vinegar layer

--- layers/+vim/vinegar/packages.el
+++ layers/+vim/vinegar/packages.el
@@ -14,6 +14,7 @@
   '(
     diff-hl
     ;; dired+
+    (dired :location built-in)
     ))
 
 (setq vinegar-excluded-packages '())
@@ -38,3 +39,31 @@
     (progn
       (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
       )))
+
+(defun vinegar/init-dired ()
+  (use-package dired
+    :defer t
+    :config
+    (evilified-state-evilify dired-mode dired-mode-map
+      "j"         'vinegar/move-down
+      "k"         'vinegar/move-up
+      "-"         'vinegar/up-directory
+      "0"         'dired-back-to-start-of-files
+      "="         'vinegar/dired-diff
+      (kbd "C-j") 'dired-next-subdir
+      (kbd "C-k") 'dired-prev-subdir
+      "I"         'vinegar/dotfiles-toggle
+      (kbd "~")   '(lambda ()(interactive) (find-alternate-file "~/"))
+      (kbd "RET") (if vinegar-reuse-dired-buffer
+                      'dired-find-alternate-file
+                    'dired-find-file)
+      "f"         'helm-find-files
+      "J"         'dired-goto-file
+      (kbd "C-f") 'find-name-dired
+      "H"         'diredp-dired-recent-dirs
+      "T"         'dired-tree-down
+      "K"         'dired-do-kill-lines
+      "r"         'revert-buffer
+      (kbd "C-r") 'dired-do-redisplay
+      "gg"        'vinegar/back-to-top
+      "G"         'vinegar/jump-to-bottom)))
