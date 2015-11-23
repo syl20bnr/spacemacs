1c8dd6c47a36d4af98c9f561df2f30229718e6b6
Martin Yrjölä <martin.yrjola@gmail.com>
2015-11-23 08:54:59 +0200

    osx layer: Set sensible Emoji font

--- layers/osx/config.el
+++ layers/osx/config.el
@@ -1,3 +1,8 @@
 (defvar osx-use-option-as-meta t
   "If non nil the option key is mapped to meta. Set to `nil` if you need the
   option key to type common characters.")
+
+;; Use the OS X Emoji font for Emoticons
+(set-fontset-font "fontset-default"
+                  '(#x1F600 . #x1F64F)
+                  (font-spec :name "Apple Color Emoji") nil 'prepend)
