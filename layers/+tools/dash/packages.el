658b8dfbf01a8703a13bc018fcb22455823d2d1b
dotneter <dotneter@mail.ru>
2015-11-30 18:00:45 +0300

    dash: Enable zeal on Microsoft Windows

--- layers/+tools/dash/packages.el
+++ layers/+tools/dash/packages.el
@@ -2,8 +2,11 @@
 (setq dash-packages '(helm-dash))
 
 (cond
- ((spacemacs/system-is-mac) (push 'dash-at-point dash-packages))
- ((spacemacs/system-is-linux) (push 'zeal-at-point dash-packages)))
+ ((spacemacs/system-is-mac)
+  (push 'dash-at-point dash-packages))
+ ((or (spacemacs/system-is-linux)
+      (spacemacs/system-is-mswindows))
+  (push 'zeal-at-point dash-packages)))
 
 (defun dash/init-helm-dash ()
   (use-package helm-dash
