e293151960657dd2bb60698e81d0bb41ab47c9f5
syl20bnr <sylvain.benner@gmail.com>
2015-12-05 20:40:27 -0500

    ruby: improve chruby configuration
    Look for a .ruby-version file, if it does not exist then use the
    currently activated ruby.
    
    Update documentation.

--- layers/+lang/ruby/config.el
+++ layers/+lang/ruby/config.el
@@ -19,16 +19,14 @@
   "If non-nil, use `enh-ruby-mode' package instead of the built-in Ruby Mode.")
 
 (defvar ruby-version-manager nil
-  "If non nil, defines the Ruby version manager (i.e. rbenv, rvm)")
+  "If non nil, defines the Ruby version manager.
+Possible values are `rbenv', `rvm' or `chruby'.)")
 
 (defvar ruby-use-ruby-test nil
   "If non-nil, use `ruby-test-mode' package instead of `rspec-mode'.")
 
 (defvar ruby-test-runner 'ruby-test
   "Test runner to use. Possible values are `ruby-test' or `rspec'.")
-
-(defvar chruby-ruby-version nil
-  "If non-nil, sets chruby ruby version, see https://github.com/plexus/chruby.el for more information.")
 
 ;; Command prefixes
 
