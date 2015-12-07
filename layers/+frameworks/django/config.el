fbd4769062e2151822c9917217c6f2420ad2d9fc
Eivind Fonn <evfonn@gmail.com>
2015-12-07 22:36:40 +0100

    Make framework layers depend on language layers
    - Ruby on rails: Ruby
    - Django: Python

---
+++ layers/+frameworks/django/config.el
@@ -0,0 +1,15 @@
+;;; config.el --- Django Layer config File for Spacemacs
+;;
+;; Copyright (c) 2012-2015 Sylvain Benner
+;; Copyright (c) 2014-2015 Andrea Moretti & Contributors
+;;
+;; Author: Andrea Moretti <axyzxp@gmail.com>
+;; URL: https://github.com/axyz
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Prerequisites
+
+(configuration-layer/declare-layer 'python)
