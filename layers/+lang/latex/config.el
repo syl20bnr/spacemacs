48f15c4768d3188d39e55da7f67735878e3d17ef
Ben <benquebec@gmail.com>
2015-12-11 13:32:16 -0500

    keybinding overhaul auctex + reftex including toc-mode (latex layer)
    deleted by error

--- layers/+lang/latex/config.el
+++ layers/+lang/latex/config.el
@@ -16,7 +16,7 @@
 (spacemacs|defvar-company-backends LaTeX-mode)
 
 (defvar latex-build-command (if (executable-find "latexmk") "LatexMk" "LaTeX")
-  "The default command to use with `SPC m b'")
+  "The default command to use with `SPC m c c'")
 
 (defvar latex-enable-auto-fill t
   "Whether to use auto-fill-mode or not in tex files.")
@@ -28,12 +28,3 @@
                            "tabular"
                            "tikzpicture")
   "List of environment names in which `auto-fill-mode' will be inhibited.")
-
-;; Command prefixes
-;; no supported
-;; (setq auctex/key-binding-prefixes '())
-;; (push (cons "mp" "LaTeX Preview") auctex/key-binding-prefixes)
-;; (push (cons "mr" "RefTeX") auctex/key-binding-prefixes)
-;; (mapc (lambda (x) (spacemacs/declare-prefix (car x) (cdr x)))
-;;       auctex/key-binding-prefixes)
-
