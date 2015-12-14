070a2b36e404ece12b3db1c26f7b16fff95efce0
Kabelo Moiloa <moiloakabelo@gmail.com>
2015-12-14 21:07:37 +0200

    Add Lean Mode

---
+++ layers/+lang/lean/config.el
@@ -0,0 +1,16 @@
+;;; config.el --- Lean Layer config File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(configuration-layer/declare-layers '(auto-completion lua syntax-checking))
+
+(spacemacs|defvar-company-backends lean-mode)
+(push 'lean-company company-backends-lean-mode)
