5361447599d4b3b2ddc9d831b0985f54f3b11ea5
Erwan Loisant <eloisant@gmail.com>
2015-12-22 10:34:58 +0100

    Auto-activate for react native index files

--- layers/+frameworks/react/packages.el
+++ layers/+frameworks/react/packages.el
@@ -66,6 +66,8 @@
   (define-derived-mode react-mode web-mode "react")
   (add-to-list 'auto-mode-alist '("\\.jsx\\'" . react-mode))
   (add-to-list 'auto-mode-alist '("\\.react.js\\'" . react-mode))
+  (add-to-list 'auto-mode-alist '("\\index.android.js\\'" . react-mode))
+  (add-to-list 'auto-mode-alist '("\\index.ios.js\\'" . react-mode))
   (add-to-list 'magic-mode-alist '("/** @jsx React.DOM */" . react-mode))
   (defun spacemacs//setup-react-mode ()
     "Adjust web-mode to accommodate react-mode"
