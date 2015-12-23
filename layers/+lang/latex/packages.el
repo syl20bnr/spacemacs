97e1ee99c403d98c3f7c1bc03576302960b4affc
justbur <justin@burkett.cc>
2015-12-23 08:46:36 -0500

    latex: Add tex-fold-mode option and reorganize
    Add the latex-enable-folding variable which turns on TeX-fold-mode and
    adds some related bindings.
    
    Reorganize the layer:
    1. Move reftex into packages.el and delete the extensions file.
    2. Don't duplicate the binding of keys. Use a loop to bind in tex-mode
       and latex-mode. Then just add the additional latex-mode bindings

--- layers/+lang/latex/packages.el
+++ layers/+lang/latex/packages.el
@@ -17,6 +17,7 @@
     company
     company-auctex
     evil-matchit
+    (reftex :location built-in)
     flycheck
     flyspell
     smartparens
@@ -36,61 +37,62 @@
             TeX-syntactic-comment t
             ;; Synctex support
             TeX-source-correlate-start-server nil
-            ;; Setup reftex style (RefTeX is supported through extension)
-            reftex-use-fonts t
             ;; Don't insert line-break at inline math
             LaTeX-fill-break-at-separators nil)
       (when latex-enable-auto-fill
         (add-hook 'LaTeX-mode-hook 'latex/auto-fill-mode))
+      (when latex-enable-folding
+        (add-hook 'LaTeX-mode-hook 'TeX-fold-mode))
       (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
       (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
       (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode))
     :config
     (progn
       ;; Key bindings for plain TeX
-      (spacemacs/set-leader-keys-for-major-mode 'tex-mode
-        "\\"  'TeX-insert-macro            ;; C-c C-m
-        dotspacemacs-major-mode-leader-key
-              'TeX-command-master          ;; C-c C-c
-        "k"   'TeX-kill-job                ;; C-c C-k
-        "b"   'latex/build
-        "f"   'TeX-fold-buffer
-        "l"   'TeX-recenter-output-buffer  ;; C-c C-l
-        "m"   'TeX-insert-macro            ;; C-c C-m
-        "v"   'TeX-view                    ;; C-c C-v
+      (dolist (mode '(tex-mode latex-mode))
+        (spacemacs/set-leader-keys-for-major-mode mode
+          "\\"  'TeX-insert-macro           ;; C-c C-m
+          "-"   'TeX-recenter-output-buffer ;; C-c C-l
+          "b"   'latex/build
+          "k"   'TeX-kill-job               ;; C-c C-k
+          "l"   'TeX-recenter-output-buffer ;; C-c C-l
+          "m"   'TeX-insert-macro           ;; C-c C-m
+          "v"   'TeX-view                   ;; C-c C-v
+          ;; TeX-doc is a very slow function
+          "hd"  'TeX-doc
+          "xb"  'latex/font-bold
+          "xc"  'latex/font-code
+          "xe"  'latex/font-emphasis
+          "xi"  'latex/font-italic
+          "xr"  'latex/font-clear
+          "xo"  'latex/font-oblique
+          "xfc" 'latex/font-small-caps
+          "xff" 'latex/font-sans-serif
+          "xfr" 'latex/font-serif)
+        (when dotspacemacs-major-mode-emacs-leader-key
+          (spacemacs/set-leader-keys-for-major-mode mode
+            dotspacemacs-major-mode-emacs-leader-key 'TeX-command-master))
+        (when dotspacemacs-major-mode-leader-key
+          (spacemacs/set-leader-keys-for-major-mode mode
+            dotspacemacs-major-mode-leader-key 'TeX-command-master))
+        (when latex-enable-folding
+          (spacemacs/set-leader-keys-for-major-mode mode
+            "ff" 'TeX-fold-buffer
+            "fe" 'TeX-fold-env
+            "fm" 'TeX-fold-macro
+            "f=" 'TeX-fold-math
+            "fr" 'TeX-fold-region))
+        (spacemacs/declare-prefix-for-mode mode "mf"  "fold")
+        (spacemacs/declare-prefix-for-mode mode "mx"  "text/fonts"))
 
-        "xb"  'latex/font-bold
-        "xc"  'latex/font-code
-        "xe"  'latex/font-emphasis
-        "xi"  'latex/font-italic
-        "xr"  'latex/font-clear
-        "xo"  'latex/font-oblique
-        "xfc" 'latex/font-small-caps
-        "xff" 'latex/font-sans-serif
-        "xfr" 'latex/font-serif)
-      (spacemacs/declare-prefix-for-mode 'tex-mode "mx" "tex/text")
-      (spacemacs/declare-prefix-for-mode 'tex-mode "mx" "tex/fonts")
-
-      ;; Key bindings for LaTeX
+      ;; Key bindings specific to LaTeX
       (spacemacs/set-leader-keys-for-major-mode 'latex-mode
-        "*"   'LaTeX-mark-section           ;; C-c *
-        "."   'LaTeX-mark-environment       ;; C-c .
-        "\\"  'TeX-insert-macro             ;; C-c C-m
-        "-"   'TeX-recenter-output-buffer   ;; C-c C-l
-        dotspacemacs-major-mode-leader-key
-              'TeX-command-master           ;; C-c C-c
-        "b"   'latex/build
-        "c"   'LaTeX-close-environment      ;; C-c ]
-        "e"   'LaTeX-environment            ;; C-c C-e
-        "f"   'TeX-fold-buffer
-        "i"   'LaTeX-insert-item            ;; C-c C-j
-        "k"   'TeX-kill-job                 ;; C-c C-k
-        "l"   'TeX-recenter-output-buffer   ;; C-c C-l
-        "m"   'TeX-insert-macro             ;; C-c C-m
-        "s"   'LaTeX-section                ;; C-c C-s
-        "v"   'TeX-view                     ;; C-c C-v
-        ;; TeX-doc is a very slow function
-        "hd"  'TeX-doc
+        "*"   'LaTeX-mark-section      ;; C-c *
+        "."   'LaTeX-mark-environment  ;; C-c .
+        "c"   'LaTeX-close-environment ;; C-c ]
+        "e"   'LaTeX-environment       ;; C-c C-e
+        "i"   'LaTeX-insert-item       ;; C-c C-j
+        "s"   'LaTeX-section           ;; C-c C-s
         "pb"  'preview-buffer
         "pc"  'preview-clearout
         "pd"  'preview-document
@@ -99,21 +101,11 @@
         "pp"  'preview-at-point
         "pr"  'preview-region
         "ps"  'preview-section
-        "xb"  'latex/font-bold
         "xB"  'latex/font-medium
-        "xc"  'latex/font-code
-        "xe"  'latex/font-emphasis
-        "xi"  'latex/font-italic
-        "xo"  'latex/font-oblique
         "xr"  'latex/font-clear
         "xfa" 'latex/font-calligraphic
-        "xfc" 'latex/font-small-caps
-        "xff" 'latex/font-sans-serif
         "xfn" 'latex/font-normal
-        "xfr" 'latex/font-serif
-        "xfu" 'latex/font-upright)
-      (spacemacs/declare-prefix-for-mode 'latex-mode "mx" "latex/text")
-      (spacemacs/declare-prefix-for-mode 'latex-mode "mx" "latex/fonts"))))
+        "xfu" 'latex/font-upright))))
 
 (when (string= latex-build-command "LatexMk")
   (defun latex/init-auctex-latexmk ()
@@ -125,6 +117,26 @@
         (spacemacs|use-package-add-hook tex
           :post-config
           (auctex-latexmk-setup))))))
+
+(defun latex/init-reftex ()
+  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+  (setq reftex-plug-into-AUCTeX '(nil nil t t t)
+        reftex-use-fonts t)
+  (spacemacs/declare-prefix-for-mode 'latex-mode "mr"  "reftex")
+  (spacemacs/set-leader-keys-for-major-mode 'latex-mode
+    "rc"    'reftex-citation
+    "rg"    'reftex-grep-document
+    "ri"    'reftex-index-selection-or-word
+    "rI"    'reftex-display-index
+    "r TAB" 'reftex-index
+    "rl"    'reftex-label
+    "rp"    'reftex-index-phrase-selection-or-word
+    "rP"    'reftex-index-visit-phrases-buffer
+    "rr"    'reftex-reference
+    "rs"    'reftex-search-document
+    "rt"    'reftex-toc
+    "rT"    'reftex-toc-recenter
+    "rv"    'reftex-view-crossref))
 
 (when (configuration-layer/layer-usedp 'auto-completion)
   (defun latex/post-init-company ()
