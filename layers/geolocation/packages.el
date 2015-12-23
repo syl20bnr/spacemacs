b444bd1866b81a78b1ba80228ac07f92dea0092b
Declan Qian <declanqian@gmail.com>
2015-12-24 03:48:42 +0800

    updated to evilified-state-evilify-map

--- layers/geolocation/packages.el
+++ layers/geolocation/packages.el
@@ -84,19 +84,23 @@ to not have to set these variables manually when enabling this layer."
     (progn
       (spacemacs/set-leader-keys
         "aw" 'sunshine-forecast
-        "aW" 'sunshine-quick-forecast)
-
-      (evilified-state-evilify sunshine-mode sunshine-mode-map
-        (kbd "q") 'quit-window
-        (kbd "i") 'sunshine-toggle-icons))
+        "aW" 'sunshine-quick-forecast))
     :config
-    ;; just in case location was not set by user, or on OS X,
-    ;; if wasn't set up automatically, will not work with Emac's
-    ;; default for ;; `calendar-location-name'
-    (when (not (boundp 'sunshine-location))
-      (setq sunshine-location (format "%s, %s"
-                                      calendar-latitude
-                                      calendar-longitude)))))
+    (progn
+      (evilified-state-evilify-map sunshine-mode-map
+        :mode sunshine-mode
+        :bindings
+        (kbd "q") 'quit-window
+        (kbd "i") 'sunshine-toggle-icons)
+
+      ;; just in case location was not set by user, or on OS X,
+      ;; if wasn't set up automatically, will not work with Emac's
+      ;; default for ;; `calendar-location-name'
+      (when (not (boundp 'sunshine-location))
+        (setq sunshine-location (format "%s, %s"
+                                        calendar-latitude
+                                        calendar-longitude)))
+  )))
 
 (defun geolocation/init-theme-changer ()
   "Initialize theme-changer"
