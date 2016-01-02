16623deb57cac7e71f600122cec92dc6476e4229
Alexandr Skurikhin <a@skurih.in>
2016-01-03 01:14:34 +0300

    Add ivy-persp-switch-project

--- layers/+distribution/spacemacs-base/packages.el
+++ layers/+distribution/spacemacs-base/packages.el
@@ -809,13 +809,11 @@ Example: (evil-map visual \"<\" \"<gv\")"
         "pT" 'projectile-find-test-file
         "py" 'projectile-find-tag)
       (when (configuration-layer/package-usedp 'persp-mode)
-          (if (configuration-layer/layer-usedp 'spacemacs-helm)
-            (spacemacs/set-leader-keys
-            "pl" 'spacemacs/helm-persp-switch-project)
-            (spacemacs/set-leader-keys
-              "pl" 'spacemacs/ivy-persp-switch-project)
-          )
-          ))
+        (if (configuration-layer/layer-usedp 'spacemacs-helm)
+            ((spacemacs/set-leader-keys
+               "pl" 'spacemacs/helm-persp-switch-project)
+             (spacemacs/set-leader-keys
+               "pl" 'spacemacs/ivy-persp-switch-project)))))
     :config
     (progn
       (projectile-global-mode)
