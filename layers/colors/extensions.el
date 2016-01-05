1fc798139b45155bc530f51f4d9b09290713f098
syl20bnr <sylvain.benner@gmail.com>
2016-01-04 23:48:55 -0500

    nyancat: Add support for emacs --daemon and GUI client

--- layers/colors/extensions.el
+++ layers/colors/extensions.el
@@ -20,31 +20,14 @@
       (setq nyan-wavy-trail t)
       (setq nyan-animate-nyancat t)
       (nyan-mode)
+      ;; explicitly re-enable the cat for the first GUI client
+      (spacemacs|do-after-display-system-init
+       (nyan-mode -1)
+       (nyan-mode))
 
       (spacemacs|add-toggle nyan-cat-progress-bar
         :status nyan-mode
         :on (nyan-mode)
         :off (nyan-mode -1)
         :documentation "Show a nyan cat progress bar in the mode-line."
-        :evil-leader "tmn")
-
-      (defun spacemacs/powerline-nyan-cat ()
-        "Construct a powerline segment for nyan cat."
-        (let* ((active (powerline-selected-window-active))
-               (l (1+ (truncate (powerline-width lhs))))
-               (r (1+ (truncate (powerline-width rhs))))
-               (face1 (if active 'powerline-active1 'powerline-inactive1))
-               (face2 (if active 'powerline-active2 'powerline-inactive2))
-               (separator-left
-                (intern (format "powerline-%s-%s"
-                                powerline-default-separator
-                                (car powerline-default-separator-dir))))
-               (separator-right
-                (intern (format "powerline-%s-%s"
-                                powerline-default-separator
-                                (cdr powerline-default-separator-dir)))))
-          (setq nyan-bar-length (min 32 (/ (- (window-total-width) l r) 2)))
-          (list
-           (funcall separator-right face2 face1)
-           (powerline-raw (nyan-create) face1)
-           (funcall separator-left face1 face2)))))))
+        :evil-leader "tmn"))))
