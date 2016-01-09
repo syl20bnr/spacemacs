8bb61da5fee135fdfa7c6d61fcbc18f3436015ff
syl20bnr <sylvain.benner@gmail.com>
2016-01-09 02:28:19 -0500

    Move avy bindings to spacemacs/init-avy, remove "SPC `"

--- layers/+distribution/spacemacs-base/keybindings.el
+++ layers/+distribution/spacemacs-base/keybindings.el
@@ -150,17 +150,12 @@ Ensure that helm is required before calling FUNC."
   "j0" 'spacemacs/push-mark-and-goto-beginning-of-line
   "j$" 'spacemacs/push-mark-and-goto-end-of-line
   "jb" 'bookmark-jump
-  "jc" 'evil-avy-goto-char-2
   "jd" 'dired-jump
   "jD" 'dired-jump-other-window
   "jf" 'find-function-at-point
   "ji" 'spacemacs/jump-in-buffer
   "jI" 'helm-imenu-in-all-buffers
-  "jl" 'evil-avy-goto-line
-  "ju" 'avy-pop-mark
-  "jU" 'spacemacs/avy-goto-url
-  "jv" 'find-variable-at-point
-  "jw" 'evil-avy-goto-word-or-subword-1)
+  "jv" 'find-variable-at-point)
 
 ;; Compilation ----------------------------------------------------------------
 (spacemacs/set-leader-keys
