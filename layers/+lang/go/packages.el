99c09c28c3e709eaafd3e26c52e3f4ff2eb1a15f
Will Maier <wcmaier@gmail.com>
2016-01-10 20:59:42 +0000

    Avoid clobbering golang variables
    Instead of checking for our platform, simply test if the variables have
    already been set. If not, let exec-path-from-shell figure out the right
    values.

--- layers/+lang/go/packages.el
+++ layers/+lang/go/packages.el
@@ -11,9 +11,9 @@
   (spacemacs/add-flycheck-hook 'go-mode-hook))
 
 (defun go/init-go-mode()
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-copy-env "GOPATH")
-    (exec-path-from-shell-copy-env "GO15VENDOREXPERIMENT"))
+  (dolist (var '("GOPATH" "GO15VENDOREXPERIMENT"))
+    (unless (getenv var)
+      (exec-path-from-shell-copy-env var)))
 
   (use-package go-mode
     :defer t
