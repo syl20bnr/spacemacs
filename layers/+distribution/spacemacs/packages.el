194a42db740a31b3df857dc93596a3d2b781512f
syl20bnr <sylvain.benner@gmail.com>
2016-01-17 23:28:09 -0500

    Move unimpaired to spacemacs layer

--- layers/+distribution/spacemacs/packages.el
+++ layers/+distribution/spacemacs/packages.el
@@ -45,6 +45,7 @@
         ;; https://github.com/7696122/evil-terminal-cursor-changer/issues/8
         (evil-terminal-cursor-changer :excluded t)
         evil-tutor
+        (evil-unimpaired :skip-install t)
         expand-region
         fancy-battery
         flx-ido
@@ -726,6 +727,86 @@
       (setq evil-tutor-working-directory
             (concat spacemacs-cache-directory ".tutor/"))
       (spacemacs/set-leader-keys "hT" 'evil-tutor-start))))
+
+(defun spacemacs/init-evil-unimpaired ()
+
+  (defun evil-unimpaired//find-relative-filename (offset)
+    (when buffer-file-name
+      (let* ((directory (f-dirname buffer-file-name))
+             (files (f--files directory (not (s-matches? "^\\.?#" it))))
+             (index (+ (-elem-index buffer-file-name files) offset))
+             (file (and (>= index 0) (nth index files))))
+        (when file
+          (f-expand file directory)))))
+
+  (defun evil-unimpaired/previous-file ()
+    (interactive)
+    (-if-let (filename (evil-unimpaired//find-relative-filename -1))
+        (find-file filename)
+      (user-error "No previous file")))
+
+  (defun evil-unimpaired/next-file ()
+    (interactive)
+    (-if-let (filename (evil-unimpaired//find-relative-filename 1))
+        (find-file filename)
+      (user-error "No next file")))
+
+  (defun evil-unimpaired/paste-above ()
+    (interactive)
+    (evil-insert-newline-above)
+    (evil-paste-after 1))
+
+  (defun evil-unimpaired/paste-below ()
+    (interactive)
+    (evil-insert-newline-below)
+    (evil-paste-after 1))
+
+  (defun evil-unimpaired/insert-space-above ()
+    (interactive)
+    (evil-insert-newline-above)
+    (forward-line))
+
+  (defun evil-unimpaired/insert-space-below ()
+    (interactive)
+    (evil-insert-newline-below)
+    (forward-line -1))
+
+  (defun evil-unimpaired/next-frame ()
+    (interactive)
+    (raise-frame (next-frame)))
+
+  (defun evil-unimpaired/previous-frame ()
+    (interactive)
+    (raise-frame (previous-frame)))
+
+  ;; from tpope's unimpaired
+  (define-key evil-normal-state-map (kbd "[ SPC")
+    'evil-unimpaired/insert-space-above)
+  (define-key evil-normal-state-map (kbd "] SPC")
+    'evil-unimpaired/insert-space-below)
+  (define-key evil-normal-state-map (kbd "[ e") 'move-text-up)
+  (define-key evil-normal-state-map (kbd "] e") 'move-text-down)
+  (define-key evil-visual-state-map (kbd "[ e") ":move'<--1")
+  (define-key evil-visual-state-map (kbd "] e") ":move'>+1")
+  ;; (define-key evil-visual-state-map (kbd "[ e") 'move-text-up)
+  ;; (define-key evil-visual-state-map (kbd "] e") 'move-text-down)
+  (define-key evil-normal-state-map (kbd "[ b") 'spacemacs/previous-useful-buffer)
+  (define-key evil-normal-state-map (kbd "] b") 'spacemacs/next-useful-buffer)
+  (define-key evil-normal-state-map (kbd "[ f") 'evil-unimpaired/previous-file)
+  (define-key evil-normal-state-map (kbd "] f") 'evil-unimpaired/next-file)
+  (define-key evil-normal-state-map (kbd "] l") 'spacemacs/next-error)
+  (define-key evil-normal-state-map (kbd "[ l") 'spacemacs/previous-error)
+  (define-key evil-normal-state-map (kbd "[ h") 'diff-hl-previous-hunk)
+  (define-key evil-normal-state-map (kbd "] h") 'diff-hl-next-hunk)
+  (define-key evil-normal-state-map (kbd "[ t") 'evil-unimpaired/previous-frame)
+  (define-key evil-normal-state-map (kbd "] t") 'evil-unimpaired/next-frame)
+  (define-key evil-normal-state-map (kbd "[ w") 'previous-multiframe-window)
+  (define-key evil-normal-state-map (kbd "] w") 'next-multiframe-window)
+  ;; select pasted text
+  (define-key evil-normal-state-map (kbd "g p") (kbd "` [ v ` ]"))
+  ;; paste above or below with newline
+  (define-key evil-normal-state-map (kbd "[ p") 'evil-unimpaired/paste-above)
+  (define-key evil-normal-state-map (kbd "] p") 'evil-unimpaired/paste-below))
 
 (defun spacemacs/init-expand-region ()
   (use-package expand-region
