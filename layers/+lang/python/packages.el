e6052976757bb8d32f154b525c03222d7911f461
Eivind Fonn <evfonn@gmail.com>
2016-01-23 15:00:23 +0100

    Re-add py-yapf

--- layers/+lang/python/packages.el
+++ layers/+lang/python/packages.el
@@ -311,6 +311,14 @@
       :init
       (push 'company-anaconda company-backends-python-mode))))
 
+(defun python/init-py-yapf ()
+  (use-package py-yapf
+    :init
+    (spacemacs/set-leader-keys-for-major-mode 'python-mode "=" 'py-yapf-buffer)
+    :config
+    (when python-enable-yapf-format-on-save
+      (add-hook 'python-mode-hook 'py-yapf-enable-on-save))))
+
 (defun python/post-init-semantic ()
   (semantic/enable-semantic-mode 'python-mode)
   (defadvice semantic-python-get-system-include-path (around semantic-python-skip-error-advice activate)
