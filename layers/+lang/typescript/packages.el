f942144b2ccc6ea4c7a97351942b260d779943de
JAremko <w3techplayground@gmail.com>
2016-01-25 18:19:39 +0200

    replaced typescript layer
    fixed docs
    
    Better docs, added linter and tsx mode
    
    fixed typo
    
    made TSX optional

--- layers/+lang/typescript/packages.el
+++ layers/+lang/typescript/packages.el
@@ -9,14 +9,69 @@
 ;;
 ;;; License: GPLv3
 
-(setq typescript-packages '(tss))
+(setq typescript-packages '(tide
+                            flycheck-typescript-tslint
+                            web-mode))
 
-(defun typescript/init-tss ()
-  "Initialize my package"
-  (use-package tss
+(defun typescript/init-tide ()
+  (use-package tide
     :defer t
-    :mode ("\\.ts\\'" . typescript-mode)
-    :init (spacemacs/set-leader-keys-for-major-mode 'typescript-mode
-            "gg" 'tss-jump-to-definition
-            "hh" 'tss-popup-help)
-    :config (tss-config-default)))
+    :commands (typescript/jump-to-type-def)
+    :init (progn
+            (evilified-state-evilify tide-references-mode tide-references-mode-map
+              (kbd "C-j") 'tide-find-previous-reference
+              (kbd "C-k") 'tide-find-next-reference
+              (kbd "C-l") 'tide-goto-reference)
+
+            (add-hook 'typescript-mode-hook
+                      (lambda ()
+                      (tide-setup)
+                      (flycheck-mode t)
+                      (setq flycheck-check-syntax-automatically '(save mode-enabled))
+                      (eldoc-mode t)
+                      (when (configuration-layer/package-usedp 'company) 
+                            (company-mode-on)))))
+    :config (progn
+             
+              (when typescript-use-tslint
+                    (use-package flycheck-typescript-tslint)
+                    (flycheck-add-next-checker 'typescript-tide
+                                               'typescript-tslint 'append))
+
+              (spacemacs/declare-prefix-for-mode 'typescript-mode "mg" "goto")
+              (spacemacs/declare-prefix-for-mode 'typescript-mode "mh" "help")
+              (spacemacs/declare-prefix-for-mode 'typescript-mode "mn" "name")
+
+              (defun typescript/jump-to-type-def()
+                (interactive)
+                (tide-jump-to-definition t))
+
+              (spacemacs/set-leader-keys-for-major-mode 'typescript-mode
+                "gb" 'tide-jump-back
+                "gm" 'typescript/jump-to-type-def
+                "gg" 'tide-jump-to-definition
+                "hd" 'tide-documentation-at-point
+                "nr" 'tide-rename-symbol
+                "r"  'tide-references
+                "s"  'tide-restart-server))))
+
+
+(when (configuration-layer/package-usedp 'web-mode) 
+  (defun typescript/init-web-mode ()
+   (use-package web-mode
+   :defer t
+   :mode ("\\.tsx\\'" . web-mode)
+   :config (add-hook 'web-mode-hook
+                     (lambda ()
+                       (when (string-equal "tsx" (file-name-extension buffer-file-name))
+                         (tide-setup)
+                         (flycheck-mode +1)
+                         (setq flycheck-check-syntax-automatically '(save mode-enabled))
+                         (eldoc-mode +1)
+                         (when (configuration-layer/package-usedp 'company) 
+                               (company-mode-on))))))))
+
+(when typescript-use-tslint
+  (defun typescript/init-flycheck-typescript-tslint ()
+    (use-package flycheck-typescript-tslint 
+    :defer t)))
