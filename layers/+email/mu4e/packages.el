8824f2da8e9de7f525c2518e429e72694878693c
Peter Hoeg <peter@speartail.com>
2016-01-27 22:18:17 +0800

    Add a bit of meat to mu4e
    This commit does a few things:
    
     - strips unnecessary (progn) from :init and :config
     - supports async mode for sending mails
     - registers imagemagick as handler for images if it exists
     - sets default downloads directory if it exists
     - sets a few (more) sane defaults
     - supports format=flowed in messages

--- layers/+email/mu4e/packages.el
+++ layers/+email/mu4e/packages.el
@@ -20,31 +20,48 @@
   (use-package mu4e
     :commands (mu4e mu4e-compose-new)
     :init
-    (progn
-      (spacemacs/set-leader-keys "a M" 'mu4e)
-      (global-set-key (kbd "C-x m") 'mu4e-compose-new))
+    (spacemacs/set-leader-keys "a M" 'mu4e)
+    (global-set-key (kbd "C-x m") 'mu4e-compose-new)
     :config
-    (progn
-      (evilified-state-evilify-map mu4e-main-mode-map
-        :mode mu4e-main-mode
-        :bindings
-        (kbd "j") 'mu4e~headers-jump-to-maildir)
-      (evilified-state-evilify-map mu4e-headers-mode-map
-        :mode mu4e-headers-mode)
-      (evilified-state-evilify-map mu4e-view-mode-map
-        :mode mu4e-view-mode)
+    (evilified-state-evilify-map mu4e-main-mode-map
+      :mode mu4e-main-mode
+      :bindings
+      (kbd "j") 'mu4e~headers-jump-to-maildir)
+    (evilified-state-evilify-map mu4e-headers-mode-map
+      :mode mu4e-headers-mode)
+    (evilified-state-evilify-map mu4e-view-mode-map
+      :mode mu4e-view-mode)
 
-      (setq mu4e-completing-read-function
-            (if (configuration-layer/layer-usedp 'spacemacs-ivy)
-                'ivy-completing-read
-              'helm-completing-read))
+    (setq mu4e-completing-read-function
+          (if (configuration-layer/layer-usedp 'spacemacs-ivy)
+              'ivy-completing-read
+            'helm-completing-read))
 
-      (add-to-list 'mu4e-view-actions
-                   '("View in browser" . mu4e-action-view-in-browser) t)
+    (when mu4e-async
+      (require 'smtpmail-async)
+      (mu4e/set-send-function 'async-smtpmail-send-it))
 
-      (when mu4e-account-alist
-        (add-hook 'mu4e-compose-pre-hook 'mu4e/set-account)
-        (add-hook 'message-sent-hook 'mu4e/mail-account-reset)))))
+    (when (fboundp 'imagemagick-register-types)
+      (imagemagick-register-types))
+
+    (let ((dir "~/Downloads"))
+      (when (file-directory-p dir)
+        (setq mu4e-attachment-dir dir)))
+
+    (setq mu4e-use-fancy-chars 't
+          mu4e-view-show-images 't
+          message-kill-buffer-on-exit 't)
+
+    (add-to-list 'mu4e-view-actions
+                 '("View in browser" . mu4e-action-view-in-browser) t)
+
+    (add-hook 'mu4e-compose-mode-hook
+              (lambda ()
+                (use-hard-newlines t 'guess)))
+
+    (when mu4e-account-alist
+      (add-hook 'mu4e-compose-pre-hook 'mu4e/set-account)
+      (add-hook 'message-sent-hook 'mu4e/mail-account-reset))))
 
 (defun mu4e/init-mu4e-maildirs-extension ()
   (use-package mu4e-maildirs-extension
@@ -53,6 +70,5 @@
 
 (defun mu4e/post-init-org ()
   ;; load org-mu4e when org is actually loaded
-  (with-eval-after-load 'org (require 'org-mu4e nil 'noerror)))
-
-
+  (when (configuration-layer/layer-usedp 'org)
+    (with-eval-after-load 'org (require 'org-mu4e nil 'noerror))))
