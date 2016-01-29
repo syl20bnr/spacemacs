72b2857d7fd1813443420042f33b33315e7fb3eb
luxbock <opieppo@gmail.com>
2016-01-29 15:35:56 +0700

    Adds Graphviz layer
    - Uses a forked version of graphviz-dot-mode.el with a local install.
      Upstream PR: ppareit/graphviz-dot-mode#18

---
+++ layers/+lang/graphviz/packages.el
@@ -0,0 +1,34 @@
+;;; packages.el --- graphviz layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+(defconst graphviz-packages
+  '((graphviz-dot-mode :location (recipe :fetcher github :repo "luxbock/graphviz-dot-mode"))))
+
+(defun graphviz/init-graphviz-dot-mode ()
+  (use-package graphviz-dot-mode
+    :mode "\\.dot$"
+    :defer t
+    :config
+    (progn
+      (spacemacs|add-toggle graphviz-live-reload
+        :status graphviz-dot-auto-preview-on-save
+        :on (graphviz-turn-on-live-preview)
+        :off (graphviz-turn-off-live-preview)
+        :documentation "Enable Graphviz live reload.")
+      (define-key graphviz-dot-mode-map (kbd "M-q") 'graphviz-dot-indent-graph)
+      (spacemacs/set-leader-keys-for-major-mode 'graphviz-dot-mode
+        "t" 'spacemacs/toggle-graphviz-live-reload
+        "c" 'compile
+        "p" 'graphviz-dot-preview
+        "," 'graphviz-dot-preview))))
+
+;;; packages.el ends here
