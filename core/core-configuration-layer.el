4be8565bb6bdddbaf721b91213244047bda180bd
syl20bnr <sylvain.benner@gmail.com>
2016-01-31 00:03:04 -0500

    core: new layer files packages-config.el and packages-funcs.el
    Start to isolate the list of packages. Also we want to define packages
    function at the top level of an elisp file in order to not prevent
    emacs from knowing where the functions are defined.
    
    This commit has a POC made with elfeed and elixir layers.

--- core/core-configuration-layer.el
+++ core/core-configuration-layer.el
@@ -637,7 +637,9 @@ path."
   "Declare all packages contained in LAYERS."
   (let ((layers2 layers)
         (warning-minimum-level :error))
-    (configuration-layer//load-layers-files layers2 '("packages.el"))
+    (configuration-layer//load-layers-files layers2 '("packages.el"
+                                                      "packages-config.el"
+                                                      "pakcages-funs.el"))
     ;; gather all the packages of current layer
     (configuration-layer//sort-packages (configuration-layer/get-packages
                                          layers2 t))))
