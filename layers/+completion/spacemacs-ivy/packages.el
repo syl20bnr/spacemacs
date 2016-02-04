cf90c8ee0774748bb6a652e4b81a6aaed4bd3a49
justbur <justin@burkett.cc>
2016-02-04 11:57:26 -0500

    ivy: Add custom transient state
    Replace the hydra usually bound to C-o

--- layers/+completion/spacemacs-ivy/packages.el
+++ layers/+completion/spacemacs-ivy/packages.el
@@ -354,7 +354,61 @@ Helm hack."
           (define-key ivy-minibuffer-map (kbd "C-h") nil)
           (define-key ivy-minibuffer-map (kbd "C-l") nil))))
       (spacemacs//hjkl-completion-navigation
-       (member dotspacemacs-editing-style '(vim hybrid))))))
+       (member dotspacemacs-editing-style '(vim hybrid)))
+
+      (defun spacemacs/counsel-up-directory-no-error ()
+        "`counsel-up-directory' ignoring errors."
+        (interactive)
+        (ignore-errors
+          (call-interactively 'counsel-up-directory)))
+
+      (require 'ivy-hydra)
+      (spacemacs|define-transient-state ivy
+        :doc "
+ Move/Resize^^^^      | Select Action^^^^   |  Call^^          |  Cancel^^    | Toggles
+--^-^-^-^-------------|--^-^-^-^------------|--^---^-----------|--^-^---------|---------------------
+ [_j_/_k_] by line    | [_s_/_w_] next/prev | [_RET_] & done   | [_i_] & ins  | [_C_] calling: %s(if ivy-calling \"on\" \"off\")
+ [_g_/_G_] first/last | [_a_]^ ^  list all  | [_TAB_] alt done | [_q_] & quit | [_m_] matcher: %s(ivy--matcher-desc)
+ [_d_/_u_] pg down/up |  ^ ^ ^ ^            | [_c_]   & cont   |  ^ ^         | [_f_] case-fold: %`ivy-case-fold-search
+ [_<_/_>_] resize     |  ^ ^ ^ ^            | [_o_]   occur    |  ^ ^         | [_t_] truncate: %`truncate-lines
+ [_h_/_l_] out/in dir |  ^ ^ ^ ^            |  ^ ^             |  ^ ^         |  ^ ^
+
+Current Action: %s(ivy-action-name)
+"
+        :foreign-keys run
+        :bindings
+        ;; arrows
+        ("j" ivy-next-line)
+        ("k" ivy-previous-line)
+        ("l" ivy-alt-done)
+        ("h" spacemacs/counsel-up-directory-no-error)
+        ("g" ivy-beginning-of-buffer)
+        ("G" ivy-end-of-buffer)
+        ("d" ivy-scroll-up-command)
+        ("u" ivy-scroll-down-command)
+        ;; actions
+        ("q" keyboard-escape-quit :exit t)
+        ("C-g" keyboard-escape-quit :exit t)
+        ("<escape>" keyboard-escape-quit :exit t)
+        ("i" nil)
+        ("C-o" nil)
+        ("TAB" ivy-alt-done :exit nil)
+        ;; ("C-j" ivy-alt-done :exit nil)
+        ;; ("d" ivy-done :exit t)
+        ("RET" ivy-done :exit t)
+        ("c" ivy-call)
+        ("C-m" ivy-done :exit t)
+        ("C" ivy-toggle-calling)
+        ("m" ivy-toggle-fuzzy)
+        (">" ivy-minibuffer-grow)
+        ("<" ivy-minibuffer-shrink)
+        ("w" ivy-prev-action)
+        ("s" ivy-next-action)
+        ("a" ivy-read-action)
+        ("t" (setq truncate-lines (not truncate-lines)))
+        ("f" ivy-toggle-case-fold)
+        ("o" ivy-occur :exit t))
+      (define-key ivy-minibuffer-map "\C-o" 'spacemacs/ivy-transient-state/body))))
 
 (defun spacemacs-ivy/init-smex ()
   (use-package smex
