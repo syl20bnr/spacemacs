92b2c4b659b9964290e033917900e12caadaf030
syl20bnr <sylvain.benner@gmail.com>
2016-02-09 18:39:49 -0500

    mu4e: slight refactor of mu4e/set-account

--- layers/+email/mu4e/funcs.el
+++ layers/+email/mu4e/funcs.el
@@ -9,22 +9,25 @@
 ;;
 ;;; License: GPLv3
 
-(defun mu4e/search-account-by-mail-address (mail-address)
+(defun mu4e//search-account-by-mail-address (mailto)
+  "Return the account given an email address in MAILTO."
   (car (rassoc-if (lambda (x)
-                    (equal (cadr (assoc 'user-mail-address x)) mail-address))
+                    (equal (cadr (assoc 'user-mail-address x)) (car mailto)))
                   mu4e-account-alist)))
 
 (defun mu4e/set-account ()
-  "Set the account for composing a message."
+  "Set the account for composing a message.
+This function tries to guess the correct account from the email address first
+then fallback to the maildir."
   (let* ((account
           (if mu4e-compose-parent-message
               (let* ((mailtos
                       (mu4e-message-field mu4e-compose-parent-message :to))
                      (mailto-account
-                      (car (cl-remove-if-not 'identity
-                                             (mapcar (lambda (x)
-                                                       (mu4e/search-account-by-mail-address (cdr x)))
-                                                     mailtos))))
+                      (car (cl-remove-if-not
+                            'identity
+                            (mapcar ´mu4e//search-account-by-mail-address
+                                    mailtos))))
                      (maildir
                       (mu4e-message-field mu4e-compose-parent-message :maildir))
                      (maildir-account
