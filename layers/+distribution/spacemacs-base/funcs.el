f441f9097cbb5de9ad38a42ce773e6b637eeac8d
Eivind Fonn <evfonn@gmail.com>
2016-02-10 20:28:03 +0100

    Don't change scratch major mode if buffer exists

--- layers/+distribution/spacemacs-base/funcs.el
+++ layers/+distribution/spacemacs-base/funcs.el
@@ -879,10 +879,12 @@ is nonempty."
 (defun spacemacs/switch-to-scratch-buffer ()
   "Switch to the `*scratch*' buffer. Create it first if needed."
   (interactive)
-  (switch-to-buffer (get-buffer-create "*scratch*"))
-  (when (and (not (eq major-mode dotspacemacs-scratch-mode))
-             (fboundp dotspacemacs-scratch-mode))
-    (funcall dotspacemacs-scratch-mode)))
+  (let ((exists (get-buffer "*scratch*")))
+    (switch-to-buffer (get-buffer-create "*scratch*"))
+    (when (and (not exists)
+               (not (eq major-mode dotspacemacs-scratch-mode))
+               (fboundp dotspacemacs-scratch-mode))
+      (funcall dotspacemacs-scratch-mode))))
 
 ;; http://stackoverflow.com/questions/11847547/emacs-regexp-count-occurrences
 (defun how-many-str (regexp str)
