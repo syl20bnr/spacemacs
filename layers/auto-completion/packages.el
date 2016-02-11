508e95759903318deb30d4da30032b18a0145610
Fredrik Bergroth <fbergroth@gmail.com>
2016-02-11 11:12:49 +0100

    company: Fix :init and use default company-frontends

--- layers/auto-completion/packages.el
+++ layers/auto-completion/packages.el
@@ -76,21 +76,21 @@
             company-minimum-prefix-length 2
             company-require-match nil
             company-dabbrev-ignore-case nil
-            company-dabbrev-downcase nil
-            company-frontends '(company-pseudo-tooltip-frontend)))
-            (defvar-local company-fci-mode-on-p nil)
+            company-dabbrev-downcase nil)
 
-        (defun company-turn-off-fci (&rest ignore)
-          (when (boundp 'fci-mode)
-            (setq company-fci-mode-on-p fci-mode)
-            (when fci-mode (fci-mode -1))))
+      (defvar-local company-fci-mode-on-p nil)
 
-        (defun company-maybe-turn-on-fci (&rest ignore)
-          (when company-fci-mode-on-p (fci-mode 1)))
+      (defun company-turn-off-fci (&rest ignore)
+        (when (boundp 'fci-mode)
+          (setq company-fci-mode-on-p fci-mode)
+          (when fci-mode (fci-mode -1))))
 
-        (add-hook 'company-completion-started-hook 'company-turn-off-fci)
-        (add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
-        (add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci)
+      (defun company-maybe-turn-on-fci (&rest ignore)
+        (when company-fci-mode-on-p (fci-mode 1)))
+
+      (add-hook 'company-completion-started-hook 'company-turn-off-fci)
+      (add-hook 'company-completion-finished-hook 'company-maybe-turn-on-fci)
+      (add-hook 'company-completion-cancelled-hook 'company-maybe-turn-on-fci))
     :config
     (progn
       (spacemacs|diminish company-mode " ⓐ" " a")
@@ -139,7 +139,11 @@
   (use-package company-quickhelp
     :if (and auto-completion-enable-help-tooltip (display-graphic-p))
     :defer t
-    :init (add-hook 'company-mode-hook 'company-quickhelp-mode)))
+    :init
+    (progn
+      (add-hook 'company-mode-hook 'company-quickhelp-mode)
+      (with-eval-after-load 'company
+        (setq company-frontends (delq 'company-echo-metadata-frontend company-frontends))))))
 
 (defun auto-completion/init-helm-c-yasnippet ()
   (use-package helm-c-yasnippet
