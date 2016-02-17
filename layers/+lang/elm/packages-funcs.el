f736f6d1cd3d81bbcb6f655596aa4ab41641671e
syl20bnr <sylvain.benner@gmail.com>
2016-02-17 16:13:24 -0500

    Remove conditions in packages-funcs.el
    Package list is not yet available when packages-funcs.el files are
    loaded.

--- layers/+lang/elm/packages-funcs.el
+++ layers/+lang/elm/packages-funcs.el
@@ -10,30 +10,25 @@
 ;;; License: GPLv3
 
 
+;; elm-mode
 
-(when (configuration-layer/package-usedp 'elm-mode)
+(defun spacemacs//elm-find-root ()
+  (setq default-directory (elm--find-dependency-file-path)))
 
-  (defun spacemacs//elm-find-root ()
-    (setq default-directory (elm--find-dependency-file-path))))
+(defun spacemacs/elm-compile-buffer-output ()
+  (interactive)
+  (let* ((fname (format "%s.js" (downcase (file-name-base (buffer-file-name))))))
+    (elm-compile--file (elm--buffer-local-file-name) fname)))
 
-
+(defun spacemacs/elm-repl-push-decl-focus ()
+  "Send current function to the REPL and focus it in insert state."
+  (interactive)
+  (elm-repl-push-decl)
+  (run-elm-interactive)
+  (evil-insert-state))
 
-(when (configuration-layer/package-usedp 'elm-mode)
-
-  (defun spacemacs/elm-compile-buffer-output ()
-    (interactive)
-    (let* ((fname (format "%s.js" (downcase (file-name-base (buffer-file-name))))))
-      (elm-compile--file (elm--buffer-local-file-name) fname)))
-
-  (defun spacemacs/elm-repl-push-decl-focus ()
-    "Send current function to the REPL and focus it in insert state."
-    (interactive)
-    (elm-repl-push-decl)
-    (run-elm-interactive)
-    (evil-insert-state))
-
-  (defun spacemacs/elm-repl-push-focus ()
-    "Send current region to the REPL and focus it in insert state."
-    (elm-repl-push)
-    (run-elm-interactive)
-    (evil-insert-state)))
+(defun spacemacs/elm-repl-push-focus ()
+  "Send current region to the REPL and focus it in insert state."
+  (elm-repl-push)
+  (run-elm-interactive)
+  (evil-insert-state))
