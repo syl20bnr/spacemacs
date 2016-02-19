ca91ff49f262f50829bb37ba58dd035aba647c68
mahinshaw <mahinshaw@gmail.com>
2016-02-19 10:30:32 -0800

    Add mapping for switching between repl connections.

--- layers/+lang/clojure/packages.el
+++ layers/+lang/clojure/packages.el
@@ -229,6 +229,7 @@ If called with a prefix argument, uses the other-window instead."
           "sI" 'cider-jack-in-clojurescript
           "sn" 'spacemacs/cider-send-ns-form-to-repl
           "sN" 'spacemacs/cider-send-ns-form-to-repl-focus
+          "so" 'cider-repl-switch-to-other
           "sq" 'cider-quit
           "sr" 'spacemacs/cider-send-region-to-repl
           "sR" 'spacemacs/cider-send-region-to-repl-focus
@@ -263,6 +264,7 @@ If called with a prefix argument, uses the other-window instead."
 
         "sc" 'cider-repl-clear-buffer
         "sn" 'cider-repl-set-ns
+        "so" 'cider-repl-switch-to-other
         "sq" 'cider-quit
         "ss" 'cider-switch-to-last-clojure-buffer
         "sx" 'cider-refresh
