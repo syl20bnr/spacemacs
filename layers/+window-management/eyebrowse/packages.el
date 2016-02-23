ecaeb8db3beda168ff621881c429ac32fef72570
bmag <bmagamb@gmail.com>
2016-02-23 15:29:41 +0200

    Eyebrowse: goto buffer in workspace
    spacemacs/find-workspace, spacemacs/display-in-workspace and
    spacemacs/goto-buffer-workspace allow us to find and display a buffer's
    workspace.

--- layers/+window-management/eyebrowse/packages.el
+++ layers/+window-management/eyebrowse/packages.el
@@ -23,6 +23,37 @@
       (define-key evil-motion-state-map "gt" 'eyebrowse-next-window-config)
       (define-key evil-motion-state-map "gT" 'eyebrowse-prev-window-config)
 
+      (defun spacemacs/find-workspace (buffer)
+        "Find Eyebrowse workspace containing BUFFER.
+If several workspaces contain BUFFER, return the first one. Workspaces are
+ordered by slot number.
+If no workspace contains
+BUFFER, return nil."
+        ;; the second element of a workspace is its window-state object
+        (--find (memq buffer (spacemacs/window-state-get-buffers (cadr it)))
+               (eyebrowse--get 'window-configs)))
+
+      (defun spacemacs/display-in-workspace (buffer alist)
+        "Display BUFFER's workspace.
+Return BUFFER's window, if exists, otherwise nil.
+If BUFFER is already visible in current workspace, just return its window
+without switching workspaces."
+        (or (get-buffer-window buffer)
+            (-when-let (workspace (spacemacs/find-workspace buffer))
+              (eyebrowse-switch-to-window-config (car workspace))
+              (get-buffer-window buffer))))
+
+      (defun spacemacs/goto-buffer-workspace (buffer)
+        "Switch to BUFFER's window in BUFFER's workspace.
+If BUFFER isn't displayed in any workspace, display it in the current
+workspace, preferably in the current window."
+        (interactive "B")
+        (pop-to-buffer buffer '((;; reuse buffer window from some workspace
+                                 spacemacs/display-in-workspace
+                                 ;; fallback to display in current window
+                                 display-buffer-same-window)
+                                (inhibit-same-window . nil))))
+
       (defun spacemacs/workspaces-ms-rename ()
         "Rename a workspace and get back to transient-state."
         (interactive)
