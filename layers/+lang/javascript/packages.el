ea3ce05dded9aa600a9fe6b0fc693fd32d66fb43
Andrew Cates <catesandrew@gmail.com>
2016-02-24 20:18:30 -0800

    fix typo for js2-imenu extras mode

--- layers/+lang/javascript/packages.el
+++ layers/+lang/javascript/packages.el
@@ -84,7 +84,7 @@
     :init
     (progn
       (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-      ;; required to make `<SPC> s l' to work correctly
+      ;; required to make `<leader> s j' or `<leacer> i j` to work correctly
       (add-hook 'js2-mode-hook 'js2-imenu-extras-mode))
     :config
     (progn
