db16c816dfac0f7a323d7f24e894b383e308376d
Damon Wang <dwang@janestreet.com>
2016-02-29 12:19:08 -0500

    Support plain-text gmail via chrome layer variable
    Those who use the chrome layer for gmail can now choose between rich-text
    composition (default) or plain-text composition. In plain-text mode, the buffer
    is de-htmlized before editing and re-htmlized afterward in a way compatible with
    the Plain-text editing mode of gmail's web interface.

--- layers/chrome/packages.el
+++ layers/chrome/packages.el
@@ -11,18 +11,41 @@
 
 (setq chrome-packages '(
                         edit-server
-                        gmail-message-mode
                         ))
 
 (defun chrome/init-edit-server ()
+  (message "init-edit-server" chrome-gmail-rich-text-format)
   (use-package edit-server
-    :init
-    (progn
-      (edit-server-start))
-    :config
-    (progn
-      (setq edit-server-default-major-mode 'markdown-mode))
-    ))
+    :init (edit-server-start)))
 
-(defun chrome/init-gmail-message-mode ( )
-  (use-package gmail-message-mode))
+;; gmail-message-mode autoloads code that drags the rest of the package in. So
+;; use-package's :if is actually useless, and if we don't want it to steal the
+;; composition buffer we need this outer cond to avoid ever reading in the file.
+(cond
+ (chrome-gmail-rich-text-format
+  (progn
+    (add-to-list 'chrome-packages 'gmail-message-mode)
+    (defun chrome/init-gmail-message-mode ()
+      (use-package gmail-message-mode
+        :if chrome-gmail-rich-text-format
+        :after edit-server
+        :init (setq edit-server-default-major-mode 'markdown-mode)))))
+ (t
+  (progn
+    (add-to-list 'chrome-packages 'edit-server-htmlize)
+    (defun chrome/init-edit-server-htmlize ()
+      (use-package edit-server-htmlize
+        :if (not chrome-gmail-rich-text-format)
+        :after edit-server
+        :init
+        (progn
+          (defun chrome/gmail-compose-buffer-p ()
+            (string-match-p "^mail\\.google\\.com/mail/u/" (buffer-name)))
+          (defun chrome/gmail-compose-dehtmlize ()
+            (when (chrome/gmail-compose-buffer-p)
+              (edit-server-dehtmlize-buffer)))
+          (defun chrome/gmail-compose-htmlize ()
+            (when (chrome/gmail-compose-buffer-p)
+              (edit-server-htmlize-buffer)))
+          (add-hook 'edit-server-start-hook 'chrome/gmail-compose-dehtmlize)
+          (add-hook 'edit-server-done-hook 'chrome/gmail-compose-htmlize)))))))
