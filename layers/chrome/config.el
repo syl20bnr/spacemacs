ed07559eacc8a20d7a6b51e843d9dcfa6b277f76
Damon Wang <dwang@janestreet.com>
2016-02-29 12:19:08 -0500

    Support plain-text gmail via chrome layer variable
    Those who use the chrome layer for gmail can now choose between rich-text
    composition (default) or plain-text composition. In plain-text mode, the buffer
    is de-htmlized before editing and re-htmlized afterward in a way compatible with
    the Plain-text editing mode of gmail's web interface.

---
+++ layers/chrome/config.el
@@ -0,0 +1,13 @@
+;;; packages.el --- Chrome Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Ben Hayden <hayden767@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar chrome-gmail-rich-text-format t
+  "Default message format in Gmail web interface.")
