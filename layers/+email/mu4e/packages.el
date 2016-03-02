c2843eaf9a5bc4633f2b7eadd9ea940d54670197
Allen Li <darkfeline@felesatra.moe>
2016-03-01 20:29:00 -0800

    Simplify completing-read selection

--- layers/+email/mu4e/packages.el
+++ layers/+email/mu4e/packages.el
@@ -51,10 +51,7 @@
                    (interactive)
                     (mu4e-view-mark-thread '(read))))
 
-      (setq mu4e-completing-read-function
-            (if (configuration-layer/layer-usedp 'spacemacs-ivy)
-                'ivy-completing-read
-              'helm--completing-read-default))
+      (setq mu4e-completing-read-function 'completing-read)
 
       (add-to-list 'mu4e-view-actions
                    '("View in browser" . mu4e-action-view-in-browser) t)
