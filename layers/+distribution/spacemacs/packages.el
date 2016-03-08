565566f2164cd29f5f9a64038dc6e9877298e49a
bmag <bmagamb@gmail.com>
2016-03-08 16:41:56 +0200

    Use window-numbering-assign-func for neotree number
    Using window-numbering-assign is simpler and more reliable than using
    window-numbering-before-hook and neo-after-create-hook.
    
    Duplicates older commit 2ff22934e706019e8e6d284e20520bfedb9a8bb7 which
    somehow didn't survive the splitting of spacemacs layer into smaller
    spacemacs-* layers.

--- layers/+distribution/spacemacs/packages.el
+++ layers/+distribution/spacemacs/packages.el
@@ -1897,15 +1897,16 @@ Open junk file using helm, with `prefix-arg' search in junk files"
         "9" 'select-window-9)
       (window-numbering-mode 1))
 
-    (defun spacemacs//window-numbering-assign (windows)
-      "Custom number assignment for special buffers."
-      (mapc (lambda (w)
-              (when (and (boundp 'neo-global--window)
-                         (eq w neo-global--window))
-                (window-numbering-assign w 0)))
-            windows))
-    (add-hook 'window-numbering-before-hook 'spacemacs//window-numbering-assign)
-    (add-hook 'neo-after-create-hook '(lambda (w) (window-numbering-update)))))
+    ;; make sure neotree is always 0
+    (defun spacemacs//window-numbering-assign ()
+      "Custom number assignment for neotree."
+      (when (and (boundp 'neo-buffer-name)
+                 (string= (buffer-name) neo-buffer-name))
+        0))
+    ;; using lambda to work-around a bug in window-numbering, see
+    ;; https://github.com/nschum/window-numbering.el/issues/10
+    (setq window-numbering-assign-func
+          (lambda () (spacemacs//window-numbering-assign)))))
 
 (defun spacemacs/init-volatile-highlights ()
   (use-package volatile-highlights
