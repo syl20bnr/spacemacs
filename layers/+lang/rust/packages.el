dab234dbd934dddcbabd59a3bbc9263838dd56eb
Steven Allen <steven@stebalien.com>
2016-03-14 17:01:06 -0400

    Use toml-mode for all cargo configs
    Specifically, `Cargo.lock` and `.cargo/config`.

--- layers/+lang/rust/packages.el
+++ layers/+lang/rust/packages.el
@@ -46,7 +46,7 @@
 
 (defun rust/init-toml-mode ()
   (use-package toml-mode
-    :defer t))
+    :mode "/\\(Cargo.lock\\|\\.cargo/config\\)\\'"))
 
 (when (configuration-layer/layer-usedp 'auto-completion)
   (defun rust/post-init-company ()
