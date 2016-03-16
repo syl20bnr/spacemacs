c92d2a992439b2500e7faffc71059b92823d33ab
Jack Henahan <jhenahan@me.com>
2016-03-16 15:04:57 -0400

    Clean up server config API

--- layers/+irc/erc/funcs.el
+++ layers/+irc/erc/funcs.el
@@ -1,19 +1,12 @@
-(defun erc//server (erc-func &rest args)
-  (funcall erc-func :server (plist-get args :server)
-           :nick (plist-get args :nick)
-           :port (plist-get args :port)
-           :password (plist-get args :password)))
-
-(defun erc/server (&rest args)
-  (apply #'erc//server
-         (if (plist-get args :ssl)
-             'erc-tls
-           'erc)
-         args))
-
 (defun erc//servers (server-list)
-  (dolist (a-server server-list)
-    (eval a-server)))
+  (dolist (s server-list)
+    (apply (if
+               (plist-get (cdr s) :ssl)
+               (progn
+                 (remf (cdr s) :ssl)
+                 'erc-tls)
+             'erc)
+           :server s)))
 
 (defun erc/default-servers ()
   (interactive)
