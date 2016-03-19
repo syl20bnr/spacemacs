63dd546e29cd9d7510e73f076885a231e9112f6c
JAremko <w3techplayground@gmail.com>
2016-03-19 15:45:40 +0200

    Add the new .org example and a formatting script
    Add ./doc-fmt/ folder that contains the .org example and
    the script(run.bash) that should be run from the repo root.
    Its purpose:
      - remove  #+HTML_HEAD_EXTRA: ... readtheorg.css" />
      - replace :TOC_4_org:noexport: with :TOC_4_gh:noexport:
      - apply the gh style TOC

---
+++ doc-fmt/run.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if ! [ -d "./.git" ]
+	then
+		echo "Should be executed from the repo root."
+		exit 1
+fi
+
+#rm #+HTML_HEAD_EXTRA: ... readtheorg.css" /> in the doc.
+find ./doc    -name "*.org" -type f -exec sed -i '/#+HTML_HEAD_EXTRA.*readtheorg.css.*/d' {} \;
+
+#rm #+HTML_HEAD_EXTRA: ... readtheorg.css" /> in the layers.
+find ./layers -name "*.org" -type f -exec sed -i '/#+HTML_HEAD_EXTRA.*readtheorg.css.*/d' {} \;
+
+
+
+#replace :TOC_4_org: with :TOC_4_gh: in the doc.
+find ./doc    -name "*.org" -type f -exec sed -i 's/:TOC_4_org:/:TOC_4_gh:/' {} \;
+
+#replace :TOC_4_org: with :TOC_4_gh: in the layers.
+find ./layers -name "*.org" -type f -exec sed -i 's/:TOC_4_org:/:TOC_4_gh:/' {} \;
+
+
+
+#apply toc-org to doc.
+find ./doc    -name "*.org" -type f -exec emacs -batch -l ./doc-fmt/toc-org-apply.el '{}' -f toc-apply \;
+
+#apply toc-org to layers.
+find ./layers -name "*.org" -type f -exec emacs -batch -l ./doc-fmt/toc-org-apply.el '{}' -f toc-apply \;
+
