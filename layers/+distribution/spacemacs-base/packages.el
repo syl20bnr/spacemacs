09098c09cfaa72f008793ef5607a4b24540d2370
sooheon <tngjs0@gmail.com>
2016-03-21 15:56:55 +0900

    Set popwin config for Help buffer :noselect t
    Fixes issue #5546. Does not change behavior when calling help directly,
    as with describe-function or describe-variable.

--- layers/+distribution/spacemacs-base/packages.el
+++ layers/+distribution/spacemacs-base/packages.el
@@ -463,7 +463,7 @@
       (setq popwin:special-display-config nil)
 
       ;; buffers that we manage
-      (push '("*Help*"                 :dedicated t :position bottom :stick t :noselect nil :height 0.4) popwin:special-display-config)
+      (push '("*Help*"                 :dedicated t :position bottom :stick t :noselect t   :height 0.4) popwin:special-display-config)
       (push '("*compilation*"          :dedicated t :position bottom :stick t :noselect t   :height 0.4) popwin:special-display-config)
       (push '("*Shell Command Output*" :dedicated t :position bottom :stick t :noselect nil            ) popwin:special-display-config)
       (push '("*Async Shell Command*"  :dedicated t :position bottom :stick t :noselect nil            ) popwin:special-display-config)
