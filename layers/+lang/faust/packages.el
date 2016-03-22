3bdcd428ffaf4ce2f6678cff37715a47e384e0fc
syl20bnr <sylvain.benner@gmail.com>
2016-03-21 20:33:35 -0400

    faust: refactor layer
    Add a funcs.el.
    Fix `use-package` usage.
    Modify key bindings to meet conventions.
    Prefix function with `spacemacs/`

--- layers/+lang/faust/packages.el
+++ layers/+lang/faust/packages.el
@@ -17,62 +17,28 @@
     faust-mode
     yasnippet))
 
+(defun faust/post-init-company ()
+  (spacemacs|add-company-hook faust-mode))
+
+;; Consider to move this package somewhere else if we use it for other languages.
+(defun faust/init-dumb-jump ()
+  (use-package dumb-jump
+    :defer t
+    :init
+    (progn
+      (add-hook 'faust-mode-hook 'dumb-jump-mode)
+      (spacemacs/set-leader-keys-for-major-mode 'faust-mode
+        "gb" 'dumb-jump-back
+        "gg" 'dumb-jump-go))))
+
 (defun faust/init-faust-mode ()
   (use-package faust-mode
     :defer t
-    :mode "\\.\\(dsp\\|lib\\)\\'")
-  (spacemacs/set-leader-keys-for-major-mode 'faust-mode
-    ;; Compile
-    "f" 'faust/faust-to-firefox
-    "j" 'faust/faust-to-jack
-    "q" 'faust/faust-to-jack-qt))
+    :mode "\\.\\(dsp\\|lib\\)\\'"
+    :init (spacemacs/set-leader-keys-for-major-mode 'faust-mode
+            "cf" 'spacemacs/faust-to-firefox
+            "cg" 'spacemacs/faust-to-jack-gtk
+            "cq" 'spacemacs/faust-to-jack-qt)))
 
-(defun faust/init-dumb-jump ()
-  ;; Load omnisharp-mode with csharp-mode, this should start the omnisharp server automatically
-  (add-hook 'faust-mode-hook 'dumb-jump-mode)
-  (use-package dumb-jump)
-  :defer t
-  (spacemacs/set-leader-keys-for-major-mode 'faust-mode
-    ;; Navigation
-    "g"   'dumb-jump-go
-    "b"   'dumb-jump-back))
-
-(when (configuration-layer/layer-usedp 'auto-completion)
-  (defun faust/post-init-company ()
-    (spacemacs|add-company-hook faust-mode)
-    (spacemacs/add-to-hooks
-     'spacemacs/load-yasnippet
-     '(faust-mode-hook))))
-
-(defun faust/faust-to-firefox ()
-  "compile a block-diagram and show it in the browser"
-  (interactive)
-  (set-process-sentinel
-   (start-process-shell-command "faust2svg" "faust-compile"
-                                (concat  "faust2svg " buffer-file-name))
-   'faust/faust2svg-sentinel))
-
-(defun faust/faust2svg-sentinel (process event)
-  "show block-diagram in browser"
-  (browse-url-of-file (concat  (file-name-sans-extension buffer-file-name) "-svg/process.svg")))
-
-(defun faust/faust-to-jack ()
-  "compile a jack-gtk program and run it"
-  (interactive)
-  (set-process-sentinel
-   (start-process-shell-command "faust2jack" "faust-compile"
-                                (concat  "faust2jack " buffer-file-name))
-   'faust/faust-run-sentinel))
-
-(defun faust/faust-to-jack-qt ()
-  "compile a jack-qt program and run it"
-  (interactive)
-  (set-process-sentinel
-   (start-process-shell-command "faust2jaqt" "faust-compile"
-                                (concat  "faust2jaqt " buffer-file-name))
-   'faust/faust-run-sentinel))
-
-(defun faust/faust-run-sentinel (process event)
-  "run the program"
-  (start-process-shell-command "faust-run" nil (file-name-sans-extension (buffer-file-name)))
-  (switch-to-buffer-other-window "faust-compile"))
+(defun faust/post-init-yasnippet ()
+  (add-hook 'faust-mode-hook 'spacemacs/load-yasnippet))
