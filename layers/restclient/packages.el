0248e384a2f6a210b9a8de6292ebb0e6a18072e4
syl20bnr <sylvain.benner@gmail.com>
2016-03-23 17:11:55 -0400

    restclient: install both ob-http and restclient
    The variable `restclient-use-org` now just set the auto mode alist it
    does not select which package to install.

--- layers/restclient/packages.el
+++ layers/restclient/packages.el
@@ -9,22 +9,28 @@
 ;;
 ;;; License: GPLv3
 (setq restclient-packages
-  '((ob-http :toggle restclient-use-org)
-    (restclient :toggle (null restclient-use-org))))
+      '(ob-http
+        restclient))
 
 (defun restclient/init-ob-http ()
   (use-package ob-http
-    :mode ("\\.http\\'" . org-mode)
-    :init (spacemacs|use-package-add-hook org
-            :post-config (add-to-list 'org-babel-load-languages '(http . t)))))
+    :init
+    (progn
+      (when restclient-use-org
+        (add-to-list 'auto-mode-alist '("\\.http\\'" . org-mode)))
+      (spacemacs|use-package-add-hook org
+        :post-config (add-to-list 'org-babel-load-languages '(http . t))))))
 
 (defun restclient/init-restclient ()
   (use-package restclient
-    :mode ("\\.http\\'" . restclient-mode)
     :defer t
-    :init (spacemacs/set-leader-keys-for-major-mode 'restclient-mode
-            "s" 'restclient-http-send-current-stay-in-window
-            "S" 'restclient-http-send-current
-            "r" 'restclient-http-send-current-raw-stay-in-window
-            "R" 'restclient-http-send-current-raw
-            "y" 'restclient-copy-curl-command)))
+    :init
+    (progn
+      (unless restclient-use-org
+        (add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode)))
+      (spacemacs/set-leader-keys-for-major-mode 'restclient-mode
+        "s" 'restclient-http-send-current-stay-in-window
+        "S" 'restclient-http-send-current
+        "r" 'restclient-http-send-current-raw-stay-in-window
+        "R" 'restclient-http-send-current-raw
+        "y" 'restclient-copy-curl-command))))
