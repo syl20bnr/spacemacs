81c276b0a36966e7ab0c413012444e76e22c381f
syl20bnr <sylvain.benner@gmail.com>
2016-03-23 10:17:46 -0400

    resclient: add ob-http / org: add support for org-babel languages
    Add layer variables `restclient-use-org` to use `ob-http` instead of
    `resctlient`
    
    Add support for easy addition of org-babel languages by appending to
    `org-babel-load-languages` alist in org post-config use-package hook.

--- layers/restclient/packages.el
+++ layers/restclient/packages.el
@@ -1,26 +1,30 @@
+;;; packages.el --- restclient Layer Packages File
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
 (setq restclient-packages
-  '(
-    restclient
-    )
-  )
+  '((ob-http :toggle restclient-use-org)
+    (restclient :toggle (null restclient-use-org))))
+
+(defun restclient/init-ob-http ()
+  (use-package ob-http
+    :mode ("\\.http\\'" . org-mode)
+    :init (spacemacs|use-package-add-hook org
+            :post-config (add-to-list 'org-babel-load-languages '(http . t)))))
 
 (defun restclient/init-restclient ()
   (use-package restclient
     :mode ("\\.http\\'" . restclient-mode)
     :defer t
-    :init
-    (progn
-
-      (defun restclient-http-send-current-raw-stay-in-window ()
-        (interactive)
-        (restclient-http-send-current t t))
-
-      (spacemacs/set-leader-keys-for-major-mode 'restclient-mode
-        "s" 'restclient-http-send-current-stay-in-window
-        "S" 'restclient-http-send-current
-        "r" 'restclient-http-send-current-raw-stay-in-window
-        "R" 'restclient-http-send-current-raw
-        "y" 'restclient-copy-curl-command
-        ))
-    )
-  )
+    :init (spacemacs/set-leader-keys-for-major-mode 'restclient-mode
+            "s" 'restclient-http-send-current-stay-in-window
+            "S" 'restclient-http-send-current
+            "r" 'restclient-http-send-current-raw-stay-in-window
+            "R" 'restclient-http-send-current-raw
+            "y" 'restclient-copy-curl-command)))
