986de34f90606b7177383f71c99be4cb362212aa
Channing Walton <channingwalton@mac.com>
2016-03-23 23:05:51 +0000

    optionally enable ensime
    a new variable, scala-auto-start-ensime, determines if ensime starts
    when a scala file is loaded.
    
    make scala-auto-start-ensime default to t
    
    The current behaviour is to autostart so this will preserve it.
    
    Adding documentation for scala-auto-start-ensime
    
    removed a space
    
    make scala-auto-start-ensime default to t
    
    The current behaviour is to autostart so this will preserve it.
    
    Adding documentation for scala-auto-start-ensime
    
    removed a space

--- layers/+lang/scala/packages.el
+++ layers/+lang/scala/packages.el
@@ -27,7 +27,8 @@
         (add-hook 'ensime-mode-hook 'scala/enable-eldoc))
       (add-hook 'scala-mode-hook 'scala/configure-flyspell)
       (add-hook 'scala-mode-hook 'scala/configure-ensime)
-      (add-hook 'scala-mode-hook 'scala/maybe-start-ensime))
+      (when scala-auto-start-ensime
+        (add-hook 'scala-mode-hook 'scala/maybe-start-ensime)))
     :config
     (progn
       (setq user-emacs-ensime-directory ".cache/ensime")
