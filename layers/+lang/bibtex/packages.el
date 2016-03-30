97aff9d8dba3dbdfcef8a647c6a6cfd0c2717d94
JP-Ellis <coujellis@gmail.com>
2016-03-30 19:44:30 +1100

    Add BibTeX layer.
    Integrates with org mode and LaTeX in order to manage citations.
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

---
+++ layers/+lang/bibtex/packages.el
@@ -0,0 +1,25 @@
+;;; packages.el --- BibTeX Layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Joshua Ellis <josh@jpellis.me>
+;; URL: https://github.com/JP-Ellis
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+(defconst bibtex-packages
+      '(
+        auctex
+        helm
+        org
+        org-ref
+        markdown-mode
+        ))
+
+;;; packages-config.el ends here
