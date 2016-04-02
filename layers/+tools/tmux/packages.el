bb50218622c1de6e12ecb50850e471908cb634be
Aaron Jensen <aaronjensen@gmail.com>
2016-04-02 11:16:37 -0700

    tmux: replace deprecated post-extensions with packages

--- layers/+tools/tmux/packages.el
+++ layers/+tools/tmux/packages.el
@@ -1,4 +1,14 @@
-(setq tmux-post-extensions '((tmux :location local)))
+;;; packages.el --- tmux Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+(setq tmux-packages '((tmux :location local)))
 
 (defun tmux/init-tmux ()
   "Initialize tmux"
