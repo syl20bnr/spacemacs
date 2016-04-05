0ec385c60c0822038a7c9976638b3cd0b00fd0a5
Fabien Dubosson <fabien.dubosson@gmail.com>
2016-04-05 13:35:01 +0200

    Fix misplaced keybinding definition for ranger

--- layers/+tools/ranger/packages.el
+++ layers/+tools/ranger/packages.el
@@ -13,11 +13,17 @@
 (setq ranger-packages '(dired
                         ranger))
 
+(defun ranger//set-leader-keys ()
+  (spacemacs/set-leader-keys
+    "ar" 'ranger
+    "ad" 'deer))
+
 (defun ranger/init-ranger ()
   (use-package ranger
     :defer t
     :init
     (progn
+      (ranger//set-leader-keys)
       ;; set up image-dired to allow picture resize
       (setq image-dired-dir (concat spacemacs-cache-directory "image-dir"))
       (unless (file-directory-p image-dired-dir)
@@ -26,6 +32,5 @@
     (define-key ranger-mode-map (kbd "-") 'ranger-up-directory)))
 
 (defun ranger/post-init-dired ()
-  (spacemacs/set-leader-keys
-    "ar" 'ranger
-    "ad" 'deer))
+  ;; Be sure to override dired bindings
+  (ranger//set-leader-keys))
