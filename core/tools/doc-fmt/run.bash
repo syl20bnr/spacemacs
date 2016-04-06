b42cba0417315e83f0e3f3d7df1f89cc44a2bf81
JAremko <w3techplayground@gmail.com>
2016-04-06 22:56:16 +0300

    Upgrade doc-fmt
    Add test mode: {repo-root}/core/tools/doc-fmt/run.bash test
      applies formatting only to {repo-root}/core/tools/doc-fmt/test.org
    
    Use "gsed" if present.
    
    Add new filters, refactor code.
    
    test.org now represents most of the .org file formatting errors.

--- core/tools/doc-fmt/run.bash
+++ core/tools/doc-fmt/run.bash
@@ -6,24 +6,31 @@ if ! [ -d "./.git" ]
 		exit 1
 fi
 
-#rm #+HTML_HEAD_EXTRA: ... readtheorg.css" /> in the doc.
-find ./doc    -name "*.org" -type f -exec gsed -i '/#+HTML_HEAD_EXTRA.*readtheorg.css.*/d' {} \;
+#Use "sed" or "gsed" if avaliable.
+seder="sed"
+if hash gsed 2>/dev/null; then
+    $seder="gsed"
+fi
 
-#rm #+HTML_HEAD_EXTRA: ... readtheorg.css" /> in the layers.
-find ./layers -name "*.org" -type f -exec gsed -i '/#+HTML_HEAD_EXTRA.*readtheorg.css.*/d' {} \;
+if [ $1 = "test" ]
+  then
+    places=("./core/tools/doc-fmt")
+  else
+    places=("./doc" "./layers")
+fi
 
-
-
-#replace :TOC_4_org: with :TOC_4_gh: in the doc.
-find ./doc    -name "*.org" -type f -exec gsed -i 's/:TOC_4_org:/:TOC_4_gh:/' {} \;
-
-#replace :TOC_4_org: with :TOC_4_gh: in the layers.
-find ./layers -name "*.org" -type f -exec gsed -i 's/:TOC_4_org:/:TOC_4_gh:/' {} \;
-
-
-
-#apply toc-org to doc.
-find ./doc    -name "*.org" -type f -exec emacs -batch -l ./core/tools/doc-fmt/toc-org-apply.el '{}' -f toc-apply \;
-
-#apply toc-org to layers.
-find ./layers -name "*.org" -type f -exec emacs -batch -l ./core/tools/doc-fmt/toc-org-apply.el '{}' -f toc-apply \;
+for place in "${places[@]}"
+do :
+   #Remove trailing delimiters in headlines
+   find $place -name "*.org" -type f -exec $seder -i 's/^\(*\+\s\+.*\)[;,.]$/\1/g' {} \;
+   #Remove trailing spaces
+   find $place -name "*.org" -type f -exec $seder -i 's/[ \t]*$//' {} \;
+   #Remove #+HTML_HEAD_EXTRA: ... readtheorg.css" />
+   find $place -name "*.org" -type f -exec $seder -i '/#+HTML_HEAD_EXTRA.*readtheorg.css.*/d' {} \;
+   #Replace multiply empty lines with a single empty line
+   find $place -name "*.org" -type f -exec $seder -i '/^$/N;/^\n$/D' {} \;
+   #Replace :TOC_4_org: with :TOC_4_gh:
+   find $place -name "*.org" -type f -exec $seder -i 's/:TOC_4_org:/:TOC_4_gh:/' {} \;
+   #apply toc-org
+   find $place -name "*.org" -type f -exec emacs -batch -l ./core/tools/doc-fmt/fmt.el '{}' -f apply-all \;
+done
