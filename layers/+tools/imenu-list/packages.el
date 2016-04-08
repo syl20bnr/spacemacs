5752c87ac0b0455032505b94016964ee4d605d5f
syl20bnr <sylvain.benner@gmail.com>
2016-04-08 18:20:50 -0400

    imenu-list: exclude IList buffer from golden-ratio

--- layers/+tools/imenu-list/packages.el
+++ layers/+tools/imenu-list/packages.el
@@ -1,7 +1,6 @@
 ;;; packages.el --- imenu-list Layer packages File for Spacemacs
 ;;
-;; Copyright (c) 2012-2014 Sylvain Benner
-;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
 ;;
 ;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
@@ -10,12 +9,16 @@
 ;;
 ;;; License: GPLv3
 
-;; List of all packages to install and/or initialize. Built-in packages
-;; which require an initialization must be listed explicitly in the list.
-(setq imenu-list-packages '(imenu-list))
+(defconst imenu-list-packages
+  '(
+    golden-ratio
+    imenu-list
+    ))
 
-;; List of packages to exclude.
-(setq imenu-list-excluded-packages '())
+(defun imenu-list/pre-init-golden-ratio ()
+  (spacemacs|use-package-add-hook golden-ratio
+    :post-config
+    (add-to-list 'golden-ratio-exclude-buffer-regexp "^\\*Ilist\\*")))
 
 (defun imenu-list/init-imenu-list ()
   (use-package imenu-list
