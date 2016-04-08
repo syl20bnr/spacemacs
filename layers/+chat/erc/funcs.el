9e1909f5e0513bee51a85a79b3047179e4a7b45e
Eivind Fonn <evfonn@gmail.com>
2016-04-08 18:03:38 +0200

    Move erc/funcs.el to new folder, add header

--- layers/+chat/erc/funcs.el
+++ layers/+chat/erc/funcs.el
@@ -1,3 +1,14 @@
+;;; funcs.el --- Spacemacs ERC Layer functions File
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
 (defun erc//servers (server-list)
   (dolist (s server-list)
     (apply (if
