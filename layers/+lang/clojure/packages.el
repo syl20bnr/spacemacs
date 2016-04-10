4c543ee25c5bf97597924bfa66dea3b78909e8aa
sooheon <tngjs0@gmail.com>
2016-04-11 00:54:58 +0900

    Remove depreciated cider-turn-on-eldoc-mode
    Use more general 'eldoc-mode instead. Also enable eldoc-mode in the
    repl as well.

--- layers/+lang/clojure/packages.el
+++ layers/+lang/clojure/packages.el
@@ -10,7 +10,6 @@
     subword
     ))
 
-
 (defun clojure/init-cider ()
   (use-package cider
     :defer t
@@ -24,6 +23,7 @@
       (push "\\*cider-repl\.\+\\*" spacemacs-useful-buffers-regexp)
       (add-hook 'clojure-mode-hook 'cider-mode)
       (add-hook 'cider-mode-hook 'eldoc-mode)
+      (add-hook 'cider-repl-mode-hook 'eldoc-mode)
       (if dotspacemacs-smartparens-strict-mode
           (add-hook 'cider-repl-mode-hook #'smartparens-strict-mode)))
     :config
