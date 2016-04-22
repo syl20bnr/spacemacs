f3639da09e10de342f3e25a2f1d569ca914342e9
syl20bnr <sylvain.benner@gmail.com>
2016-04-21 22:22:02 -0400

    cfengine: reformat readme and various fixes
    - Simplify README.org to follow the conventions and better integrate
    with the other READMEs
    - Remove unnecessary comments in code
    - Add explicitly cfengine3-mode to the list of package
    - Move eldoc hook to an eldoc post init function
    - Defer loading of cfengine3-mode
    - Move auto-mode-alist to init function

--- layers/+config-files/cfengine/config.el
+++ layers/+config-files/cfengine/config.el
@@ -1,15 +1,12 @@
-;; Figure out which cfengine mode (cfengine2 or cfengine3) to use for =.cf=
-;; files based on file content.
-;; (add-to-list 'auto-mode-alist '("\\.cf\\'" . cfengine-auto-mode))
-(add-to-list 'auto-mode-alist '("\\.cf\\'" . cfengine3-mode))
+;;; config.el --- CFEgine Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Nick Anderson <nick@cmdln.org>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
 
-;; Perhaps we should bind this to something some day.
-;; (global-set-key [(control f4)] 'cfengine3-reformat-json-string)
-
-;; Enable eldoc-mode for cfengine
-;; This puts function prototypes into the minibuffer when hovering
-;; over the function.
-(add-hook 'cfengine3-mode-hook 'eldoc-mode)
-
-;; Enable auto completion for cfengine
 (spacemacs|defvar-company-backends cfengine3-mode)
--- layers/+config-files/cfengine/packages.el
+++ layers/+config-files/cfengine/packages.el
@@ -9,39 +9,26 @@
 ;;
 ;;; License: GPLv3
 
-;;; Commentary:
+(defconst cfengine-packages
+  '(
+    (cfengine3-mode :location built-in)
+    company
+    eldoc
+    flycheck
+    ))
 
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `cfengine-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `cfengine/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `cfengine/pre-init-PACKAGE' and/or
-;;   `cfengine/post-init-PACKAGE' to customize the package as it is loaded.
-
-(setq cfengine-packages
-      '(
-	flycheck
-	company))
-	;; cfengine3-mode is included in emacs itself.
-
-(defun cfengine/init-cfengine()
+(defun cfengine/init-cfengine3-mode ()
   (use-package cfengine3-mode
-    :init
-      (progn
-        (spacemacs/set-leader-keys-for-major-mode 'cfengine3-mode
-          "j" 'cfengine3-reformat-json-string))))
+    :defer t
+    :mode ("\\.cf\\'" . cfengine3-mode)
+    :init (spacemacs/set-leader-keys-for-major-mode 'cfengine3-mode
+            "j" 'cfengine3-reformat-json-string)))
 
 (defun cfengine/post-init-company ()
   (spacemacs|add-company-hook cfengine3-mode))
+
+(defun cfengine/post-init-eldoc ()
+  (add-hook 'cfengine3-mode-hook 'eldoc-mode))
 
 (defun cfengine/post-init-flycheck ()
   (spacemacs/add-flycheck-hook 'cfengine3-mode-hook))
