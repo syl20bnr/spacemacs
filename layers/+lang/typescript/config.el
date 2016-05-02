75754a3a9e537bea127d4b1581254aee4227cb22
JAremko <w3techplayground@gmail.com>
2016-05-02 12:11:16 +0300

    Make tide formatter default

--- layers/+lang/typescript/config.el
+++ layers/+lang/typescript/config.el
@@ -3,3 +3,9 @@
 
 (defvar typescript-fmt-on-save nil
   "Run formatter on buffer save.")
+
+(defvar typescript-fmt-tool 'tide
+  "The name of the tool to be used
+for TypeScript source code formatting.
+Currently avaliable 'tide (default)
+and 'typescript-formatter .")
