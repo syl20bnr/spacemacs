a114c88bf122a597af8abf276cd7a8e6579a0a7e
Eivind Fonn <evfonn@gmail.com>
2016-05-05 12:58:45 +0200

    Fix indentation in syntax-checking/packages.el

--- layers/+checkers/syntax-checking/packages.el
+++ layers/+checkers/syntax-checking/packages.el
@@ -58,19 +58,19 @@
                   #b00000000)))
 
       (flycheck-define-error-level 'error
- 	:severity 2
+        :severity 2
         :overlay-category 'flycheck-error-overlay
         :fringe-bitmap 'my-flycheck-fringe-indicator
         :fringe-face 'flycheck-fringe-error)
 
       (flycheck-define-error-level 'warning
-	:severity 1
+        :severity 1
         :overlay-category 'flycheck-warning-overlay
         :fringe-bitmap 'my-flycheck-fringe-indicator
         :fringe-face 'flycheck-fringe-warning)
 
       (flycheck-define-error-level 'info
-	:severity 0
+        :severity 0
         :overlay-category 'flycheck-info-overlay
         :fringe-bitmap 'my-flycheck-fringe-indicator
         :fringe-face 'flycheck-fringe-info)
