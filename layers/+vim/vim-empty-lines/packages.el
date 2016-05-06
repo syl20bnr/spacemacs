d0452a861a2e491cd831001a3791c0cd66d9ea60
sooheon <tngjs0@gmail.com>
2016-05-07 01:11:42 +0900

    Disable vim-empty-lines in unnecessary buffers
    *Messages* and which-key--buffer do not need empty line indicators.
    
    Also break up an unnecessarily long line.

--- layers/+vim/vim-empty-lines/packages.el
+++ layers/+vim/vim-empty-lines/packages.el
@@ -8,11 +8,12 @@
   (use-package vim-empty-lines-mode
     :diminish vim-empty-lines-mode
     :init
-    (spacemacs/add-to-hooks (lambda () (vim-empty-lines-mode -1)) '(comint-mode-hook
-                                                                    eshell-mode-hook
-                                                                    eww-mode-hook
-                                                                    shell-mode-hook
-                                                                    term-mode-hook))
+    (spacemacs/add-to-hooks (lambda () (vim-empty-lines-mode -1))
+                            '(comint-mode-hook
+                              eshell-mode-hook
+                              eww-mode-hook
+                              shell-mode-hook
+                              term-mode-hook))
     :config
     (progn
       (global-vim-empty-lines-mode)
@@ -21,9 +22,10 @@
         :documentation
         "Display an overlay of ~ on empty lines."
         :evil-leader "t~")
-      ;; don't enable it on spacemacs home buffer
-      (with-current-buffer  "*spacemacs*"
-        (vim-empty-lines-mode -1))
+      ;; Don't enable it where it is detrimental.
+      (with-current-buffer "*spacemacs*" (vim-empty-lines-mode -1))
+      (with-current-buffer "*Messages*" (vim-empty-lines-mode -1))
+      (with-current-buffer which-key--buffer (vim-empty-lines-mode -1))
       ;; after a major mode is loaded, check if the buffer is read only
       ;; if so, disable vim-empty-lines-mode
       (add-hook 'after-change-major-mode-hook (lambda ()
