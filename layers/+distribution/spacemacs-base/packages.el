3f2bdcc03b4846f998dc7effb44b9d10aa524f7a
syl20bnr <sylvain.benner@gmail.com>
2016-05-14 11:46:38 -0400

    Move restart-emacs to spacemacs-ui

--- layers/+distribution/spacemacs-base/packages.el
+++ layers/+distribution/spacemacs-base/packages.el
@@ -38,7 +38,6 @@
         projectile
         (recentf :location built-in)
         request
-        restart-emacs
         (savehist :location built-in)
         (saveplace :location built-in)
         spacemacs-theme
@@ -531,51 +530,6 @@
 (defun spacemacs-base/init-request ()
   (setq request-storage-directory (concat spacemacs-cache-directory
                                           "request/")))
-
-(defun spacemacs-base/init-restart-emacs()
-  (use-package restart-emacs
-    :defer t
-    :init
-    (defun spacemacs/restart-emacs (&optional args)
-      "Restart emacs."
-      (interactive)
-      (setq spacemacs-really-kill-emacs t)
-      (restart-emacs args))
-    (defun spacemacs/restart-emacs-resume-layouts (&optional args)
-      "Restart emacs and resume layouts."
-      (interactive)
-      (spacemacs/restart-emacs (cons "--resume-layouts" args)))
-    (defun spacemacs/restart-emacs-debug-init (&optional args)
-      "Restart emacs and enable debug-init."
-      (interactive)
-      (spacemacs/restart-emacs (cons "--debug-init" args)))
-    (defun spacemacs/restart-stock-emacs-with-packages (packages &optional args)
-      "Restart emacs without the spacemacs configuration, enable
-debug-init and load the given list of packages."
-      (interactive
-       (progn
-         (unless package--initialized
-           (package-initialize t))
-         (let ((packages (append (mapcar 'car package-alist)
-                                 (mapcar 'car package-archive-contents)
-                                 (mapcar 'car package--builtins))))
-           (setq packages (mapcar 'symbol-name packages))
-           (let ((val (completing-read-multiple "Packages to load (comma separated): "
-                                                packages nil t)))
-             `(,val)))))
-      (let ((load-packages-string (mapconcat (lambda (pkg) (format "(use-package %s)" pkg))
-                                             packages " ")))
-        (spacemacs/restart-emacs-debug-init
-         (append (list "-q" "--execute"
-                       (concat "(progn (package-initialize) "
-                               "(require 'use-package)"
-                               load-packages-string ")"))
-                 args))))
-    (spacemacs/set-leader-keys
-      "qd" 'spacemacs/restart-emacs-debug-init
-      "qD" 'spacemacs/restart-stock-emacs-with-packages
-      "qr" 'spacemacs/restart-emacs-resume-layouts
-      "qR" 'spacemacs/restart-emacs)))
 
 (defun spacemacs-base/init-savehist ()
   (use-package savehist
--- layers/+spacemacs/spacemacs-ui/packages.el
+++ layers/+spacemacs/spacemacs-ui/packages.el
@@ -19,6 +19,7 @@
         flx-ido
         info+
         open-junk-file
+        restart-emacs
         window-numbering))
 
 ;; Paradox from MELPA is not compatible with 24.3, so we use
@@ -251,6 +252,51 @@ on whether the spacemacs-ivy layer is used or not, with
         "o" 'paradox-menu-visit-homepage)
       (spacemacs/set-leader-keys
         "ak" 'spacemacs/paradox-list-packages))))
+
+(defun spacemacs-ui/init-restart-emacs()
+  (use-package restart-emacs
+    :defer t
+    :init
+    (defun spacemacs/restart-emacs (&optional args)
+      "Restart emacs."
+      (interactive)
+      (setq spacemacs-really-kill-emacs t)
+      (restart-emacs args))
+    (defun spacemacs/restart-emacs-resume-layouts (&optional args)
+      "Restart emacs and resume layouts."
+      (interactive)
+      (spacemacs/restart-emacs (cons "--resume-layouts" args)))
+    (defun spacemacs/restart-emacs-debug-init (&optional args)
+      "Restart emacs and enable debug-init."
+      (interactive)
+      (spacemacs/restart-emacs (cons "--debug-init" args)))
+    (defun spacemacs/restart-stock-emacs-with-packages (packages &optional args)
+      "Restart emacs without the spacemacs configuration, enable
+debug-init and load the given list of packages."
+      (interactive
+       (progn
+         (unless package--initialized
+           (package-initialize t))
+         (let ((packages (append (mapcar 'car package-alist)
+                                 (mapcar 'car package-archive-contents)
+                                 (mapcar 'car package--builtins))))
+           (setq packages (mapcar 'symbol-name packages))
+           (let ((val (completing-read-multiple "Packages to load (comma separated): "
+                                                packages nil t)))
+             `(,val)))))
+      (let ((load-packages-string (mapconcat (lambda (pkg) (format "(use-package %s)" pkg))
+                                             packages " ")))
+        (spacemacs/restart-emacs-debug-init
+         (append (list "-q" "--execute"
+                       (concat "(progn (package-initialize) "
+                               "(require 'use-package)"
+                               load-packages-string ")"))
+                 args))))
+    (spacemacs/set-leader-keys
+      "qd" 'spacemacs/restart-emacs-debug-init
+      "qD" 'spacemacs/restart-stock-emacs-with-packages
+      "qr" 'spacemacs/restart-emacs-resume-layouts
+      "qR" 'spacemacs/restart-emacs)))
 
 (defun spacemacs-ui/init-window-numbering ()
   (use-package window-numbering
