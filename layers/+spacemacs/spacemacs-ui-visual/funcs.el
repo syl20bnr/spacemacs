0b84b1be66f8cb0346242986adfc5fe34448580a
syl20bnr <sylvain.benner@gmail.com>
2016-05-19 20:21:30 -0400

    Fix errors with last revert and move neotree funcs

--- layers/+spacemacs/spacemacs-ui-visual/funcs.el
+++ layers/+spacemacs/spacemacs-ui-visual/funcs.el
@@ -9,6 +9,59 @@
 ;;
 ;;; License: GPLv3
 
+
+;; neotree
+
+(defun spacemacs/neotree-expand-or-open ()
+  "Collapse a neotree node."
+  (interactive)
+  (let ((node (neo-buffer--get-filename-current-line)))
+    (when node
+      (if (file-directory-p node)
+          (progn
+            (neo-buffer--set-expand node t)
+            (neo-buffer--refresh t)
+            (when neo-auto-indent-point
+              (next-line)
+              (neo-point-auto-indent)))
+        (call-interactively 'neotree-enter)))))
+
+(defun spacemacs/neotree-collapse ()
+  "Collapse a neotree node."
+  (interactive)
+  (let ((node (neo-buffer--get-filename-current-line)))
+    (when node
+      (when (file-directory-p node)
+        (neo-buffer--set-expand node nil)
+        (neo-buffer--refresh t))
+      (when neo-auto-indent-point
+        (neo-point-auto-indent)))))
+
+(defun spacemacs/neotree-collapse-or-up ()
+  "Collapse an expanded directory node or go to the parent node."
+  (interactive)
+  (let ((node (neo-buffer--get-filename-current-line)))
+    (when node
+      (if (file-directory-p node)
+          (if (neo-buffer--expanded-node-p node)
+              (spacemacs/neotree-collapse)
+            (neotree-select-up-node))
+        (neotree-select-up-node)))))
+
+(defun neotree-find-project-root ()
+  (interactive)
+  (if (neo-global--window-exists-p)
+      (neotree-hide)
+    (let ((origin-buffer-file-name (buffer-file-name)))
+      (neotree-find (projectile-project-root))
+      (neotree-find origin-buffer-file-name))))
+
+(defun spacemacs//neotree-maybe-attach-window ()
+  (when (get-buffer-window (neo-global--get-buffer))
+    (neo-global--attach)))
+
+
+;; vi-tilde-fringe
 
 (defun spacemacs/disable-vi-tilde-fringe ()
   "Disable `vi-tilde-fringe' in the current buffer."
--- layers/+spacemacs/spacemacs-ui-visual/packages.el
+++ layers/+spacemacs/spacemacs-ui-visual/packages.el
@@ -185,54 +185,6 @@
             neo-modern-sidebar t
             neo-vc-integration nil)
 
-      (defun spacemacs/neotree-expand-or-open ()
-        "Collapse a neotree node."
-        (interactive)
-        (let ((node (neo-buffer--get-filename-current-line)))
-          (when node
-            (if (file-directory-p node)
-                (progn
-                  (neo-buffer--set-expand node t)
-                  (neo-buffer--refresh t)
-                  (when neo-auto-indent-point
-                    (next-line)
-                    (neo-point-auto-indent)))
-              (call-interactively 'neotree-enter)))))
-
-      (defun spacemacs/neotree-collapse ()
-        "Collapse a neotree node."
-        (interactive)
-        (let ((node (neo-buffer--get-filename-current-line)))
-          (when node
-            (when (file-directory-p node)
-              (neo-buffer--set-expand node nil)
-              (neo-buffer--refresh t))
-            (when neo-auto-indent-point
-              (neo-point-auto-indent)))))
-
-      (defun spacemacs/neotree-collapse-or-up ()
-        "Collapse an expanded directory node or go to the parent node."
-        (interactive)
-        (let ((node (neo-buffer--get-filename-current-line)))
-          (when node
-            (if (file-directory-p node)
-                (if (neo-buffer--expanded-node-p node)
-                    (spacemacs/neotree-collapse)
-                  (neotree-select-up-node))
-              (neotree-select-up-node)))))
-
-      (defun neotree-find-project-root ()
-        (interactive)
-        (if (neo-global--window-exists-p)
-            (neotree-hide)
-          (let ((origin-buffer-file-name (buffer-file-name)))
-            (neotree-find (projectile-project-root))
-            (neotree-find origin-buffer-file-name))))
-
-      (defun spacemacs//neotree-maybe-attach-window ()
-        (when (get-buffer-window (neo-global--get-buffer))
-          (neo-global--attach)))
-
       (defun spacemacs//neotree-key-bindings ()
         "Set the key bindings for a neotree buffer."
         (evilified-state-evilify-map neotree-mode-map
@@ -259,7 +211,8 @@
 
       (spacemacs/set-leader-keys
         "ft" 'neotree-toggle
-        "pt" 'neotree-find-project-root))
+        "pt" 'neotree-find-project-root))))
+
 (defun spacemacs-ui-visual/init-popup ())
 
 (defun spacemacs-ui-visual/init-popwin ()
