d44a73fe7177b62a70d05dd7c2ce8b6e4cf67de7
Nikola Henezi <nikola@henezi.com>
2016-06-05 09:14:04 +0200

    Fixing dash layer variable names.
    As reported in #5694 dash layer cannot be configured according to the
    documentation. This commit fixes that, and allows you to actually define docset
    path with `helm-dash-docset-newpath`. It also defines more sensible default for
    docset `~/.docsets`, which is the default path for helm-dash.
    
    Fixes #5694

--- layers/+tools/dash/packages.el
+++ layers/+tools/dash/packages.el
@@ -25,7 +25,7 @@
             helm-dash-common-docsets (helm-dash-installed-docsets))
       (message (format "activated %d docsets from: %s"
                        (length helm-dash-common-docsets) path)))
-    (dash//activate-package-docsets dash-helm-dash-docset-path)))
+    (dash//activate-package-docsets helm-dash-docset-newpath)))
 
 (defun dash/init-dash-at-point ()
   (use-package dash-at-point
