8f65b1b8be87fe836c23bcf2400ff5e8b8fa4646
syl20bnr <sylvain.benner@gmail.com>
2016-06-06 22:19:14 -0400

    ruby: document previous commit and move function to funcs.el

---
+++ layers/+lang/ruby/funcs.el
@@ -0,0 +1,16 @@
+;;; funcs.el --- Ruby Layer functions File
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defun ruby/rspec-verify-directory (dir)
+  "Launch tests in DIR directory.
+Called interactively it prompts for a directory."
+  (interactive "Drspec directory: ")
+  (rspec-run-single-file dir (rspec-core-options)))
--- layers/+lang/ruby/packages.el
+++ layers/+lang/ruby/packages.el
@@ -148,9 +148,6 @@
     ;; setup the hook via an autoload
     :config
     (progn
-      (defun ruby/rspec-verify-directory (dir)
-        (interactive "Drspec directory: ")
-        (rspec-run-single-file dir (rspec-core-options)))
       (spacemacs|hide-lighter rspec-mode)
       (dolist (mode '(ruby-mode enh-ruby-mode))
         (spacemacs/set-leader-keys-for-major-mode mode
