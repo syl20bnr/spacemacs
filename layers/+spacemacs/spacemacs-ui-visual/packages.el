89d5aad0a5f653a375c8cec23eab329e8247b6e3
syl20bnr <sylvain.benner@gmail.com>
2016-06-08 10:06:02 -0400

    Various tweaks to golden-ratio configuration
    - Prefer add-to-list function to alter golden-ratio variables so the lists
    are less likely to grows as users reload their spacemacs config.
    - Move some config to layers when appropriate
    - Sort alphabetically the values
    - Remove some duplicates in exclude-modes

--- layers/+spacemacs/spacemacs-ui-visual/packages.el
+++ layers/+spacemacs/spacemacs-ui-visual/packages.el
@@ -64,79 +64,75 @@
       :evil-leader "tg")
     :config
     (progn
-      (setq golden-ratio-exclude-modes '("bs-mode"
-                                         "calc-mode"
-                                         "ediff-mode"
-                                         "dired-mode"
-                                         "gud-mode"
-                                         "gdb-locals-mode"
-                                         "gdb-registers-mode"
-                                         "gdb-breakpoints-mode"
-                                         "gdb-threads-mode"
-                                         "gdb-frames-mode"
-                                         "gdb-inferior-io-mode"
-                                         "gud-mode"
-                                         "gdb-inferior-io-mode"
-                                         "gdb-disassembly-mode"
-                                         "gdb-memory-mode"
-                                         "restclient-mode"
-                                         "speedbar-mode"
-                                         ))
+      ;; golden-ratio-exclude-modes
+      (dolist (m '("bs-mode"
+                   "calc-mode"
+                   "ediff-mode"
+                   "dired-mode"
+                   "gud-mode"
+                   "gdb-locals-mode"
+                   "gdb-registers-mode"
+                   "gdb-breakpoints-mode"
+                   "gdb-threads-mode"
+                   "gdb-frames-mode"
+                   "gdb-inferior-io-mode"
+                   "gdb-disassembly-mode"
+                   "gdb-memory-mode"
+                   "speedbar-mode"
+                   ))
+        (add-to-list 'golden-ratio-exclude-modes m))
 
       (add-to-list 'golden-ratio-exclude-buffer-regexp "^\\*[hH]elm.*")
 
-      (setq golden-ratio-extra-commands
-            (append golden-ratio-extra-commands
-                    '(ace-window
-                      ace-delete-window
-                      ace-select-window
-                      ace-swap-window
-                      ace-maximize-window
-                      avy-pop-mark
-                      evil-avy-goto-word-or-subword-1
-                      evil-avy-goto-line
-                      windmove-left
-                      windmove-right
-                      windmove-up
-                      windmove-down
-                      quit-window
-                      evil-window-delete
-                      evil-window-split
-                      evil-window-vsplit
-                      evil-window-left
-                      evil-window-right
-                      evil-window-up
-                      evil-window-down
-                      evil-window-bottom-right
-                      evil-window-top-left
-                      evil-window-mru
-                      evil-window-next
-                      evil-window-prev
-                      evil-window-new
-                      evil-window-vnew
-                      evil-window-rotate-upwards
-                      evil-window-rotate-downwards
-                      evil-window-move-very-top
-                      evil-window-move-far-left
-                      evil-window-move-far-right
-                      evil-window-move-very-bottom
-                      select-window-0
-                      select-window-1
-                      select-window-2
-                      select-window-3
-                      select-window-4
-                      select-window-5
-                      select-window-6
-                      select-window-7
-                      select-window-8
-                      select-window-9
-                      buf-move-left
-                      buf-move-right
-                      buf-move-up
-                      buf-move-down
-                      ess-eval-buffer-and-go
-                      ess-eval-function-and-go
-                      ess-eval-line-and-go)))
+      ;; golden-ratio-extra-commands
+      (dolist (f '(ace-window
+                   ace-delete-window
+                   ace-select-window
+                   ace-swap-window
+                   ace-maximize-window
+                   avy-pop-mark
+                   buf-move-left
+                   buf-move-right
+                   buf-move-up
+                   buf-move-down
+                   evil-avy-goto-word-or-subword-1
+                   evil-avy-goto-line
+                   evil-window-delete
+                   evil-window-split
+                   evil-window-vsplit
+                   evil-window-left
+                   evil-window-right
+                   evil-window-up
+                   evil-window-down
+                   evil-window-bottom-right
+                   evil-window-top-left
+                   evil-window-mru
+                   evil-window-next
+                   evil-window-prev
+                   evil-window-new
+                   evil-window-vnew
+                   evil-window-rotate-upwards
+                   evil-window-rotate-downwards
+                   evil-window-move-very-top
+                   evil-window-move-far-left
+                   evil-window-move-far-right
+                   evil-window-move-very-bottom
+                   quit-window
+                   select-window-0
+                   select-window-1
+                   select-window-2
+                   select-window-3
+                   select-window-4
+                   select-window-5
+                   select-window-6
+                   select-window-7
+                   select-window-8
+                   select-window-9
+                   windmove-left
+                   windmove-right
+                   windmove-up
+                   windmove-down))
+        (add-to-list 'golden-ratio-extra-commands f))
 
       ;; Disable auto-resizing for some buffers
       (defun spacemacs/no-golden-ratio-for-buffers (bufname)
