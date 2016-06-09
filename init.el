0283f6475b4207ff4f2f217322075eacd02030fc
syl20bnr <sylvain.benner@gmail.com>
2016-06-08 23:18:55 -0400

    Release note for 0.105.21 and bump version

--- init.el
+++ init.el
@@ -13,7 +13,8 @@
 ;; (package-initialize)
 
 (setq gc-cons-threshold 100000000)
-(defconst spacemacs-version         "0.105.20" "Spacemacs version.")
+
+(defconst spacemacs-version         "0.105.21" "Spacemacs version.")
 (defconst spacemacs-emacs-min-version   "24.3" "Minimal version of Emacs.")
 
 (if (not (version<= spacemacs-emacs-min-version emacs-version))
