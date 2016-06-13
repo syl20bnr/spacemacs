c073d248ace11c92a5bb83d0cd97eda4c34e55d2
Tommi Komulainen <tommi.komulainen@iki.fi>
2016-06-13 22:43:38 +0200

    restclient: add key bindings for jump to next / previous query
    `gj`       jump to next query
    `gk`       jump to previous query
    `SPC m n`  jump to next query
    `SPC m p`  jump to previous query

--- layers/+tools/restclient/packages.el
+++ layers/+tools/restclient/packages.el
@@ -38,11 +38,16 @@
       (unless restclient-use-org
         (add-to-list 'auto-mode-alist '("\\.http\\'" . restclient-mode)))
       (spacemacs/set-leader-keys-for-major-mode 'restclient-mode
+        "n" 'restclient-jump-next
+        "p" 'restclient-jump-prev
         "s" 'restclient-http-send-current-stay-in-window
         "S" 'restclient-http-send-current
         "r" 'restclient-http-send-current-raw-stay-in-window
         "R" 'restclient-http-send-current-raw
-        "y" 'restclient-copy-curl-command))))
+        "y" 'restclient-copy-curl-command)
+      (evil-define-key 'normal restclient-mode-map
+        "gj" 'restclient-jump-next
+        "gk" 'restclient-jump-prev))))
 
 (defun restclient/post-init-company ()
   (spacemacs|add-company-hook restclient-mode))
