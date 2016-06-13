ad72dab624af86e0261ca65af59eb6e5e2186659
syl20bnr <sylvain.benner@gmail.com>
2016-06-12 23:57:34 -0400

    Move pcre2el key bindings to SPC x r prefix
    Also document it.
    Move everything to spacemacs-base since the new prefix is not as invasive

--- layers/+distributions/spacemacs-base/packages.el
+++ layers/+distributions/spacemacs-base/packages.el
@@ -217,7 +217,27 @@
 
 (defun spacemacs-base/init-pcre2el ()
   (use-package pcre2el
-    :defer t))
+    :defer t
+    :init
+    (progn
+      (spacemacs/declare-prefix "xr" "regular expressions")
+      (spacemacs/declare-prefix "xre" "elisp")
+      (spacemacs/declare-prefix "xrp" "pcre")
+      (spacemacs/set-leader-keys
+        "xr/"  'rxt-explain
+        "xr'"  'rxt-convert-to-strings
+        "xrt"  'rxt-toggle-elisp-rx
+        "xrx"  'rxt-convert-to-rx
+        "xrc"  'rxt-convert-syntax
+        "xre/" 'rxt-explain-elisp
+        "xre'" 'rxt-elisp-to-strings
+        "xrep" 'rxt-elisp-to-pcre
+        "xret" 'rxt-toggle-elisp-rx
+        "xrex" 'rxt-elisp-to-rx
+        "xrp/" 'rxt-explain-pcre
+        "xrp'" 'rxt-pcre-to-strings
+        "xrpe" 'rxt-pcre-to-elisp
+        "xrpx" 'rxt-pcre-to-rx))))
 
 (defun spacemacs-base/init-process-menu ()
   (evilified-state-evilify process-menu-mode process-menu-mode-map))
