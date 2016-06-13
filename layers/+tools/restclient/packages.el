9e9ef1c4ee41154e9c818b48f6b78c665ad19777
Tommi Komulainen <tommi.komulainen@iki.fi>
2016-06-13 22:47:14 +0200

    restclient: add autocompletion for methods and headers
    Provided by company-restclient.

--- layers/+tools/restclient/packages.el
+++ layers/+tools/restclient/packages.el
@@ -10,6 +10,8 @@
 ;;; License: GPLv3
 (setq restclient-packages
       '(
+        company
+        (company-restclient :toggle (configuration-layer/package-usedp 'company))
         golden-ratio
         ob-http
         restclient
@@ -40,3 +42,11 @@
         "r" 'restclient-http-send-current-raw-stay-in-window
         "R" 'restclient-http-send-current-raw
         "y" 'restclient-copy-curl-command))))
+
+(defun restclient/post-init-company ()
+  (spacemacs|add-company-hook restclient-mode))
+
+(defun restclient/init-company-restclient ()
+  (use-package company-restclient
+    :defer t
+    :init (push 'company-restclient company-backends-restclient-mode)))
