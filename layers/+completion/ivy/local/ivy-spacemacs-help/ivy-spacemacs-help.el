ad6078935b31c4dcac66d0cf80a5b2bf5fdb2a04
ralesi <scio62@gmail.com>
2016-06-14 14:16:05 -0700

    Fix :owner reference in ivy-spacemacs-help

--- layers/+completion/ivy/local/ivy-spacemacs-help/ivy-spacemacs-help.el
+++ layers/+completion/ivy/local/ivy-spacemacs-help/ivy-spacemacs-help.el
@@ -207,18 +207,20 @@
            (lambda (a x) (max (length (symbol-name (oref x :name))) a))
            ivy-spacemacs-help-all-layers :initial-value 0)))
         (owners (cl-remove-duplicates
-                 (mapcar (lambda (pkg) (oref pkg :owner))
+                 (mapcar (lambda (pkg)
+                           (car (oref pkg :owners)))
                          ivy-spacemacs-help-all-packages))))
     (dolist (pkg ivy-spacemacs-help-all-packages)
       (push (list (format (concat "%-" left-column-width "S %s %s")
-                          (oref pkg :owner)
+                          (car (oref pkg :owners ))
                           (propertize (symbol-name (oref pkg :name))
                                       'face 'font-lock-type-face)
                           (propertize
                            (if (package-installed-p (oref pkg :name))
                                "[installed]" "")
                            'face 'font-lock-comment-face))
-                  (symbol-name (oref pkg :owner))
+                  (symbol-name
+                   (car (oref pkg :owners )))
                   (symbol-name (oref pkg :name)))
             result))
     (dolist (layer (delq nil
