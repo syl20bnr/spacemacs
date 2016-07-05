e4df90dcbbedb0fdc9fe97bbad17cc2193c08fc5
Ayush Goyal <ayush@helpshift.com>
2016-07-06 04:47:14 +0530

    Fix yaml layer autocompletion
    Autocompletion was not working with yasnippets for yaml layer. This also
    fixes autocompletion for ansible layer.

---
+++ layers/+lang/yaml/config.el
@@ -0,0 +1,14 @@
+;;; config.el --- YAML Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; variables
+
+(spacemacs|defvar-company-backends yaml-mode)
