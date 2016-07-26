60f25fa19e5065e25e09dbc1fc0d60ffb1f791b5
Eivind Fonn <evfonn@gmail.com>
2016-07-26 20:10:45 +0900

    Fix emmet expansion if yas isn’t turned on

---
+++ layers/+lang/html/funcs.el
@@ -0,0 +1,16 @@
+;;; funcs.el --- HTML Layer functions File for Spacemacs
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defun spacemacs/emmet-expand ()
+  (interactive)
+  (if (bound-and-true-p yas-minor-mode)
+      (call-interactively 'emmet-expand-yas)
+    (call-interactively 'emmet-expand-line)))
