8de8a7a3e440184034efe497775a3dd14dc6bb0d
Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
2016-08-11 16:29:51 +1000

    Added a geben configuration layer
    A simple configuration layer with key bindings for
    geben-on-emacs. The leader is SPC G.

---
+++ layers/+tools/geben/config.el
@@ -0,0 +1,18 @@
+;;; config.el --- geben layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(spacemacs/declare-prefix "G" "geben")
+(spacemacs/declare-prefix "Gs" "geben-steps")
+(spacemacs/declare-prefix "Gc" "geben-context")
+(spacemacs/declare-prefix "Gt" "geben-backtrace")
+(spacemacs/declare-prefix "Gb" "geben-breakpoint")
+(spacemacs/declare-prefix "Gbl" "geben-breakpoint-list")
+(spacemacs/declare-prefix "Gbs" "geben-breakpoint-set")
