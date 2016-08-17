c69aea09a5327cc7274002122f36b3bb94452048
Riccardo Murri <riccardo.murri@gmail.com>
2016-08-17 22:19:45 +0200

    Move definition of `spacemacs-spaceline-additional-segments` to `config.el`

--- layers/+distribution/spacemacs/config.el
+++ layers/+distribution/spacemacs/config.el
@@ -12,3 +12,15 @@
 ;; Prerequisites
 
 (configuration-layer/declare-layers '(spacemacs-base spacemacs-layouts))
+
+(defcustom spacemacs-spaceline-additional-segments
+  '((new-version :when active))
+  "Additional segments for the Spacemacs modeline.
+
+They are inserted in the modeline between `global' and
+`buffer-position'.
+
+Must be a list of valid segments; see `spaceline-install' for
+more information on what constitutes a valid segment."
+  :type '(repeat sexp)
+  :group 'spacemacs)
--- layers/+distribution/spacemacs/packages.el
+++ layers/+distribution/spacemacs/packages.el
@@ -9,18 +9,6 @@
 ;;
 ;;; License: GPLv3
 
-(defcustom spacemacs-spaceline-additional-segments
-  '((new-version :when active))
-  "Additional segments for the Spacemacs modeline.
-
-They are inserted in the modeline between `global' and
-`buffer-position'.
-
-Must be a list of valid segments; see `spaceline-install' for
-more information on what constitutes a valid segment."
-  :type '(repeat sexp)
-  :group 'spacemacs)
-
 (setq spacemacs-packages
       '(
         ;; default
