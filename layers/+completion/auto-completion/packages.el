6d454a2313987a8a1124a57764b3b6482c45f57c
bmag <bmagamb@gmail.com>
2016-08-26 15:37:05 +0300

    auto-completion: manual help tooltip and minor fix
    New value `manual` for auto-completion-enable-help-tooltip enables the
    user to display help tooltip on-demand. Other values remain the same:
    nil for no tooltip at all, t (or any non-nil non-manual value) for
    automatic tooltip.
    
    Minor fix: call `company-quickhelp-mode` just once, because it's a
    global minor-mode.

--- layers/+completion/auto-completion/packages.el
+++ layers/+completion/auto-completion/packages.el
@@ -114,12 +114,14 @@
 
 (defun auto-completion/init-company-quickhelp ()
   (use-package company-quickhelp
-    :defer t
+    :commands company-quickhelp-manual-begin
     :init
     (spacemacs|do-after-display-system-init
-     (add-hook 'company-mode-hook 'company-quickhelp-mode)
      (with-eval-after-load 'company
-       (setq company-frontends (delq 'company-echo-metadata-frontend company-frontends))))))
+       (setq company-frontends (delq 'company-echo-metadata-frontend company-frontends))
+       (define-key company-active-map (kbd "M-h") #'company-quickhelp-manual-begin)
+       (unless (eq auto-completion-enable-help-tooltip 'manual)
+         (company-quickhelp-mode))))))
 
 (defun auto-completion/init-helm-c-yasnippet ()
   (use-package helm-c-yasnippet
