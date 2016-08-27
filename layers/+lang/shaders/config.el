545c752431b06ddf97b60f61275f5b0b3d3df0de
d12frosted <d12frosted@gmail.com>
2016-08-27 12:59:52 +0300

    add glsl-company to shaders layer

---
+++ layers/+lang/shaders/config.el
@@ -0,0 +1,14 @@
+;;; config.el --- Shaders Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Boris Buliga <boris@d12frosted.io>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+
+(spacemacs|defvar-company-backends glsl-mode)
