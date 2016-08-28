78d76758e5695bc8612c9a8244ae61d61ab956a6
d12frosted <d12frosted@gmail.com>
2016-08-28 14:20:44 +0300

    add missing headers to osx layer files

--- layers/+os/osx/config.el
+++ layers/+os/osx/config.el
@@ -1,3 +1,14 @@
+;;; config.el --- OSX Layer config File for Spacemacs
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
 (defvar osx-use-option-as-meta t
   "If non nil the option key is mapped to meta. Set to `nil` if you need the
   option key to type common characters.")
