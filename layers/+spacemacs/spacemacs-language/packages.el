6d17a90d0cc94bb025bcd663de99bcc2601e2b50
Muneeb Shaikh <muneeb@reversehack.in>
2016-09-06 12:29:48 +0530

    fix void variable when not using osx layer
    Fix `Symbol's value as variable is void: osx-use-dictionary-app` when
    not using osx layer.

--- layers/+spacemacs/spacemacs-language/packages.el
+++ layers/+spacemacs/spacemacs-language/packages.el
@@ -10,7 +10,7 @@
 ;;; License: GPLv3
 
 (setq spacemacs-language-packages
-      '((define-word :toggle (not osx-use-dictionary-app))
+      '((define-word :toggle (not (bound-and-true-p osx-use-dictionary-app)))
         google-translate))
 
 (defun spacemacs-language/init-define-word ()
