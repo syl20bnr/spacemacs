01002b5a03b96a3df4b82a83c034efdf0db2812f
bmag <bmagamb@gmail.com>
2016-10-08 15:02:10 +0300

    New dotvariable: auto-generate layout names
    If dotspacemacs-auto-generate-layout-names is non-nil, and the user
    tries to open a layout in a position that doesn't yet have a layout,
    then create a new layout with an automatically generated name.

--- layers/+spacemacs/spacemacs-layouts/config.el
+++ layers/+spacemacs/spacemacs-layouts/config.el
@@ -33,3 +33,25 @@
 
 (defvar spacemacs--layouts-autosave-timer nil
   "Timer for layouts auto-save.")
+
+(defvar spacemacs-generic-layout-names
+  '(("zebra" "zucchini" "zen" "yellow" "yeti" "yard") ; grab-bag
+    ("baboon" "banana" "blue")                        ; 2nd layout
+    ("crab" "cabbage" "crayon")                       ; 3rd
+    ("deer" "doughnut" "door")                        ; 4th
+    ("elephant" "eggplant" "extreme")                 ; 5th
+    ("falcon" "fig" "fjord")                          ; 6th
+    ("gnu" "garlic" "guardian")                       ; 7th
+    ("horse" "honey" "hallelujah")                    ; 8th
+    ("iguana" "ice-cream" "internet")                 ; 9th
+    ("jellyfish" "jalapeno" "jolt"))                  ; 10th (aka 0th)
+  "Names for auto-generated layout names.
+Used by `spacemacs//generate-layout-name'.
+
+Must be a list with 10 entries, where each entry is a list of
+names. The 2nd list contains possible names for the 2nd
+layout (or 10th) layout, the 3rd list contains names for the 3rd
+layout, the 4th for the 4th, and so on until the 10th (aka layout
+number 0). The first list is sepcial - it is a grab-bag for names
+in case none of the regular names can be used for a new layout.")
+
