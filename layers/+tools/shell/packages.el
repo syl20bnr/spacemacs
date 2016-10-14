2db643954d121606e7f196aa69d95f801be2613d
Jeremy Bi <bixuanxbi@gmail.com>
2016-10-14 11:19:38 +0800

    `font-lock-unfontify-region-function` should no longer be set/reset.
    Due to upstream changes.

--- layers/+tools/shell/packages.el
+++ layers/+tools/shell/packages.el
@@ -250,7 +250,6 @@
       (add-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
       (setq comint-output-filter-functions
             (remove 'ansi-color-process-output comint-output-filter-functions))
-      (setq font-lock-unfontify-region-function 'xterm-color-unfontify-region)
       (add-hook 'eshell-mode-hook 'spacemacs/init-eshell-xterm-color))))
 
 (defun shell/post-init-vi-tilde-fringe ()
