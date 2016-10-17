syl20bnr/spacemacs

commit 42a33f3ae8c58b8f416207e6d553dbcbd78e5385

elixir: add support for org-babel
Closes #1918

layers/+lang/elixir/packages.el
@@ -18,6 +18,7 @@
     flycheck-mix
     ggtags
     helm-gtags
+    ob-elixir
     popwin
     smartparens
     ))
@@ -138,6 +139,17 @@
   (spacemacs/add-flycheck-hook 'elixir-mode)
   (add-hook 'elixir-mode-hook
             'spacemacs//elixir-flycheck-check-on-save-only t))
+
+(defun elixir/pre-init-org ()
+  (spacemacs|use-package-add-hook org
+    :post-config (add-to-list 'org-babel-load-languages '(python . t))))
+
+(defun elixir/init-ob-elixir ()
+  (spacemacs|use-package-add-hook org
+    :post-config
+    (use-package ob-elixir
+      :init (add-to-list 'org-babel-load-languages '(elixir . t)))))
+
 
 (defun elixir/pre-init-popwin ()
   (spacemacs|use-package-add-hook popwin

commit 5ab39a3e14826625363d53cd9afc566966617065

Fix a typo introduced in 42a33f3ae8c58b8f416207e6d553dbcbd78e5385

layers/+lang/elixir/packages.el
@@ -142,7 +142,7 @@
 
 (defun elixir/pre-init-org ()
   (spacemacs|use-package-add-hook org
-    :post-config (add-to-list 'org-babel-load-languages '(python . t))))
+    :post-config (add-to-list 'org-babel-load-languages '(elixir . t))))
 
 (defun elixir/init-ob-elixir ()
   (spacemacs|use-package-add-hook org