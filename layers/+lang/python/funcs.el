110ceee7cfe164abe23ac2f3f2235de75077a8c0
yangguang760 <yangguang760@gmail.com>
2016-10-20 16:03:18 +0800

    fix debug string for python3
    fix debug string for python3:
    ipdb  ->  ipdb3
    pudb  -> pudb3

--- layers/+lang/python/funcs.el
+++ layers/+lang/python/funcs.el
@@ -41,6 +41,8 @@
   (let ((trace (cond ((spacemacs/pyenv-executable-find "wdb") "import wdb; wdb.set_trace()")
                      ((spacemacs/pyenv-executable-find "ipdb") "import ipdb; ipdb.set_trace()")
                      ((spacemacs/pyenv-executable-find "pudb") "import pudb; pudb.set_trace()")
+                     ((spacemacs/pyenv-executable-find "ipdb3") "import ipdb; ipdb.set_trace()")
+                     ((spacemacs/pyenv-executable-find "pudb3") "import pudb; pudb.set_trace()")
                      (t "import pdb; pdb.set_trace()")))
         (line (thing-at-point 'line)))
     (if (and line (string-match trace line))
