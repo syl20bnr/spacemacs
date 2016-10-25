e0321f4f3858d2dfa6e0c30d049c8e1db12c0add
Kenji Miyazaki <kenjimyzk@gmail.com>
2016-10-26 00:27:11 +0900

    Japanese Layer commit updated

--- layers/+intl/japanese/packages.el
+++ layers/+intl/japanese/packages.el
@@ -8,7 +8,6 @@
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
-
 ;; List of all packages to install and/or initialize. Built-in packages
 ;; which require an initialization must be listed explicitly in the list.
 (setq japanese-packages
