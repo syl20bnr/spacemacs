718c62405d1169dd809d3eeb517a1dd4d0d7165d
dchingal <dchingal@qti.qualcomm.com>
2016-11-07 14:34:52 +0000

    bind helm-cscope-find-assignments-to-this-symbol

--- layers/+tags/cscope/packages.el
+++ layers/+tags/cscope/packages.el
@@ -47,6 +47,7 @@
     (defun spacemacs/setup-helm-cscope (mode)
       "Setup `helm-cscope' for MODE"
       (spacemacs/set-leader-keys-for-major-mode mode
+        "g=" 'helm-cscope-find-assignments-to-this-symbol
         "gc" 'helm-cscope-find-called-function
         "gC" 'helm-cscope-find-calling-this-funtcion
         "gd" 'helm-cscope-find-global-definition
