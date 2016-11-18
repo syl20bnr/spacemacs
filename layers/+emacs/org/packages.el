744b163311101ebd5cd138a73b2723fc23ee7c48
syl20bnr <sylvain.benner@gmail.com>
2016-11-18 09:40:05 -0500

    Org: Move SPC m e to SPC m e e for org-export-dispatch
    In order to be able to support more conversion format

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -159,7 +159,8 @@ Will work on both org-mode and any mode that accepts plain html."
             (insert (format tag ""))
             (forward-char -8))))
 
-      (dolist (prefix '(("mx" . "text")
+      (dolist (prefix '(("me" . "export")
+                        ("mx" . "text")
                         ("mh" . "headings")
                         ("mi" . "insert")
                         ("mS" . "subtrees")
@@ -173,7 +174,7 @@ Will work on both org-mode and any mode that accepts plain html."
         "c" 'org-capture
         "d" 'org-deadline
         "D" 'org-insert-drawer
-        "e" 'org-export-dispatch
+        "ee" 'org-export-dispatch
         "f" 'org-set-effort
         "P" 'org-set-property
         ":" 'org-set-tags
