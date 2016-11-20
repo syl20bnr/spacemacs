237b2a73734e988fc8fd4a653bf9deee82d4dda3
syl20bnr <sylvain.benner@gmail.com>
2016-11-20 13:21:58 -0500

    Move `SPC b m` to kill other buffers to `SPC b C-d`
    Also move `SPC b C-d` to kill other buffer with regexp to `SPC b C-D`
    Update Documentation accordingly.

--- layers/+distributions/spacemacs-base/keybindings.el
+++ layers/+distributions/spacemacs-base/keybindings.el
@@ -121,7 +121,8 @@
   "bd"    'spacemacs/kill-this-buffer
   "be"    'spacemacs/safe-erase-buffer
   "bh"    'spacemacs/home
-  "b C-d" 'spacemacs/kill-matching-buffers-rudely
+  "b C-d" 'spacemacs/kill-other-buffers
+  "b C-D" 'spacemacs/kill-matching-buffers-rudely
   "bn"    'next-buffer
   "bm"    'view-echo-area-messages
   "bN"    'spacemacs/new-empty-buffer
