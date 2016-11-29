ac35a2fbe6d598621c31f30e925425f26685927c
Oleg Pykhalov <go.wigust@gmail.com>
2016-11-29 09:03:39 +0300

    dark-window: Switch color if cycle spacemacs-theme

--- layers/+themes/dark-window/packages.el
+++ layers/+themes/dark-window/packages.el
@@ -15,15 +15,19 @@
 (defun dark-window/init-frame-cmds ()
   (use-package frame-cmds
     :config
-    ;; Nicolas Petton http://nicolas.petton.fr/blog/emacs-dark-window-decoration.html
+    ;; Based on Nicolas Petton code http://nicolas.petton.fr/blog/emacs-dark-window-decoration.html
     (progn
-      (defun set-selected-frame-dark ()
+      (defun set-selected-frame-color ()
+        (when (and (eq window-system 'x) (eq spacemacs--cur-theme 'spacemacs-dark))
+          (interactive)
+          (let ((frame-name (get-frame-name (selected-frame))))
+            (call-process-shell-command
+             (concat "xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT \"dark\" -name \"" frame-name "\"")))))
+
+      (when (and (eq window-system 'x) (eq spacemacs--cur-theme 'spacemacs-light))
         (interactive)
         (let ((frame-name (get-frame-name (selected-frame))))
           (call-process-shell-command
-           (concat "xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT \"dark\" -name \"" frame-name "\""))))
-
-      (when (window-system)
-        (set-selected-frame-dark)))))
+           (concat "xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT \"light\" -name \"" frame-name "\"")))))))
 
 ;;; packages.el ends here
