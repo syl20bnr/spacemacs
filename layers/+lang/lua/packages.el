f4f744dbb9ff0945561a9777317178ae136ba268
d12frosted <boris@d12frosted.io>
2016-12-02 20:42:11 +0200

    add missing header in lua/packages.el file

--- layers/+lang/lua/packages.el
+++ layers/+lang/lua/packages.el
@@ -1,11 +1,22 @@
+;;; packages.el --- Lua Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
 (setq lua-packages
-  '(
-    company
-    flycheck
-    ggtags
-    helm-gtags
-    lua-mode
-    ))
+      '(
+        company
+        flycheck
+        ggtags
+        helm-gtags
+        lua-mode
+        ))
 
 (defun lua/post-init-flycheck ()
   (spacemacs/add-flycheck-hook 'lua-mode))
