83dfd2a8600ce25e986e0494461e5816064501dd
Michal Muskala <michal@muskala.eu>
2016-12-22 09:31:05 +0100

    Prevent from inserting too many "end"s in Elixir
    Closes #8030

--- layers/+lang/elixir/funcs.el
+++ layers/+lang/elixir/funcs.el
@@ -9,11 +9,27 @@
 ;;
 ;;; License: GPLv3
 
+(defun spacemas//elixir-looking-back-special-p (expr)
+  (save-excursion
+    (when (or (looking-back " ")
+              (looking-back "-")) (backward-char))
+    (looking-back expr)))
+
+(defun spacemacs//elixir-point-after-fn-p (id action context)
+  (save-excursion
+    (when (looking-back id) (backward-char))
+    (looking-back "fn")))
+
 (defun spacemacs//elixir-do-end-close-action (id action context)
   (when (eq action 'insert)
-    (newline-and-indent)
-    (forward-line -1)
-    (indent-according-to-mode)))
+    (cond ((spacemas//elixir-looking-back-special-p id)
+           (insert " ") (backward-char))
+          ((looking-back "(")
+           (insert ") ") (backward-char) (backward-char))
+          (t
+           (newline-and-indent)
+           (forward-line -1)
+           (indent-according-to-mode)))))
 
 (defun spacemacs//elixir-enable-compilation-checking ()
   "Enable compile checking if `elixir-enable-compilation-checking' is non nil."
