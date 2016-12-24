25240285d794273a8e9ef612faed488249435fc3
syl20bnr <sylvain.benner@gmail.com>
2016-12-24 11:56:24 -0500

    emacs-lisp: fix eval-last-sexp and add SPC m e C
    Important evil advices for eval-last-sexp were removed for smartparens users,
    it made eval-last-sexp not working correctly in normal state by evaluating the
    sexp before the evil cursor and it was impossible to evaluate an sexp when the
    cursor was on the last character of a line.
    
    Also add SPC m e C which will execute the first defun or setq sexp encountered
    before point.

--- layers/+lang/emacs-lisp/funcs.el
+++ layers/+lang/emacs-lisp/funcs.el
@@ -29,3 +29,36 @@
              (not (fboundp symb)))
         (find-variable-other-window symb)
       (find-function-at-point))))
+
+
+;; smartparens integration
+
+(defun spacemacs/eval-current-form-sp (&optional arg)
+  "Call `eval-last-sexp' after moving out of one level of
+parentheses. Will exit any strings and/or comments first.
+An optional ARG can be used which is passed to `sp-up-sexp' to move out of more
+than one sexp.
+Requires smartparens because all movement is done using `sp-up-sexp'."
+  (interactive "p")
+  (require 'smartparens)
+  (let ((evil-move-beyond-eol t))
+    ;; evil-move-beyond-eol disables the evil advices around eval-last-sexp
+    (save-excursion
+      (let ((max 10))
+        (while (and (> max 0)
+                    (sp-point-in-string-or-comment))
+          (decf max)
+          (sp-up-sexp)))
+      (sp-up-sexp arg)
+      (call-interactively 'eval-last-sexp))))
+
+(defun spacemacs/eval-current-symbol-sp ()
+  "Call `eval-last-sexp' on the symbol around point.
+Requires smartparens because all movement is done using `sp-forward-symbol'."
+  (interactive)
+  (require 'smartparens)
+  (let ((evil-move-beyond-eol t))
+    ;; evil-move-beyond-eol disables the evil advices around eval-last-sexp
+    (save-excursion
+      (sp-forward-symbol)
+      (call-interactively 'eval-last-sexp))))
