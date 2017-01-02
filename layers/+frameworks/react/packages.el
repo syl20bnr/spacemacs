417a52da5d685bc86e405559931f637d875fb195
syl20bnr <sylvain.benner@gmail.com>
2017-01-01 22:45:23 -0500

    javascript and react: reformat code and move funcs to funcs.el
    Some conventional cleanup.
    Note that the function `spacemacs/js2-refactor-set-key-bindings` has been
    removed since it is not used anywhere.

--- layers/+frameworks/react/packages.el
+++ layers/+frameworks/react/packages.el
@@ -34,28 +34,16 @@
 
 (defun react/post-init-evil-matchit ()
   (with-eval-after-load 'evil-matchit
-    (plist-put evilmi-plugins 'react-mode '((evilmi-simple-get-tag evilmi-simple-jump)
-                                            (evilmi-javascript-get-tag evilmi-javascript-jump)
-                                            (evilmi-html-get-tag evilmi-html-jump)))))
+    (plist-put evilmi-plugins 'react-mode
+               '((evilmi-simple-get-tag evilmi-simple-jump)
+                 (evilmi-javascript-get-tag evilmi-javascript-jump)
+                 (evilmi-html-get-tag evilmi-html-jump)))))
 
 (defun react/post-init-flycheck ()
   (with-eval-after-load 'flycheck
     (dolist (checker '(javascript-eslint javascript-standard))
       (flycheck-add-mode checker 'react-mode)))
-  (defun react/use-eslint-from-node-modules ()
-    (let* ((root (locate-dominating-file
-                  (or (buffer-file-name) default-directory)
-                  "node_modules"))
-           (global-eslint (executable-find "eslint"))
-           (local-eslint (expand-file-name "node_modules/.bin/eslint"
-                                           root))
-           (eslint (if (file-executable-p local-eslint)
-                       local-eslint
-                     global-eslint)))
-      (setq-local flycheck-javascript-eslint-executable eslint)))
-
-  (add-hook 'react-mode-hook #'react/use-eslint-from-node-modules)
-
+  (add-hook 'react-mode-hook #'spacemacs//react-use-eslint-from-node-modules)
   (spacemacs/add-flycheck-hook 'react-mode))
 
 (defun react/post-init-js-doc ()
@@ -82,15 +70,4 @@
   (add-to-list 'auto-mode-alist '("\\index.ios.js\\'" . react-mode))
   (add-to-list 'magic-mode-alist '("/\\*\\* @jsx React\\.DOM \\*/" . react-mode))
   (add-to-list 'magic-mode-alist '("^import React" . react-mode))
-  (defun spacemacs//setup-react-mode ()
-    "Adjust web-mode to accommodate react-mode"
-    (emmet-mode 0)
-    ;; See https://github.com/CestDiego/emmet-mode/commit/3f2904196e856d31b9c95794d2682c4c7365db23
-    (setq-local emmet-expand-jsx-className? t)
-    ;; Enable js-mode snippets
-    (yas-activate-extra-mode 'js-mode)
-    ;; Force jsx content type
-    (web-mode-set-content-type "jsx")
-    ;; Don't auto-quote attribute values
-    (setq-local web-mode-enable-auto-quoting nil))
   (add-hook 'react-mode-hook 'spacemacs//setup-react-mode))
