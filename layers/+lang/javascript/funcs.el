c725823e73cec04a3eed4f8c80c23df6a03da911
syl20bnr <sylvain.benner@gmail.com>
2017-01-01 22:45:23 -0500

    javascript and react: reformat code and move funcs to funcs.el
    Some conventional cleanup.
    Note that the function `spacemacs/js2-refactor-set-key-bindings` has been
    removed since it is not used anywhere.

--- layers/+lang/javascript/funcs.el
+++ layers/+lang/javascript/funcs.el
@@ -10,6 +10,75 @@
 ;;; License: GPLv3
 
 
+;; js-doc
+
+(defun spacemacs/js-doc-require ()
+  "Lazy load js-doc"
+  (require 'js-doc))
+(add-hook 'js2-mode-hook 'spacemacs/js-doc-require)
+
+(defun spacemacs/js-doc-set-key-bindings (mode)
+  "Setup the key bindings for `js2-doc' for the given MODE."
+  (spacemacs/declare-prefix-for-mode mode "mrd" "documentation")
+  (spacemacs/set-leader-keys-for-major-mode mode
+    "rdb" 'js-doc-insert-file-doc
+    "rdf" 'js-doc-insert-function-doc
+    "rdt" 'js-doc-insert-tag
+    "rdh" 'js-doc-describe-tag))
+
+;; js-refactor
+
+(defun spacemacs/js2-refactor-require ()
+  "Lazy load js2-refactor"
+  (require 'js2-refactor))
+
+
+;; coffee
+
+(defun javascript/coffee-indent ()
+  (if (coffee-line-wants-indent)
+      ;; We need to insert an additional tab because
+      ;; the last line was special.
+      (coffee-insert-spaces (+ (coffee-previous-indent) coffee-tab-width))
+    ;; otherwise keep at the same indentation level
+    (coffee-insert-spaces (coffee-previous-indent))))
+
+
+;; skewer
+
+(defun spacemacs/skewer-start-repl ()
+  "Attach a browser to Emacs and start a skewer REPL."
+  (interactive)
+  (run-skewer)
+  (skewer-repl))
+
+(defun spacemacs/skewer-load-buffer-and-focus ()
+  "Execute whole buffer in browser and switch to REPL in insert state."
+  (interactive)
+  (skewer-load-buffer)
+  (skewer-repl)
+  (evil-insert-state))
+
+(defun spacemacs/skewer-eval-defun-and-focus ()
+  "Execute function at point in browser and switch to REPL in insert state."
+  (interactive)
+  (skewer-eval-defun)
+  (skewer-repl)
+  (evil-insert-state))
+
+(defun spacemacs/skewer-eval-region (beg end)
+  "Execute the region as JavaScript code in the attached browser."
+  (interactive "r")
+  (skewer-eval (buffer-substring beg end) #'skewer-post-minibuffer))
+
+(defun spacemacs/skewer-eval-region-and-focus (beg end)
+  "Execute the region in browser and swith to REPL in insert state."
+  (interactive "r")
+  (spacemacs/skewer-eval-region beg end)
+  (skewer-repl)
+  (evil-insert-state))
+
+
 ;; tern
 
 (defun spacemacs//set-tern-key-bindings (mode)
