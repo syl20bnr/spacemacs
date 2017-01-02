a5ac11b8323106b34956dab3f1e29af1060c6a91
syl20bnr <sylvain.benner@gmail.com>
2017-01-02 01:36:15 -0500

    Fix helm :config: ‘fuzzy-match’ not supported in async sources.
    Fixes #8091

--- layers/+spacemacs/spacemacs-completion/funcs.el
+++ layers/+spacemacs/spacemacs-completion/funcs.el
@@ -39,7 +39,8 @@
   "Function to be used as advice to activate fuzzy matching for all sources."
   (let ((source-type (cadr args))
         (props (cddr args)))
-    (unless (eq source-type 'helm-source-async)
+    (unless (memq source-type '(helm-source-async
+                                helm-locate-source))
       (plist-put props :fuzzy-match (eq 'always dotspacemacs-helm-use-fuzzy))))
   (apply f args))
 
