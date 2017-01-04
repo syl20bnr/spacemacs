973684bfcd578ec1f5bb7e26b2a7b8c7a498fd8e
Oleg Pykhalov <go.wigust@gmail.com>
2017-01-04 15:58:01 +0300

    mu4e: Create email with attachments from dired
    Attachments are mostly handled using the helm baloo interface below,
    but sometimes we want to send files from a directory: C-c C-m C-a

--- layers/+email/mu4e/packages.el
+++ layers/+email/mu4e/packages.el
@@ -58,7 +58,24 @@
 
       (when mu4e-account-alist
         (add-hook 'mu4e-compose-pre-hook 'mu4e/set-account)
-        (add-hook 'message-sent-hook 'mu4e/mail-account-reset)))))
+        (add-hook 'message-sent-hook 'mu4e/mail-account-reset))
+
+      (require 'gnus-dired)
+      ;; make the `gnus-dired-mail-buffers' function also work on
+      ;; message-mode derived modes, such as mu4e-compose-mode
+      (defun gnus-dired-mail-buffers ()
+	"Return a list of active message buffers."
+	(let (buffers)
+	  (save-current-buffer
+	    (dolist (buffer (buffer-list t))
+	      (set-buffer buffer)
+	      (when (and (derived-mode-p 'message-mode)
+			 (null message-sent-message-via))
+		(push (buffer-name buffer) buffers))))
+	  (nreverse buffers)))
+
+      (setq gnus-dired-mail-mode 'mu4e-user-agent)
+      (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode))))
 
 (defun mu4e/init-mu4e-alert ()
   (use-package mu4e-alert
