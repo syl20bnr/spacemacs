550821baae38b157fd047e61a276bad4548d5542
deb0ch <thomas.de.beauchene@gmail.com>
2017-01-05 11:15:54 +0100

    configure xkcd inside the :init progn clause

--- layers/+fun/xkcd/packages.el
+++ layers/+fun/xkcd/packages.el
@@ -25,5 +25,5 @@
         "h" 'xkcd-prev
         "j" 'xkcd-next
         "k" 'xkcd-prev
-        "l" 'xkcd-next))
-    (push "*xkcd*" spacemacs-useful-buffers-regexp)))
+        "l" 'xkcd-next)
+      (push "*xkcd*" spacemacs-useful-buffers-regexp))))
