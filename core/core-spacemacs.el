bba788e069858ab1aaf5261168d11658b1ecf721
syl20bnr <sylvain.benner@gmail.com>
2017-01-25 00:24:02 -0500

    core: fix startup graphical glitch
    Since Emacs 25.1 I got screen flickering when packages are installed on
    startup.
    Shuffling the UI elements removal seems to fix it on my system using X.
    
    It may have the opposite effect on other users setup.

--- core/core-spacemacs.el
+++ core/core-spacemacs.el
@@ -145,13 +145,13 @@ the final step of executing code in `emacs-startup-hook'.")
 (defun spacemacs//removes-gui-elements ()
   "Remove the menu bar, tool bar and scroll bars."
   ;; removes the GUI elements
+  (when (and (fboundp 'tool-bar-mode) (not (eq tool-bar-mode -1)))
+    (tool-bar-mode -1))
   (unless (spacemacs/window-system-is-mac)
     (when (and (fboundp 'menu-bar-mode) (not (eq menu-bar-mode -1)))
       (menu-bar-mode -1)))
   (when (and (fboundp 'scroll-bar-mode) (not (eq scroll-bar-mode -1)))
     (scroll-bar-mode -1))
-  (when (and (fboundp 'tool-bar-mode) (not (eq tool-bar-mode -1)))
-    (tool-bar-mode -1))
   ;; tooltips in echo-aera
   (when (and (fboundp 'tooltip-mode) (not (eq tooltip-mode -1)))
     (tooltip-mode -1)))
