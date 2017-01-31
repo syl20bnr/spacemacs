8417e90b2a66d20561dca42383d4658a2eeef179
hodge <hodge.shen@gmail.com>
2017-01-31 15:13:46 +0800

    add chrome find

--- layers/+tools/chrome/packages.el
+++ layers/+tools/chrome/packages.el
@@ -33,11 +33,20 @@
     :defer t
     :init
     (progn
-      (defun my-flymd-browser-function (url)
+      (defun start-browser(browser url)
         (let ((process-environment (browse-url-process-environment)))
           (apply 'start-process
-                 (concat "google-chrome " url) nil
-                 chrome-exec-path
+                 (concat "flymd" url) nil
+                 browser
                  (list "--allow-file-access-from-files" url))))
+      (defun my-flymd-browser-function (url)
+        (let ((browser-path
+               (cond
+                ((executable-find "chromium") (executable-find "chromium"))
+                ((executable-find "google-chrome") (executable-find "goolge-chrome"))
+                ((executable-find "goolge-chrome-stable") (executable-find "goolge-chrome-stable"))
+                (chrome-exec-path chrome-exec-path)
+                (t (message "no useful browser")))))
+          (start-browser browser-path url)))
       (setq flymd-browser-open-function 'my-flymd-browser-function)
       )))
