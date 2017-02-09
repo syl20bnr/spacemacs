d67be001c4bfa91ea9290e00ce4c93a2d2461de0
syl20bnr <sylvain.benner@gmail.com>
2017-02-09 17:59:49 -0500

    github: move load-gh-pulls-mode to funcs.el

--- layers/+source-control/github/packages.el
+++ layers/+source-control/github/packages.el
@@ -71,15 +71,7 @@
     :pre-config
     (progn
       (use-package magit-gh-pulls
-        :init
-        (progn
-          (defun spacemacs/load-gh-pulls-mode ()
-            "Start `magit-gh-pulls-mode' only after a manual request."
-            (interactive)
-            (magit-gh-pulls-mode)
-            (magit-gh-pulls-popup))
-
-          (define-key magit-mode-map "#" 'spacemacs/load-gh-pulls-mode))
+        :init (define-key magit-mode-map "#" 'spacemacs/load-gh-pulls-mode)
         :config
         (spacemacs|diminish magit-gh-pulls-mode "Github-PR")))))
 
