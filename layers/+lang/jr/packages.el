d7bc5a369830c9418c5e512e58d13853ec86d24d
syl20bnr <sylvain.benner@gmail.com>
2017-02-10 14:48:17 -0500

    Add JR programming language layer

---
+++ layers/+lang/jr/packages.el
@@ -0,0 +1,20 @@
+;;; packages.el --- JR Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst jr-packages
+  '(
+    (jr-mode :location local)
+    ))
+
+(defun jr/init-jr-mode ()
+  (use-package jr-mode
+    :commands jr-mode
+    :mode "\\.jr\\'"))
