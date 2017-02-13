be33df7dc86e689a4ed7cb4916aaa395b92d2206
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2017-02-13 14:37:13 +0200

    Fixes keybindings for gfm-mode

--- layers/+lang/markdown/packages.el
+++ layers/+lang/markdown/packages.el
@@ -81,64 +81,67 @@ Will work on both org-mode and any mode that accepts plain html."
                         ("mx" . "markdown/text")))
         (spacemacs/declare-prefix-for-mode
          'markdown-mode (car prefix) (cdr prefix)))
-      (spacemacs/set-leader-keys-for-major-mode 'markdown-mode
-        ;; Movement
-        "{"   'markdown-backward-paragraph
-        "}"   'markdown-forward-paragraph
-        ;; Completion, and Cycling
-        "]"   'markdown-complete
-        ;; Indentation
-        ">"   'markdown-indent-region
-        "<"   'markdown-exdent-region
-        ;; Buffer-wide commands
-        "c]"  'markdown-complete-buffer
-        "cc"  'markdown-check-refs
-        "ce"  'markdown-export
-        "cm"  'markdown-other-window
-        "cn"  'markdown-cleanup-list-numbers
-        "co"  'markdown-open
-        "cp"  'markdown-preview
-        "cv"  'markdown-export-and-preview
-        "cw"  'markdown-kill-ring-save
-        ;; headings
-        "hi"  'markdown-insert-header-dwim
-        "hI"  'markdown-insert-header-setext-dwim
-        "h1"  'markdown-insert-header-atx-1
-        "h2"  'markdown-insert-header-atx-2
-        "h3"  'markdown-insert-header-atx-3
-        "h4"  'markdown-insert-header-atx-4
-        "h5"  'markdown-insert-header-atx-5
-        "h6"  'markdown-insert-header-atx-6
-        "h!"  'markdown-insert-header-setext-1
-        "h@"  'markdown-insert-header-setext-2
-        ;; Insertion of common elements
-        "-"   'markdown-insert-hr
-        "if"  'markdown-insert-footnote
-        "ii"  'markdown-insert-image
-        "ik"  'spacemacs/insert-keybinding-markdown
-        "iI"  'markdown-insert-reference-image
-        "il"  'markdown-insert-link
-        "iL"  'markdown-insert-reference-link-dwim
-        "iw"  'markdown-insert-wiki-link
-        "iu"  'markdown-insert-uri
-        ;; Element removal
-        "k"   'markdown-kill-thing-at-point
-        ;; List editing
-        "li"  'markdown-insert-list-item
-        ;; region manipulation
-        "xb"  'markdown-insert-bold
-        "xi"  'markdown-insert-italic
-        "xc"  'markdown-insert-code
-        "xC"  'markdown-insert-gfm-code-block
-        "xq"  'markdown-insert-blockquote
-        "xQ"  'markdown-blockquote-region
-        "xp"  'markdown-insert-pre
-        "xP"  'markdown-pre-region
-        ;; Following and Jumping
-        "N"   'markdown-next-link
-        "f"   'markdown-follow-thing-at-point
-        "P"   'markdown-previous-link
-        "<RET>" 'markdown-jump)
+      ;; note: `gfm-mode' is part of `markdown-mode.el' so we can define its key
+      ;; bindings here
+      (dolist (mode '(markdown-mode gfm-mode))
+        (spacemacs/set-leader-keys-for-major-mode mode
+          ;; Movement
+          "{"   'markdown-backward-paragraph
+          "}"   'markdown-forward-paragraph
+          ;; Completion, and Cycling
+          "]"   'markdown-complete
+          ;; Indentation
+          ">"   'markdown-indent-region
+          "<"   'markdown-exdent-region
+          ;; Buffer-wide commands
+          "c]"  'markdown-complete-buffer
+          "cc"  'markdown-check-refs
+          "ce"  'markdown-export
+          "cm"  'markdown-other-window
+          "cn"  'markdown-cleanup-list-numbers
+          "co"  'markdown-open
+          "cp"  'markdown-preview
+          "cv"  'markdown-export-and-preview
+          "cw"  'markdown-kill-ring-save
+          ;; headings
+          "hi"  'markdown-insert-header-dwim
+          "hI"  'markdown-insert-header-setext-dwim
+          "h1"  'markdown-insert-header-atx-1
+          "h2"  'markdown-insert-header-atx-2
+          "h3"  'markdown-insert-header-atx-3
+          "h4"  'markdown-insert-header-atx-4
+          "h5"  'markdown-insert-header-atx-5
+          "h6"  'markdown-insert-header-atx-6
+          "h!"  'markdown-insert-header-setext-1
+          "h@"  'markdown-insert-header-setext-2
+          ;; Insertion of common elements
+          "-"   'markdown-insert-hr
+          "if"  'markdown-insert-footnote
+          "ii"  'markdown-insert-image
+          "ik"  'spacemacs/insert-keybinding-markdown
+          "iI"  'markdown-insert-reference-image
+          "il"  'markdown-insert-link
+          "iL"  'markdown-insert-reference-link-dwim
+          "iw"  'markdown-insert-wiki-link
+          "iu"  'markdown-insert-uri
+          ;; Element removal
+          "k"   'markdown-kill-thing-at-point
+          ;; List editing
+          "li"  'markdown-insert-list-item
+          ;; region manipulation
+          "xb"  'markdown-insert-bold
+          "xi"  'markdown-insert-italic
+          "xc"  'markdown-insert-code
+          "xC"  'markdown-insert-gfm-code-block
+          "xq"  'markdown-insert-blockquote
+          "xQ"  'markdown-blockquote-region
+          "xp"  'markdown-insert-pre
+          "xP"  'markdown-pre-region
+          ;; Following and Jumping
+          "N"   'markdown-next-link
+          "f"   'markdown-follow-thing-at-point
+          "P"   'markdown-previous-link
+          "<RET>" 'markdown-jump))
       (when (eq 'eww markdown-live-preview-engine)
         (spacemacs/set-leader-keys-for-major-mode 'markdown-mode
           "cP"  'markdown-live-preview-mode))
