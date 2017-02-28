syl20bnr/spacemacs

commit eef1a87e98d8258c2a295566d954bb7b391942fb

Fix evil-search module activation
The search module must be set before requering evil otherwise the variable is
set but it has not effect and `isearch` module is the active module.

layers/+distributions/spacemacs-bootstrap/packages.el
@@ -41,10 +41,10 @@
     :override-mode-name spacemacs-leader-override-mode))
 
 (defun spacemacs-bootstrap/init-evil ()
-  (with-eval-after-load 'hybrid-mode
-    ;; ensure that the search module is set at startup
-    (spacemacs/set-evil-search-module dotspacemacs-editing-style)
-    (add-hook 'spacemacs-editing-style-hook 'spacemacs/set-evil-search-module))
+  ;; ensure that the search module is set at startup
+  ;; must be called before evil is required to really take effect.
+  (spacemacs/set-evil-search-module dotspacemacs-editing-style)
+  (add-hook 'spacemacs-editing-style-hook 'spacemacs/set-evil-search-module)
 
   ;; evil-mode is mandatory for Spacemacs to work properly
   ;; evil must be require explicitly, the autoload seems to not

layers/+distributions/spacemacs-base/local/hybrid-mode/hybrid-mode.el
@@ -52,7 +52,6 @@ behavior (for instance it support C-r pasting)."
   :group 'spacemacs
   :type 'boolean)
 
-
 (defvar hybrid-mode-default-state-backup evil-default-state
   "Backup of `evil-default-state'.")
 

layers/+distributions/spacemacs-bootstrap/funcs.el
@@ -49,7 +49,8 @@
   (cond
    ((or (eq 'vim style)
         (and (eq 'hybrid style)
-             hybrid-mode-use-evil-search-module))
+             (or (not (boundp 'hybrid-mode-use-evil-search-module))
+                 hybrid-mode-use-evil-search-module)))
     (setq-default evil-search-module 'evil-search))
    (t
     (setq-default evil-search-module 'isearch))))

commit b76832d9c53a957dbbc6e4d2bef93087c7815db2

revert bug introduced by commit eef1a87
Signed-off-by: Luke Lazurite <aoser97+dev@gmail.com>

layers/+distributions/spacemacs-bootstrap/packages.el
@@ -41,10 +41,10 @@
     :override-mode-name spacemacs-leader-override-mode))
 
 (defun spacemacs-bootstrap/init-evil ()
-  ;; ensure that the search module is set at startup
-  ;; must be called before evil is required to really take effect.
-  (spacemacs/set-evil-search-module dotspacemacs-editing-style)
-  (add-hook 'spacemacs-editing-style-hook 'spacemacs/set-evil-search-module)
+  (with-eval-after-load 'hybrid-mode
+    ;; ensure that the search module is set at startup
+    (spacemacs/set-evil-search-module dotspacemacs-editing-style)
+    (add-hook 'spacemacs-editing-style-hook 'spacemacs/set-evil-search-module))
 
   ;; evil-mode is mandatory for Spacemacs to work properly
   ;; evil must be require explicitly, the autoload seems to not