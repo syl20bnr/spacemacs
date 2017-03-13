d41eb4cb27cb575d01e6003e2c302a08109cbf81
Harry Hull <harry.hull@gliffy.com>
2017-03-13 14:55:31 -0700

    Use terraform fmt by default in the terraform layer

---
+++ layers/+tools/terraform/config.el
@@ -0,0 +1,15 @@
+;;; config.el -- terraform Layer configuration file for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Harry Hull <harry.hull1@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; variables
+
+(defvar terraform-fmt-on-save t
+  "If set then use terraform fmt before saving the terraform buffer. Enabled by default.")
