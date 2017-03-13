cf2f0232a747f130e775007a6e46a7b58ae75113
Harry Hull <harry.hull@gliffy.com>
2017-03-13 14:55:31 -0700

    Use terraform fmt by default in the terraform layer

--- layers/+tools/terraform/packages.el
+++ layers/+tools/terraform/packages.el
@@ -13,4 +13,8 @@
 
 (defun terraform/init-terraform-mode ()
   (use-package terraform-mode
-    :defer t))
+    :defer t
+    :config
+    (if terraform-fmt-on-save
+        (add-hook 'terraform-mode-hook 'terraform-format-on-save-mode))
+    ))
