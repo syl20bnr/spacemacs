78f098ae46b87edc12797feef7d31427922c08e4
syl20bnr <sylvain.benner@gmail.com>
2017-03-30 22:00:21 -0400

    Revert go-coverage to SPC m g c and move create-tages to SPC m g C
    SPC m g C is only used in Clojure whereas SPC m g c is used in various layers.
    This is simpler to just move create tags to SPC m g C and move the clojure
    bindings to SPC m g c (since it is not used in the clojure layer).

--- layers/+lang/go/packages.el
+++ layers/+lang/go/packages.el
@@ -114,7 +114,7 @@
         "ed" 'go-download-play
         "xx" 'spacemacs/go-run-main
         "ga" 'ff-find-other-file
-        "gС" 'go-coverage
+        "gc" 'go-coverage
         "tt" 'spacemacs/go-run-test-current-function
         "ts" 'spacemacs/go-run-test-current-suite
         "tp" 'spacemacs/go-run-package-tests
