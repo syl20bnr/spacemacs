d499c08b8b84c7dc31467e3da633229075ee5b72
jupl <juan.placencia.512@gmail.com>
2017-03-31 18:38:43 -0500

    Use add-node-modules-path to automatically find local executables

--- layers/+frameworks/react/packages.el
+++ layers/+frameworks/react/packages.el
@@ -39,7 +39,6 @@
   (with-eval-after-load 'flycheck
     (dolist (checker '(javascript-eslint javascript-standard))
       (flycheck-add-mode checker 'react-mode)))
-  (add-hook 'react-mode-hook #'spacemacs//react-use-eslint-from-node-modules)
   (spacemacs/enable-flycheck 'react-mode))
 
 (defun react/post-init-js-doc ()
