bb7e304e9b39023b5d693605097f25c3650915c6
syl20bnr <sylvain.benner@gmail.com>
2017-04-09 15:48:17 -0400

    perl6: refactor layer to be more idiomatic

--- layers/+lang/perl6/packages.el
+++ layers/+lang/perl6/packages.el
@@ -1,6 +1,6 @@
 ;;; packages.el --- perl6 layer packages file for Spacemacs.
 ;;
-;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
 ;;
 ;; Author:  Bahtiar `kalkin-`''Gadimov <bahtiar@gadimov.de>
 ;; URL: https://github.com/syl20bnr/spacemacs
@@ -9,43 +9,30 @@
 ;;
 ;;; License: GPLv3
 
-(defvar perl6-packages
+(defconst perl6-packages
   '(company
+    evil
     flycheck
-    flycheck-perl6
+    (flycheck-perl6 :toggle (configuration-layer/package-usedp 'flycheck))
     perl6-mode
-    json-mode
-    )
-)
+    ))
 
 (defun perl6/post-init-company ()
   (spacemacs|add-company-backends
     :backends company-capf
     :modes perl6-mode))
 
-(defun perl6/init-json-mode () (use-package json-mode :defer t))
+(defun perl6/post-init-evil ()
+  (add-to-list 'spacemacs-jump-handlers-perl6-mode 'evil-jump-to-tag))
+
+(defun perl6/post-init-flycheck ()
+  (spacemacs/enable-flycheck 'perl6-mode))
+
+(defun perl6/init-flycheck-perl6 ()
+  (with-eval-after-load 'flycheck
+    (require 'flycheck-perl6)))
 
 (defun perl6/init-perl6-mode()
   (use-package perl6-mode
     :defer t
-    :init
-    (progn
-      (add-to-list 'spacemacs-jump-handlers-perl6-mode 'evil-jump-to-tag)
-      (add-to-list 'auto-mode-alist '("/perl6/site/sources/" . perl6-mode))
-      (when (configuration-layer/package-usedp 'company)
-        (push 'company-capf company-backends-perl6-mode)
-        )
-      (spacemacs|define-text-object "«" "double-angle-bracket" "«" "»")
-      (spacemacs|define-text-object "｢" "corner-bracket" "｢" "｣")
-      (spacemacs|define-text-object "‘" "single-quotation-mark" "‘" "’")
-      (spacemacs|define-text-object "“" "double-quotation-mark" "“" "”")
-      )
-    )
-  )
-
-(defun perl6/init-flycheck-perl6 () (use-package flycheck-perl6 :ensure t :if
-                                      (configuration-layer/package-usedp 'flycheck)) )
-
-(defun perl6/post-init-flycheck () (spacemacs/add-flycheck-hook 'perl6-mode))
-
-;;; packages.el ends here
+    :mode (("/perl6/site/sources/" . perl6-mode))))
--- layers/+lang/protobuf/packages.el
+++ layers/+lang/protobuf/packages.el
@@ -1,6 +1,6 @@
 ;;; packages.el --- Protocol Buffers Layer packages File for Spacemacs
 ;;
-;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
 ;;
 ;; Author: Amol Mandhane <https://github.com/amol-mandhane>
 ;; URL: https://github.com/syl20bnr/spacemacs
