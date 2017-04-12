b9be68a0a1ab92fe57413d7d6f1d7b055042ed49
MadAnd <dev@madand.net>
2017-04-12 15:29:39 +0300

    Fix signatures of spacemacs/evil-mc-paste-{after,before}
    Fixes #8672

--- layers/+spacemacs/spacemacs-evil/funcs.el
+++ layers/+spacemacs/spacemacs-evil/funcs.el
@@ -84,14 +84,14 @@ Otherwise, revert to the default behavior (i.e. enable `evil-insert-state')."
        (or (not (fboundp 'evil-mc-get-cursor-count))
            (eq (evil-mc-get-cursor-count) 1))))
 
-(defun spacemacs/evil-mc-paste-after (&optional count &optional register)
+(defun spacemacs/evil-mc-paste-after (&optional count register)
   "Disable paste transient state if there is more that 1 cursor."
   (interactive "p")
   (if (spacemacs//paste-transient-state-p)
       (spacemacs/paste-transient-state/evil-paste-after)
     (evil-paste-after count register)))
 
-(defun spacemacs/evil-mc-paste-before (&optional count &optional register)
+(defun spacemacs/evil-mc-paste-before (&optional count register)
   "Disable paste transient state if there is more that 1 cursor."
   (interactive "p")
   (if (spacemacs//paste-transient-state-p)
