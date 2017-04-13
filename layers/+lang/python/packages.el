6e0563cc77469b54d9e93793cef4db4581b1bb9f
syl20bnr <sylvain.benner@gmail.com>
2017-04-12 23:18:42 -0400

    python: hide YAPF lighter in modeline
    Circled lighters in modeline are reserved for toggles under SPC t and SPC T

--- layers/+lang/python/packages.el
+++ layers/+lang/python/packages.el
@@ -445,5 +445,5 @@ fix this issue."
       (spacemacs/set-leader-keys-for-major-mode 'python-mode
         "=" 'yapfify-buffer)
       (when python-enable-yapf-format-on-save
-        (add-hook 'python-mode-hook 'yapf-mode))
-      (spacemacs|diminish yapf-mode " Ⓨ" " Y"))))
+        (add-hook 'python-mode-hook 'yapf-mode)))
+    :config (spacemacs|hide-lighter yapf-mode)))
