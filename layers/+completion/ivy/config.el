057208e918bee635e7a28f0c14ac49b64b4a8731
syl20bnr <sylvain.benner@gmail.com>
2017-04-12 23:35:23 -0400

    Remove unecessary parenthesis in comments

--- layers/+completion/ivy/config.el
+++ layers/+completion/ivy/config.el
@@ -15,7 +15,7 @@
 (defvar spacemacs--counsel-commands
   '(;; --line-number forces line numbers (disabled by default on windows)
     ;; no --vimgrep because it adds column numbers that wgrep can't handle
-    ;; (see https://github.com/syl20bnr/spacemacs/pull/8065 )
+    ;; see https://github.com/syl20bnr/spacemacs/pull/8065
     ("rg" . "rg --smart-case --no-heading --color never --line-number %s %S .")
     ("ag" . "ag --nocolor --nogroup %s %S .")
     ("pt" . "pt -e --nocolor --nogroup %s %S .")
