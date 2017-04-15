243aadd8c6b9faaab8f7cb6c1f12b19b30b6d7d3
Kevin Sonney <kevin@sonney.com>
2017-04-15 17:32:21 -0400

    Added Mastodon.el

---
+++ layers/+web-services/mastodon/packages.el
@@ -0,0 +1,24 @@
+;;; packages.el --- mastodon layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Kevin Sonney <kevin@sonney.com>
+;; URL: https://github.com/ksonney/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst mastodon-packages
+  '(
+    (mastodon :location (recipe
+                         :fetcher github
+                         :repo "jdenen/mastodon.el"
+                         :files ("lisp/*.el" "README.org")))))
+
+(defun mastodon/init-mastodon ()
+  ;; (use-package mastodon)
+  (require 'mastodon)
+  (setq mastdon-token-file "~/emacs.d/private/mastodon.plstore"))
+
+;;; packages.el ends here
