9e3907675990a13cd56c0d04f2ebbbae364cf747
Jonathan del Strother <jdelStrother@gmail.com>
2017-04-17 16:29:23 +0100

    Use add-node-modules-path to lookup flow for flycheck-flow

--- layers/+lang/flow-type/packages.el
+++ layers/+lang/flow-type/packages.el
@@ -16,15 +16,20 @@
     (flycheck-flow :toggle (configuration-layer/package-usedp 'flycheck))
     eldoc
     js2-mode
-    web-mode))
+    web-mode
+    add-node-modules-path))
 
 (defun flow-type/init-flow-minor-mode()
+  (use-package flow-minor-mode)
   (spacemacs/declare-prefix-for-mode 'flow-minor-mode "mf" "flow" "flow type checker commands")
   (spacemacs/set-leader-keys-for-minor-mode 'flow-minor-mode
     "fd" 'flow-minor-jump-to-definition
     "fc" 'flow-minor-status
     "ft" 'flow-minor-type-at-pos
-  ))
+    ))
+
+(defun flow-type/init-add-node-modules-path()
+  (use-package add-node-modules-path))
 
 (defun flow-type/post-init-eldoc()
   (when (configuration-layer/package-usedp 'js2-mode)
@@ -37,11 +42,13 @@
 
 (defun flow-type/post-init-js2-mode()
   (add-hook 'js2-mode-hook 'flow-minor-enable-automatically)
+  (add-hook 'js2-mode-hook 'add-node-modules-path)
   (add-to-list 'spacemacs-jump-handlers-js2-mode 'flow-minor-jump-to-definition))
 
 (defun flow-type/post-init-web-mode()
   (when (configuration-layer/layer-usedp 'react)
     (add-hook 'react-mode-hook 'flow-minor-enable-automatically)
+    (add-hook 'react-mode-hook 'add-node-modules-path)
     (add-to-list 'spacemacs-jump-handlers-react-mode 'flow-minor-jump-to-definition)
     ))
 
