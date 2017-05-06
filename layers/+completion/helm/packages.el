c4ce10ad821f48066b9b5b371e2a0ca86d3a0b69
Aaron Jensen <aaronjensen@gmail.com>
2017-05-06 16:33:09 -0700

    helm/ivy: Limit rg results to 150 columns
    Emacs doesn't handle long line lengths very well. If you accidentally rg a file
    a file that has very long line lengths it can freeze emacs. This prevents rg
    from returning any results with more than 150 characters. Instead, it will
    indicate that there were results in that file but they were elided.

--- layers/+completion/helm/packages.el
+++ layers/+completion/helm/packages.el
@@ -250,7 +250,7 @@ Search for a search tool in the order provided by `dotspacemacs-search-tools'."
         ;; --line-number forces line numbers (disabled by default on windows)
         ;; no --vimgrep because it adds column numbers that wgrep can't handle
         ;; see https://github.com/syl20bnr/spacemacs/pull/8065
-        (let ((helm-ag-base-command "rg --smart-case --no-heading --color never --line-number"))
+        (let ((helm-ag-base-command "rg --smart-case --no-heading --color never --line-number --max-columns 150"))
           (helm-do-ag dir)))
 
       (defun spacemacs/helm-files-do-rg-region-or-symbol ()
@@ -316,7 +316,7 @@ Search for a search tool in the order provided by `dotspacemacs-search-tools'."
         ;; --line-number forces line numbers (disabled by default on windows)
         ;; no --vimgrep because it adds column numbers that wgrep can't handle
         ;; see https://github.com/syl20bnr/spacemacs/pull/8065
-        (let ((helm-ag-base-command "rg --smart-case --no-heading --color never --line-number"))
+        (let ((helm-ag-base-command "rg --smart-case --no-heading --color never --line-number --max-columns 150"))
           (helm-do-ag-buffers)))
 
       (defun spacemacs/helm-buffers-do-rg-region-or-symbol ()
