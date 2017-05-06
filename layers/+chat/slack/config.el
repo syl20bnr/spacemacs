90d99808784bf237c02c883215830e1e94184204
Benjamin Reynolds <breyno127@gmail.com>
2017-05-06 12:20:27 -0500

    Allow customization of slack custom layout name and binding
    This commit adds two configuration variables to the slack layer allowing a user
    to customize the name and keybinding of the slack custom layout, following the
    conventions from the erc and mu4e layouts.

---
+++ layers/+chat/slack/config.el
@@ -0,0 +1,18 @@
+;;; config.el --- slack layer configuration file for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Benjamin Reynolds <breyno127@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; Variables
+
+(defvar slack-spacemacs-layout-name "@Slack"
+  "Name used in the setup for `spacemacs-layouts' micro-state")
+
+(defvar slack-spacemacs-layout-binding "s"
+  "Binding used in the setup for `spacemacs-layouts' micro-state")
