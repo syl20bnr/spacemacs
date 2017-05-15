bc6c38c7e0ad9dbbefa5fd4aff99e1c824c871fe
Robbert van der Helm <mail@robbertvanderhelm.nl>
2017-05-15 12:16:50 +0200

    Add a layer for LanguageTool support
    LanguageTool is an automated proofreader and grammar checker. This layer adds
    the ability to check the current buffer's contents using LanguageTool.

---
+++ layers/+tools/languagetool/packages.el
@@ -0,0 +1,29 @@
+;;; packages.el --- languagetool layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Robbert van der Helm <mail@robbertvanderhelm.nl>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst languagetool-packages
+  '((langtool :toggle (spacemacs//languagetool-detect))))
+
+(defun languagetool/init-langtool ()
+    (use-package langtool
+      :defer t
+      :init
+      (progn
+        ;; The whitespace rules give a lot of false positives when linting rich
+        ;; text.
+        (setq-default langtool-disabled-rules '("WHITESPACE_RULE"))
+        (spacemacs/set-leader-keys
+          "Sl" 'spacemacs/languagetool-toggle
+          "SL" 'langtool-correct-buffer)
+        (define-key evil-normal-state-map (kbd "[ a")
+          'spacemacs/languagetool-previous-error)
+        (define-key evil-normal-state-map (kbd "] a")
+          'spacemacs/languagetool-next-error))))
