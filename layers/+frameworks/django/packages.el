fc0c448ab1ee054b618a9ac8fe586fcf2d4dd899
BorisVerk <boris.verk@gmail.com>
2017-05-15 12:13:07 -0600

    added names to django mode prefixes

--- layers/+frameworks/django/packages.el
+++ layers/+frameworks/django/packages.el
@@ -18,36 +18,43 @@
   (use-package pony-mode
     :defer t
     :init (progn
+            (spacemacs/declare-prefix-for-mode 'python-mode "mj" "django")
+            (spacemacs/declare-prefix-for-mode 'python-mode "mja" "fabric")
+            (spacemacs/declare-prefix-for-mode 'python-mode "mjf" "files")
+            (spacemacs/declare-prefix-for-mode 'python-mode "mji" "interactive")
+            (spacemacs/declare-prefix-for-mode 'python-mode "mjr" "runserver")
+            (spacemacs/declare-prefix-for-mode 'python-mode "mjs" "south/syncdb")
+            (spacemacs/declare-prefix-for-mode 'python-mode "mjt" "test")
+
             (spacemacs/set-leader-keys-for-major-mode 'python-mode
-              ; d*j*ango f*a*bric
               "jaf" 'pony-fabric
               "jad" 'pony-fabric-deploy
-              ; d*j*ango *f*iles
+
               "jfs" 'pony-goto-settings
               "jfc" 'pony-setting
               "jft" 'pony-goto-template
               "jfr" 'pony-resolve
-              ; d*j*ango *i*nteractive
+
               "jid" 'pony-db-shell
               "jis" 'pony-shell
-              ; d*j*ango *m*anage
+
               ; not including one-off management commands like "flush" and
               ; "startapp" even though they're implemented in pony-mode,
               ; because this is much handier
               "jm" 'pony-manage
-              ; d*j*ango *r*unserver
+
               "jrd" 'pony-stopserver
               "jro" 'pony-browser
               "jrr" 'pony-restart-server
               "jru" 'pony-runserver
               "jrt" 'pony-temp-server
-              ; d*j*ango *s*outh/*s*yncdb
+
               "jsc" 'pony-south-convert
               "jsh" 'pony-south-schemamigration
               "jsi" 'pony-south-initial
               "jsm" 'pony-south-migrate
               "jss" 'pony-syncdb
-              ; d*j*ango *t*est
+
               "jtd" 'pony-test-down
               "jte" 'pony-test-goto-err
               "jto" 'pony-test-open
