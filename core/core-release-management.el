eb3414867babb5e682bcece5a502ee7bba1e3f56
Fuqiao Xue <xfq.free@gmail.com>
2017-05-17 09:46:34 +0800

    Mention the change of dotspacemacs-check-for-update in the documentation
    Its default value was changed to nil in 9d72b122459a45edc81511522f72c7ba58488e50.

--- core/core-release-management.el
+++ core/core-release-management.el
@@ -84,7 +84,7 @@ users on `develop' branch must manually pull last commits instead."
           (t (message "Update aborted.")))))
 
 (defun spacemacs/check-for-new-version (force &optional interval)
-  "Periodicly check for new for new Spacemacs version.
+  "Periodically check for new for new Spacemacs version.
 Update `spacemacs-new-version' variable if any new version has been
 found."
   (interactive "P")
