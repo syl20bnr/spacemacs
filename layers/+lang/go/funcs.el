aac37d2b66064c6dbf24709c090b1bfbf62ff696
Eivind Fonn <evfonn@gmail.com>
2017-05-25 13:20:36 +0200

    Some refactoring of go layer

--- layers/+lang/go/funcs.el
+++ layers/+lang/go/funcs.el
@@ -30,3 +30,43 @@
                                       go-test
                                       go-errcheck))
    (flycheck-gometalinter-setup))
+
+(defun spacemacs/go-run-tests (args)
+  (interactive)
+  (compilation-start (concat "go test " args " " go-use-test-args)
+                     nil (lambda (n) go-test-buffer-name) nil))
+
+(defun spacemacs/go-run-package-tests ()
+  (interactive)
+  (spacemacs/go-run-tests ""))
+
+(defun spacemacs/go-run-package-tests-nested ()
+  (interactive)
+  (spacemacs/go-run-tests "./..."))
+
+(defun spacemacs/go-run-test-current-function ()
+  (interactive)
+  (if (string-match "_test\\.go" buffer-file-name)
+      (let ((test-method (if go-use-gocheck-for-testing
+                             "-check.f"
+                           "-run")))
+        (save-excursion
+          (re-search-backward "^func[ ]+\\(([[:alnum:]]*?[ ]?[*]?[[:alnum:]]+)[ ]+\\)?\\(Test[[:alnum:]_]+\\)(.*)")
+          (spacemacs/go-run-tests (concat test-method "='" (match-string-no-properties 2) "$'"))))
+    (message "Must be in a _test.go file to run go-run-test-current-function")))
+
+(defun spacemacs/go-run-test-current-suite ()
+  (interactive)
+  (if (string-match "_test\.go" buffer-file-name)
+      (if go-use-gocheck-for-testing
+          (save-excursion
+            (re-search-backward "^func[ ]+\\(([[:alnum:]]*?[ ]?[*]?\\([[:alnum:]]+\\))[ ]+\\)?Test[[:alnum:]_]+(.*)")
+            (spacemacs/go-run-tests (concat "-check.f='" (match-string-no-properties 2) "'")))
+        (message "Gocheck is needed to test the current suite"))
+    (message "Must be in a _test.go file to run go-test-current-suite")))
+
+(defun spacemacs/go-run-main ()
+  (interactive)
+  (shell-command
+   (format "go run %s"
+           (shell-quote-argument (buffer-file-name (buffer-base-buffer))))))
