syl20bnr/spacemacs

commit bfb565eea9a395892b9961f970bf76de53754dc8

Fix pasting ignoring register selection
Pasting in evil-mode has been broken since
`58458f2d2abcc1211444c2060ab598f55e518da4`, as `p` and `P` would always paste
from the unnamed register instead of the register selected with `"`. I've
removed the function's second parameter, as it wasn't being used anywhere.

Fixes #8759

layers/+spacemacs/spacemacs-evil/funcs.el
@@ -84,16 +84,16 @@ Otherwise, revert to the default behavior (i.e. enable `evil-insert-state')."
        (or (not (fboundp 'evil-mc-get-cursor-count))
            (eq (evil-mc-get-cursor-count) 1))))
 
-(defun spacemacs/evil-mc-paste-after (&optional count register)
+(defun spacemacs/evil-mc-paste-after (&optional count)
   "Disable paste transient state if there is more that 1 cursor."
   (interactive "p")
   (if (spacemacs//paste-transient-state-p)
       (spacemacs/paste-transient-state/evil-paste-after)
-    (evil-paste-after count register)))
+    (evil-paste-after count evil-this-register)))
 
-(defun spacemacs/evil-mc-paste-before (&optional count register)
+(defun spacemacs/evil-mc-paste-before (&optional count)
   "Disable paste transient state if there is more that 1 cursor."
   (interactive "p")
   (if (spacemacs//paste-transient-state-p)
       (spacemacs/paste-transient-state/evil-paste-before)
-    (evil-paste-before count register)))
+    (evil-paste-before count evil-this-register)))

commit 31163062b77d087c623f6a6ed6a66850f3cf9184

spacemacs/evil-mc-paste-{before,after}: fix multiple cursor paste
* restore these functions' signatures to those of their stock evil
  counterparts (evil-paste-{before,after}), allowing
  `evil-mc-execute-evil-paste` to paste at each cursor when there
  are multiple instead of pasting at only one cursor

* retain these functions' ability to directly take a register as
  input when called interactively

* regression was caused by bfb565eea9a395892b9961f970bf76de53754dc8

layers/+spacemacs/spacemacs-evil/funcs.el
@@ -84,16 +84,16 @@ Otherwise, revert to the default behavior (i.e. enable `evil-insert-state')."
        (or (not (fboundp 'evil-mc-get-cursor-count))
            (eq (evil-mc-get-cursor-count) 1))))
 
-(defun spacemacs/evil-mc-paste-after (&optional count)
+(defun spacemacs/evil-mc-paste-after (&optional count register)
   "Disable paste transient state if there is more that 1 cursor."
   (interactive "p")
   (if (spacemacs//paste-transient-state-p)
       (spacemacs/paste-transient-state/evil-paste-after)
-    (evil-paste-after count evil-this-register)))
+    (evil-paste-after count (or register evil-this-register))))
 
-(defun spacemacs/evil-mc-paste-before (&optional count)
+(defun spacemacs/evil-mc-paste-before (&optional count register)
   "Disable paste transient state if there is more that 1 cursor."
   (interactive "p")
   (if (spacemacs//paste-transient-state-p)
       (spacemacs/paste-transient-state/evil-paste-before)
-    (evil-paste-before count evil-this-register)))
+    (evil-paste-before count (or register evil-this-register))))