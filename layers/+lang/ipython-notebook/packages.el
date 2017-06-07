e6a0ed0204230fc1bf17ae78c6ad4ee92204b813
Eivind Fonn <evfonn@gmail.com>
2017-06-07 13:42:47 +0200

    ein: remove spurious spaces

--- layers/+lang/ipython-notebook/packages.el
+++ layers/+lang/ipython-notebook/packages.el
@@ -61,17 +61,17 @@
         "R" 'ein:worksheet-rename-sheet
         "RET" 'ein:worksheet-execute-cell-and-goto-next
         ;; Output
-        " C-l" 'ein:worksheet-clear-output
-        " C-S-l" 'ein:worksheet-clear-all-output
+        "C-l" 'ein:worksheet-clear-output
+        "C-S-l" 'ein:worksheet-clear-all-output
         ;;Console
-        " C-o" 'ein:console-open
+        "C-o" 'ein:console-open
         ;; Merge cells
-        " C-k" 'ein:worksheet-merge-cell
-        " C-j" 'spacemacs/ein:worksheet-merge-cell-next
-        " s" 'ein:worksheet-split-cell-at-point
+        "C-k" 'ein:worksheet-merge-cell
+        "C-j" 'spacemacs/ein:worksheet-merge-cell-next
+        "s" 'ein:worksheet-split-cell-at-point
         ;; Notebook
-        " C-s" 'ein:notebook-save-notebook-command
-        " C-r" 'ein:notebook-rename-command
+        "C-s" 'ein:notebook-save-notebook-command
+        "C-r" 'ein:notebook-rename-command
         "1" 'ein:notebook-worksheet-open-1th
         "2" 'ein:notebook-worksheet-open-2th
         "3" 'ein:notebook-worksheet-open-3th
