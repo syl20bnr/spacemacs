2db99588949133bf41ddfe64687eaa7e11c7092d
Eivind Fonn <evfonn@gmail.com>
2017-06-10 16:31:05 +0200

    string-inflection: transient state for cycling

--- layers/+spacemacs/spacemacs-editing/packages.el
+++ layers/+spacemacs/spacemacs-editing/packages.el
@@ -362,8 +362,13 @@
   (use-package string-inflection
     :init
     (progn
+      (spacemacs|define-transient-state string-inflection
+        :title "String inflection transient state"
+        :doc "\n [_i_] cycle"
+        :bindings
+        ("i" string-inflection-all-cycle))
       (spacemacs/set-leader-keys
-        "xii" 'string-inflection-all-cycle
+        "xii" 'spacemacs/string-inflection-transient-state/body
         "xiu" 'string-inflection-underscore
         "xi_" 'string-inflection-underscore
         "xiU" 'string-inflection-upcase
