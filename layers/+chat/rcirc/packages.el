37cdf07e9186414c8f712276f911d7d492996187
Eivind Fonn <evfonn@gmail.com>
2017-06-18 12:47:11 +0200

    rcirc: move defuns to funcs.el

--- layers/+chat/rcirc/packages.el
+++ layers/+chat/rcirc/packages.el
@@ -40,10 +40,6 @@
     :binding rcirc-spacemacs-layout-binding
     :body
     (progn
-      (defun spacemacs-layouts/add-rcirc-buffer-to-persp ()
-        (persp-add-buffer (current-buffer)
-                          (persp-get-by-name
-                           rcirc-spacemacs-layout-name)))
       (add-hook 'rcirc-mode-hook #'spacemacs-layouts/add-rcirc-buffer-to-persp)
       (call-interactively 'spacemacs/rcirc))))
 
@@ -53,37 +49,9 @@
     :init
     (progn
       (spacemacs/add-to-hook 'rcirc-mode-hook '(rcirc-omit-mode
-                                                rcirc-track-minor-mode))
-
-      (defun spacemacs//rcirc-with-authinfo (arg)
-        "Fire rcirc with support for authinfo."
-        (unless arg
-          (if (file-exists-p "~/.authinfo.gpg")
-              (spacemacs//rcirc-authinfo-config)
-            (message "Cannot find file ~/.authinfo.gpg")))
-        (rcirc arg))
-
-      (defun spacemacs//rcirc-with-znc (arg)
-        "Fire rcirc with support for znc."
-        (if arg
-            (rcirc arg)
-          (setq rcirc-server-alist
-                ;; This will replace :auth with the correct thing, see the
-                ;; doc for that function
-                (spacemacs//znc-rcirc-server-alist-get-authinfo
-                 rcirc-server-alist))
-          (spacemacs//znc-rcirc-connect)))
+                                         rcirc-track-minor-mode))
 
       (spacemacs/set-leader-keys "air" 'spacemacs/rcirc)
-      (defun spacemacs/rcirc (arg)
-        "Launch rcirc."
-        (interactive "P")
-        (require 'rcirc)
-        ;; dispatch to rcirc launcher with appropriate support
-        (cond
-         (rcirc-enable-authinfo-support (spacemacs//rcirc-with-authinfo arg))
-         (rcirc-enable-znc-support (spacemacs//rcirc-with-znc arg))
-         (t (rcirc arg))))
       (evil-set-initial-state 'rcirc-mode 'insert))
     :config
     (progn
@@ -115,38 +83,13 @@
 
       ;; add a key for EMMS integration
       (when (boundp 'emms-track-description)
-        (defun rcirc/insert-current-emms-track ()
-          (interactive)
-          (insert (emms-track-description (emms-playlist-current-selected-track))))
-        (define-key rcirc-mode-map (kbd "C-c C-e") 'rcirc/insert-current-emms-track))
+        (define-key rcirc-mode-map (kbd "C-c C-e") 'spacemacs/rcirc-insert-current-emms-track))
 
       ;; Minimal logging to `~/.emacs.d/.cache/rcirc-logs/'
       ;; by courtesy of Trent Buck.
       (setq rcirc-log-directory (concat spacemacs-cache-directory "/rcirc-logs/"))
       (setq rcirc-log-flag t)
-      (defun rcirc-write-log (process sender response target text)
-        (when rcirc-log-directory
-          (with-temp-buffer
-            ;; Sometimes TARGET is a buffer :-(
-            (when (bufferp target)
-              (setq target (with-current-buffer buffer rcirc-target)))
-            ;; Sometimes buffer is not anything at all!
-            (unless (or (null target) (string= target ""))
-              ;; Print the line into the temp buffer.
-              (insert (format-time-string "%Y-%m-%d %H:%M "))
-              (insert (format "%-16s " (rcirc-user-nick sender)))
-              (unless (string= response "PRIVMSG")
-                (insert "/" (downcase response) " "))
-              (insert text "\n")
-              ;; Append the line to the appropriate logfile.
-              (let ((coding-system-for-write 'no-conversion)
-                    (logfile (concat rcirc-log-directory  (downcase target))))
-                (when (not (file-directory-p (file-name-directory logfile)))
-                  (make-directory (file-name-directory logfile)))
-                (write-region (point-min) (point-max)
-                              logfile
-                              t 'quietly))))))
-      (add-hook 'rcirc-print-hooks 'rcirc-write-log)
+      (add-hook 'rcirc-print-hooks 'spacemacs//rcirc-write-log)
 
       ;; dependencies
       ;; will autoload rcirc-notify
@@ -161,11 +104,4 @@
     :defer t
     :config
     (progn
-      (defun spacemacs/rcirc-notify-beep (msg)
-        "Beep when notifying."
-        (let ((player "mplayer")
-              (sound (concat spacemacs-start-directory "site-misc/startup.ogg")))
-          (when (and (executable-find player)
-                     (file-exists-p sound)))
-          (start-process "beep-process" nil player sound)))
       (add-hook 'rcirc-notify-page-me-hooks 'spacemacs/rcirc-notify-beep))))
