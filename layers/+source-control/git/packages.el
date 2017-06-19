37ae70878521ea1f4c52a076f2606b22da1821dc
d12frosted <boris@d12frosted.io>
2017-06-19 11:34:25 +0300

    install orgit only when org package is used
    Fixes #8890

--- layers/+source-control/git/packages.el
+++ layers/+source-control/git/packages.el
@@ -25,7 +25,7 @@
         magit-gitflow
         ;; not compatible with magit 2.1 at the time of release
         ;; magit-svn
-        orgit
+        (orgit :toggle (configuration-layer/package-usedp 'org))
         smeargle
         ))
 
