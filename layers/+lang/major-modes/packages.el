28823177097ba59d2b654b5f980746a755d93c66
Eivind Fonn <evfonn@gmail.com>
2017-06-19 10:25:14 +0200

    Move extra-langs to major-modes

---
+++ layers/+lang/major-modes/packages.el
@@ -0,0 +1,42 @@
+(setq major-modes-packages
+      '(
+        arduino-mode
+        julia-mode
+        matlab-mode
+        qml-mode
+        scad-mode
+        stan-mode
+        thrift
+        wolfram-mode
+        ))
+
+(defun major-modes/init-arduino-mode ())
+
+(defun major-modes/init-julia-mode ())
+
+(defun major-modes/init-matlab-mode ()
+  (use-package matlab-mode
+    :defer t
+    :init
+    ;; Explicitly run prog-mode hooks since matlab-mode does not derive from
+    ;; prog-mode major-mode
+    (add-hook 'matlab-mode-hook 'spacemacs/run-prog-mode-hooks)))
+
+(defun major-modes/init-qml-mode ()
+  (use-package qml-mode
+    :defer t
+    :mode "\\.qml\\'"))
+
+(defun major-modes/init-scad-mode ())
+
+(defun major-modes/init-stan-mode ())
+
+(defun major-modes/init-thrift ())
+
+;; .m files are not associated because conflict with more common Objective-C and
+;; MATLAB/Octave, manually invoke for .m files.
+(defun major-modes/init-wolfram-mode ()
+  (use-package wolfram-mode
+    :defer t
+    :interpreter "\\(Wolfram\\|Mathematica\\)Script\\( -script\\)?"
+    :mode "\\.wl\\'"))
