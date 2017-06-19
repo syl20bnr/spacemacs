25366dfe36ad6c45213787489e6e9ac6343c37ae
Sven Fischer <sven@leiderfischer.de>
2017-06-19 10:51:36 +0200

    major-modes: add pkgbuild

--- layers/+lang/major-modes/packages.el
+++ layers/+lang/major-modes/packages.el
@@ -4,6 +4,7 @@
         julia-mode
         (logcat :location (recipe :fetcher github :repo "dcolascione/logcat-mode"))
         matlab-mode
+        pkgbuild-mode
         qml-mode
         scad-mode
         stan-mode
@@ -26,6 +27,20 @@
     ;; Explicitly run prog-mode hooks since matlab-mode does not derive from
     ;; prog-mode major-mode
     (add-hook 'matlab-mode-hook 'spacemacs/run-prog-mode-hooks)))
+
+(defun major-modes/init-pkgbuild-mode ()
+  (use-package pkgbuild-mode
+    :mode ("\\`PKGBUILD\\'" . pkgbuild-mode)
+    :defer t
+    :init
+    (progn
+      (spacemacs/set-leader-keys-for-major-mode 'pkgbuild-mode
+        "r" 'pkgbuild-increase-release-tag
+        "b" 'pkgbuild-makepkg
+        "a" 'pkgbuild-tar
+        "u" 'pkgbuild-browse-url
+        "m" 'pkgbuild-update-sums-line
+        "e" 'pkgbuild-etags))))
 
 (defun major-modes/init-qml-mode ()
   (use-package qml-mode
