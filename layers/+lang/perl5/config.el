08757ac6194f2ab9f072d975223140508f827fe5
CeleritasCelery <t.macman@gmail.com>
2017-06-24 15:17:56 -0600

    added cperl keybindings
    added keybindings for cperl POD and HERE functions, added dump jump support, and fixed tab command

---
+++ layers/+lang/perl5/config.el
@@ -0,0 +1,12 @@
+;;; config.el --- Perl5 Layer config File for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Troy Hinckley <troyhinckley@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(spacemacs|define-jump-handlers cperl-mode)
