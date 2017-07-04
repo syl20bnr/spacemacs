039707501166d87b830c343bdccae1390e75f0f2
JAremko <w3techplayground@gmail.com>
2017-07-04 11:41:21 +0300

    Implement and bind "distraction free mode" to SPC w C

--- layers/+distributions/spacemacs-base/funcs.el
+++ layers/+distributions/spacemacs-base/funcs.el
@@ -130,8 +130,26 @@ automatically applied to."
   (when (require 'centered-buffer-mode nil t)
     (call-interactively 'spacemacs-centered-buffer-mode)))
 
+(defun spacemacs/toggle-centered-buffer-mode-frame ()
+  "Open current buffer in the new frame centered and without mode-line."
+  (interactive)
+  (when (require 'centered-buffer-mode nil t)
+    (switch-to-buffer-other-frame (current-buffer) t)
+    (toggle-frame-fullscreen)
+    (run-with-idle-timer
+     ;; FIXME: We need this delay to make sure that the
+     ;; `toggle-frame-fullscreen' fully "finished"
+     ;; it will be better to use something more reliable
+     ;; instead :)
+     1
+     nil
+     (lambda ()
+       (call-interactively 'spacemacs-centered-buffer-mode)
+       (setq mode-line-format nil)))))
+
 (defun spacemacs/centered-buffer-mode-full-width ()
   "Center buffer in the frame."
+  ;; FIXME Needs new key-binding.
   (interactive)
   (when (require 'centered-buffer-mode nil t)
     (spacemacs/maximize-horizontally)
