5e7eb5d01ca1c8d4f57664da634c9a84805acbdf
Matt Bray <mattjbray@gmail.com>
2017-07-04 16:29:54 +0100

    Add kubernetes layer.

---
+++ layers/+tools/kubernetes/packages.el
@@ -0,0 +1,32 @@
+;;; packages.el --- kubernetes layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Matt Bray & Contributors
+;;
+;; Author: Matt Bray <mattjbray@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Code:
+
+(defconst kubernetes-packages
+  '(
+    kubernetes
+    kubernetes-evil
+    )
+  )
+
+(defun kubernetes/init-kubernetes ()
+  (use-package kubernetes
+    :defer t
+    ;; Autoload for 'kubernetes-overview is defined in "kubernetes-overview.el".
+    ;; Add an autoload for the whole 'kubernetes package when kubernetes-overview is called.
+    :commands (kubernetes-overview)
+    :init (spacemacs/set-leader-keys "aK" 'kubernetes-overview)))
+
+(defun kubernetes/init-kubernetes-evil ()
+  (use-package kubernetes-evil :after kubernetes-overview))
+
+;;; packages.el ends here
