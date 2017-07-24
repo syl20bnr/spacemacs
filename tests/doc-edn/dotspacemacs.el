ab857b8a50d42f2c70d904d6e76a4fe8fdbb610b
JAremko <w3techplayground@gmail.com>
2017-07-24 22:39:02 +0300

    core-documentation-edn refactoring

--- tests/doc-edn/dotspacemacs.el
+++ tests/doc-edn/dotspacemacs.el
@@ -2,17 +2,8 @@
   (setq-default
    dotspacemacs-distribution 'spacemacs
    dotspacemacs-configuration-layers '(
-                                       (org :variables
-                                            org-enable-github-support t
-                                            org-enable-bootstrap-support t
-                                            org-enable-reveal-js-support t
-                                            )
+                                       org
                                        bibtex
-                                       (latex :variables
-                                              latex-enable-auto-fill t
-                                              latex-enable-folding t
-                                              )
-                                       html
                                        )))
 (defun dotspacemacs/init ())
 (defun dotspacemacs/user-init ())
