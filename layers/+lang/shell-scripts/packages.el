c6afc2a0fcd0da8607ed439ec290f0749e5963f9
smile13241324 <smile13241324@gmail.com>
2017-08-06 00:10:39 +0200

    Add insert commands from sh-script package to major mode menu

--- layers/+lang/shell-scripts/packages.el
+++ layers/+lang/shell-scripts/packages.el
@@ -38,8 +38,8 @@
 
 (defun shell-scripts/init-flycheck-bashate ()
   (use-package flycheck-bashate
-  :defer t
-  :init (add-hook 'sh-mode-hook 'flycheck-bashate-setup)))
+    :defer t
+    :init (add-hook 'sh-mode-hook 'flycheck-bashate-setup)))
 
 (defun shell-scripts/init-fish-mode ()
   (use-package fish-mode
@@ -50,8 +50,23 @@
     :defer t
     :init
     (progn
+      ;; Add meaningful names for prefix categories
+      (spacemacs/declare-prefix-for-mode 'sh-mode "mi" "insert")
+      (spacemacs/declare-prefix-for-mode 'sh-mode "mg" "goto")
+
+      ;; Add standard key bindings for insert commands
       (spacemacs/set-leader-keys-for-major-mode 'sh-mode
-        "\\" 'sh-backslash-region)
+        "\\" 'sh-backslash-region
+        "ic" 'sh-case
+        "ii" 'sh-if
+        "if" 'sh-function
+        "io" 'sh-for
+        "ie" 'sh-indexed-loop
+        "iw" 'sh-while
+        "ir" 'sh-repeat
+        "is" 'sh-select
+        "iu" 'sh-until
+        "ig" 'sh-while-getopts)
 
       ;; Use sh-mode when opening `.zsh' files, and when opening Prezto runcoms.
       (dolist (pattern '("\\.zsh\\'"
@@ -80,7 +95,10 @@
     :defer t
     :init
     (progn
+      ;; Insert shebang must be available for non shell modes like python or
+      ;; groovy but also in the major mode menu with shell specific inserts
+      (spacemacs/set-leader-keys-for-major-mode 'sh-mode
+        "i!" 'spacemacs/insert-shebang)
       (spacemacs/set-leader-keys "i!" 'spacemacs/insert-shebang)
       ;; we don't want to insert shebang lines automatically
       (remove-hook 'find-file-hook 'insert-shebang))))
-
