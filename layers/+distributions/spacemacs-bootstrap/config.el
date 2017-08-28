b2eaf9fd30e9a7e66c1f40912af09d2a9552aca6
syl20bnr <sylvain.benner@gmail.com>
2017-08-28 11:02:44 -0400

    groovy: update indent-variable-alist to support new groovy-mode

--- layers/+distributions/spacemacs-bootstrap/config.el
+++ layers/+distributions/spacemacs-bootstrap/config.el
@@ -12,8 +12,9 @@
 ;; Thanks to `editorconfig-emacs' for many of these
 (defvar spacemacs--indent-variable-alist
   ;; Note that derived modes must come before their sources
-  '(((awk-mode c-mode c++-mode java-mode groovy-mode
+  '(((awk-mode c-mode c++-mode java-mode
       idl-mode java-mode objc-mode pike-mode) . c-basic-offset)
+    (groovy-mode . groovy-indent-offset)
     (python-mode . python-indent-offset)
     (cmake-mode . cmake-tab-width)
     (coffee-mode . coffee-tab-width)
