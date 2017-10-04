51de051d4290456416bee86bfff9f037fd03ab92
ISELECT1\naylyn.gaffney <naylyn.gaffney@iselect.com.au>
2017-10-04 22:26:26 +1100

    Remove ess-R-object-popup from ess layer.
    As described in issue #9464, this package was removed from melpa for
    not having a GPL compatible license and is causing an error every time
    spacemacs starts.
    
    Continuation of commit 8c3cf7c3ae3794721f8e72a0bb36f1d82faab731

--- layers/+lang/ess/packages.el
+++ layers/+lang/ess/packages.el
@@ -103,7 +103,6 @@
       "st" 'ess-eval-function
       ;; R helpers
       "hd" 'ess-R-dv-pprint
-      "hi" 'ess-R-object-popup
       "ht" 'ess-R-dv-ctable
       )
     (define-key ess-mode-map (kbd "<s-return>") 'ess-eval-line)
@@ -111,8 +110,6 @@
     (define-key inferior-ess-mode-map (kbd "C-k") 'comint-previous-input)))
 
 (defun ess/init-ess-R-data-view ())
-
-(defun ess/init-ess-R-object-popup ())
 
 (defun ess/init-ess-smart-equals ()
   (use-package ess-smart-equals
