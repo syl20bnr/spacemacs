6e985067b14518b779cef9508f78abddc132c59e
Adriean Khisbe <adriean.khisbe@live.fr>
2017-10-25 13:37:49 +0200

    hide automatic nameless behind a nameless-auto-mode switch

--- layers/+lang/emacs-lisp/packages.el
+++ layers/+lang/emacs-lisp/packages.el
@@ -182,7 +182,8 @@
   (use-package nameless
     :defer t
     :init (progn
-            (add-hook 'emacs-lisp-mode-hook 'nameless-mode-from-hook)
+            (when (bound-and-true-p nameless-auto-mode)
+             (add-hook 'emacs-lisp-mode-hook 'nameless-mode-from-hook))
             (spacemacs|add-toggle nameless
               :status nameless-mode
               :on (nameless-mode)
