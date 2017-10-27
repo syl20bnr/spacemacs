60f11d8afca117a18c2f196bcfca49117b5e52df
Steven Allen <steven@stebalien.com>
2017-10-26 17:58:41 -0700

    Fix using eyebrowse without persp-mode

--- layers/+spacemacs/spacemacs-layouts/packages.el
+++ layers/+spacemacs/spacemacs-layouts/packages.el
@@ -85,14 +85,17 @@
       ;; declare in the layout transient state
       (spacemacs/set-leader-keys "bW" 'spacemacs/goto-buffer-workspace)
       ;; hooks
-      (add-hook 'persp-before-switch-functions
-                #'spacemacs/update-eyebrowse-for-perspective)
-      (add-hook 'eyebrowse-post-window-switch-hook
-                #'spacemacs/save-eyebrowse-for-perspective)
-      (add-hook 'persp-activated-functions
-                #'spacemacs/load-eyebrowse-for-perspective)
-      (add-hook 'persp-before-save-state-to-file-functions #'spacemacs/update-eyebrowse-for-perspective)
-      (add-hook 'persp-after-load-state-functions #'spacemacs/load-eyebrowse-after-loading-layout)
+      (when (configuration-layer/package-used-p 'persp-mode)
+        (add-hook 'persp-before-switch-functions
+                  #'spacemacs/update-eyebrowse-for-perspective)
+        (add-hook 'eyebrowse-post-window-switch-hook
+                  #'spacemacs/save-eyebrowse-for-perspective)
+        (add-hook 'persp-activated-functions
+                  #'spacemacs/load-eyebrowse-for-perspective)
+        (add-hook 'persp-before-save-state-to-file-functions
+                  #'spacemacs/update-eyebrowse-for-perspective)
+        (add-hook 'persp-after-load-state-functions
+                  #'spacemacs/load-eyebrowse-after-loading-layout))
       ;; vim-style tab switching
       (define-key evil-motion-state-map "gt" 'eyebrowse-next-window-config)
       (define-key evil-motion-state-map "gT" 'eyebrowse-prev-window-config))))
