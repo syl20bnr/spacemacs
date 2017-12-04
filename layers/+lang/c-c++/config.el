a3f439a550e8fc4f66b092d5ceb9a52e63626730
Alexander Dalshov <adalshov@charlesworthresearch.com>
2017-12-04 14:53:34 +0300

    c-c++ add option to control cmake-ide package

--- layers/+lang/c-c++/config.el
+++ layers/+lang/c-c++/config.el
@@ -20,6 +20,9 @@
 (defvar c-c++-enable-clang-support nil
   "If non nil Clang related packages and configuration are enabled.")
 
+(defvar c-c++-enable-cmake-ide-support nil
+  "If non nil CMake related packages and configuration are enabled.")
+
 (defvar c-c++-enable-clang-format-on-save nil
   "If non-nil, automatically format code with ClangFormat on
   save. Clang support has to be enabled for this to work.")
