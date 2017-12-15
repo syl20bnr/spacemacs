2c4ac9b2f4c20663a7b11bea0d92ac719be7d9f3
timor <timor.dd@googlemail.com>
2017-12-15 13:00:54 +0100

    Add Layer for the Factor Programming Language

---
+++ layers/+lang/factor/config.el
@@ -0,0 +1,9 @@
+;;; config.el --- Factor Layer Configuration File for Spacemacs
+;;
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(spacemacs|define-jump-handlers factor-mode 'fuel-edit-word)
