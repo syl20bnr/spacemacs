c25f1c6d9c07d0b27a42651e1684c2558f676f8f
syl20bnr <sylvain.benner@gmail.com>
2018-01-22 22:05:30 -0500

    Bump version to 0.200.11 Hotfix release

--- core/core-spacemacs-buffer.el
+++ core/core-spacemacs-buffer.el
@@ -13,7 +13,7 @@
 ;;
 ;;; Code:
 
-(defconst spacemacs-buffer-version-info "0.200.10"
+(defconst spacemacs-buffer-version-info "0.200.11"
   "Current version used to display addition release information.")
 
 (defconst spacemacs-buffer-name "*spacemacs*"
