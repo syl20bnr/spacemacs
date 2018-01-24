c6a197af87cce88348baa3ad76343ab86c065b74
Yuan Fu <casouri@gmail.com>
2018-01-23 19:06:17 -0500

    Add lsp-python layer
    Fix readme.org
    
    Update with some minor changes
    
    I added quickrun package to execute python script with ease.
    It is owned by lsp-python since no one is using this great package in other layers.
    
    Fix readme

---
+++ layers/+tools/lsp-python/layers.el
@@ -0,0 +1,12 @@
+;;; packages.el --- lsp-python layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Yuan Fu <casouri@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(configuration-layer/declare-layer 'lsp)
