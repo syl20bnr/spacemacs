d1b7872ced8da3532ac82b09c42251ea6c818663
syl20bnr <sylvain.benner@gmail.com>
2018-01-29 13:52:39 -0500

    ansible: fix error void function ansible:auto-decrypt-encrypt

--- layers/+tools/ansible/funcs.el
+++ layers/+tools/ansible/funcs.el
@@ -10,12 +10,19 @@
 ;;; License: GPLv3
 
 (defun spacemacs//ansible-should-enable? ()
+  "Return non-nil if `ansible' should be enabled for the current file."
   (and (stringp buffer-file-name)
        (string-match spacemacs--ansible-filename-re buffer-file-name)))
 
 (defun spacemacs/ansible-maybe-enable ()
+  "Enable `ansible-mode' if required."
   (when (spacemacs//ansible-should-enable?)
     (ansible 1)))
+
+(defun spacemacs/ansible-auto-decrypt-encrypt-vault ()
+  "Auto decrypt/encrypt Vault files."
+  (when (spacemacs//ansible-should-enable?)
+    (ansible::auto-decrypt-encrypt)))
 
 (defun spacemacs/ansible-company-maybe-enable ()
   "Add the ansible company backend only for when ansible mode is active."
@@ -23,5 +30,6 @@
     (add-to-list 'company-backends 'company-ansible)))
 
 (defun spacemacs/ansible-doc-maybe-enable ()
+  "Enable `ansible-doc-mode` if possible.'"
   (when (spacemacs//ansible-should-enable?)
     (ansible-doc-mode 1)))
