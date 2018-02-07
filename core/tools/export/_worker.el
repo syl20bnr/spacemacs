191476fe0483d7a87de228e7619709072dd7df51
JAremko <w3techplayground@gmail.com>
2018-02-07 06:17:57 +0200

    work on the new export

--- core/tools/export/_worker.el
+++ core/tools/export/_worker.el
@@ -431,7 +431,9 @@ CONTENTS holds the contents of the item.  INFO is a plist holding
 contextual information."
   (let ((type (org-element-property
                :type
-               (org-export-get-parent item))))
+               (org-export-get-parent item)))
+        (item-tag (org-element-property :tag item))
+        (checkbox (org-element-property :checkbox item)))
     (unless (or (eq 'ordered type)
                 (eq 'unordered type)
                 (eq 'descriptive type))
@@ -443,19 +445,20 @@ contextual information."
     (format (concat "{:tag :list-item "
                     ":type :%s "
                     ":bullet %s "
-                    ":counter %s "
                     ":checkbox %s "
-                    ":item-tag \"%s\" "
+                    ":item-tag %s "
                     ":children [%s]}")
             type
             (format
              "\"%s\""
              (spacemacs/org-edn-escape-string
               (org-element-property :bullet item)))
-            (org-element-property :counter item)
-            (org-element-property :checkbox item)
-            (spacemacs/org-edn-escape-string
-             (format "%s" (org-element-property :tag item)))
+            (when checkbox (format ":%s" (symbol-name checkbox)))
+            (when item-tag
+              (if (char-or-string-p item-tag)
+                  (format "{:tag :plain-text :value \"%s\"}"
+                          (spacemacs/org-edn-escape-string item-tag))
+                (org-export-data-with-backend item-tag 'spacemacs-edn info)))
             contents)))
 
 ;;;; Keyword
