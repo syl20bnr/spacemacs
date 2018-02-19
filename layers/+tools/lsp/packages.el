acdb93bf3a9fc3e76182f3d55ec504e4da219305
syl20bnr <sylvain.benner@gmail.com>
2018-02-18 22:48:12 -0500

    lsp: move helm-xref and ivy-xref ownership to helm and ivy layers

--- layers/+tools/lsp/packages.el
+++ layers/+tools/lsp/packages.el
@@ -1,8 +1,6 @@
 (defconst lsp-packages
   '(
     (company-lsp :requires company)
-    (helm-xref :requires helm)
-    (ivy-xref :requires ivy)
     lsp-mode
     lsp-ui
     ))
@@ -15,36 +13,7 @@
     ;; don't filter results on the client side.
     (setq company-transformers nil
           company-lsp-async t
-          company-lsp-cache-candidates nil)
-    ;; (spacemacs|add-company-backends :backends company-lsp :modes c-mode-common)
-    ))
-
-(defun lsp/init-helm-xref ()
-  (use-package helm-xref
-    :defer t
-    :init
-    (progn
-      ;; This is required to make xref-find-references not give a prompt.
-      ;; xref-find-references asks the identifier (which has no text property) and then passes it to lsp-mode, which requires the text property at point to locate the references.
-      ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=29619
-      (setq xref-prompt-for-identifier
-            '(not xref-find-definitions xref-find-definitions-other-window xref-find-definitions-other-frame xref-find-references spacemacs/jump-to-definition))
-
-      ;; Use helm-xref to display xref.el results.
-      (setq xref-show-xrefs-function #'helm-xref-show-xrefs)
-      )))
-
-(defun lsp/init-ivy-xref ()
-  (use-package ivy-xref
-    :defer t
-    :init
-    (progn
-      (setq xref-prompt-for-identifier
-            '(not xref-find-definitions xref-find-definitions-other-window xref-find-definitions-other-frame xref-find-references spacemacs/jump-to-definition))
-
-      ;; Use ivy-xref to display xref.el results.
-      (setq xref-show-xrefs-function #'ivy-xref-show-xrefs)
-      )))
+          company-lsp-cache-candidates nil)))
 
 (defun lsp/init-lsp-mode ()
   (use-package lsp-mode
