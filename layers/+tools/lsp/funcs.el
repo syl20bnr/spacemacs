4d6ab1d7a6254425e4279a7461dd7059200686ed
syl20bnr <sylvain.benner@gmail.com>
2018-02-18 22:53:07 -0500

    lsp: add files header and rename lsp// functions
    Prefer spacemacs// namespace for all spacemacs added functions.

--- layers/+tools/lsp/funcs.el
+++ layers/+tools/lsp/funcs.el
@@ -1,5 +1,16 @@
-(defun lsp//sync-peek-face ()
-  "Synchronize the face used in lsp-ui peek window according to the theme."
+;;; packages.el --- Language Server Protocol functions File for Spacemacs
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Fangrui Song <i@maskray.me>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defun spacemacs//lsp-sync-peek-face ()
+  "Synchronize the face used in `lsp-ui' peek window according to the theme."
   (set-face-attribute 'lsp-ui-peek-list nil
                       :background (face-attribute 'hl-line :background nil t))
   (set-face-attribute 'lsp-ui-peek-peek nil
@@ -8,11 +19,13 @@
                       :background (face-attribute 'highlight :background nil t)
                       :foreground (face-attribute 'default :foreground nil t))
   (set-face-attribute 'lsp-ui-peek-filename nil
-                      :foreground (face-attribute 'font-lock-constant-face :foreground nil t))
+                      :foreground (face-attribute 'font-lock-constant-face
+                                                  :foreground nil t))
   (set-face-attribute 'lsp-ui-peek-highlight nil
                       :background (face-attribute 'highlight :background nil t)
                       :foreground (face-attribute 'highlight :foreground nil t)
-                      :distant-foreground (face-attribute 'highlight :foreground nil t))
+                      :distant-foreground (face-attribute 'highlight
+                                                          :foreground nil t))
   (set-face-attribute 'lsp-ui-peek-header nil
                       :background (face-attribute 'highlight :background nil t)
                       :foreground (face-attribute 'default :foreground nil t))
