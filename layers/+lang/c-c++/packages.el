72ea3eff6bc2b80d5777889f6960ffd2e8690f93
Aaron Jensen <aaronjensen@gmail.com>
2018-03-03 17:01:25 -0800

    cc-mode: move c-toggle-auto-newline to hook

--- layers/+lang/c-c++/packages.el
+++ layers/+lang/c-c++/packages.el
@@ -42,11 +42,11 @@
     :init
     (progn
       (add-to-list 'auto-mode-alist
-                   `("\\.h\\'" . ,c-c++-default-mode-for-headers)))
+                   `("\\.h\\'" . ,c-c++-default-mode-for-headers))
+      (add-hook 'c-mode-common-hook (lambda () (c-toggle-auto-newline 1))))
     :config
     (progn
       (require 'compile)
-      (c-toggle-auto-newline 1)
       (dolist (mode c-c++-modes)
         (spacemacs/declare-prefix-for-mode mode "mc" "compile")
         (spacemacs/declare-prefix-for-mode mode "mg" "goto")
