1ecffa983496d1248ea056a66d39730d1d37058e
Miciah Masters <miciah.masters@gmail.com>
2018-06-01 12:19:14 -0400

    Bind SPC f i to spacemacs/insert-file

--- layers/+spacemacs/spacemacs-defaults/keybindings.el
+++ layers/+spacemacs/spacemacs-defaults/keybindings.el
@@ -213,6 +213,7 @@
   "feU"  'configuration-layer/update-packages
   "fCd" 'spacemacs/unix2dos
   "fCu" 'spacemacs/dos2unix
+  "fi" 'spacemacs/insert-file
   "fg" 'rgrep
   "fl" 'find-file-literally
   "fE" 'spacemacs/sudo-edit
