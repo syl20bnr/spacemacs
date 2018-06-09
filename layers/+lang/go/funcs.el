0e0d5088b9fec8970f4f51fbfbe6cd5a8df65c13
syl20bnr <sylvain.benner@gmail.com>
2018-06-09 00:45:48 -0400

    go: remove unused function load-gopath-file

--- layers/+lang/go/funcs.el
+++ layers/+lang/go/funcs.el
@@ -13,18 +13,6 @@
   "Set the tab width."
   (setq-local tab-width go-tab-width))
 
-(defun load-gopath-file (gopath name)
-  "Search for NAME file in all paths referenced in GOPATH."
-  (let* ((sep (if (spacemacs/system-is-mswindows) ";" ":"))
-         (paths (split-string gopath sep))
-         found)
-    (cl-loop for p in paths
-          for file = (concat p name) when (file-exists-p file)
-          do
-          (load-file file)
-          (setq found t)
-          finally return found)))
-
 (defun spacemacs//go-enable-gometalinter ()
    "Enable `flycheck-gometalinter' and disable overlapping `flycheck' linters."
    (setq flycheck-disabled-checkers '(go-gofmt
