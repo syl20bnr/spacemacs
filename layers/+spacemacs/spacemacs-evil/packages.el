8425d16c553687ab38c638e2c4d206d34d145fd2
syl20bnr <sylvain.benner@gmail.com>
2018-06-14 21:28:33 -0400

    Lazy load evil-indent-plus

--- layers/+spacemacs/spacemacs-evil/packages.el
+++ layers/+spacemacs/spacemacs-evil/packages.el
@@ -155,7 +155,17 @@
 
 (defun spacemacs-evil/init-evil-indent-plus ()
   (use-package evil-indent-plus
-    :init (evil-indent-plus-default-bindings)))
+    :defer t
+    :init
+    (progn
+      (define-key evil-inner-text-objects-map "i" 'evil-indent-plus-i-indent)
+      (define-key evil-outer-text-objects-map "i" 'evil-indent-plus-a-indent)
+      (define-key evil-inner-text-objects-map "I" 'evil-indent-plus-i-indent-up)
+      (define-key evil-outer-text-objects-map "I" 'evil-indent-plus-a-indent-up)
+      (define-key evil-inner-text-objects-map "J"
+        'evil-indent-plus-i-indent-up-down)
+      (define-key evil-outer-text-objects-map "J"
+        'evil-indent-plus-a-indent-up-down))))
 
 (defun spacemacs-evil/init-evil-lion ()
   (use-package evil-lion
