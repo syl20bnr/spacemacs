b5550ba5eaed3fe02abfb250f5bc62e4e11b347f
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2018-06-24 21:24:16 +0200

    Revert "[CORE] synchronize the environment variables from the default shell"
    This reverts commit 75f1d915a8be3c5cc9485341d4bbd871a2ca6e82.

--- layers/+lang/go/packages.el
+++ layers/+lang/go/packages.el
@@ -108,7 +108,8 @@
     (progn
       ;; get go packages much faster
       (setq go-packages-function 'spacemacs/go-packages-gopkgs)
-      (add-hook 'go-mode-hook 'spacemacs//go-set-tab-width))
+      (add-hook 'go-mode-hook 'spacemacs//go-set-tab-width)
+      (spacemacs/copy-env-list '("GOPATH" "GOROOT" "GO15VENDOREXPERIMENT")))
     :config
     (progn
       (add-hook 'before-save-hook 'gofmt-before-save)
