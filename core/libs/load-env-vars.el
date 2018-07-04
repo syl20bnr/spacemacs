21551b6995e38110ce61fec58f608aa180b1d4fc
syl20bnr <sylvain.benner@gmail.com>
2018-07-04 03:08:52 -0400

    core: fix parsing of env vars
    Was working on Windows but not macOS.. weird

--- core/libs/load-env-vars.el
+++ core/libs/load-env-vars.el
@@ -52,7 +52,7 @@
    (or
     (and "'" (group (0+ (or "\\'" (not (any "'"))))) "'") ;; single quoted value
     (and ?\" (group (0+ (or "\\\"" (not (any "\""))))) ?\") ;; double quoted value
-    (group (1+ (not (in "#")))) ;; unquoted value
+    (group (1+ (not (in "#" "\n")))) ;; unquoted value
     )
    (0+ space)
    (optional "#" (0+ any))
