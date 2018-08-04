5c6057226ea031a43b1a815d2ab5e08c6cb671ba
Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
2018-08-04 22:38:05 +0300

    Encapsulates multiple cursors functionality in a layer

---
+++ layers/+misc/multiple-cursors/packages.el
@@ -0,0 +1,35 @@
+;; -*- lexical-binding: t -*-
+;;
+;;; packages.el --- Spacemacs Multiple Cursors Layer packages File
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Codruț Constantin Gușoi <codrut.gusoi@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq multiple-cursors-packages
+  '(evil-mc
+     ))
+
+(defun multiple-cursors/init-evil-mc ()
+  (use-package evil-mc
+    :config
+    (progn
+      (setq evil-mc-one-cursor-show-mode-line-text nil)
+      (when (or (spacemacs/system-is-mac) (spacemacs/system-is-mswindows))
+        (setq evil-mc-enable-bar-cursor nil))
+
+      ;; evil-mc is not compatible with the paste transient state
+      (define-key evil-normal-state-map "p" 'spacemacs/evil-mc-paste-after)
+      (define-key evil-normal-state-map "P" 'spacemacs/evil-mc-paste-before)
+
+      (dolist (state-map `(,evil-normal-state-map ,evil-insert-state-map))
+        (dolist (keybinding `((,(kbd "C-M-j") . evil-mc-make-cursor-move-next-line)
+                              (,(kbd "C-M-k") . evil-mc-make-cursor-move-prev-line)))
+          (define-key state-map (car keybinding) (cdr keybinding))))
+
+      (global-evil-mc-mode 1))))
