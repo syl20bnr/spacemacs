1a154f9c0c610c33713283a8e68148f53d4026d0
M. Domenzain <luis.domenzain@parrot.com>
2018-08-09 15:53:49 +0200

    update copyright notice

--- layers/+window-management/exwm/packages.el
+++ layers/+window-management/exwm/packages.el
@@ -1,7 +1,7 @@
 ;;; packages.el --- exwm Layer packages File for Spacemacs
 ;;
 ;; Copyright (c) 2012-2014 Sylvain Benner
-;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;; Copyright (c) 2014-2018 Sylvain Benner & Contributors
 ;;
 ;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
