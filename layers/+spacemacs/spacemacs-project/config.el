6213d1b66a8e2d4819ebd77c4e4a37ae67678c69
FieryCod <kwcharllie379@gmail.com>
2018-08-09 17:15:03 +0200

    Fix 'projectile-keymap-prefix for counsel-projectile

---
+++ layers/+spacemacs/spacemacs-project/config.el
@@ -0,0 +1,21 @@
+;;; config.el --- Projectile Layer Configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+
+;; Layer Variables
+
+(defvar projectile-keymap-prefix (kbd "C-c C-p")
+  "Default projectile-keymap-prefix.
+   Required by counsel-projectile to work.
+   Must not be nil")
+
+
+;; Private Variables
