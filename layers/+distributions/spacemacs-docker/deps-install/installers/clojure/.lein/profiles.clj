bd4eb59c8da8ff19d997ea4368b2b0470e9627be
JAremko <w3techplayground@gmail.com>
2018-08-29 22:34:58 +0300

    [spacemacs-docker] up clojure installer

--- layers/+distributions/spacemacs-docker/deps-install/installers/clojure/.lein/profiles.clj
+++
@@ -1,5 +0,0 @@
-{:repl {:plugins [[cider/cider-nrepl "0.17.0"]
-                  [refactor-nrepl "2.3.1"]
-                  [lein-cljfmt "0.5.7"]]
-:dependencies [[alembic "0.3.2"]
-               [org.clojure/tools.nrepl "0.2.12"]]}}
