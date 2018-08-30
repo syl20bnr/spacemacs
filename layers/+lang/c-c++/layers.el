be587cc90edea0af7957669c42b7be109a904a65
cormacc <cormac.cannon@neuromoddevices.com>
2018-08-30 21:03:11 +0100

    Added cquery and ccls backend support to c-c++ layer
    <<Amendment 1 01/09/2018>>
    Incorporated initial feedback from multiple sources (PR discussion)
    
    <<Amendment 2 04/09/2018>>
    Incorporated feedback from MaskRay.
    Fixed projectile post-config hooks.
    Added `c-c++-adopt-subprojects' layer option.
    
    <<Amendment 3 07/09/18>>
    Corrected cache section in README to describe new cache defaults introduced by
    Amendment 2.
    
    <<Amendment 4 13/09/18>>
    Corrected config var names in readme.
    Rebased on current develop branch tip.
    
    <<Amendment 5 14/09/18>>
    Minor adaptations for compatibility with upstream ccls changes.
    
    <<Amendment 6 21/09/18>>
    Bindings updated for consistency with lsp layer lsp-navigation config variable.
    
    <<Amendment 7 1/11/18>>
    Rebased
    
    <<Amendment 8 06/11/18>>
    Incorporated readme correction from svenihoney.
    
    <<Amendment 9 06/11/18>>
    Updated c-c++/refresh-index wrapper for ccls function renamed upstream.
    Moved 2 bindings from 'l' to 'b' to reflect updated lsp-layer mnemonic

---
+++ layers/+lang/c-c++/layers.el
@@ -0,0 +1,13 @@
+;;; layers.el --- C/C++ Layer declarations File for Spacemacs
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(when (member c-c++-backend '(lsp-cquery lsp-ccls))
+  (configuration-layer/declare-layer 'lsp))
