d40d45c256eb6b0d1731ef1d8550edfcadb6d2dd
duianto <duianto@users.noreply.github.com>
2018-09-09 14:55:15 +0200

    [treemacs] give names to which-key +prefix entries
    problem:
    Which-key subgroups get the default name `+prefix`. The subgroup has to be
    opened to find out which keys it contains.
    
    solution:
    Add more descriptive names:
    ```
    c      treemacs-create
    o      treemacs-visit-node
    oa     treemacs-visit-node-ace
    t      treemacs-toggles
    y      treemacs-copy
    C-p    treemacs-projects
    C-p c  treemacs-projects-collapse
    ```

--- layers/+filetree/treemacs/packages.el
+++ layers/+filetree/treemacs/packages.el
@@ -44,7 +44,15 @@
         "ft"    'treemacs
         "fB"    'treemacs-bookmark
         "fT"    'treemacs-find-file
-        "f M-t" 'treemacs-find-tag))
+        "f M-t" 'treemacs-find-tag)
+      (which-key-add-major-mode-key-based-replacements 'treemacs-mode
+        "c"     "treemacs-create"
+        "o"     "treemacs-visit-node"
+        "oa"    "treemacs-visit-node-ace"
+        "t"     "treemacs-toggles"
+        "y"     "treemacs-copy"
+        "C-p"   "treemacs-projects"
+        "C-p c" "treemacs-projects-collapse"))
     :config
     (progn
       (spacemacs/define-evil-state-face "treemacs" "MediumPurple1")
