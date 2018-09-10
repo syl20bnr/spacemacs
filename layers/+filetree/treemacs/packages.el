8a4bf9c655744081eebb030f587777c061f224e5
duianto <duianto@users.noreply.github.com>
2018-09-10 19:55:44 +0200

    filetrees: remap winum-select-window-0-or-10
    Problem:
    `C-x w 0` calls `winum-select-window-0-or-10` which shows:
    winum-select-window-by-number: No window numbered 10
    
    Solution (Thanks Miciah):
    Remap `winum-select-window-0-or-10` to the current filetree command:
    `neotree-show` or `treemacs-select-window`
    
    Then all three key bindings:
    `C-x w 0`, `SPC 0` and `M-0` will call the same filetree command.
    
    Additionally:
    Renamed the remapped which-key name to the filetree command.
    
    Wrapped lines to keep them under 80 chars.

--- layers/+filetree/treemacs/packages.el
+++ layers/+filetree/treemacs/packages.el
@@ -85,10 +85,15 @@
   (spacemacs|use-package-add-hook winum
     :post-config
     (progn
-      ;; window 0 is reserved for file trees
-      (spacemacs/set-leader-keys "0" 'treemacs-select-window)
-      (define-key winum-keymap (kbd "M-0") 'treemacs-select-window)
+      ;; `0', `M-0' and `C-x w 0' are bound to `winum-select-window-0-or-10'
+      (define-key winum-keymap
+        [remap winum-select-window-0-or-10] #'treemacs-select-window)
+      ;; replace the which-key name
+      (push '((nil . "winum-select-window-0-or-10") .
+              (nil . "treemacs-select-window"))
+            which-key-replacement-alist)
       (with-eval-after-load 'treemacs
         (dolist (n (number-sequence 1 5))
           (add-to-list 'winum-ignored-buffers
-                       (format "%sFramebuffer-%s*" treemacs--buffer-name-prefix n)))))))
+                       (format "%sFramebuffer-%s*"
+                               treemacs--buffer-name-prefix n)))))))
