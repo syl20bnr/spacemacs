7f42e0f8563aef6bf4e759844397068629522f7d
Jean Rigotti <gmgotti@protonmail.com>
2018-09-23 17:19:18 +0200

    Add elasticsearch layer

---
+++ layers/+tools/elasticsearch/packages.el
@@ -0,0 +1,29 @@
+;;; packages.el --- elasticsearch layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Jean Rigotti <gmgotti@pm.me>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst elasticsearch-packages
+  '(
+    company
+    org
+    es-mode
+    ))
+
+(defun elasticsearch/init-es-mode ()
+  (use-package es-mode
+    :defer t
+    :mode ("\\.es\\'" . es-mode)))
+
+(defun elasticsearch/post-init-company ()
+  (spacemacs|add-company-backends :backends es-company-backend :modes es-mode))
+
+(defun elasticsearch/pre-init-org ()
+  (spacemacs|use-package-add-hook org
+    :post-config (add-to-list 'org-babel-load-languages '(elasticsearch . t))))
