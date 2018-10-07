f46dfd92488532545d4751e6761902e675fcf3e5
Ag Ibragimov <agzam.ibragimov@gmail.com>
2018-10-06 18:34:30 -0700

    Adds a convenient keybinding for correcting word at point
    supplemental to `z=`

---
+++ layers/+checkers/spell-checking/keybindings.el
@@ -0,0 +1,13 @@
+;;; keybindigs.el --- Spell Checking Layer keybindigs File for Spacemacs
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(spacemacs/set-leader-keys
+  "xx" #'ispell-word)
