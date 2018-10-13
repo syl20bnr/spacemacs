8e4db48024d4bab780b71a024d284cd693e65e34
JAremko <w3techplayground@gmail.com>
2018-10-13 11:47:20 +0300

    [TravisCI] Add documentation autofixer

--- .travisci/pub_org_fmt.sh
+++ .travisci/pub_org_fmt.sh
@@ -11,61 +11,46 @@
 ##
 ## License: GPLv3
 
-echo_headline () {
-    printf '=%.0s' {1..70}
-    printf "\n$1\n"
-    printf '=%.0s' {1..70}
-    echo
+fold_start() {
+    echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"
+}
+
+fold_end() {
+    echo -e "\ntravis_fold:end:$1\r"
 }
 
 cd  ~/.emacs.d
 
-if  [ "$TRAVIS_BRANCH" = "master" ]; then
-    PUBLISH="spacemacs.org"
-elif [ "$TRAVIS_BRANCH" = "develop" ]; then
-    PUBLISH="develop.spacemacs.org"
+if  [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_BRANCH" = "develop" ]; then
+    PUBLISH="spacemacs"
 else
     echo "branch is \"${TRAVIS_BRANCH}\". Won't publish."
     exit 0
 fi
 
-echo_headline "CONFIGURING GIT USER:"
-git config --global user.name "${BOT_NAME}"
-git config --global user.email "${BOT_EMAIL}"
-git config --global push.default simple
-git config --global hub.protocol https
-export GITHUB_TOKEN=$BOT_TK
-
-echo_headline "CLONING TARGET REPOSITORY:"
+fold_start "CLONING_TARGET_REPOSITORY"
 target_URL="https://github.com/syl20bnr/${PUBLISH}.git"
-git clone "${target_URL}" -b gh-pages "/tmp/${PUBLISH}"
+git clone "${target_URL}" -b "${TRAVIS_BRANCH}" "/tmp/${PUBLISH}"
 if [ $? -ne 0 ]; then
     echo "Failed to clone \"${target_URL}\""
     exit 2
 fi
-echo "Done."
-rsync -avh ~/.emacs.d/export/ "/tmp/${PUBLISH}"
-git add --all
+fold_end "CLONING_TARGET_REPOSITORY"
+
+fold_start "SELECTING_CHANGED_FILES"
+rsync -avh ~/.emacs.d "/tmp/${PUBLISH}"
+git add -u
 git diff --cached --exit-code
 if [ $? -eq 0 ]; then
     echo "Nothing to commit - exiting."
     exit 0
 fi
+fold_end "SELECTING_CHANGED_FILES"
 
-hub_version="2.2.9"
-hub_url="https://github.com/github/hub/releases/download/"
-hub_url+="v${hub_version}/hub-linux-amd64-${hub_version}.tgz"
-
+fold_start "PUSHING_CHANGES_TO_${BOT_NAME}/${PUBLISH}"
 cd "/tmp/${PUBLISH}"
-echo_headline "PUSHING CHANGES TO ${BOT_NAME}/${PUBLISH}:"
-curl -L $hub_url | tar \
-                       --strip-components=2 \
-                       -xz \
-                       --wildcards \
-                       -C /tmp/ \
-                       "*hub"
 /tmp/hub add --all
-/tmp/hub commit -m "doc update:$(date -u)"
+/tmp/hub commit -m "documentation fixes:$(date -u)"
 if [ $? -ne 0 ]; then
     echo "hub commit failed"
     exit 2
@@ -82,19 +67,20 @@ printf  "  UserKnownHostsFile=/dev/null\n" >> ~/.ssh/config
 fork_url="https://${BOT_NAME}:${BOT_TK}"
 fork_url+="@github.com/${BOT_NAME}/${PUBLISH}.git"
 git remote set-url "${BOT_NAME}" "${fork_url}"
-/tmp/hub push -f "${BOT_NAME}" gh-pages > /dev/null 2>&1
+/tmp/hub push -f "${BOT_NAME}" "${TRAVIS_BRANCH}" > /dev/null 2>&1
 #                      prevent token leak ^^^^^^^^^^^^^^
 # But it's actually not necessary since TravisCI masks secrets in logs
 if [ $? -ne 0 ]; then
     echo "hub push to \"${BOT_NAME}\" failed"
     exit 2
 fi
-echo_headline "OPENING PR TO syl20bnr/${PUBLISH}.git"
-echo "Documentation updates (autoexport)" > msg
-echo "beep beep boop... Beep?" >> msg
+fold_end "PUSHING_CHANGES_TO_${BOT_NAME}/${PUBLISH}"
+
+fold_start "OPENING_PR_TO_syl20bnr/${PUBLISH}.git"
+echo "[Spacebot]Documentation fixes" > msg
+echo "Merge with care - I'm just a stupid bot. Beep boop." >> msg
 /tmp/hub pull-request -F msg
 if [ $? -ne 0 ]; then
     echo "Seems like PR already exists (not a problem)"
 fi
-echo "DONE!"
-exit 0
+fold_end "OPENING_PR_TO_syl20bnr/${PUBLISH}.git"
