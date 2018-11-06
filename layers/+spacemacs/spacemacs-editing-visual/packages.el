69a89cc41243cfb189238a2297fde4498d7fcf6d
JAremko <w3techplayground@gmail.com>
2018-11-07 01:55:34 +0200

    Replace centered-buffer-mode with writeroom-mode

--- layers/+spacemacs/spacemacs-editing-visual/packages.el
+++ layers/+spacemacs/spacemacs-editing-visual/packages.el
@@ -12,23 +12,23 @@
 (setq spacemacs-editing-visual-packages
       '(
         ;; default
-        (centered-buffer-mode :location local)
-        column-enforce-mode
         (hide-comnt :location local)
+        ;; see https://github.com/syl20bnr/spacemacs/issues/2529
+        ;; waiting for an overlay bug to be fixed
+        (hl-anything :excluded t)
+        column-enforce-mode
         highlight-indentation
         highlight-numbers
         highlight-parentheses
-        ;; waiting for an overlay bug to be fixed
-        ;; see https://github.com/syl20bnr/spacemacs/issues/2529
-        (hl-anything :excluded t)
         indent-guide
         rainbow-delimiters
         volatile-highlights
+        writeroom-mode
         ))
 
 ;; Initialization of packages
 
-(defun spacemacs-editing-visual/init-centered-buffer-mode ())
+(defun spacemacs-editing-visual/init-writeroom-mode ())
 
 (defun spacemacs-editing-visual/init-column-enforce-mode ()
   (use-package column-enforce-mode
