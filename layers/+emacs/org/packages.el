7c24e95880385b68aa6aff614384c063f73a411d
Richard Kim <emacs18@gmail.com>
2018-11-13 20:48:12 -0800

    removed :location of ox-gfm

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -33,8 +33,7 @@
         (org-projectile :requires projectile)
         (ox-twbs :toggle org-enable-bootstrap-support)
         ;; use a for of ox-gfm to fix index generation
-        (ox-gfm :location (recipe :fetcher github :repo "syl20bnr/ox-gfm")
-                :toggle org-enable-github-support)
+        (ox-gfm :toggle org-enable-github-support)
         (ox-reveal :toggle org-enable-reveal-js-support)
         persp-mode
         (ox-hugo :toggle org-enable-hugo-support)
