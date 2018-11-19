9d316ecc7ebd62d984f268bc61b9b609efd4d844
duianto <duianto@users.noreply.github.com>
2018-11-19 22:07:53 +0100

    Add missing layer file headers

--- layers/+lang/factor/config.el
+++ layers/+lang/factor/config.el
@@ -1,5 +1,8 @@
 ;;; config.el --- Factor Layer Configuration File for Spacemacs
 ;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: timor <timor.dd@googlemail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
--- layers/+vim/vinegar/keybindings.el
+++ layers/+vim/vinegar/keybindings.el
@@ -1,3 +1,14 @@
+;;; config.el --- Vinegar Layer keybindings File for Spacemacs
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
 (define-key evil-normal-state-map (kbd "-") 'dired-jump)
 
 (add-hook 'dired-mode-hook 'vinegar/dired-setup)
