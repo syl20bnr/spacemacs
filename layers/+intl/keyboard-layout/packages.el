52bbab6343b62e0ff2003b54b1c6c3cf2552753c
CharlesHD <charleshdespointes@gmail.com>
2018-11-19 11:41:42 +0100

    keyboard-layout: add undo-tree visualizer navigation keys

--- layers/+intl/keyboard-layout/packages.el
+++ layers/+intl/keyboard-layout/packages.el
@@ -33,6 +33,7 @@
     org-agenda
     ranger
     twittering-mode
+    undo-tree
     ))
 
 (defun keyboard-layout/pre-init-ace-window ()
@@ -602,3 +603,16 @@
       "J"
       "K"
       "L")))
+
+(defun keyboard-layout/pre-init-undo-tree ()
+  (kl|config undo-tree
+    :description
+    "Remap navigation keys in `undo-tree-visualizer-mode'."
+    :loader
+    (spacemacs|use-package-add-hook undo-tree :post-config BODY)
+    :common
+    (kl/evil-correct-keys 'evilified undo-tree-visualizer-mode-map
+      "h"
+      "j"
+      "k"
+      "l")))
