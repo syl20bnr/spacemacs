be2ac6313dae3b92de5ca759083dfdb33c949a1f
br3athein <br3athein@gmail.com>
2018-12-06 21:10:05 +0200

    layers/php: Add debugging & refactoring capabilities
    Features:
    * configure geben package
    * configure phpactor package
    * add company-phpactor completion backend

--- layers/+lang/php/packages.el
+++ layers/+lang/php/packages.el
@@ -23,7 +23,10 @@
         php-mode
         phpcbf
         phpunit
+        phpactor
+        (company-phpactor :requires company)
         (company-php :requires company)
+        geben
         ))
 
 (defun php/init-drupal-mode ()
@@ -72,12 +75,52 @@
         "tc" 'phpunit-current-class
         "tp" 'phpunit-current-project)))
 
+(defun php/init-phpactor ()
+  (use-package phpactor
+    :defer t
+    :config
+    (progn
+      (spacemacs/declare-prefix-for-mode 'php-mode "mg" "references")
+      (spacemacs/declare-prefix-for-mode 'php-mode "mrg" "generate")
+      (spacemacs/declare-prefix-for-mode 'php-mode "mre" "extract")
+      (spacemacs/declare-prefix-for-mode 'php-mode "mrm" "methods")
+      (spacemacs/declare-prefix-for-mode 'php-mode "mrc" "classes")
+      (spacemacs/declare-prefix-for-mode 'php-mode "mrp" "properties")
+      (spacemacs/declare-prefix-for-mode 'php-mode "mP" "phpactor")
+
+      (spacemacs/set-leader-keys-for-major-mode 'php-mode
+        "ri"  #'phpactor-import-class
+        "Rv"  #'phpactor-rename-variable-local
+        "RV"  #'phpactor-rename-variable-file
+        "rn"  #'phpactor-fix-namespace
+        "rv"  #'phpactor-change-visibility
+        "rga" #'phpactor-generate-accessors
+        "rgm" #'phpactor-generate-method
+        "rcn" #'phpactor-create-new-class
+        "rcc" #'phpactor-copy-class
+        "rcm" #'phpactor-move-class
+        "rci" #'phpactor-inflect-class
+        "rpc" #'phpactor-complete-constructor
+        "rpp" #'phpactor-complete-properties
+        "rec" #'phpactor-extract-constant
+        "ree" #'phpactor-extract-expression
+        "rem" #'phpactor-extract-method
+        "rmc" #'phpactor-implement-contracts
+        "Ps"  #'phpactor-status
+        "Pu"  #'phpactor-install-or-update)
+
+      (setq-default phpactor-references-list-col1-width 72))))
+
 (defun php/init-phpcbf ()
   (use-package phpcbf
     :defer t))
 
 (defun php/init-phpunit ()
   (use-package phpunit
+    :defer t))
+
+(defun php/init-company-phpactor ()
+  (use-package company-phpactor
     :defer t))
 
 (defun php/init-company-php ()
@@ -89,4 +132,43 @@
       (add-hook 'php-mode-hook 'ac-php-core-eldoc-setup)
       (spacemacs|add-company-backends
         :modes php-mode
-        :backends company-ac-php-backend))))
+        :backends (company-ac-php-backend company-phpactor)))))
+
+(defun php/init-geben ()
+  (use-package geben
+    :config
+    (progn
+      (setq geben-temporary-file-directory (concat spacemacs-cache-directory "geben"))
+
+      (spacemacs/declare-prefix-for-mode 'php-mode "md" "debug")
+      (spacemacs/set-leader-keys-for-major-mode 'php-mode
+        "dx" #'geben
+        "dX" #'geben-end
+        "db" #'geben-add-current-line-to-predefined-breakpoints
+        "dC" #'geben-clear-predefined-breakpoints)
+      (evilified-state-evilify-map geben-mode-map
+        :mode 'php-mode
+        :bindings
+        "q"  #'geben-stop
+        "n"  #'geben-step-over
+        "s"  #'geben-step-into
+        "r"  #'geben-step-out
+        "L"  #'geben-where
+        "v"  #'geben-display-context
+        "c"  #'geben-run-to-cursor
+        "bb" #'geben-set-breakpoint-line
+        "bc" #'geben-set-breakpoint-conditional
+        "be" #'geben-set-breakpoint-exception
+        "w"  #'geben-show-backtrace
+        "gf" #'geben-find-file)
+      (add-hook 'geben-mode-hook 'evil-evilified-state)
+      (evil-set-initial-state 'geben-context-mode 'evilified)
+      (evilified-state-evilify-map geben-context-mode-map
+        :mode geben-context-mode
+        :bindings
+        "q"  #'geben-quit-window
+        "j"  #'widget-forward
+        "k"  #'widget-backward
+        (kbd "<tab>") 'widget-button-press)
+      (evilified-state-evilify-map geben-backtrace-mode-map
+        :mode geben-backtrace-mode))))
