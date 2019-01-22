df34916300356744e7febda8f91da177b7e09e3e
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-01-22 13:00:45 -0800

    Add solidity layer

---
+++ layers/+lang/solidity/layers.el
@@ -0,0 +1,12 @@
+;;; layers.el --- Solidity Layer Configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2019 Sylvain Benner & Contributors
+;;
+;; Author: Seong Yong-ju <sei40kr@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(configuration-layer/declare-layer-dependencies '(node))
