170bb98cb5f502d5523ca3efc555b4fc694d12fb
Daniel Cole <dcole@virtu.com>
2019-02-01 16:18:55 -0500

    Fix lsp find-* functions using peek-navigation
    With this fix lsp find-* functions correctly use xref (simple-navigation).
    
    Fixes issue #11931.

--- layers/+tools/lsp/funcs.el
+++ layers/+tools/lsp/funcs.el
@@ -123,7 +123,7 @@ https://github.com/emacs-lsp/lsp-javascript/issues/9#issuecomment-379515379"
   (intern (concat layer-name "/" nav-mode "-" (symbol-name kind))))
 
 (defun spacemacs//lsp-define-custom-extension (layer-name nav-mode kind request &optional extra)
-  (let ((lsp-extension-fn (if (eq nav-mode "find")
+  (let ((lsp-extension-fn (if (equal nav-mode "find")
                             'lsp-find-locations
                             'lsp-ui-peek-find-custom))
          (extension-name (spacemacs//lsp-get-extension-name layer-name nav-mode kind))
