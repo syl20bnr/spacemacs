5cfd2f54495e29767fe380b42023a253d11f6396
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-02-02 22:08:19 +0000

    start adding yasnippet-snippets

---
+++ layers/+misc/yasnippet-snippets/funcs.el
@@ -0,0 +1,14 @@
+;;; funcs.el --- Yasnippet-snippets layer functions for Spacemacs
+;;
+;; Copyright (c) 2019 Andrea Crotti
+;;
+;; Author: Andrea Crotti <andrea.crotti.0@gmail.com>
+;; URL: https://github.com/AndreaCrotti/yasnippet-snippets
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defun spacemacs//yasnippet-snippets ()
+   "Enable `flycheck-gometalinter' and disable overlapping `flycheck' linters."
+   )
