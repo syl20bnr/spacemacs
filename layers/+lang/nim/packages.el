132ea5f09957a2c1739abda44fac8d29591d29ed
Uros Perisic <uros.m.perisic@gmail.com>
2019-02-04 07:42:20 -0800

    Make `spacemacs/nim-compile-run` work with current buffer file.
    Naming the main file `main.nim` is a `C` convention, not a `Nim` convention. In
    fact, `Nimble` defaults to naming the main file after the project at
    initialization. I will soon add a function that parses the Nimble configuration
    for project entry point and compiles that. Until then, this is a solid
    compromise between flexibility and reliability. I also added a doc-string to the
    function.

--- layers/+lang/nim/packages.el
+++ layers/+lang/nim/packages.el
@@ -39,8 +39,9 @@
     :config
     (progn
       (defun spacemacs/nim-compile-run ()
+        "Compile current buffer file."
         (interactive)
-        (shell-command "nim compile --run main.nim"))
+        (shell-command (concat "nim compile --run " (buffer-file-name))))
 
       (spacemacs/set-leader-keys-for-major-mode 'nim-mode
         "cr" 'spacemacs/nim-compile-run
