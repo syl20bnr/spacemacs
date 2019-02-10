f40361fd64950f9eece1be792d02369d4e15552d
Dave Aitken <dave.aitken@gmail.com>
2019-02-10 18:54:49 +0000

    Add reasonml layer

---
+++ layers/+lang/reasonml/layers.el
@@ -0,0 +1,12 @@
+;;; layers.el --- reasonml layer layers file for Spacemacs
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Dave Aitken <dave.aitken@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(configuration-layer/declare-layer 'ocaml)
