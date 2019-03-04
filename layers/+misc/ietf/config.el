446299ffbdcd2991fcb132145b7b1cee4c239c12
Christian E. Hopps <chopps@gmail.com>
2019-03-03 20:24:46 -0500

    IETF related layers and packages.

---
+++ layers/+misc/ietf/config.el
@@ -0,0 +1,13 @@
+;;; packages.el --- ietf layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Christian Hopps <chopps@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar ietf-docs-cache (concat spacemacs-cache-directory "ietf-docs-cache")
+  "Where cached IETF documents are stored.")
