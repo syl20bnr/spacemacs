62fea42576413891ce5116c37aa0dcae3abdea0e
yuhan0 <qythium@gmail.com>
2019-03-08 20:57:32 +0800

    fix typos in spacemacs/search-dir* bindings

--- layers/+completion/ivy/packages.el
+++ layers/+completion/ivy/packages.el
@@ -100,20 +100,20 @@
         "sgF" 'spacemacs/search-grep-region-or-symbol
         "sgp" 'counsel-git-grep
         "sgP" 'spacemacs/counsel-git-grep-region-or-symbol
-        "skd" 'spacemacs/search-ack-grep
-        "skD" 'spacemacs/search-ack-grep-region-or-symbol
+        "skd" 'spacemacs/search-dir-ack
+        "skD" 'spacemacs/search-dir-ack-region-or-symbol
         "skf" 'spacemacs/search-ack
         "skF" 'spacemacs/search-ack-region-or-symbol
         "skp" 'spacemacs/search-project-ack
         "skP" 'spacemacs/search-project-ack-region-or-symbol
-        "srd" 'spacemacs/search-rg-grep
-        "srD" 'spacemacs/search-rg-grep-region-or-symbol
+        "srd" 'spacemacs/search-dir-rg
+        "srD" 'spacemacs/search-dir-rg-region-or-symbol
         "srf" 'spacemacs/search-rg
         "srF" 'spacemacs/search-rg-region-or-symbol
         "srp" 'spacemacs/search-project-rg
         "srP" 'spacemacs/search-project-rg-region-or-symbol
-        "std" 'spacemacs/search-pt-grep
-        "stD" 'spacemacs/search-pt-grep-region-or-symbol
+        "std" 'spacemacs/search-dir-pt
+        "stD" 'spacemacs/search-dir-pt-region-or-symbol
         "stf" 'spacemacs/search-pt
         "stF" 'spacemacs/search-pt-region-or-symbol
         "stp" 'spacemacs/search-project-pt
