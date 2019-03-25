8b7a275beba87144d5beacd1cca4d40185397116
Jason Walker <jason.walker@wicourts.gov>
2019-03-25 10:54:40 -0500

    Review changes

--- layers/+lang/purescript/packages.el
+++ layers/+lang/purescript/packages.el
@@ -39,7 +39,7 @@
     (progn
       (add-hook 'purescript-mode-hook 'turn-on-purescript-indentation)
       (add-hook 'purescript-mode-hook 'purescript-decl-scan-mode)
-      (spacemacs/declare-prefix "mg" "jump")
+      (spacemacs/declare-prefix "mg" "goto")
       (spacemacs/declare-prefix "mi" "imports")
       (spacemacs/set-leader-keys-for-major-mode 'purescript-mode
         "i="  'purescript-mode-format-imports
@@ -54,7 +54,7 @@
     (progn
       (spacemacs/register-repl 'psci 'psci "purescript")
       (add-hook 'purescript-mode-hook 'inferior-psci-mode)
-      (spacemacs/declare-prefix "ms" "psci")
+      (spacemacs/declare-prefix "ms" "repl")
       (spacemacs/set-leader-keys-for-major-mode 'purescript-mode
         "'"  'psci
         "sb" 'psci/load-current-file!
@@ -74,7 +74,7 @@
       (customize-set-variable 'psc-ide-rebuild-on-save purescript-enable-rebuild-on-save)
 
       (add-to-list 'spacemacs-jump-handlers-purescript-mode 'psc-ide-goto-definition)
-      (spacemacs/declare-prefix "mmi" "imports")
+      (spacemacs/declare-prefix "mmi" "insert/import")
       (spacemacs/set-leader-keys-for-major-mode 'purescript-mode
         "mt"  'psc-ide-add-clause
         "mc"  'psc-ide-case-split
