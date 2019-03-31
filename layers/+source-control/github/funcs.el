b28f1a2abafc33a0474fcea80fed793dc294871f
syl20bnr <sylvain.benner@gmail.com>
2019-03-31 12:14:20 -0400

    github: add function to get repo for a forge notification error

--- layers/+source-control/github/funcs.el
+++ layers/+source-control/github/funcs.el
@@ -8,3 +8,43 @@
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; License: GPLv3
+
+
+;; Forge
+
+(defun spacemacs/forge-get-info-from-fetched-notification-error (err)
+  "Return info for given s-exp error return by `forge-pull-notifications'.
+
+Call this function interactively and paste the s-exp from the error returned by
+the `forge-pull-notifications' function.
+
+Example of error:
+
+error in process filter: ghub--signal-error: peculiar error:
+((path \"query\" \"_Z2l0aHViLmNvbTowMTA6UmVwb3NpdG9yeTI5MDM3NDE6NTI0NzY1\")
+ (extensions (code . \"undefinedField\")
+ (typeName . \"Query\")
+ (fieldName . \"nil\"))
+ (locations ((line . 2) (column . 1)))
+ (message . \"Field 'nil' doesn't exist on type 'Query'\"))
+
+Function adapted from issue:
+https://github.com/magit/forge/issues/80#issuecomment-456103195
+"
+  (interactive "xs-exp: ")
+  (message "%s" err)
+  (let* ((query_value (third (car err)))
+         (result (car (forge-sql
+                       [:select [owner name]
+                                :from repository
+                                :where (= id $s1)]
+                       (base64-encode-string
+                        (mapconcat
+                         #'identity
+                         (butlast
+                          (split-string
+                           (base64-decode-string (substring query_value 1))
+                           ":"))
+                         ":")
+                        t)))))
+    (message "repository: %s/%s" (car result) (cadr result))))
