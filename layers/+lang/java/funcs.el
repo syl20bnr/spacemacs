4bf5b7be7b61bd029b50f60013e48505a57fbea9
Teo Klestrup Röijezon <teo@nullable.se>
2019-04-18 11:32:18 +0200

    Moved Ensime support to the Scala layer, removed Java support

--- layers/+lang/java/funcs.el
+++ layers/+lang/java/funcs.el
@@ -29,7 +29,6 @@
   (pcase java-backend
     (`meghanada (spacemacs//java-setup-meghanada))
     (`eclim (spacemacs//java-setup-eclim))
-    (`ensime (spacemacs//java-setup-ensime))
     (`lsp (spacemacs//java-setup-lsp))))
 
 (defun spacemacs//java-setup-company ()
@@ -37,7 +36,6 @@
   (pcase java-backend
     (`meghanada (spacemacs//java-setup-meghanada-company))
     (`eclim (spacemacs//java-setup-eclim-company))
-    (`ensime (spacemacs//java-setup-ensime-company))
     (`lsp (spacemacs//java-setup-lsp-company))))
 
 (defun spacemacs//java-setup-flycheck ()
@@ -45,139 +43,8 @@
   (pcase java-backend
     (`meghanada (spacemacs//java-setup-meghanada-flycheck))
     (`eclim (spacemacs//java-setup-eclim-flycheck))
-    (`ensime (spacemacs//java-setup-ensime-flycheck))
     (`lsp (spacemacs//java-setup-lsp-flycheck))))
 
-(defun spacemacs//java-setup-flyspell ()
-  "Conditionally setup flyspell based on backend."
-  (pcase java-backend
-    (`ensime (spacemacs//java-setup-ensime-flyspell))))
-
-(defun spacemacs//java-setup-eldoc ()
-  "Conditionally setup eldoc based on backend."
-  (pcase java-backend
-    ;; meghanada setup eldoc on its own
-    (`ensime (spacemacs//java-setup-ensime-eldoc))))
-
-
-
-;; ensime
-
-(autoload 'ensime-config-find-file "ensime-config")
-(autoload 'ensime-config-find "ensime-config")
-(autoload 'projectile-project-p "projectile")
-
-(defun spacemacs//java-setup-ensime ()
-  "Setup ENSIME."
-  ;; jump handler
-  (add-to-list 'spacemacs-jump-handlers-java-mode 'ensime-edit-definition)
-  ;; ensure the file exists before starting `ensime-mode'
-  (cond
-   ((and (buffer-file-name) (file-exists-p (buffer-file-name)))
-    (ensime-mode))
-   ((buffer-file-name)
-    (add-hook 'after-save-hook 'ensime-mode nil t))))
-
-(defun spacemacs//java-setup-ensime-company ()
-  "Setup ENSIME auto-completion.")
-
-(defun spacemacs//java-setup-ensime-flycheck ()
-  "Setup ENSIME syntax checking.")
-
-(defun spacemacs//java-setup-ensime-flyspell ()
-  "Setup ENSIME spell checking."
-  (flyspell-mode)
-  (setq-local flyspell-generic-check-word-predicate
-              'spacemacs//ensime-flyspell-verify))
-
-(defun spacemacs//java-setup-ensime-eldoc ()
-  "Setup ENSIME eldoc."
-  (setq-local eldoc-documentation-function
-              (lambda ()
-                (when (ensime-connected-p)
-                  (ensime-type-at-point))))
-  (eldoc-mode))
-
-(defun spacemacs//ensime-maybe-start ()
-  (when (buffer-file-name)
-    (let ((ensime-buffer (spacemacs//ensime-buffer-for-file (buffer-file-name)))
-          (file (ensime-config-find-file (buffer-file-name)))
-          (is-source-file (s-matches? (rx (or "/src/" "/test/"))
-                                      (buffer-file-name))))
-
-      (when (and is-source-file (null ensime-buffer))
-        (noflet ((ensime-config-find (&rest _) file))
-          (save-window-excursion
-            (ensime)))))))
-
-(defun spacemacs//ensime-buffer-for-file (file)
-  "Find the Ensime server buffer corresponding to FILE."
-  (let ((default-directory (file-name-directory file)))
-    (-when-let (project-name (projectile-project-p))
-      (--first (-when-let (bufname (buffer-name it))
-                 (and (s-contains? "inferior-ensime-server" bufname)
-                      (s-contains? (file-name-nondirectory project-name)
-                                   bufname)))
-               (buffer-list)))))
-
-(defun spacemacs//ensime-flyspell-verify ()
-  "Prevent common flyspell false positives in scala-mode."
-  (and (flyspell-generic-progmode-verify)
-       (not (s-matches? (rx bol (* space) "package") (current-line)))))
-
-;; interactive functions
-
-(defun spacemacs/ensime-gen-and-restart()
-  "Regenerate `.ensime' file and restart the ensime server."
-  (interactive)
-  (progn
-    (sbt-command ";ensimeConfig;ensimeConfigProject")
-    (ensime-shutdown)
-    (ensime)))
-
-(defun spacemacs/ensime-inf-eval-buffer-switch ()
-  "Send buffer content to shell and switch to it in insert mode."
-  (interactive)
-  (ensime-inf-eval-buffer)
-  (ensime-inf-switch)
-  (evil-insert-state))
-
-(defun spacemacs/ensime-inf-eval-region-switch (start end)
-  "Send region content to shell and switch to it in insert mode."
-  (interactive "r")
-  (ensime-inf-switch)
-  (ensime-inf-eval-region start end)
-  (evil-insert-state))
-
-(defun spacemacs/ensime-refactor-accept ()
-  (interactive)
-  (funcall continue-refactor)
-  (ensime-popup-buffer-quit-function))
-
-(defun spacemacs/ensime-refactor-cancel ()
-  (interactive)
-  (funcall cancel-refactor)
-  (ensime-popup-buffer-quit-function))
-
-(defun spacemacs/ensime-completing-dot ()
-  "Insert a period and show company completions."
-  (interactive "*")
-  (when (s-matches? (rx (+ (not space)))
-                    (buffer-substring (line-beginning-position) (point)))
-    (delete-horizontal-space t))
-  (company-abort)
-  (insert ".")
-  (company-complete))
-
-(defun spacemacs/ensime-yank-type-at-point ()
-  "Yank to kill ring and print short type name at point to the minibuffer."
-  (interactive)
-  (ensime-type-at-point '(4)))
-
-(defun spacemacs/ensime-yank-type-at-point-full-name ()
-  "Yank to kill ring and print full type name at point to the minibuffer."
-  (interactive)
-  (ensime-type-at-point '(4) t))
 
 
 ;; eclim
