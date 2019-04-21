02bf928815002b9fc11f9cd945ff511b5b4829e9
Thanh Vuong <thanhvg@gmail.com>
2019-04-20 21:58:21 -0600

    [core] fix spacemacs/error-delegate
    determine flycheck or emacs error backend using next-error-function instead of
    next-error-find-buffer

--- core/core-funcs.el
+++ core/core-funcs.el
@@ -340,8 +340,7 @@ current frame."
 Delegates to flycheck if it is enabled and the next-error buffer
 is not visible. Otherwise delegates to regular Emacs next-error."
   (if (and (bound-and-true-p flycheck-mode)
-           (let ((buf (ignore-errors (next-error-find-buffer))))
-             (not (and buf (get-buffer-window buf)))))
+           (not next-error-function))
       'flycheck
     'emacs))
 
