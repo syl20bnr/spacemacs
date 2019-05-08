01c48e0c758aefb1accd80ea667891d82cc19ad7
bb2020 <bb2020@users.noreply.github.com>
2019-05-09 00:10:08 +0300

    [shell] Add ivy completion

--- layers/+tools/shell/funcs.el
+++ layers/+tools/shell/funcs.el
@@ -174,6 +174,23 @@ is achieved by adding the relevant text properties."
   (define-key eshell-mode-map
     (kbd "M-l") 'spacemacs/helm-eshell-history))
 
+(defun spacemacs/ivy-eshell-history ()
+  (interactive)
+  (counsel-esh-history)
+  (evil-insert-state))
+
+(defun spacemacs/pcomplete-std-complete ()
+  (interactive)
+  (pcomplete-std-complete)
+  (evil-insert-state))
+
+(defun spacemacs/init-ivy-eshell ()
+  "Initialize ivy-eshell."
+  (spacemacs/set-leader-keys-for-major-mode 'eshell-mode
+    "H" #'spacemacs/ivy-eshell-history)
+  (define-key eshell-mode-map (kbd "M-l") #'spacemacs/ivy-eshell-history)
+  (define-key eshell-mode-map (kbd "<tab>") #'spacemacs/pcomplete-std-complete))
+
 (defun term-send-tab ()
   "Send tab in term mode."
   (interactive)
