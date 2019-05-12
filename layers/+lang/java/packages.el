1ef2b0e665101bd6e7c87e09b3aa9d4863bc5634
Ivan Yonchovski <yyoncho@gmail.com>
2019-05-12 13:37:48 +0300

    [java] Sync java lsp backend with latest lsp-java functionality
    - new keybinds for the new functionality

--- layers/+lang/java/packages.el
+++ layers/+lang/java/packages.el
@@ -439,11 +439,12 @@
     :config
     (progn
       ;; key bindings
-      (dolist (prefix '(("mc" . "compile")
+      (dolist (prefix '(("mc" . "compile/create")
                         ("mg" . "goto")
                         ("mr" . "refactor")
-                        ("mra" . "add")
-                        ("mrc" . "create")
+                        ("mra" . "add/assign")
+                        ("mrc" . "create/convert")
+                        ("mrg" . "generate")
                         ("mre" . "extract")
                         ("mq" . "lsp")
                         ("mt" . "test")
@@ -452,15 +453,33 @@
           'java-mode (car prefix) (cdr prefix)))
       (spacemacs/set-leader-keys-for-major-mode 'java-mode
         "pu"  'lsp-java-update-user-settings
+
+        ;; refactoring
         "ro" 'lsp-java-organize-imports
-        "rai" 'lsp-java-add-import
-        "ram" 'lsp-java-add-unimplemented-methods
         "rcp" 'lsp-java-create-parameter
         "rcf" 'lsp-java-create-field
+        "rci" 'lsp-java-conver-to-static-import
         "rec" 'lsp-java-extract-to-constant
         "rel" 'lsp-java-extract-to-local-variable
         "rem" 'lsp-java-extract-method
+
+        ;; assign/add
+        "rai" 'lsp-java-add-import
+        "ram" 'lsp-java-add-unimplemented-methods
+        "rat" 'lsp-java-add-throws
+        "raa" 'lsp-java-assign-all
+        "raf" 'lsp-java-assign-to-field
+
+        ;; generate
+        "rgt" 'lsp-java-generate-to-string
+        "rge" 'lsp-java-generate-equals-and-hash-code
+        "rgo" 'lsp-java-generate-overrides
+        "rgg" 'lsp-java-generate-getters-and-setters
+
+        ;; create/compile
         "cc"  'lsp-java-build-project
+        "cp"  'lsp-java-spring-initializr
+
         "an"  'lsp-java-actionable-notifications
 
         ;; dap-mode
