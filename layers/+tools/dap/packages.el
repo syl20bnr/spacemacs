44a320efd6b364d50b64f4c70b89e18715687138
Ivan Yonchovski <yyoncho@gmail.com>
2019-05-19 13:47:23 +0300

    [dap] Evilfy dap-mode windows

--- layers/+tools/dap/packages.el
+++ layers/+tools/dap/packages.el
@@ -15,6 +15,10 @@
 (defun dap/init-dap-mode ()
   (use-package dap-mode
     :after (lsp-mode)
+    :hook ((dap-ui-sessions-mode . evil-evilified-state)
+           (dap-ui-breakpoints-ui-list-mode . evil-evilified-state)
+           (dap-ui-locals-mode . evil-evilified-state)
+           (dap-ui-inspect-mode . evil-evilified-state))
     :config
     (dap-mode 1)
     (dap-ui-mode 1)))
