c0235429480fc3b81d7e7aa831e54dd7c0f69538
Oguz Serbetci <oguz.serbetci@gmail.com>
2019-05-20 23:52:23 +0200

    Add org-brain bindings for visiting entries and adding headlines and resources

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -566,10 +566,13 @@ Headline^^            Visit entry^^               Filter^^                    Da
       (spacemacs/declare-prefix-for-mode 'org-mode "mBa" "add")
       (spacemacs/declare-prefix-for-mode 'org-mode "mBg" "goto")
       (spacemacs/set-leader-keys-for-major-mode 'org-mode
-        "Bac" 'org-brain-add-child
         "Bv" 'org-brain-visualize
+        "Bac" 'org-brain-add-child
+        "Bah" 'org-brain-add-child-headline
         "Bap" 'org-brain-add-parent
+        "Bar" 'org-brain-add-resource
         "Baf" 'org-brain-add-friendship
+        "Bgg" 'org-brain-goto
         "Bgc" 'org-brain-goto-child
         "Bgp" 'org-brain-goto-parent
         "Bgf" 'org-brain-goto-friend
