66f229fdd123c22c20c0fc442dfdb56bddd5c02c
aschaap <8865084+aschaap@users.noreply.github.com>
2019-05-30 16:35:48 -0400

    Removing line causing Emacs to freeze on Org files with :ignore:; fixes #12392

--- layers/+lang/bibtex/packages.el
+++ layers/+lang/bibtex/packages.el
@@ -74,8 +74,7 @@
         "li" 'isbn-to-bibtex
         "lp" 'pubmed-insert-bibtex-from-pmid))))
 
-(defun bibtex/pre-init-org-ref ()
-  (add-hook 'org-mode-hook (lambda () (require 'org-ref))))
+(defun bibtex/pre-init-org-ref ())
 
 (defun bibtex/post-init-markdown-mode ()
   (spacemacs/set-leader-keys-for-major-mode 'markdown-mode
