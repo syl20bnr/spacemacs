9f2ab45cebaecb68d90c5a012432346f44b66cdf
John Stevenson <john@jr0cket.co.uk>
2019-06-12 16:41:09 +0100

    [spell-checking] Add keybinding for flyspell-correct-at-point
    Add a keybinding to provide a simple way to correct the spelling of a word at
    the current point.  This enables fixing the spelling mistake as it happens with
    low ceremony.

--- layers/+checkers/spell-checking/packages.el
+++ layers/+checkers/spell-checking/packages.el
@@ -53,7 +53,9 @@ Spell Commands^^          Add To Dictionary^^               Other
 [_b_] check whole buffer  [_B_] add word to dict (buffer)   [_t_] toggle spell check
 [_d_] change dictionary   [_G_] add word to dict (global)   [_q_] exit
 [_n_] next spell error    [_S_] add word to dict (session)  [_Q_] exit and disable spell check
-[_c_] correct word"
+[_c_] correct words
+[_s_] correct at point
+"
         :on-enter (flyspell-mode)
         :bindings
         ("B" spacemacs/add-word-to-dict-buffer)
@@ -65,6 +67,7 @@ Spell Commands^^          Add To Dictionary^^               Other
         ("Q" flyspell-mode :exit t)
         ("q" nil :exit t)
         ("S" spacemacs/add-word-to-dict-session)
+        ("s" flyspell-correct-at-point)
         ("t" spacemacs/toggle-spelling-checking))
 
       (spacemacs/set-leader-keys "S." 'spacemacs/spell-checking-transient-state/body)
