a3846f5af1170bac7523b9ce6b19001362a66c7f
Binbin Ye <phantom2501@gmail.com>
2019-06-17 00:43:00 +0900

    Add a default value for kbd-mac-command

--- layers/+os/osx/keybindings.el
+++ layers/+os/osx/keybindings.el
@@ -58,7 +58,9 @@ other than the three sane values listed above."
                         '((hyper . "H-")
                           (super . "s-")
                           (alt   . "A-")))))
-      (when found (kbd (concat (cdr found) keys)))))
+      (if found
+          (kbd (concat (cdr found) keys))
+        (kbd (concat "H-" keys)))))
 
   ;; Keybindings
   (global-set-key (kbd-mac-command "=") 'spacemacs/scale-up-font)
