cd24a73429fcce6ae23ed1557d0f2562ce533f2d
Hong Xu <hong@topbug.net>
2019-07-07 10:29:31 -0700

    Add cliphist to the xclipboard layer.

---
+++ layers/+tools/xclipboard/funcs.el
@@ -0,0 +1,14 @@
+;;; funcs.el --- xclipboard layer functions file for Spacemacs.
+;;
+;; Copyright (c) 2012-2019 Sylvain Benner & Contributors
+;;
+;; Author: Hong Xu <hong@topbug.net>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defun spacemacs/xclipboard-cliphist-paste-item-rectangle ()
+  (interactive)
+  (cliphist-paste-item 1))
--- layers/+tools/xclipboard/packages.el
+++ layers/+tools/xclipboard/packages.el
@@ -1,9 +1,10 @@
 ;;; packages.el --- xclipboard layer packages file for Spacemacs.
 ;;
-;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;; Copyright (c) 2012-2019 Sylvain Benner & Contributors
 ;;
 ;; Authors: Charles Weill <weill@google.com>
 ;;          Google LLC.
+;;          Hong Xu <hong@topbug.net>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
@@ -13,7 +14,17 @@
 (defconst xclipboard-packages
   '(
     (spacemacs-xclipboard :location local)
+    (cliphist :requires ivy
+              :toggle xclipboard-enable-cliphist)
     ))
+
+(defun xclipboard/init-cliphist ()
+  (use-package cliphist
+    :init (spacemacs/set-leader-keys
+            "xP" 'cliphist-paste-item
+            "xR" 'spacemacs/xclipboard-cliphist-paste-item-rectangle
+            "xs" 'cliphist-select-item)
+    :config (setq cliphist-cc-kill-ring t)))
 
 (defun xclipboard/init-spacemacs-xclipboard ()
   (use-package spacemacs-xclipboard
