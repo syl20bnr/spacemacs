0e2935b8d6b8473c92d3037a2f6c346b03e606a9
syl20bnr <sylvain.benner@gmail.com>
2019-07-26 22:44:35 -0400

    Add autoloads for winner-mode

--- layers/+spacemacs/spacemacs-defaults/packages.el
+++ layers/+spacemacs/spacemacs-defaults/packages.el
@@ -434,7 +434,7 @@
 
 (defun spacemacs-defaults/init-winner ()
   (use-package winner
-    :defer t
+    :commands (winner-undo winner-redo)
     :init
     (with-eval-after-load 'winner
       (setq spacemacs/winner-boring-buffers '("*Completions*"
