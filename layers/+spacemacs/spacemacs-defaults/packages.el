85fca8f32367cbcf64a8176ad8314363edf94249
smile13241324 <smile13241324@gmail.com>
2019-08-04 14:49:10 +0200

    Restore winner-mode auto start without breaking dumping
    As winner-mode was moved from init.el to dump-init.el winner-mode
    was not longer properly started for non-dumped emacs sessions.
    Having winner-mode auto activate at startup did fix the issue
    for non-dumping users but in turn broke the dumping process.
    
    I have now moved the necessary winner-mode init to
    spacemacs/setup-startup-hook which is only called
    in the non-dumping case. To allow customizations I activate
    winner-mode prior to loading user-init this will allow users
    to deactivate winner-mode in the dotfile and fix ediff themselves
    if this is wished for.

--- layers/+spacemacs/spacemacs-defaults/packages.el
+++ layers/+spacemacs/spacemacs-defaults/packages.el
@@ -436,7 +436,6 @@
   (use-package winner
     :commands (winner-undo winner-redo)
     :init
-    (winner-mode t)
     (with-eval-after-load 'winner
       (setq spacemacs/winner-boring-buffers '("*Completions*"
                                               "*Compile-Log*"
