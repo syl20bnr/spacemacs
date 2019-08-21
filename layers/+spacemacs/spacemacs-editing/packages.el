dcb4da02a8758e7cb700d69689670b85024b068d
syl20bnr <sylvain.benner@gmail.com>
2019-08-20 22:54:46 -0400

    Bump undo limits to avoid frequent No further undo information
    See https://github.com/syl20bnr/spacemacs/issues/12110#issuecomment-504079398

--- layers/+spacemacs/spacemacs-editing/packages.el
+++ layers/+spacemacs/spacemacs-editing/packages.el
@@ -407,7 +407,12 @@
   (use-package undo-tree
     :defer t
     :init (setq undo-tree-visualizer-timestamps t
-                undo-tree-visualizer-diff t)
+                undo-tree-visualizer-diff t
+                ;; 10X bump of the undo limits to avoid issues with premature
+                ;; Emacs GC which truncages the undo history very aggresively
+                undo-limit 800000
+                undo-strong-limit 12000000
+                undo-outer-limit 120000000)
     :config
     (progn
       ;; restore diff window after quit.  TODO fix upstream
