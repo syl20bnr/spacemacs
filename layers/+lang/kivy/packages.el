26dd7b188accdece72f47cefbb5f1cce14b05396
Ryota Kayanuma <picosushi12+git@gmail.com>
2019-08-25 14:05:45 +0900

    Change author

--- layers/+lang/kivy/packages.el
+++ layers/+lang/kivy/packages.el
@@ -2,7 +2,7 @@
 ;;
 ;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
 ;;
-;; Author:  <mijoharas@archlinux>
+;; Author: Ryota Kayanuma <picosushi12@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
