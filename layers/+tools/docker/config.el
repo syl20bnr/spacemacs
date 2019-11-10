b26430df2a9156485c0baa4ec6a3dbeb24e834e1
Seong Yong-ju <sei40kr@gmail.com>
2019-11-10 14:53:44 +0900

    Add LSP backend support for Dockerfile

---
+++ layers/+tools/docker/config.el
@@ -0,0 +1,17 @@
+;;; config.el --- docker Layer Configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2019 Sylvain Benner & Contributors
+;;
+;; Author: Seong Yong-ju <sei40kr@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(spacemacs|define-jump-handlers dockerfile-mode)
+
+;; Variables
+
+(defvar docker-dockerfile-backend nil
+  "The backend to use for IDE features. Possible values are `lsp'.")
---
+++ layers/+tools/docker/layers.el
@@ -0,0 +1,14 @@
+;;; layers.el --- docker Layer declarations File for Spacemacs
+;;
+;; Copyright (c) 2012-2019 Sylvain Benner & Contributors
+;;
+;; Author: Seong Yong-ju <sei40kr@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(when (and (boundp 'docker-dockerfile-backend)
+           (eq docker-dockerfile-backend 'lsp))
+  (configuration-layer/declare-layer-dependencies '(lsp)))
