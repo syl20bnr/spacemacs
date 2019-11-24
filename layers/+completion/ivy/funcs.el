syl20bnr/spacemacs

commit 9c613b8770e9ed191c71b3da19458502b7ddefd8

Optimize =spacemacs/counsel-search= for =ag= and =rg=
Use =counsel-ag= and =counsel-rg= when possible. They have more
optimizations, such as switching directory, the ability to turn
on and off casefold, etc. Counsel will also carry future
improvement with these two functions, and spacemacs will
automatically benefit from it.

layers/+completion/ivy/funcs.el
@@ -1,6 +1,6 @@
 ;;; funcs.el --- Ivy Layer functions File for Spacemacs
 ;;
-;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;; Copyright (c) 2012-2019 Sylvain Benner & Contributors
 ;;
 ;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
@@ -158,28 +158,37 @@ that directory."
                       (throw 'tool tool)))
                   (throw 'tool "grep")))
           (default-directory
-            (or initial-directory (read-directory-name "Start from directory: "))))
-    (ivy-read
-     (format "%s from [%s]: "
-             tool
-             (if (< (length default-directory)
-                    spacemacs--counsel-search-max-path-length)
-                 default-directory
-               (concat
-                "..." (substring default-directory
-                                 (- (length default-directory)
-                                    spacemacs--counsel-search-max-path-length)
-                                 (length default-directory)))))
-     (spacemacs//make-counsel-search-function tool)
-     :initial-input (when initial-input (rxt-quote-pcre initial-input))
-     :dynamic-collection t
-     :history 'counsel-git-grep-history
-     :action #'counsel-git-grep-action
-     :caller 'spacemacs/counsel-search
-     :keymap spacemacs--counsel-map
-     :unwind (lambda ()
-               (counsel-delete-process)
-               (swiper--cleanup)))))
+            (or initial-directory (read-directory-name "Start from directory: ")))
+          (display-directory
+           (if (< (length default-directory)
+                  spacemacs--counsel-search-max-path-length)
+               default-directory
+             (concat
+              "..." (substring default-directory
+                               (- (length default-directory)
+                                  spacemacs--counsel-search-max-path-length)
+                               (length default-directory))))))
+    (cond ((eq tool "ag")
+           (counsel-ag initial-input initial-directory nil
+                       (format "ag from [%s]: " display-directory)))
+          ((eq tool "rg")
+           (counsel-rg initial-input initial-directory nil
+                       (format "rg from [%s]: " display-directory)))
+          (t
+           (ivy-read
+            (format "%s from [%s]: "
+                    tool
+                    display-directory)
+            (spacemacs//make-counsel-search-function tool)
+            :initial-input (when initial-input (rxt-quote-pcre initial-input))
+            :dynamic-collection t
+            :history 'counsel-git-grep-history
+            :action #'counsel-git-grep-action
+            :caller 'spacemacs/counsel-search
+            :keymap spacemacs--counsel-map
+            :unwind (lambda ()
+                      (counsel-delete-process)
+                      (swiper--cleanup)))))))
 
 ;; Define search functions for each tool
 (cl-loop

commit d031dbd0d18430f750edf5777dcde40409adfef6

[ivy] Fix spacemacs/counsel-search with ag and rg
The commit 9c613b8 introduced a regression for ivy users that use ag or rg as
the search tool. It made `C-c C-e` and `F3` keybindings unavailable, and thus
making it impossible to make multi-file edits.

Partially fixes #12838

layers/+completion/ivy/funcs.el
@@ -117,11 +117,15 @@
             (counsel-git-grep-action c))
           next-error-function 'spacemacs/gne-next)))
 
-(defvar spacemacs--counsel-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "<f3>") 'spacemacs//counsel-save-in-buffer)
-    (define-key map (kbd "C-c C-e") 'spacemacs//counsel-edit)
-    map))
+(defun spacemacs//counsel-search-add-extra-bindings (map)
+  "Add extra counsel-search related keybindings to MAP, then return MAP.
+See `spacemacs/counsel-search' and `counsel-ag'."
+  (define-key map (kbd "<f3>") 'spacemacs//counsel-save-in-buffer)
+  (define-key map (kbd "C-c C-e") 'spacemacs//counsel-edit)
+  map)
+
+(defvar spacemacs--counsel-map (spacemacs//counsel-search-add-extra-bindings
+                                (make-sparse-keymap)))
 
 (defun spacemacs/ivy--regex-plus (str)
   "Build a regex sequence from STR.

layers/+completion/ivy/packages.el
@@ -137,6 +137,7 @@
         (define-key counsel-find-file-map (kbd "C-h") 'counsel-up-directory))
 
       (define-key read-expression-map (kbd "C-r") 'counsel-minibuffer-history)
+      (spacemacs//counsel-search-add-extra-bindings counsel-ag-map)
       ;; remaps built-in commands that have a counsel replacement
       (counsel-mode 1)
       (spacemacs|hide-lighter counsel-mode)