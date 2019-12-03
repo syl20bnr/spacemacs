dd070e3b75e807f5ded1c80ebc6352b2d8577ca7
duianto <otnaiud@gmail.com>
2019-12-03 16:00:56 +0100

    [semantic] Moved toggle keys to the layer
    SPC T S and SPC T C-S showed void variable
    semantic-stickyfunc messages when the semantic
    layer wasn't installed.

--- layers/+emacs/semantic/packages.el
+++ layers/+emacs/semantic/packages.el
@@ -29,8 +29,20 @@
   (spacemacs|use-package-add-hook semantic
     :post-init (add-to-list 'semantic-default-submodes
                             'global-semantic-stickyfunc-mode)))
+
 (defun semantic/init-stickyfunc-enhance ()
-  (use-package stickyfunc-enhance :defer t))
+  (use-package stickyfunc-enhance
+    :defer t
+    :init
+    (progn
+      (spacemacs|add-toggle semantic-stickyfunc
+        :mode semantic-stickyfunc-mode
+        :documentation "Enable semantic-stickyfunc."
+        :evil-leader "TS")
+      (spacemacs|add-toggle semantic-stickyfunc-globally
+        :mode global-semantic-stickyfunc-mode
+        :documentation "Enable semantic-stickyfunc globally."
+        :evil-leader "T C-S"))))
 
 (defun spacemacs//disable-semantic-idle-summary-mode ()
   (semantic-idle-summary-mode 0))
