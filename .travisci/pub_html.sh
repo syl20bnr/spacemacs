6d9ffd4f99ea030fb1fa6dcc58104c0d2a0b956b
JAremko <w3techplayground@gmail.com>
2019-12-09 23:40:56 +0200

    [CI] re-export token

--- .travisci/pub_html.sh
+++ .travisci/pub_html.sh
@@ -30,6 +30,8 @@ else
     exit 0
 fi
 
+export GITHUB_TOKEN=$BOT_TK
+
 fold_start "CLONING_TARGET_REPOSITORY"
 target_URL="https://github.com/syl20bnr/${PUBLISH}.git"
 git clone "${target_URL}" -b gh-pages "/tmp/${PUBLISH}"
