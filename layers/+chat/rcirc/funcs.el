4b451a5fe3e10075cb2aa8855bdd6477089860ef
duianto <otnaiud@gmail.com>
2020-01-07 12:03:54 +0100

    Fix void destructuring-bind after cl pkg deprecation
    destructuring-bind is a macro in the deprecated package: cl
    
    SPC-TAB calls: spacemacs/alternate-buffer
    In the spacemacs-base distribution, it shows the message:
    spacemacs/alternate-buffer: Symbol’s function definition is void: destructuring-bind
    
    Also renamed three instances of destructuring-bind in the rirc layer.

--- layers/+chat/rcirc/funcs.el
+++ layers/+chat/rcirc/funcs.el
@@ -120,9 +120,9 @@ This doesn't support the chanserv auth method. "
 
 (defun spacemacs//znc-auth-source-fetch-password (server)
   "Given a server with at least :host :port :login, return the :password"
-  (destructuring-bind (&key host auth &allow-other-keys)
+  (cl-destructuring-bind (&key host auth &allow-other-keys)
       (cdr server)
-    (destructuring-bind (&key secret &allow-other-keys)
+    (cl-destructuring-bind (&key secret &allow-other-keys)
         (car (auth-source-search :host host
                                  :port "irc"
                                  :user auth
@@ -150,7 +150,7 @@ This doesn't support the chanserv auth method. "
   (cl-loop
    for s in rcirc-server-alist
    collect
-   (destructuring-bind (&key host
+   (cl-destructuring-bind (&key host
                              (port rcirc-default-port)
                              (nick rcirc-default-nick)
                              (user-name rcirc-default-user-name)
