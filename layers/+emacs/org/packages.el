427008036c0b8091754255652025014ecd259d33
Keith Pinson <keith.pinson@banno.com>
2020-01-24 12:27:37 -0500

    fix(org-present): exiting presentation always removes inline images
    Spacemacs configures `org-present` to inline images while presenting. That much
    makes sense. But it has a hook to turn them off unconditionally when exiting
    presentation mode. This causes a problem if you just want images to show in and
    out of presentation mode.
    
    STEPS TO REPRODUCE
    1. Create an Org file
    2. Add a link to a file
    3. Set `#+STARTUP: inlineimages`
    4. `M-x org-present`
    5. Exit presentation mode
    
    EXPECTED
    Because of `#+STARTUP: inlineimages` images are still inlined.
    
    ACTUAL
    The inline images have been removed upon exiting the presentation mode.
    
    FIX
    Check `org-startup-with-inline-images` on exiting presentation mode and only
    remove the inline images if it is false.
    
    TESTING
    To ensure that this did not introduce a regression of the converse behavior, I
    also tested with `#+STARTUP: noinlineimages`, and it still turned off the images
    when exiting the presentation.

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -692,7 +692,8 @@ Headline^^            Visit entry^^               Filter^^                    Da
       (defun spacemacs//org-present-end ()
         "Terminate `org-present' mode"
         (org-present-small)
-        (org-remove-inline-images)
+        (if (not org-startup-with-inline-images)
+            (org-remove-inline-images))
         (org-present-show-cursor)
         (org-present-read-write))
       (add-hook 'org-present-mode-hook 'spacemacs//org-present-start)
