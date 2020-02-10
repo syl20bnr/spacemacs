24d9c3f7ee84e03476bb8e0ddcdac548df7f05c8
Lucius Hu <lebensterben@users.noreply.github.com>
2020-02-10 16:48:43 -0500

    [rust] Add key binding to switch lsp rust server
    `lsp-mode` supports two LSP server backend for Rust language, i.e.
    `rls` and `rust-analyzer`.
    
    `rust-analyzer` is experimental and lacks certain functionalities
    such as `DAP` support.
    
    `lsp-mode` provides a function `lsp-rust-switch-server` that changes
    the priority of LSP server backend for *new* LSP session.
    
    This commit adds a key binding `SPC m s s` to `lsp-rust-switch-server`.

--- layers/+lang/rust/funcs.el
+++ layers/+lang/rust/funcs.el
@@ -44,7 +44,11 @@
 (defun spacemacs//rust-setup-lsp ()
   "Setup lsp backend"
   (if (configuration-layer/layer-used-p 'lsp)
-      (lsp)
+      (progn 
+        (lsp)
+        (spacemacs/declare-prefix-for-mode 'rust-mode "ms" "switch")
+        (spacemacs/set-leader-keys-for-major-mode 'rust-mode
+          "ss" 'lsp-rust-switch-server))
     (spacemacs//lsp-layer-not-installed-message)))
 
 (defun spacemacs//rust-setup-lsp-company ()
