a725900de92670531db446a6582beae99f2c27fd
Ivan Yonchovski <yyoncho@gmail.com>
2020-04-25 18:38:53 +0300

    [fasd] Use helm-fasd in fasd layer when helm is present
    - Underestimated but very handy package.

--- layers/+tools/fasd/packages.el
+++ layers/+tools/fasd/packages.el
@@ -1,21 +1,25 @@
 (setq fasd-packages
-  '(
-    fasd
-    ))
+      '((helm-fasd :requires helm
+                   :location (recipe :repo "ajsalminen/helm-fasd"
+                                     :fetcher github
+                                     :files ("*.el")))
+        (fasd :toggle (not (configuration-layer/layer-used-p 'helm)))))
 
-(defun fasd-find-file-only ()
-  (interactive)
-  (fasd-find-file -1))
 
-(defun fasd-find-directory-only ()
-  (interactive)
-  (fasd-find-file 1))
 
 (defun fasd/init-fasd ()
   "initializes fasd-emacs and adds a key binding to `SPC f z'"
   (use-package fasd
     :init
     (progn
+      (defun fasd-find-file-only ()
+        (interactive)
+        (fasd-find-file -1))
+
+      (defun fasd-find-directory-only ()
+        (interactive)
+        (fasd-find-file 1))
+
       (global-fasd-mode 1)
       (spacemacs/declare-prefix "fa" "fasd-find")
       (spacemacs/set-leader-keys "fad" 'fasd-find-directory-only)
@@ -23,7 +27,14 @@
       (spacemacs/set-leader-keys "fas" 'fasd-find-file)
 
       ;; we will fall back to using the default completing-read function, which is helm once helm is loaded.
-      (setq fasd-completing-read-function 'nil)
-      )
-    )
-  )
+      (setq fasd-completing-read-function 'nil))))
+
+(defun fasd/init-helm-fasd ()
+  "initializes fasd-emacs and adds a key binding to `SPC f z'"
+  (use-package helm-fasd
+    :init
+    (progn
+      (spacemacs/declare-prefix "fa" "fasd find")
+      (spacemacs/set-leader-keys "fad" #'helm-fasd-directories)
+      (spacemacs/set-leader-keys "faf" #'helm-fasd-files)
+      (spacemacs/set-leader-keys "fas" #'helm-fasd))))
