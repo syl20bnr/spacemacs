015303a7014be847e0f3339b76d632b8c2a84b78
Maximilian Wolff <smile13241324@gmail.com>
2020-05-19 00:00:44 +0200

    [extempore] Fix BEGIN_SRC block in doc and provide better eldoc setup
    A BEGIN_SRC block in the documents had no language specified
    which would break CI.
    
    I have also changed the eldoc setup to run each time the mode is
    entered instead of only in the initially loaded buffer.

---
+++ layers/+lang/extempore/funcs.el
@@ -0,0 +1,14 @@
+;;; funcs.el --- Extempore Layer functions File for Spacemacs
+;;
+;; Copyright (c) 2012-2020 Sylvain Benner & Contributors
+;;
+;; Author: Maximilian Wolff <smile13241324@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defun spacemacs//extempore-setup-eldoc ()
+  (setq-local eldoc-documentation-function 'extempore-eldoc-documentation-function)
+  (eldoc-mode))
--- layers/+lang/extempore/packages.el
+++ layers/+lang/extempore/packages.el
@@ -15,36 +15,36 @@
 (defun extempore/init-extempore-mode ()
   "Initialize extempore mode"
   (use-package extempore-mode
-	:defer t
-	:mode
-	(("\\.xtm$" . extempore-mode))
-	:init
-	(spacemacs/register-repl 'extempore-mode 'extempore-repl "extempore")
-	:config
-	(progn
-	  (spacemacs/declare-prefix-for-mode 'extempore-mode "mc" "process")
-	  (spacemacs/declare-prefix-for-mode 'extempore-mode "me" "eval")
+    :defer t
+    :mode
+    (("\\.xtm$" . extempore-mode))
+    :init
+    (spacemacs/register-repl 'extempore-mode 'extempore-repl "extempore")
+    :config
+    (progn
+      (spacemacs/declare-prefix-for-mode 'extempore-mode "mc" "process")
+      (spacemacs/declare-prefix-for-mode 'extempore-mode "me" "eval")
 
-	  (spacemacs/set-leader-keys-for-major-mode 'extempore-mode
-		"'"  'extempore-repl
-		","  'lisp-state-toggle-lisp-state
+      (spacemacs/set-leader-keys-for-major-mode 'extempore-mode
+        "'"  'extempore-repl
+        ","  'lisp-state-toggle-lisp-state
 
-		"cc" 'switch-to-extempore
-		"cj" 'extempore-connect
+        "cc" 'switch-to-extempore
+        "cj" 'extempore-connect
 
-		"ee" 'extempore-send-last-sexp
-		"ef" 'extempore-send-definition
-		"er" 'extempore-send-region
-		"eb" 'extempore-send-buffer-or-region
-		(setq extempore-tab-completion nil)
+        "ee" 'extempore-send-last-sexp
+        "ef" 'extempore-send-definition
+        "er" 'extempore-send-region
+        "eb" 'extempore-send-buffer-or-region
+        (setq extempore-tab-completion nil)
 
-		(set-face-attribute 'extempore-blink-face nil :foreground "#272822" :background "#FD971F")
-		(set-face-attribute 'extempore-sb-blink-face nil :foreground "#272822" :background "#39FF14")
+        (set-face-attribute 'extempore-blink-face nil :foreground "#272822" :background "#FD971F")
+        (set-face-attribute 'extempore-sb-blink-face nil :foreground "#272822" :background "#39FF14")
 
-		;; stop the ' (quote) character being paired by smartparens
-		(with-eval-after-load 'smartparens
-		  (sp-local-pair 'extempore-mode "'" nil :actions nil)
-		  (sp-local-pair 'extempore-mode "`" nil :actions nil))))))
+        ;; stop the ' (quote) character being paired by smartparens
+        (with-eval-after-load 'smartparens
+          (sp-local-pair 'extempore-mode "'" nil :actions nil)
+          (sp-local-pair 'extempore-mode "`" nil :actions nil))))))
 
 (defun extempore/post-init-eldoc ()
-  (setq-local eldoc-documentation-function 'extempore-eldoc-documentation-function))
+  (add-hook 'extempore-mode-hook #'spacemacs//extempore-setup-eldoc))
