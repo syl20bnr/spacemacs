f5fddc4256399ff3cd479221f4a7adbe2db17dc3
Keith Pinson <keith.pinson@banno.com>
2020-05-28 12:36:21 -0400

    [scala] drop support for Unicode arrows, now deprecated in Scala
    As of Scala 2.13, Unicode arrows are deprecated:
    
    * https://github.com/scala/scala/pull/7540
    * https://github.com/scala/scala-dev/issues/585
    * https://github.com/scala/bug/issues/11210
    
    Using one will give a deprecation warnings like so:
    
    > The unicode arrow `⇒` is deprecated, use `=>` instead. If you still wish to
    > display it as one character, consider using a font with programming ligatures
    > such as Fira Code.
    
    As such the Scala layer's version slick capability to replace ASCII arrows with
    Unicode ones is no longer useful, and I have removed it.
    
    Based on my tests it doesn't seem that there is a need for a more graceful way
    to deprecate this: i.e. nothing fails if there is extra junk in `:variables`.

--- layers/+lang/scala/packages.el
+++ layers/+lang/scala/packages.el
@@ -220,46 +220,6 @@
       (evil-define-key 'insert scala-mode-map
         (kbd "RET") 'scala/newline-and-indent-with-asterisk)
 
-      ;; Automatically replace arrows with unicode ones when enabled
-      (defconst scala-unicode-arrows-alist
-        '(("=>" . "⇒")
-          ("->" . "→")
-          ("<-" . "←")))
-
-      (defun scala/replace-arrow-at-point ()
-        "Replace the arrow before the point (if any) with unicode ones.
-An undo boundary is inserted before doing the replacement so that
-it can be undone."
-        (let* ((end (point))
-               (start (max (- end 2) (point-min)))
-               (x (buffer-substring start end))
-               (arrow (assoc x scala-unicode-arrows-alist)))
-          (when arrow
-            (undo-boundary)
-            (backward-delete-char 2)
-            (insert (cdr arrow)))))
-
-      (defun scala/gt ()
-        "Insert a `>' to the buffer.
-If it's part of a right arrow (`->' or `=>'),replace it with the corresponding
-unicode arrow."
-        (interactive)
-        (insert ">")
-        (scala/replace-arrow-at-point))
-
-      (defun scala/hyphen ()
-        "Insert a `-' to the buffer.
-If it's part of a left arrow (`<-'),replace it with the unicode arrow."
-        (interactive)
-        (insert "-")
-        (scala/replace-arrow-at-point))
-
-      (when scala-use-unicode-arrows
-        (define-key scala-mode-map
-          (kbd ">") 'scala/gt)
-        (define-key scala-mode-map
-          (kbd "-") 'scala/hyphen))
-
       (evil-define-key 'normal scala-mode-map "J" 'spacemacs/scala-join-line)
 
       ;; Compatibility with `aggressive-indent'
