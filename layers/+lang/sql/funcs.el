f752947d18fe4fb54b9f0f67d1139c68ac252a9b
Shunya Ishii <sh141ts@gmail.com>
2020-06-25 21:42:37 +0900

    [SQL] Add lsp support

--- layers/+lang/sql/funcs.el
+++ layers/+lang/sql/funcs.el
@@ -1,6 +1,6 @@
 ;;; funcs.el --- SQL Layer functions File for Spacemacs
 ;;
-;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;; Copyright (c) 2012-2020 Sylvain Benner & Contributors
 ;;
 ;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
@@ -16,3 +16,28 @@
    spacemacs-sql-startable (remove-if-not
                             (lambda (product) (sql-get-product-feature (car product) :sqli-program))
                             sql-product-alist)))
+
+(defun spacemacs//sql-backend ()
+  "Returns selected backend."
+  (if sql-backend
+      sql-backend
+    (cond
+     ((configuration-layer/layer-used-p 'lsp) 'lsp)
+     (t 'company-sql))))
+
+(defun spacemacs//sql-setup-company ()
+  "Conditionally setup company based on backend."
+  (pcase (spacemacs//sql-backend)
+    ('company-sql (spacemacs|add-company-backends
+                    :backends company-capf
+                    :modes sql-mode))
+    ;; Activate lsp company explicitly to activate
+    ;; standard backends as well
+    (`lsp (spacemacs|add-company-backends
+            :backends company-capf
+            :modes sql-mode))))
+
+(defun spacemacs//sql-setup-backend ()
+  "Conditionally setup sql backend."
+  (pcase (spacemacs//sql-backend)
+    (`lsp (lsp))))
