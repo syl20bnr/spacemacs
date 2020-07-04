8d9245efb429d467567ca251b84cdd94269669c7
Ivan Yonchovski <yyoncho@gmail.com>
2020-07-04 19:28:38 +0300

    Make spacemacs/useful-buffer-p much faster
    - `with-current-buffer` triggers a lot of hooks.

--- layers/+spacemacs/spacemacs-defaults/funcs.el
+++ layers/+spacemacs/spacemacs-defaults/funcs.el
@@ -175,8 +175,7 @@ automatically applied to."
 (defun spacemacs/useful-buffer-p (buffer)
   "Determines if a buffer is useful."
   (let ((buf-name (buffer-name buffer)))
-    (or (with-current-buffer buffer
-          (derived-mode-p 'comint-mode))
+    (or (provided-mode-derived-p (buffer-local-value 'major-mode buffer) 'comint-mode)
         (cl-loop for useful-regexp in spacemacs-useful-buffers-regexp
                  thereis (string-match-p useful-regexp buf-name))
         (cl-loop for useless-regexp in spacemacs-useless-buffers-regexp
