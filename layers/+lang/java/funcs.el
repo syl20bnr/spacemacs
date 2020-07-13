4e57eaceea74613e9a737882b5690eef24aa1f1a
Keith Pinson <keith.pinson@banno.com>
2020-07-13 10:19:38 -0400

    Remove gradle-mode to stop it breaking bindings of other major modes
    After using Java mode, Gradle keybindings infect or even clobber keybindings of
    any subsequently used major-mode. For example, the critical Agda major mode
    keybinding of `l` as `agda2-load` gets with a Gradle menu!
    
    The cause, found by @duianto, is that `emacs-gradle-mode` sets itself to a
    [global](https://github.com/jacobono/emacs-gradle-mode/blob/e4d665d5784ecda7ddfba015f07c69be3cfc45f2/gradle-mode.el#L176-L183)
    minor mode. The docs for `define-minor-mode` say about `:global:`
    
    > If non-nil specifies that the minor mode is not meant to be
    > buffer-local, so don't make the variable MODE buffer-local.
    > By default, the mode is buffer-local.
    
    I don't know why `gradle-mode` is doing this; presumably there is some reason or
    need for it. But the author of that package hasn't been on GitHub since 2017,
    and the last update of the package itself is from early 2015.
    
    To seal the deal, `gradle-mode` hasn't been working anyway (at least, I've never
    got it to work). I've been running Gradle by launching a terminal within Emacs.
    
    Fixes #13750.

--- layers/+lang/java/funcs.el
+++ layers/+lang/java/funcs.el
@@ -82,24 +82,6 @@
   (mvn-compile))
 
 
-;; Gradle
-
-(defun spacemacs/gradle-clean ()
-  "Execute 'gradle clean' command."
-  (interactive)
-  (gradle-execute "clean"))
-
-(defun spacemacs/gradle-clean-build ()
-  "Execute 'gradle clean build' command."
-  (interactive)
-  (gradle-execute "clean build"))
-
-(defun spacemacs/gradle-test-buffer ()
-  "Execute 'gradle test' command against current buffer tests."
-  (interactive)
-  (gradle-single-test (file-name-base (buffer-file-name))))
-
-
 ;; Misc
 
 (defun spacemacs//java-delete-horizontal-space ()
