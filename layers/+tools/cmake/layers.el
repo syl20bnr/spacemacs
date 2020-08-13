76301564c5fa6fbd4a9a07ef35529d931aecd367
Maximilian Wolff <smile13241324@gmail.com>
2020-08-13 23:22:11 +0200

    [cmake] Add lsp server support

---
+++ layers/+tools/cmake/layers.el
@@ -0,0 +1,14 @@
+;;; layers.el --- cmake Layer layers File for Spacemacs
+;;
+;; Copyright (c) 2012-2020 Sylvain Benner & Contributors
+;;
+;; Author: Maximilian Wolff <smile13241324@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(when (and (boundp 'cmake-backend)
+           (eq cmake-backend 'lsp))
+  (configuration-layer/declare-layer-dependencies '(lsp)))
