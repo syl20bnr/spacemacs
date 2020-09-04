14a767fc664c725da644811921803e7bcd0cb31b
enderger <endergeryt@gmail.com>
2020-09-04 09:16:43 -0700

    Replace Depreciated Package in Perl6 Layer (#13899)
    * Switch from nabero's ron mode to my own custom ron-mode (hosted on MELPA)
    * Replace depreciated perl6-mode withraku-mode
    * Remember to update the init function

--- layers/+lang/perl6/packages.el
+++ layers/+lang/perl6/packages.el
@@ -15,7 +15,7 @@
     evil
     flycheck
     (flycheck-perl6 :requires flycheck)
-    perl6-mode
+    raku-mode
     ))
 
 (defun perl6/post-init-company ()
@@ -33,7 +33,7 @@
   (with-eval-after-load 'flycheck
     (require 'flycheck-perl6)))
 
-(defun perl6/init-perl6-mode()
-  (use-package perl6-mode
+(defun perl6/init-raku-mode()
+  (use-package raku-mode
     :defer t
     :mode (("/perl6/site/sources/" . perl6-mode))))
