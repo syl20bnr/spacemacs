642c597b9f2e23cfb3d0f218f33be91b61208f92
JAremko <w3techplayground@gmail.com>
2020-09-05 13:16:15 +0300

    [tests] remove mirror related code

--- .github/workflows/scripts/dotspacemacs.el
+++
@@ -1,7 +0,0 @@
-(defun dotspacemacs/layers ()
-  (setq-default
-   dotspacemacs-distribution 'spacemacs))
-(defun dotspacemacs/init ())
-(defun dotspacemacs/user-init ())
-(defun dotspacemacs/config ())
-(defun dotspacemacs/user-config ())
