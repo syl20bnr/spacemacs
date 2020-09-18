5428e5e5a0d8ac5cae531ea71521b6a3089593f9
Ade Attwood <code@adeattwood.co.uk>
2020-09-18 20:18:34 +0100

    Move ligatures under unicode-fonts

--- layers/+fonts/unicode-fonts/config.el
+++ layers/+fonts/unicode-fonts/config.el
@@ -14,3 +14,24 @@
   on macs. This should only be set when using the multi-color
   patch as emacs-plus does. It is unnecessary to set this when
   using the macOS port version.")
+
+
+(defvar unicode-fonts-enable-ligatures nil
+  "If you want to enable font ligatures")
+
+(defvar unicode-fonts-ligature-set '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                       "\\" "://")
+  "List of ligatures you would like enabled")
+
+(defvar unicode-fonts-ligature-modes '(t) "The enabled modes ligatures are enabled in default is all modes")
--- layers/+fonts/unicode-fonts/packages.el
+++ layers/+fonts/unicode-fonts/packages.el
@@ -12,10 +12,10 @@
 ;;; Code:
 
 (defconst unicode-fonts-packages
-  '(
-    unicode-fonts
+  '(unicode-fonts
     persistent-soft
-    ))
+    (ligature :location (recipe :fetcher github :repo "mickeynp/ligature.el")
+              :toggle (and (> emacs-major-version 26) unicode-fonts-enable-ligatures))))
 
 (defun unicode-fonts/init-persistent-soft ()
   (use-package persistent-soft
@@ -29,5 +29,12 @@
                  (eq window-system 'ns))
         (setq unicode-fonts-skip-font-groups '(decorative low-quality-glyphs)))
       (unicode-fonts-setup))))
+
+(defun unicode-fonts/init-ligature ()
+  "Initialise the ligatures for emacs 27+"
+  (when (> emacs-major-version 26)
+    (dolist (mode unicode-fonts-ligature-modes)
+      (ligature-set-ligatures mode unicode-fonts-ligature-set))
+    (global-ligature-mode t)))
 
 ;;; packages.el ends here
