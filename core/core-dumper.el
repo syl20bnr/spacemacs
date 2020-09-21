fd5113d0fb4aeebc95d4c339d4180f026dee53f0
syl20bnr <sylvain.benner@gmail.com>
2020-09-21 01:13:14 -0400

    [core] Add a global spinner in mode line when dumping Spacemacs

--- core/core-dumper.el
+++ core/core-dumper.el
@@ -9,6 +9,8 @@
 ;;
 ;;; License: GPLv3
 
+(require 'spinner)
+
 (defvar spacemacs-dump-mode 'not-dumped
   "Spacemacs dump mode, can be `not-dumped', `dumped' or `dumping'.")
 
@@ -21,10 +23,19 @@
 (defvar spacemacs-dump-load-path nil
   "Variable to backup the `load-path' variable built during the dump creation.")
 
+(defvar spacemacs-dump-spinner nil
+  "Spinner curently being displayed on the `global-mode-string'.")
+
+(defvar spacemacs--dump-old-global-mode-string nil
+  "Copy of `global-mode-string' before the spinner is created")
+
 (defconst spacemacs-dump-directory
   (concat spacemacs-cache-directory "dumps/"))
 
 (defconst spacemacs-dump-buffer-name "*spacemacs-dumper*")
+
+(defconst spacemacs--dump-spinner-construct
+  '("" (:eval (spinner-print spacemacs-dump-spinner))))
 
 (defun spacemacs/dump-save-load-path ()
   "Save `load-path' variable."
@@ -107,13 +118,20 @@ When universal prefix argument is passed then display the process buffer."
   (interactive "P")
   (when spacemacs-dump-process
     (message "Cancel running dumping process to start a new one.")
+    (spinner-stop)
     (delete-process spacemacs-dump-process))
   (when-let ((buf (get-buffer spacemacs-dump-buffer-name)))
     (with-current-buffer buf
       (erase-buffer)))
   (make-directory spacemacs-dump-directory t)
-  (let* ((dump-file (concat spacemacs-dump-directory dotspacemacs-emacs-dumper-dump-file))
+  (let* ((dump-file (concat spacemacs-dump-directory
+                            dotspacemacs-emacs-dumper-dump-file))
          (dump-file-temp (concat dump-file ".new")))
+    (unless (equal global-mode-string spacemacs--dump-spinner-construct)
+      (setq spacemacs--dump-old-global-mode-string global-mode-string))
+    (setq spacemacs-dump-spinner (make-spinner 'progress-bar nil 1))
+    (spinner-start spacemacs-dump-spinner)
+    (setq global-mode-string spacemacs--dump-spinner-construct)
     (setq spacemacs-dump-process
           (make-process
            :name "spacemacs-dumper"
@@ -131,6 +149,8 @@ When universal prefix argument is passed then display the process buffer."
                    (delete-file dump-file-temp nil)
                    (goto-char (point-max))
                    (insert "Failed\n")))
+               (spinner-stop)
+               (setq global-mode-string spacemacs--dump-old-global-mode-string)
                (delete-process spacemacs-dump-process)
                (setq spacemacs-dump-process nil)))
            :command
