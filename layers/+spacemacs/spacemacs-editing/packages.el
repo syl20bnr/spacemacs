d3bf6ec34bfa9f5c7e4182e308015f4f17d14db3
Keith Pinson <keith.pinson@banno.com>
2020-10-14 15:44:55 -0400

    [editing] Integrate evil-vimish-fold via dotspacemacs-folding-method
    Not confident that I have plugged this in everywhere that I should have. However
    I tested locally and it seems to be working.

--- layers/+spacemacs/spacemacs-editing/packages.el
+++ layers/+spacemacs/spacemacs-editing/packages.el
@@ -31,6 +31,8 @@
         string-inflection
         undo-tree
         uuidgen
+        (vimish-fold :toggle (eq 'vimish dotspacemacs-folding-method))
+        (evil-vimish-fold :toggle (eq 'vimish dotspacemacs-folding-method))
         ws-butler))
 
 ;; Initialization of packages
@@ -275,6 +277,18 @@
 ;; Note: The key binding for the fold transient state is defined in
 ;; evil config
 
+(defun spacemacs-editing/init-vimish-fold ()
+  (use-package vimish-fold
+    :ensure
+    :after evil))
+
+(defun spacemacs-editing/init-evil-vimish-fold ()
+  (use-package evil-vimish-fold
+    :ensure
+    :after vimish-fold
+    :init
+    (setq evil-vimish-fold-target-modes '(prog-mode conf-mode text-mode))
+    :config (global-evil-vimish-fold-mode)))
 
 (defun spacemacs-editing/init-password-generator ()
   (use-package password-generator
