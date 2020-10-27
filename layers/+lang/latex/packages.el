c20c35f83d7133eacfd0a23017955a63c191b137
Maximilian Wolff <smile13241324@gmail.com>
2020-10-27 22:48:22 +0100

    Add lsp support to latex layer

--- layers/+lang/latex/packages.el
+++ layers/+lang/latex/packages.el
@@ -9,25 +9,29 @@
 ;;
 ;;; License: GPLv3
 
-(setq latex-packages
-      '(
-        auctex
-        (auctex-latexmk :toggle (string= "LatexMk" latex-build-command))
-        (company-auctex :requires company)
-        (company-reftex :requires company)
-        evil-matchit
-        (reftex :location built-in)
-        flycheck
-        flyspell
-        ggtags
-        counsel-gtags
-        helm-gtags
-        (magic-latex-buffer :toggle latex-enable-magic)
-        smartparens
-        typo
-        yasnippet
-        which-key
-        ))
+(defconst latex-packages
+  '(
+    auctex
+    (auctex-latexmk :toggle (string= "LatexMk" latex-build-command))
+    company
+    (company-auctex :requires company)
+    (company-reftex :requires company)
+    counsel-gtags
+    evil-matchit
+    flycheck
+    flyspell
+    ggtags
+    helm-gtags
+    (lsp-latex :requires lsp-mode)
+    (magic-latex-buffer :toggle latex-enable-magic)
+    smartparens
+    (reftex :location built-in)
+    typo
+    which-key
+    yasnippet))
+
+(defun latex/post-init-company ()
+  (spacemacs//latex-setup-company))
 
 (defun latex/init-auctex ()
   (use-package tex
@@ -48,7 +52,8 @@
         (add-hook 'LaTeX-mode-hook 'TeX-fold-mode))
       (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
       (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
-      (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode))
+      (add-hook 'LaTeX-mode-hook 'TeX-PDF-mode)
+      (add-hook 'LaTeX-mode-hook #'spacemacs//latex-setup-backend))
     :config
     (progn
       ;; Key bindings for plain TeX
@@ -59,8 +64,6 @@
           "%"   'TeX-comment-or-uncomment-paragraph          ;; C-c %
           ";"   'comment-or-uncomment-region                 ;; C-c ; or C-c :
           ;; TeX-command-run-all runs compile and open the viewer
-          "a"   'TeX-command-run-all                         ;; C-c C-a
-          "b"   'latex/build
           "k"   'TeX-kill-job                                ;; C-c C-k
           "l"   'TeX-recenter-output-buffer                  ;; C-c C-l
           "m"   'TeX-insert-macro                            ;; C-c C-m
@@ -78,6 +81,14 @@
           "xfc" 'latex/font-small-caps
           "xff" 'latex/font-sans-serif
           "xfr" 'latex/font-serif)
+        (spacemacs/declare-prefix-for-mode mode "mxf" "fonts")
+        (unless (and (eq (spacemacs//latex-backend) 'lsp)
+                     (eq mode 'latex-mode))
+          (spacemacs/declare-prefix-for-mode mode "mh" "help")
+          (spacemacs/declare-prefix-for-mode mode "mx" "text/fonts")
+          (spacemacs/set-leader-keys-for-major-mode mode
+            "a"   'TeX-command-run-all                         ;; C-c C-a
+            "b"   'latex/build))
         (when dotspacemacs-major-mode-emacs-leader-key
           (spacemacs/set-leader-keys-for-major-mode mode
             dotspacemacs-major-mode-emacs-leader-key 'TeX-command-master))
@@ -98,17 +109,13 @@
             "zP" 'TeX-fold-clearout-paragraph
             "zr" 'TeX-fold-region
             "zR" 'TeX-fold-clearout-region
-            "zz" 'TeX-fold-dwim))
-        (spacemacs/declare-prefix-for-mode mode "mh" "help")
-        (spacemacs/declare-prefix-for-mode mode "mx" "text/fonts")
-        (spacemacs/declare-prefix-for-mode mode "mz" "fold"))
+            "zz" 'TeX-fold-dwim)
+          (spacemacs/declare-prefix-for-mode mode "mz" "fold")))
 
       ;; Key bindings specific to LaTeX
       (spacemacs/set-leader-keys-for-major-mode 'latex-mode
         "*"   'LaTeX-mark-section      ;; C-c *
         "."   'LaTeX-mark-environment  ;; C-c .
-        "c"   'LaTeX-close-environment ;; C-c ]
-        "e"   'LaTeX-environment       ;; C-c C-e
         "ii"   'LaTeX-insert-item       ;; C-c C-j
         "s"   'LaTeX-section           ;; C-c C-s
         "fe"  'LaTeX-fill-environment  ;; C-c C-q C-e
@@ -128,6 +135,19 @@
         "xfa" 'latex/font-calligraphic
         "xfn" 'latex/font-normal
         "xfu" 'latex/font-upright)
+
+      ;; Rebind latex keys to avoid conflicts with lsp mode
+      (if (eq (spacemacs//latex-backend) 'lsp)
+          (spacemacs/set-leader-keys-for-major-mode 'latex-mode
+            "au"   'TeX-command-run-all
+            "c"   'latex/build
+            "ic"   'LaTeX-close-environment ;; C-c ]
+            "ie"   'LaTeX-environment)       ;; C-c C-e
+        (spacemacs/set-leader-keys-for-major-mode 'latex-mode
+          "c"   'LaTeX-close-environment ;; C-c ]
+          "e"   'LaTeX-environment))       ;; C-c C-e
+
+      ;; Declare prefixes
       (spacemacs/declare-prefix-for-mode 'latex-mode "mi" "insert")
       (spacemacs/declare-prefix-for-mode 'latex-mode "mp" "preview")
       (spacemacs/declare-prefix-for-mode 'latex-mode "mf" "fill"))))
@@ -142,30 +162,11 @@
     :defer t
     :init (setq auctex-latexmk-inherit-TeX-PDF-mode t)))
 
-(defun latex/init-company-auctex ()
-  (use-package company-auctex
-    :defer t
-    :init (spacemacs|add-company-backends
-            :backends
-            (company-auctex-macros
-             company-auctex-symbols
-             company-auctex-environments)
-            :modes LaTeX-mode)))
-
-(defun latex/init-company-reftex ()
-  (use-package company-reftex
-    :defer t
-    :init (spacemacs|add-company-backends
-            :backends
-            company-reftex-labels
-            company-reftex-citations
-            :modes LaTeX-mode)))
-
 (defun latex/post-init-evil-matchit ()
   (add-hook 'LaTeX-mode-hook 'evil-matchit-mode))
 
 (defun latex/post-init-flycheck ()
-  (spacemacs/enable-flycheck 'LaTeX-mode))
+  (spacemacs/enable-flycheck 'latex-mode))
 
 (defun latex/post-init-flyspell ()
   (spell-checking/add-flyspell-hook 'LaTeX-mode-hook))
@@ -174,21 +175,22 @@
   (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
   (setq reftex-plug-into-AUCTeX '(nil nil t t t)
         reftex-use-fonts t)
-  (spacemacs/declare-prefix-for-mode 'latex-mode "mr" "reftex")
-  (spacemacs/set-leader-keys-for-major-mode 'latex-mode
-    "rc"    'reftex-citation
-    "rg"    'reftex-grep-document
-    "ri"    'reftex-index-selection-or-word
-    "rI"    'reftex-display-index
-    "r TAB" 'reftex-index
-    "rl"    'reftex-label
-    "rp"    'reftex-index-phrase-selection-or-word
-    "rP"    'reftex-index-visit-phrases-buffer
-    "rr"    'reftex-reference
-    "rs"    'reftex-search-document
-    "rt"    'reftex-toc
-    "rT"    'reftex-toc-recenter
-    "rv"    'reftex-view-crossref))
+  (let ((prefix (if (eq (spacemacs//latex-backend) 'lsp) "R" "r")))
+    (spacemacs/declare-prefix-for-mode 'latex-mode (concat "m" prefix) "reftex")
+    (spacemacs/set-leader-keys-for-major-mode 'latex-mode
+      (concat prefix "c")    'reftex-citation
+      (concat prefix "g")    'reftex-grep-document
+      (concat prefix" i")    'reftex-index-selection-or-word
+      (concat prefix "I")    'reftex-display-index
+      (concat prefix "TAB") 'reftex-index
+      (concat prefix "l")    'reftex-label
+      (concat prefix "p")    'reftex-index-phrase-selection-or-word
+      (concat prefix "P")    'reftex-index-visit-phrases-buffer
+      (concat prefix "r")    'reftex-reference
+      (concat prefix "s")    'reftex-search-document
+      (concat prefix "t")    'reftex-toc
+      (concat prefix "T")    'reftex-toc-recenter
+      (concat prefix "v")    'reftex-view-crossref)))
 
 (defun latex/post-init-counsel-gtags ()
   (spacemacs/counsel-gtags-define-keys-for-mode 'latex-mode))
@@ -226,3 +228,15 @@
             magic-latex-enable-pretty-symbols t
             magic-latex-enable-block-align nil
             magic-latex-enable-inline-image nil))))
+
+(defun latex/init-lsp-latex ()
+  (use-package lsp-latex
+    :defer t))
+
+(defun latex/init-company-auctex ()
+  (use-package company-auctex
+    :defer t))
+
+(defun latex/init-company-reftex ()
+  (use-package company-reftex
+    :defer t))
