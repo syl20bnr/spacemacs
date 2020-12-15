eb31481c177308818983b5b9e9f983a11cdf54fa
JAremko <w3techplayground@gmail.com>
2020-12-15 19:03:52 +0200

    Refactor some names and simplify tests

--- tests/core/core-spacebind-utest.el
+++ tests/core/core-spacebind-utest.el
@@ -11,70 +11,7 @@
 (require 'core-spacebind)
 (require 'cl-lib)
 
-(defconst test-spacebind-moked-fns-sig
-  '((spacemacs/add-key-based-replacements-for-minor-mode
-     (MODE KEY-SEQUENCE REPLACEMENT &rest MORE))
-    (spacemacs/declare-prefix
-      (PREFIX NAME &optional LONG-NAME))
-    (spacemacs/declare-prefix-for-mode
-      (MODE PREFIX NAME &optional LONG-NAME))
-    (spacemacs/set-leader-keys
-      (KEY DEF &rest BINDINGS))
-    (spacemacs/set-leader-keys-for-major-mode
-      (MODE KEY DEF &rest BINDINGS))
-    (spacemacs/set-leader-keys-for-minor-mode
-      (MODE KEY DEF &rest BINDINGS))
-    (which-key-add-key-based-replacements
-      (KEY-SEQUENCE REPLACEMENT &rest MORE))
-    (which-key-add-major-mode-key-based-replacements
-      (MODE KEY-SEQUENCE REPLACEMENT &rest MORE))
-    (spacemacs/add-which-key-fn-key-seq-override
-     (FN-NAME KEY-REP LABEL)))
-  "Signature of the functions that we will mock for the `spacebind' tests.")
-
-;;;; Helpers:
-(defmacro test-spacebind|subst-eval (rep-fun &rest body)
-  "Substitute calls to binding functions with calls to REP-FUN.
-REP-FUN applied to the form: (fn-sym (args)).
-Binding functions are listed in `test-spacebind-moked-fns-sig'. "
-  `(cl-letf
-       (((symbol-function 'spacemacs/leader-key) (lambda () "SPC"))
-        ((symbol-function 'spacemacs/major-mode-prefix) (lambda () "m"))
-        ,@(mapcar
-           (lambda (seg)
-             (let* ((f-s (car seg))
-                    (args (cadr seg))
-                    (pl-args (cl-set-difference args '(&rest &optional))))
-               `((symbol-function ',f-s)
-                 (lambda ,args (,rep-fun ',f-s (list ,@pl-args))))))
-           test-spacebind-moked-fns-sig))
-     ,@body))
-
-(defmacro test-spacebind|log-calls (&rest body)
-  "Evaluate BODY while mocking and logging calls to the binding functions.
-The log is returned.
-Binding functions are listed in `test-spacebind-moked-fns-sig'.
-NOTE: `spacebind--eager-bind' set to true. "
-  `(let ((spacebind--eager-bind t)
-         (acc nil))
-     (test-spacebind|subst-eval
-      (lambda (fn args)
-        (push (cons fn args) acc))
-      ,@body)
-     acc))
-
-;; Example:
-(thread-last (spacemacs|spacebind
-              :major
-              (major-foo-mode
-               ("a" "section a"
-                ("a" foo-fn "execute foo-fn"))))
-  (test-spacebind|log-calls)
-  (format "%S")
-  (insert)
-  ;; Prevents execution
-  (declare))
-
+;;;; Helpers
 (defun test-spacebind/plist-diff (plist1 plist2)
   "Returns difference of PLIST1 and PLIST2 using `equal'."
   (cl-labels ((recur
@@ -116,32 +53,32 @@ NOTE: `spacebind--eager-bind' set to true."
                   ()
                   (setq ret-plist (list
                                    :minor-mode-replacements
-                                   spacebind--bs-add-minor-mode-replacements
+                                   spacebind--bs-minor-mode-replacements
                                    :major-mode-replacements
-                                   spacebind--bs-add-major-mode-replacements
+                                   spacebind--bs-major-mode-replacements
                                    :declare-prefix
                                    spacebind--bs-declare-prefix
                                    :declare-prefix-for-mode
                                    spacebind--bs-declare-prefix-for-mode
                                    :set-leader-keys
-                                   spacebind--bs-set-leader-keys
+                                   spacebind--bs-leader-keys
                                    :set-leader-keys-for-major-mode
-                                   spacebind--bs-set-leader-keys-for-major-mode
+                                   spacebind--bs-leader-keys-for-major-mode
                                    :set-leader-keys-for-minor-mode
-                                   spacebind--bs-set-leader-keys-for-minor-mode
+                                   spacebind--bs-leader-keys-for-minor-mode
                                    :global-replacements
                                    spacebind--bs-global-replacements
                                    :fn-key-seq-override
-                                   spacebind--bs-add-fn-key-seq-override)
+                                   spacebind--bs-fn-key-seq-override)
                         spacebind--bs-global-replacements nil
-                        spacebind--bs-set-leader-keys-for-minor-mode nil
-                        spacebind--bs-set-leader-keys-for-major-mode nil
-                        spacebind--bs-set-leader-keys nil
+                        spacebind--bs-leader-keys-for-minor-mode nil
+                        spacebind--bs-leader-keys-for-major-mode nil
+                        spacebind--bs-leader-keys nil
                         spacebind--bs-declare-prefix-for-mode nil
                         spacebind--bs-declare-prefix nil
-                        spacebind--bs-add-major-mode-replacements nil
-                        spacebind--bs-add-minor-mode-replacements nil
-                        spacebind--bs-add-fn-key-seq-override nil
+                        spacebind--bs-major-mode-replacements nil
+                        spacebind--bs-minor-mode-replacements nil
+                        spacebind--bs-fn-key-seq-override nil
                         spacebind--timer [t])))
        (progn
          ,@body
@@ -164,30 +101,30 @@ NOTE: `spacebind--eager-bind' set to true."
 NOTE: `spacebind--eager-bind' set to true. "
   `(cl-letf* ((invalid-key-seqs '())
               (spacebind--eager-bind t)
-              (spacebind--bs-add-minor-mode-replacements '())
-              (spacebind--bs-add-major-mode-replacements '())
+              (spacebind--bs-minor-mode-replacements '())
+              (spacebind--bs-major-mode-replacements '())
               (spacebind--bs-declare-prefix '())
               (spacebind--bs-declare-prefix-for-mode '())
-              (spacebind--bs-set-leader-keys '())
-              (spacebind--bs-set-leader-keys-for-major-mode '())
-              (spacebind--bs-set-leader-keys-for-minor-mode '())
+              (spacebind--bs-leader-keys '())
+              (spacebind--bs-leader-keys-for-major-mode '())
+              (spacebind--bs-leader-keys-for-minor-mode '())
               (spacebind--bs-global-replacements '())
-              (spacebind--bs-add-fn-key-seq-override '())
+              (spacebind--bs-fn-key-seq-override '())
               (spacebind--timer [t])
               (called nil)
               ((symbol-function 'spacebind//process-bind-stack)
                (lambda () (progn))))
      ,@body
      (dolist (el (apply 'append
-                        (append spacebind--bs-add-minor-mode-replacements
-                                spacebind--bs-add-major-mode-replacements
+                        (append spacebind--bs-minor-mode-replacements
+                                spacebind--bs-major-mode-replacements
                                 spacebind--bs-declare-prefix
                                 spacebind--bs-declare-prefix-for-mode
-                                spacebind--bs-set-leader-keys
-                                spacebind--bs-set-leader-keys-for-major-mode
-                                spacebind--bs-set-leader-keys-for-minor-mode
+                                spacebind--bs-leader-keys
+                                spacebind--bs-leader-keys-for-major-mode
+                                spacebind--bs-leader-keys-for-minor-mode
                                 spacebind--bs-global-replacements
-                                spacebind--bs-add-fn-key-seq-override)))
+                                spacebind--bs-fn-key-seq-override)))
        (when (listp el) ;; all list arguments are key sequences.
          (condition-case err (kbd (string-join el " "))
            ((error nil) (push err invalid-key-seqs)))))
@@ -536,134 +473,6 @@ NOTE: `spacebind--eager-bind' set to true. "
        ((("C-v" "b") "call bar-fn"))
        :fn-key-seq-override
        (("spacemacs/python-toggle-breakpoint" "->b" "toggle breakpoint"))))
-    (eq nil)
-    (should)))
-
-(ert-deftest test-spacebind-always-generates-right-calls ()
-  (thread-first
-      (spacemacs|spacebind
-       :major
-       (py-mode
-        "Docstring for documentation"
-        ("C-p" "compile/execute"
-         ("TAB" spacemacs/python-execute-file "execute file")
-         ("C" spacemacs/python-execute-file-focus "execute file and focus"))
-        ("d" "debug"
-         (("b" :label "->b") spacemacs/python-toggle-breakpoint "toggle
-                                                                 breakpoint"))
-        ("r" "refactor"
-         ("i" spacemacs/python-remove-unused-imports "remove unused import"))
-        ("s" "REPL"
-         ("s" spacemacs/python-shell-send-buffer-switch
-          "send buffer to REPL and focus | on the buffer")
-         ("S" python-shell-send-buffer
-          ("send buffer to REPL" :label "buffer -> REPL | without
-                                                          focusing"))
-         ("d" spacemacs/python-shell-send-defun-switch
-          "send function around point to REPL and focus")
-         ("D" python-shell-send-defun
-          "send function around point to REPL")
-         ("r" spacemacs/python-shell-send-region-switch
-          "send region to REPL and focus")
-         ("R" python-shell-send-region "send region to REPL")))
-       :minor
-       (some-minor-mode
-        ("a" "section under a key"
-         ("b" foo-fn "call foo-fn")
-         ("c" "sub section under c key"
-          ("d" "sub sub section under d key"
-           ("e" baz-fn "call baz-fn")))))
-       (some-another-minor-mode
-        ("a" "section under a key"
-         ("b" "sub section under b key"
-          ("c" "sub sub section under c key"
-           ("b" baz-fn "call baz-fn")))))
-       :global
-       (("C-v" "section under a key"
-         ("b" bar-fn "call bar-fn"))))
-    (test-spacebind|log-calls)
-    (cl-set-exclusive-or
-     '((spacemacs/add-which-key-fn-key-seq-override
-        "spacemacs/python-toggle-breakpoint" "->b" "toggle breakpoint")
-       (which-key-add-key-based-replacements
-         "SPC C-v b" "call bar-fn" nil)
-       (spacemacs/set-leader-keys-for-minor-mode
-         some-minor-mode "a b" foo-fn nil)
-       (spacemacs/set-leader-keys-for-minor-mode
-         some-minor-mode "a c d e" baz-fn nil)
-       (spacemacs/set-leader-keys-for-minor-mode
-         some-another-minor-mode "a b c b" baz-fn nil)
-       (spacemacs/set-leader-keys-for-major-mode
-         py-mode "C-p TAB" spacemacs/python-execute-file nil)
-       (spacemacs/set-leader-keys-for-major-mode
-         py-mode "C-p C" spacemacs/python-execute-file-focus nil)
-       (spacemacs/set-leader-keys-for-major-mode
-         py-mode "d b" spacemacs/python-toggle-breakpoint nil)
-       (spacemacs/set-leader-keys-for-major-mode
-         py-mode "r i" spacemacs/python-remove-unused-imports nil)
-       (spacemacs/set-leader-keys-for-major-mode
-         py-mode "s s" spacemacs/python-shell-send-buffer-switch nil)
-       (spacemacs/set-leader-keys-for-major-mode
-         py-mode "s S" python-shell-send-buffer nil)
-       (spacemacs/set-leader-keys-for-major-mode
-         py-mode "s d" spacemacs/python-shell-send-defun-switch nil)
-       (spacemacs/set-leader-keys-for-major-mode
-         py-mode "s D" python-shell-send-defun nil)
-       (spacemacs/set-leader-keys-for-major-mode
-         py-mode "s r" spacemacs/python-shell-send-region-switch nil)
-       (spacemacs/set-leader-keys-for-major-mode
-         py-mode "s R" python-shell-send-region nil)
-       (spacemacs/set-leader-keys
-         "C-v b" bar-fn nil)
-       (spacemacs/declare-prefix-for-mode
-         py-mode "C-p" "compile/execute" nil)
-       (spacemacs/declare-prefix-for-mode
-         py-mode "d" "debug" nil)
-       (spacemacs/declare-prefix-for-mode
-         py-mode "r" "refactor" nil)
-       (spacemacs/declare-prefix-for-mode
-         py-mode "s" "REPL" nil)
-       (spacemacs/declare-prefix-for-mode
-         some-minor-mode "a" "section under a key" nil)
-       (spacemacs/declare-prefix-for-mode
-         some-minor-mode "a c" "sub section under c key" nil)
-       (spacemacs/declare-prefix-for-mode
-         some-minor-mode "a c d" "sub sub section under d key" nil)
-       (spacemacs/declare-prefix-for-mode
-         some-another-minor-mode "a" "section under a key" nil)
-       (spacemacs/declare-prefix-for-mode
-         some-another-minor-mode "a b" "sub section under b key" nil)
-       (spacemacs/declare-prefix-for-mode
-         some-another-minor-mode "a b c" "sub sub section under c key" nil)
-       (spacemacs/declare-prefix
-         "C-v" "section under a key" nil)
-       (which-key-add-major-mode-key-based-replacements
-         py-mode "SPC m C-p TAB" "execute file" nil)
-       (which-key-add-major-mode-key-based-replacements
-         py-mode "SPC m C-p C" "execute file and focus" nil)
-       (which-key-add-major-mode-key-based-replacements
-         py-mode "SPC m d b" "toggle breakpoint" nil)
-       (which-key-add-major-mode-key-based-replacements
-         py-mode "SPC m r i" "remove unused import" nil)
-       (which-key-add-major-mode-key-based-replacements
-         py-mode "SPC m s s" "send buffer to REPL and focus" nil)
-       (which-key-add-major-mode-key-based-replacements
-         py-mode "SPC m s S" "buffer -> REPL" nil)
-       (which-key-add-major-mode-key-based-replacements
-         py-mode "SPC m s d" "send function around point to REPL and focus" nil)
-       (which-key-add-major-mode-key-based-replacements
-         py-mode "SPC m s D" "send function around point to REPL" nil)
-       (which-key-add-major-mode-key-based-replacements
-         py-mode "SPC m s r" "send region to REPL and focus" nil)
-       (which-key-add-major-mode-key-based-replacements
-         py-mode "SPC m s R" "send region to REPL" nil)
-       (spacemacs/add-key-based-replacements-for-minor-mode
-        some-minor-mode "SPC a b" "call foo-fn" nil)
-       (spacemacs/add-key-based-replacements-for-minor-mode
-        some-minor-mode "SPC a c d e" "call baz-fn" nil)
-       (spacemacs/add-key-based-replacements-for-minor-mode
-        some-another-minor-mode "SPC a b c b" "call baz-fn" nil))
-     :test 'equal)
     (eq nil)
     (should)))
 
