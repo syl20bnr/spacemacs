bf32e4333916e3790f9a1ab225a896f03bf9a6b7
Maximilian Wolff <smile13241324@gmail.com>
2021-01-24 22:48:04 +0100

    [command-log] Add experimental support for keycast
    Only added the package and provided proper keybindings.
    
    Individual setup of =keycast-insert-after= for each of
    the supported modelines should be contributed on a per
    user basis.

--- layers/+tools/command-log/packages.el
+++ layers/+tools/command-log/packages.el
@@ -1,6 +1,6 @@
 ;;; packages.el --- command-log Layer packages File for Spacemacs
 ;;
-;; Copyright (c) 2012-2020 Sylvain Benner & Contributors
+;; Copyright (c) 2012-2021 Sylvain Benner & Contributors
 ;;
 ;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
@@ -9,21 +9,24 @@
 ;;
 ;;; License: GPLv3
 
-(setq command-log-packages
-      ;; bmag/command-log-mode is a fork of lewang/command-log-mode, and there
-      ;; is an open PR to merge the fork into the original repo.
-      ;; TODO when the PR is merged upstream, change to use the original package
-      ;; from MELPA (IOW remove :location argument)
-      '((command-log-mode :location (recipe :fetcher github
-                                            :repo "bmag/command-log-mode"
-                                            :branch "color"))))
+(defconst command-log-packages
+  ;; bmag/command-log-mode is a fork of lewang/command-log-mode, and there
+  ;; is an open PR to merge the fork into the original repo.
+  ;; TODO when the PR is merged upstream, change to use the original package
+  ;; from MELPA (IOW remove :location argument)
+  '((command-log-mode :location (recipe :fetcher github
+                                        :repo "bmag/command-log-mode"
+                                        :branch "color"))
+    keycast))
 
 (defun command-log/init-command-log-mode ()
   (use-package command-log-mode
     :commands global-command-log-mode
     ;; :commands (clm/open-command-log-buffer global-command-log-mode spacemacs/toggle-command-log-mode)
     :init
-    (spacemacs/set-leader-keys "atl" #'global-command-log-mode)
+    (progn
+      (spacemacs/declare-prefix "atl" "command log")
+      (spacemacs/set-leader-keys "atll" #'global-command-log-mode))
     :config
     (setq clm/log-command-exceptions* (append clm/log-command-exceptions*
                                               '(evil-next-line
@@ -31,3 +34,10 @@
                                                 evil-forward-char
                                                 evil-backward-char))
           command-log-mode-auto-show t)))
+
+(defun command-log/init-keycast()
+  (use-package keycast
+    :init
+    (progn
+      (spacemacs/set-leader-keys "atlk" #'keycast-mode)
+      (setq keycast-insert-after "%e"))))
