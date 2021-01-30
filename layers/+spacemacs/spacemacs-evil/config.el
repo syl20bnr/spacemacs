06ff592b2fbd5894ed15f50369a63958855c6c0f
Thanh Vuong <thanhvg@gmail.com>
2021-01-29 23:09:39 -0700

    [evil-collection] support for dired

--- layers/+spacemacs/spacemacs-evil/config.el
+++ layers/+spacemacs/spacemacs-evil/config.el
@@ -19,5 +19,5 @@
 (defvar evil-lisp-safe-structural-editing-modes '()
   "A list of major mode symbols where safe structural editing is supported.")
 
-(defvar spacemacs-evil-collection-allowed-list '(eww)
+(defvar spacemacs-evil-collection-allowed-list '(eww dired)
   "List of modes Spacemacs will allow to be evilified by ‘evil-collection-init’.")
