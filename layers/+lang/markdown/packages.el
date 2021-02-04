2003217601f90ecfd6702f39b81bfee3c1d8b59a
sunlin <sunlin7@yahoo.com>
2021-02-04 08:34:02 +0800

    Update layers/+lang/markdown/packages.el
    Co-authored-by: Maximilian Wolff <smile13241324@gmail.com>

--- layers/+lang/markdown/packages.el
+++ layers/+lang/markdown/packages.el
@@ -66,17 +66,16 @@
       ;; use proper syntax highlighting
       (setq markdown-fontify-code-blocks-natively t)
       ;; set the markdown-command
-      (if markdown-executable     ; verify the executable for user defined value
-          (if (executable-find markdown-executable)
-              (setq markdown-command markdown-executable)
-            (warn "Non-exist markdown-executable: %s" markdown-executable))
-        (when (or (null markdown-command) ; markdown-command is nil or not exist
-                  (not (executable-find markdown-command)))
-          ;; try more markdown CLIs
-          (let ((cmd (cl-loop for cmd in '("markdown_py")
-                              when (executable-find cmd)
-                              return (file-name-nondirectory it))))
-            (when cmd (setq markdown-command cmd)))))
+(setq markdown-command
+      (if-let ((command (cl-loop for cmd in (if markdown-executable
+                                                (list markdown-executable))
+                                              (list "markdown" "pandoc" "markdown_py"))
+                                 when (executable-find cmd)
+                                 return (file-name-nondirectory it))))
+          command
+        (user-error (if markdown-executable
+                        (format "Cannot find markdown-executable %s" markdown-executable)
+                      "Please refer to the layer documentation and install a markdown command"))))
       ;; Declare prefixes and bind keys
       (dolist (prefix '(("mc" . "markdown/command")
                         ("mh" . "markdown/header")
