1d84447e6814b5dfc9408b5442654efbbf022d2a
Maximilian Wolff <smile13241324@gmail.com>
2021-02-07 23:01:43 +0100

    [fasd] Revise layer
    Add missing standard headers
    Make funcs.el define only functions
    and move init code for ivy into
    fasd-init function.

--- layers/+tools/fasd/funcs.el
+++ layers/+tools/fasd/funcs.el
@@ -1,10 +1,12 @@
-(with-eval-after-load 'fasd
-  (defun ivy-search-from-action (x)
-    (if (file-directory-p x)
-        (spacemacs/counsel-search dotspacemacs-search-tools nil x)
-      (message "Selected item is not a directory path")))
-  
-  (ivy-set-actions
-   'fasd-find-file
-   '(("o" fasd-find-file-action "find-file")
-     ("s" ivy-search-from-action "search-from"))))
+;;; funcs.el --- fasd Layer packages File for Spacemacs
+;;
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defun ivy-search-from-action (x)
+  (if (file-directory-p x)
+      (spacemacs/counsel-search dotspacemacs-search-tools nil x)
+    (message "Selected item is not a directory path")))
