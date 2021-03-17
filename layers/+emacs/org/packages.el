587f5f5038a108d93d46be03ae54783a71510f69
Kazark <kazark@zoho.com>
2021-03-17 18:22:48 -0400

    [org] add keybinding for org-copy-tree as with org-cut-tree
    Used =y= as a mnemonic for yank (in the Vim, not the Emacs, sense).

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -263,6 +263,7 @@ Will work on both org-mode and any mode that accepts plain html."
         "sA" 'org-archive-subtree-default
         "sb" 'org-tree-to-indirect-buffer
         "sd" 'org-cut-subtree
+        "sy" 'org-copy-subtree
         "sh" 'org-promote-subtree
         "sj" 'org-move-subtree-down
         "sk" 'org-move-subtree-up
