ee56edf5ae6df4cef893149ad0a2004b9d8673a8
Lucius Hu <lebensterben@users.noreply.github.com>
2021-03-18 01:20:38 -0400

    terraform: refactor
    - Moved backend determination to `config.el`
    - Refactored function
    - Replaced `pcase` form with only one-arm with `when` or `unless` form

--- layers/+tools/terraform/funcs.el
+++ layers/+tools/terraform/funcs.el
@@ -21,28 +21,17 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-(defun spacemacs//terraform-backend ()
-  "Returns selected backend."
-  (if terraform-backend
-      terraform-backend
-    (cond
-     ((configuration-layer/layer-used-p 'lsp) 'lsp)
-     (t 'company-terraform))))
-
 (defun spacemacs//terraform-setup-company ()
   "Conditionally setup company based on backend."
-  (pcase (spacemacs//terraform-backend)
-    ('company-terraform (spacemacs|add-company-backends
-                          :backends company-terraform
-                          :modes terraform-mode))
-    ;; Activate lsp company explicitly to activate
-    ;; standard backends as well
-    (`lsp (spacemacs|add-company-backends
-            :backends company-capf
-            :modes terraform-mode))))
-
+  (spacemacs|add-company-backends
+    :backends (pcase terraform-backend
+                ('company-terraform 'company-terraform)
+                ;; Activate lsp company explicitly to activate
+                ;; standard backends as well
+                ('lsp 'company-capf))
+    :modes terraform-mode))
 
 (defun spacemacs//terraform-setup-backend ()
   "Conditionally setup terraform backend."
-  (pcase (spacemacs//terraform-backend)
-    (`lsp (lsp))))
+  (when (eq terraform-backend 'lsp)
+    (lsp)))
