01a84d9fe8023d688af6d015e4fdb1bc50da9935
syl20bnr <sylvain.benner@gmail.com>
2021-03-20 21:14:12 -0400

    [defaults] Improve scrolling with mouse wheel
    Thanks to Profpatsch.
    
    Fixes https://github.com/syl20bnr/spacemacs/issues/1781

--- layers/+spacemacs/spacemacs-defaults/config.el
+++ layers/+spacemacs/spacemacs-defaults/config.el
@@ -60,9 +60,6 @@ It runs `tabulated-list-revert-hook', then calls `tabulated-list-print'."
   ;; (tabulated-list-print t)
   (tabulated-list-print))
 
-;; Mouse cursor in terminal mode
-(xterm-mouse-mode 1)
-
 ;; Highlight and allow to open http link at point in programming buffers
 ;; goto-address-prog-mode only highlights links in strings and comments
 (add-hook 'prog-mode-hook 'goto-address-prog-mode)
@@ -77,6 +74,19 @@ It runs `tabulated-list-revert-hook', then calls `tabulated-list-print'."
 
 ;; Don't try to ping things that look like domain names
 (setq ffap-machine-p-known 'reject)
+
+;; ---------------------------------------------------------------------------
+;; Mouse
+;; ---------------------------------------------------------------------------
+
+;; Mouse cursor in terminal mode
+(xterm-mouse-mode 1)
+
+(when (boundp 'mouse-wheel-scroll-amount)
+  ;; scroll two line at a time (less "jumpy" than defaults)
+  (setq mouse-wheel-scroll-amount '(2)
+        ;; don't accelerate scrolling
+        mouse-wheel-progressive-speed nil))
 
 ;; ---------------------------------------------------------------------------
 ;; Edit
