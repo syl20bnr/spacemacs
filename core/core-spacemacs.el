a9aab5e2a33e8ec1127280299c591a956c8f5c66
syl20bnr <sylvain.benner@gmail.com>
2021-03-25 22:40:43 -0400

    [init] Disable GUI elements in early-init as well
    Can provide a smoother startup by reducing graphical artifacts.

--- core/core-spacemacs.el
+++ core/core-spacemacs.el
@@ -61,7 +61,7 @@ the final step of executing code in `emacs-startup-hook'.")
   (setq ad-redefinition-action 'accept)
   ;; this is for a smoother UX at startup (i.e. less graphical glitches)
   (hidden-mode-line-mode)
-  (spacemacs//removes-gui-elements)
+  (spacemacs/removes-gui-elements)
   (spacemacs//setup-ido-vertical-mode)
   ;; explicitly set the preferred coding systems to avoid annoying prompt
   ;; from emacs (especially on Microsoft Windows)
@@ -161,20 +161,6 @@ the final step of executing code in `emacs-startup-hook'.")
     (spacemacs/load-spacemacs-env))
   ;; install the dotfile if required
   (dotspacemacs/maybe-install-dotfile))
-
-(defun spacemacs//removes-gui-elements ()
-  "Remove the menu bar, tool bar and scroll bars."
-  ;; removes the GUI elements
-  (when (and (fboundp 'scroll-bar-mode) (not (eq scroll-bar-mode -1)))
-    (scroll-bar-mode -1))
-  (when (and (fboundp 'tool-bar-mode) (not (eq tool-bar-mode -1)))
-    (tool-bar-mode -1))
-  (unless (spacemacs/window-system-is-mac)
-    (when (and (fboundp 'menu-bar-mode) (not (eq menu-bar-mode -1)))
-      (menu-bar-mode -1)))
-  ;; tooltips in echo-aera
-  (when (and (fboundp 'tooltip-mode) (not (eq tooltip-mode -1)))
-    (tooltip-mode -1)))
 
 (defun spacemacs//setup-ido-vertical-mode ()
   "Setup `ido-vertical-mode'."
--- early-init.el
+++ early-init.el
@@ -22,3 +22,19 @@
 ;; the package manager before loading the init file, so this file is neither
 ;; needed nor loaded on those versions.
 (setq package-enable-at-startup nil)
+
+(defun spacemacs/removes-gui-elements ()
+  "Remove the menu bar, tool bar and scroll bars."
+  ;; removes the GUI elements
+  (when (and (fboundp 'scroll-bar-mode) (not (eq scroll-bar-mode -1)))
+    (scroll-bar-mode -1))
+  (when (and (fboundp 'tool-bar-mode) (not (eq tool-bar-mode -1)))
+    (tool-bar-mode -1))
+  (unless (memq (window-system) '(mac ns))
+    (when (and (fboundp 'menu-bar-mode) (not (eq menu-bar-mode -1)))
+      (menu-bar-mode -1)))
+  ;; tooltips in echo-aera
+  (when (and (fboundp 'tooltip-mode) (not (eq tooltip-mode -1)))
+    (tooltip-mode -1)))
+
+(spacemacs/removes-gui-elements)
