175840bc6e326069495d806e3c49dd839fe7170c
Luis Ch <luischa123@gmail.com>
2021-04-19 12:26:19 -0500

    react: fix file auto detection (#14680)

--- layers/+frameworks/react/funcs.el
+++ layers/+frameworks/react/funcs.el
@@ -63,7 +63,7 @@
 (defun spacemacs//javascript-jsx-file-p ()
   "Enable rjsx mode by using magic-mode-alist."
   (when buffer-file-name
-    (and (memq (file-name-extension buffer-file-name) '("js" "jsx"))
+    (and (member (file-name-extension buffer-file-name) '("js" "jsx"))
          (re-search-forward "\\(^\\s-*import React\\|\\( from \\|require(\\)[\"']react\\)"
                             magic-mode-regexp-match-limit t)
          (save-excursion
