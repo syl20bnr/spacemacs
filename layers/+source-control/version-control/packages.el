ccd793e2dc7d2b3a057b567e3938a79695ded8b3
Michael Peyton Jones <me@michaelpj.com>
2021-06-19 21:42:44 +0100

    vc: use smerge-vc-next-conflict instead of smerge-next

--- layers/+source-control/version-control/packages.el
+++ layers/+source-control/version-control/packages.el
@@ -279,21 +279,21 @@
       (spacemacs|transient-state-format-hint smerge
         spacemacs--smerge-ts-full-hint
         "\n
- Movement^^^^         Merge Action^^      Diff^^            Other
- ---------------^^^^  ----------------^^  --------------^^  ---------------------------^^
- [_n_]^^   next hunk  [_b_] keep base     [_<_] base/mine   [_C_] combine curr/next hunks
- [_N_/_p_] prev hunk  [_m_] keep mine     [_=_] mine/other  [_u_] undo
- [_j_]^^   next line  [_a_] keep all      [_>_] base/other  [_q_] quit
- [_k_]^^   prev line  [_o_] keep other    [_r_] refine
- ^^^^                 [_c_] keep current  [_e_] ediff       [_?_]^^ toggle help
- ^^^^                 [_K_] kill current")
+ Movement^^^^             Merge Action^^      Diff^^            Other
+ -------------------^^^^  ----------------^^  --------------^^  ---------------------------^^
+ [_n_]^^   next conflict  [_b_] keep base     [_<_] base/mine   [_C_] combine curr/next hunks
+ [_N_/_p_] prev conflict  [_m_] keep mine     [_=_] mine/other  [_u_] undo
+ [_j_]^^   next line      [_a_] keep all      [_>_] base/other  [_q_] quit
+ [_k_]^^   prev line      [_o_] keep other    [_r_] refine
+ ^^^^                     [_c_] keep current  [_e_] ediff       [_?_]^^ toggle help
+ ^^^^                     [_K_] kill current")
       (spacemacs|define-transient-state smerge
         :title "Smerge Transient State"
         :hint-is-doc t
         :dynamic-hint (spacemacs//smerge-ts-hint)
         :bindings
         ;; move
-        ("n" smerge-next)
+        ("n" smerge-vc-next-conflict)
         ("N" smerge-prev)
         ("p" smerge-prev)
         ("j" evil-next-line)
