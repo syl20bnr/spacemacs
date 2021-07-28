5cadadc3eac25aaf6fda623dc1efbf2ad48b6b99
SpacemacsBot <not@an.actual.email.beep.boop>
2021-07-28 13:12:20 +0000

    [bot] "built_in_updates" Wed Jul 28 13:12:20 UTC 2021

--- core/libs/package-build.el
+++ core/libs/package-build.el
@@ -93,14 +93,23 @@
   (if package-build-stable
       'package-build-get-tag-version
     'package-build-get-timestamp-version)
-  "The function used to determine the version of a package.
+  "The function used to determine the revision and version of a package.
 
 The default depends on the value of option `package-build-stable'.
 
-Called with one argument, the recipe object.  In Git repositories
-the latest commit isn't necessarily checked out, but is available
-as \"origin/HEAD\".  In Mercurial repositories the latest commit
-is checked out."
+This function is called with one argument, the recipe object, and
+must return (REVISION . VERSION), where REVISION is the \"current\"
+revision according to some definition of the authors choosing and
+VERSION is the version string corresponding to that.
+
+REVISION should be determined first.  If it is necessary for that
+to be checked out to determine VERSION, then this function has to
+do so by calling `package-build--checkout-1'.  If not, then this
+step can be omitted.  Note that a helper functions might call the
+checkout function themselves; `package-build--get-timestamp' does.
+
+It might be appropriate to respect the `commit' and `branch' slots
+of the recipe."
   :group 'package-build
   :set-after '(package-build-stable)
   :type 'function)
@@ -147,6 +156,13 @@ The string in the capture group should be parsed as valid by `version-to-list'."
   :group 'package-build
   :type 'string)
 
+(defcustom package-build-allowed-git-protocols '("https" "file" "ssh")
+  "Protocols that can be used to fetch from upstream with git.
+By default insecure protocols, such as \"http\" or \"git\", are
+disallowed."
+  :group 'package-build
+  :type '(repeat string))
+
 ;;; Generic Utilities
 
 (defun package-build--message (format-string &rest args)
@@ -171,7 +187,11 @@ Otherwise do nothing.  FORMAT-STRING and ARGS are as per that function."
           version)))
 
 (defun package-build-get-timestamp-version (rcp)
-  (let ((rev (and (cl-typep rcp 'package-git-recipe) "origin/HEAD")))
+  (let ((rev (and (cl-typep rcp 'package-git-recipe)
+                  (or (oref rcp commit)
+                      (when-let ((branch (oref rcp branch)))
+                        (concat "origin/" branch))
+                      "origin/HEAD"))))
     (cons (package-build--get-commit rcp rev)
           (package-build--parse-time
            (package-build--get-timestamp rcp rev)
@@ -265,19 +285,26 @@ is used instead."
   (package-build--message "Fetcher: %s" (package-recipe--fetcher rcp))
   (package-build--message "Source:  %s\n" (package-recipe--upstream-url rcp)))
 
+(cl-defmethod package-build--checkout-1 :before ((_rcp package-recipe) rev)
+  (when rev
+    (package-build--message "Checking out %s" rev)))
+
 ;;;; Git
 
 (cl-defmethod package-build--checkout ((rcp package-git-recipe))
   (let ((dir (package-recipe--working-tree rcp))
-        (url (package-recipe--upstream-url rcp)))
+        (url (package-recipe--upstream-url rcp))
+        (protocol (package-recipe--upstream-protocol rcp)))
+    (unless (member protocol package-build-allowed-git-protocols)
+      (error "Fetching using the %s protocol is not allowed" protocol))
     (cond
      ((and (file-exists-p (expand-file-name ".git" dir))
            (string-equal (package-build--used-url rcp) url))
       (package-build--message "Updating %s" dir)
       (package-build--run-process dir nil "git" "fetch" "-f" "--all" "--tags")
-      ;; We later checkout "origin/HEAD".  Sadly "git fetch" cannot
-      ;; be told to keep it up-to-date, so we have to make a second
-      ;; request.
+      ;; We might later checkout "origin/HEAD". Sadly "git fetch"
+      ;; cannot be told to keep it up-to-date, so we have to make
+      ;; a second request.
       (package-build--run-process dir nil "git" "remote" "set-head"
                                   "origin" "--auto"))
      (t
@@ -297,13 +324,8 @@ is used instead."
       (package-build--checkout-1 rcp rev)
       version)))
 
-(cl-defmethod package-build--checkout-1 ((rcp package-git-recipe) &optional rev)
+(cl-defmethod package-build--checkout-1 ((rcp package-git-recipe) rev)
   (let ((dir (package-recipe--working-tree rcp)))
-    (unless rev
-      (setq rev (or (oref rcp commit)
-                    (when-let ((branch (oref rcp branch)))
-                      (concat "origin/" branch))
-                    "origin/HEAD")))
     (package-build--run-process dir nil "git" "reset" "--hard" rev)
     (package-build--run-process dir nil "git" "submodule" "sync" "--recursive")
     (package-build--run-process dir nil "git" "submodule" "update"
@@ -315,6 +337,8 @@ is used instead."
 
 (cl-defmethod package-build--get-timestamp ((rcp package-git-recipe) rev)
   (let ((default-directory (package-recipe--working-tree rcp)))
+    ;; package-build--expand-source-file-list expects REV to be checked out.
+    (package-build--checkout-1 rcp rev)
     (car (apply #'process-lines
                 "git" "log" "--first-parent" "-n1" "--pretty=format:'\%ci'"
                 rev "--" (package-build--expand-source-file-list rcp)))))
@@ -348,7 +372,7 @@ is used instead."
       (package-build--checkout-1 rcp rev)
       version)))
 
-(cl-defmethod package-build--checkout-1 ((rcp package-hg-recipe) &optional rev)
+(cl-defmethod package-build--checkout-1 ((rcp package-hg-recipe) rev)
   (when rev
     (package-build--run-process (package-recipe--working-tree rcp)
                                 nil "hg" "update" rev)))
