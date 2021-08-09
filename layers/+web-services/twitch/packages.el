06cfd27276f1e376b4f7eb20d80d1c83ef9dfb27
Benedikt Broich <b.broich@posteo.de>
2021-08-09 16:46:54 +0200

    Add twitch layer
    Update CHANGELOG.develop
    
    Update requires helm
    
    Update layers/+web-services/twitch/packages.el

---
+++ layers/+web-services/twitch/packages.el
@@ -0,0 +1,50 @@
+;;; packages.el --- twitch layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2021 Sylvain Benner & Contributors
+;;
+;; Author: Benedikt Broich <b.broich@posteo.de>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
+(defconst twitch-packages
+  '(
+    (twitch-api :location (recipe :fetcher github
+                                  :repo "benediktbroich/twitch-api"))
+    (helm-twitch :location (recipe :fetcher github
+                                   :repo "benediktbroich/helm-twitch")
+                 :requires helm)))
+
+(defun twitch/init-twitch-api ()
+  (use-package twitch-api
+    :init (progn
+            (when (configuration-layer/package-usedp 'erc)
+              (spacemacs/set-leader-keys
+                "acit" 'twitch-api-erc-tls)))))
+
+(defun twitch/init-helm-twitch ()
+  (use-package helm-twitch
+    :defer t
+    :init (progn
+            (spacemacs/declare-prefix "aws" "stream")
+            (spacemacs/set-leader-keys
+              "awst" 'helm-twitch)
+            (when (configuration-layer/package-usedp 'streamlink)
+              (setq helm-twitch-enable-livestreamer-actions t))
+            (when (configuration-layer/package-usedp 'erc)
+              (setq helm-twitch-enable-chat-actions t)))))
