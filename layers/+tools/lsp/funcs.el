f61a7cd453648299468992ed3f4a04a38d3eaf7f
Wieland Hoffmann <mineo@users.noreply.github.com>
2021-11-18 06:33:38 +0100

    Add a keybinding for lsp-treemacs-type-hierarchy (#15163)
    * lsp: Bind =~SPC m g T~ to lsp-treemacs-type-hierarchy
    This allows to show the type hierarchy for the type at point.
    
    * lsp: Goto call and type hierarchy are provided by lsp-treemacs

--- layers/+tools/lsp/funcs.el
+++ layers/+tools/lsp/funcs.el
@@ -127,7 +127,8 @@
     "xL" #'lsp-lens-hide)
   (when (configuration-layer/package-used-p 'lsp-treemacs)
     (spacemacs/set-leader-keys-for-minor-mode 'lsp-mode
-      "gh" #'lsp-treemacs-call-hierarchy)))
+      "gh" #'lsp-treemacs-call-hierarchy
+      "gT" #'lsp-treemacs-type-hierarchy)))
 
 (defun spacemacs//lsp-bind-simple-navigation-functions (prefix-char)
   (spacemacs/set-leader-keys-for-minor-mode 'lsp-mode
