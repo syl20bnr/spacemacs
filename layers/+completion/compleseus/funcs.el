1846716f15cc780dd77c53f0351fd06db1ff0c38
Thanh Vuong <thanhvg@gmail.com>
2021-12-19 22:12:12 -0700

    [compleseus] integration with which-key
    https://github.com/oantolin/embark/wiki/Additional-Configuration#use-which-key-like-a-key-menu-prompt
    
    embark-act `M-o` menu will be handled by which-key

--- layers/+completion/compleseus/funcs.el
+++ layers/+completion/compleseus/funcs.el
@@ -151,3 +151,35 @@ Binding a key to: `find-file' calls: `ido-find-file'"
   (interactive)
   (selectrum-previous-candidate (or n 1))
   (spacemacs/embark-preview))
+
+;; which-key integration functions for embark
+;; https://github.com/oantolin/embark/wiki/Additional-Configuration#use-which-key-like-a-key-menu-prompt
+(defun spacemacs/embark-which-key-indicator ()
+  "An embark indicator that displays keymaps using which-key.
+The which-key help message will show the type and value of the
+current target followed by an ellipsis if there are further
+targets."
+  (lambda (&optional keymap targets prefix)
+    (if (null keymap)
+        (which-key--hide-popup-ignore-command)
+      (which-key--show-keymap
+       (if (eq (plist-get (car targets) :type) 'embark-become)
+           "Become"
+         (format "Act on %s '%s'%s"
+                 (plist-get (car targets) :type)
+                 (embark--truncate-target (plist-get (car targets) :target))
+                 (if (cdr targets) "…" "")))
+       (if prefix
+           (pcase (lookup-key keymap prefix 'accept-default)
+             ((and (pred keymapp) km) km)
+             (_ (key-binding prefix 'accept-default)))
+         keymap)
+       nil nil t (lambda (binding)
+                   (not (string-suffix-p "-argument" (cdr binding))))))))
+
+(defun spacemacs/embark-hide-which-key-indicator (fn &rest args)
+  "Hide the which-key indicator immediately when using the completing-read prompter."
+  (which-key--hide-popup-ignore-command)
+  (let ((embark-indicators
+         (remq #'spacemacs/embark-which-key-indicator embark-indicators)))
+      (apply fn args)))
