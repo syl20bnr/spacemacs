e8f3579989b283b3ca3574f04a732a29690ea0ff
Thanh Vuong <thanhvg@gmail.com>
2022-01-08 22:55:01 -0700

    [tree-sitter] add option to seletively disable tree sitter hl mode

--- layers/+tools/tree-sitter/funcs.el
+++ layers/+tools/tree-sitter/funcs.el
@@ -1,4 +1,4 @@
-;;; config.el --- tree-sitter layer config file for Spacemacs.
+;;; funcs.el --- tree-sitter layer packages file for Spacemacs.
 ;;
 ;; Copyright (c) 2012-2021 Sylvain Benner & Contributors
 ;;
@@ -20,18 +20,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(defvar tree-sitter-syntax-highlight-enable t
-  "If non nil, use tree-sitter for syntax highlighting where supported.")
-
-(defvar tree-sitter-indent-enable nil
-  "If non nil, use tree-sitter for indentation where supported.")
-
-(defvar tree-sitter-fold-enable nil
-  "If non nil, use tree-sitter for code folding where supported.")
-
-(defvar tree-sitter-fold-indicators-enable t
-  "If non nil, and `tree-sitter-fold-enable' is non nil, show fold indicators in fringe.")
-
-(defvar spacemacs-tree-sitter-hl-black-list
-  '()
-  "List of major modes not allow tree sitter highlight mode.")
+(defun spacemacs//tree-sitter-hl-maybe ()
+  "Turn on `tree-sitter-hl-mode' if allowed."
+  (unless (member major-mode spacemacs-tree-sitter-hl-black-list)
+    (tree-sitter-hl-mode +1)))
