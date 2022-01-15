a62b94348971f7da90a3e1807e6a39b0faed3d0b
SpacemacsBot <86630153+SpacemacsBot@users.noreply.github.com>
2022-01-15 04:30:28 +0200

    [bot] built_in_updates (#15274)
    Co-authored-by: SpacemacsBot <not@an.actual.email.beep.boop>

--- core/libs/spacemacs-theme/spacemacs-theme-pkg.el
+++ core/libs/spacemacs-theme/spacemacs-theme-pkg.el
@@ -1,1 +1,1 @@
-(define-package "spacemacs-theme" "0.1" "Color theme with a dark and light versions" 'nil :url "https://github.com/nashamri/spacemacs-theme" :keywords '("color" "theme"))
+(define-package "spacemacs-theme" "0.2" "Color theme with a dark and light versions" 'nil :url "https://github.com/nashamri/spacemacs-theme" :keywords '("color" "theme"))
