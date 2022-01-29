cde2b748c745c04badf80afe45f5a1a8b45790ea
Lucius Hu <lebensterben@users.noreply.github.com>
2022-01-28 21:19:45 -0500

    tabs: Rework
    - Replaced tab movement commands key bindings with
      vim-style bindings:
      - <kbd>g t</kbd>/<kbd>g T</kbd> for going to next/previous tab
        (instead of <kbd>Ctrl-{</kbd>/<kbd>Ctrl-}</kbd>)
      - <kbd>g Ctrl-t</kbd>/<kbd>g Ctrl-T</kbd> for moving current
        tab to right/left
        (instead of <kbd>Ctrl-Meta-{</kbd>/<kbd>Ctrl-Meta-}</kbd>)
    - Removed unused layer variables from config.el
    - Improved documentation in README.org
    - Added header in funcs.el

--- layers/+emacs/tabs/funcs.el
+++ layers/+emacs/tabs/funcs.el
@@ -1,3 +1,25 @@
+;;; funcs.el --- tabs layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2018, 2020 Sylvain Benner & Contributors
+;;
+;; Author: Deepu Puthrote <git@deepumohan.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 (defun spacemacs//tabs-timer-initialize (secs)
   (setq spacemacs-tabs-timer (run-with-timer secs nil (lambda () (centaur-tabs-local-mode 1)))))
 
