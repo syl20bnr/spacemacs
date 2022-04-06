5c0650282fe09f852ecd4109bf2a6bc9cb5a950b
David McFarland <mcfarland@hotmail.com>
2022-04-06 15:59:38 -0300

    core-jump: fix incorrect lookup of :async (#15449)

--- core/core-jump.el
+++ core/core-jump.el
@@ -76,8 +76,8 @@ They are in order: `spacemacs-jump-handlers',
     (let ((old-buffer (current-buffer))
           (old-point (point)))
       (dolist (-handler (spacemacs//get-jump-handlers))
-        (let* ((handler (if (listp -handler) (car -handler) -handler))
-               (async (plist-get (cdr-safe handler) :async)))
+        (let ((handler (if (listp -handler) (car -handler) -handler))
+              (async (plist-get (cdr-safe -handler) :async)))
           (ignore-errors
             (call-interactively handler))
           (when (or (eq async t)
