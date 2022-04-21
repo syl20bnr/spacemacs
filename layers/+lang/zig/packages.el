syl20bnr/spacemacs

commit 41393004881109d014dc2166c7110545ece9c32e

[zig] Adjust backend variables to match new style

layers/+lang/zig/packages.el
@@ -22,17 +22,9 @@
 
 
 (defconst zig-packages
-  '(
-    zig-mode
-    )
+  '(zig-mode)
   "The list of Lisp packages required by the zig layer.")
 
 (defun zig/init-zig-mode ()
   (use-package zig-mode
-    :defer t
-    :init
-    (progn
-      (add-hook 'zig-mode-hook #'spacemacs//zig-setup-backend)
-      )))
-
-;;; packages.el ends here
+    :hook (zig-mode-hook . spacemacs//zig-setup-backend)))

layers/+lang/zig/config.el
@@ -23,7 +23,7 @@
 
 (spacemacs|define-jump-handlers zig-mode)
 
-(defvar zig-backend nil
+(defvar zig-backend (if (configuration-layer/layer-used-p 'lsp) 'lsp 'zig-mode)
   "The backend to use for IDE features.
 Possible values are `lsp' or `zig-mode'.
 If `nil' then `zig-mode' is the default backend unless the `lsp' layer is used.")

layers/+lang/zig/funcs.el
@@ -21,23 +21,12 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-(defun spacemacs//zig-backend ()
-  "Returns selected backend."
-  (if zig-backend
-      zig-backend
-    (cond
-     ((configuration-layer/layer-used-p 'lsp) 'lsp)
-     (t 'zig-mode))))
-
 (defun spacemacs//zig-setup-backend ()
   "Conditionally setup zig backend."
-  (pcase (spacemacs//zig-backend)
+  (pcase zig-backend
     (`lsp (spacemacs//zig-setup-zls))))
 
 ;; lsp
-
-;; zls
-
 (defun spacemacs//zig-setup-zls ()
   "Setup LSP zls."
   (if (configuration-layer/layer-used-p 'lsp)

commit b70a9ab5920579986e409a91c6b7b6746b3338a2

[zig] Fix setup backend hook
A typo introduced in 41393004881109d014dc2166c7110545ece9c32e prevented
the backend to actually load lsp. This commit fixes this.

layers/+lang/zig/packages.el
@@ -27,4 +27,4 @@
 
 (defun zig/init-zig-mode ()
   (use-package zig-mode
-    :hook (zig-mode-hook . spacemacs//zig-setup-backend)))
+    :hook (zig-mode . spacemacs//zig-setup-backend)))