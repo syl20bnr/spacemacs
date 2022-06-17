27b95a7916ee265c54753325cc27fffaa5a6b8af
Ray Wang <rayw.public@gmail.com>
2022-06-17 17:55:44 +0800

    [translate] allow user to customize renders

---
+++ layers/+tools/translate/config.el
@@ -0,0 +1,30 @@
+;;; funcs.el --- Semantic Layer functions File for Spacemacs
+;;
+;; Copyright (c) 2012-2022 Sylvain Benner & Contributors
+;;
+;; Author: Ray Wang <rayw.public@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+(defvar translate/paragraph-render 'posframe
+  "Paragraph render for displaying translate result.
+Currently support `posframe' and `buffer'.")
+
+(defvar translate/word-render 'posframe
+  "Word render for displaying translate result.
+Currently support `posframe' and `buffer'.")
