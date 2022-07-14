82e3b38dd0ccf63fa62157eca0c650d7c0460bfd
Benedict HW <19878148+BenedictHW@users.noreply.github.com>
2022-07-14 12:26:17 -0400

    [spacemacs-editing] set 'undo-tree-enable-undo-in-region' to t
    Where the default value is currently nil.

--- layers/+spacemacs/spacemacs-editing/packages.el
+++ layers/+spacemacs/spacemacs-editing/packages.el
@@ -536,6 +536,7 @@
     (progn
       (setq undo-tree-visualizer-timestamps t
             undo-tree-visualizer-diff t
+            undo-tree-enable-undo-in-region t
             ;; 10X bump of the undo limits to avoid issues with premature
             ;; Emacs GC which truncages the undo history very aggresively
             undo-limit 800000
