6f34ec4de91861515a1e0d58e31c713a24dce364
Elliott Shugerman <eeshugerman@gmail.com>
2022-11-18 07:09:41 -0800

    Fix evil bindings in ediff

--- layers/+spacemacs/spacemacs-evil/config.el
+++ layers/+spacemacs/spacemacs-evil/config.el
@@ -31,5 +31,5 @@
 (defvar evil-lisp-safe-structural-editing-modes '()
   "A list of major mode symbols where safe structural editing is supported.")
 
-(defvar spacemacs-evil-collection-allowed-list '(eww dired quickrun)
+(defvar spacemacs-evil-collection-allowed-list '(eww dired quickrun ediff)
   "List of modes Spacemacs will allow to be evilified by ‘evil-collection-init’.")
