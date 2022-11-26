7881e45768da568c8c15b6244b08645e20961600
G Derber <gderber@users.noreply.github.com>
2022-11-26 01:12:29 -0500

    Create layer 'apache' (#15812)
    * Create layer 'apache'
    
    * Replace 'nginx' with 'apache'

---
+++ layers/+tools/apache/packages.el
@@ -0,0 +1,27 @@
+;;; packages.el --- apache layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2022 Sylvain Benner & Contributors
+;;
+;; Author: Nathaniel Waisbrot <code@waisbrot.net>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+(defconst apache-packages '(apache-mode))
+
+(defun apache/init-apache-mode()
+  (use-package apache-mode :defer t))
