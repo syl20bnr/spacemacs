f619b83499a5165834d4485cbf191002eca9b18e
Claudio Bley <claudio.bley@gmail.com>
2023-01-23 14:08:41 +0100

    [nixos] Fix nixos-format-on-save for newer nixos-mode
    ```
    Before-save hook error: (void-function nix-format-before-save)
    ```
    
    This was changed in upstream [commit][1].
    
    [1]: https://github.com/NixOS/nix-mode/commit/fb6c1ca5dd22580267142f19f78608a99de77552

--- layers/+os/nixos/packages.el
+++ layers/+os/nixos/packages.el
@@ -59,7 +59,7 @@
       (spacemacs/set-leader-keys-for-major-mode 'nix-mode
         "==" 'nix-format-buffer)
       (when nixos-format-on-save
-        (add-hook 'before-save-hook 'nix-format-before-save)))
+        (add-hook 'nix-mode-hook 'nixfmt-on-save-mode)))
     :config
     (electric-indent-mode -1)))
 
