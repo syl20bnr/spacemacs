00bae656b56e051c84735a760fe108966e2ec802
Codruț Constantin Gușoi <mail+git@codrut.pro>
2023-02-19 06:58:43 +0100

    Fix face warnings by using unspecified instead of nil (#15898)
    * Fix face warnings by using unspecified instead of nil
    
    * Remove forced overwrite for whitespace
    
    For literal years now I've been thinking there's something wrong with my
    themes, because whitespace-mode would use the wrong colour for
    highlighting spaces. Turns out it was this piece of config that was
    messing up the themes.
    
    If changes like this are needed, then they should be imlpemented by
    theme maintainers, not us.
    
    * Remove face attribute changes that do nothing

--- layers/+spacemacs/spacemacs-defaults/packages.el
+++ layers/+spacemacs/spacemacs-defaults/packages.el
@@ -526,14 +526,6 @@
       (add-hook 'diff-mode-hook 'spacemacs//set-whitespace-style-for-diff))
     :config
     (progn
-      (set-face-attribute 'whitespace-space nil
-                          :background 'unspecified
-                          :foreground (face-attribute 'font-lock-warning-face
-                                                      :foreground))
-      (set-face-attribute 'whitespace-tab nil
-                          :background 'unspecified)
-      (set-face-attribute 'whitespace-indentation nil
-                          :background 'unspecified)
       (spacemacs|diminish whitespace-mode " ⓦ" " w")
       (spacemacs|diminish global-whitespace-mode " ⓦ" " w"))))
 
