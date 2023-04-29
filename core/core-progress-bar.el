2febc53faa55db3581860be49aa37fc38fbae645
Bryce Carson <bcars268@mtroyal.ca>
2023-04-29 13:32:42 -0600

    Update function documentation.

--- core/core-progress-bar.el
+++ core/core-progress-bar.el
@@ -33,22 +33,26 @@
 (defvar spacemacs-loading-dots-chunk-threshold 0)
 
 (defun spacemacs/init-progress-bar (max)
-  "Initialize the progress bar."
-  (when dotspacemacs-loading-progress-bar
-    (setq spacemacs-loading-dots-count (window-total-size nil 'width))
-    (setq spacemacs-loading-dots-chunk-size (/ spacemacs-loading-dots-count
-                                               spacemacs-loading-dots-chunk-count))
-    (setq spacemacs-loading-dots-chunk-threshold
-          (- (/ max spacemacs-loading-dots-chunk-count) 5))
-    (setq spacemacs-loading-counter 0)
-    (setq spacemacs-loading-value 0)
-    (spacemacs-buffer/set-mode-line (make-string spacemacs-loading-dots-count
-                                                 spacemacs-loading-char-light))
-    (spacemacs//redisplay)))
+  "Initialize the package-loading progress bar when the
+customziation variable dotspacemacs-loading-progress-bar is
+non-nil."
+  (setq spacemacs-loading-dots-count (window-total-size nil 'width))
+  (setq spacemacs-loading-dots-chunk-size (/ spacemacs-loading-dots-count
+                                             spacemacs-loading-dots-chunk-count))
+  (setq spacemacs-loading-dots-chunk-threshold
+        (- (/ max spacemacs-loading-dots-chunk-count) 5))
+  (setq spacemacs-loading-counter 0)
+  (setq spacemacs-loading-value 0)
+  (spacemacs-buffer/set-mode-line (make-string spacemacs-loading-dots-count
+                                               spacemacs-loading-char-light))
+  (spacemacs//redisplay))
 
 (defun spacemacs/update-progress-bar ()
-  "Update progress bar by incrementing its value by 1.
-Display the progress bar by chunks of size `spacemacs-loading-dots-chunk-threshold'"
+  "When the customization variable
+dotspacemacs-loading-progress-bar is non-nil, update the
+package-loading progress bar by incrementing its value by 1. The
+progress is displayed in chunks of size
+`spacemacs-loading-dots-chunk-threshold'."
   (when (and (not noninteractive)
              (> spacemacs-loading-dots-chunk-threshold 0)
              dotspacemacs-loading-progress-bar)
