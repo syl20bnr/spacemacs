220ed0443c2c355d1c3ff7265c394e2f4e22f50a
Bryce Carson <bcars268@mtroyal.ca>
2023-04-29 13:18:58 -0600

    Wrap the init function body in an `when`
    Resolves https://github.com/syl20bnr/spacemacs/pull/15997#discussion_r1181115472.

--- core/core-progress-bar.el
+++ core/core-progress-bar.el
@@ -34,16 +34,17 @@
 
 (defun spacemacs/init-progress-bar (max)
   "Initialize the progress bar."
-  (setq spacemacs-loading-dots-count (window-total-size nil 'width))
-  (setq spacemacs-loading-dots-chunk-size (/ spacemacs-loading-dots-count
-                                             spacemacs-loading-dots-chunk-count))
-  (setq spacemacs-loading-dots-chunk-threshold
-        (- (/ max spacemacs-loading-dots-chunk-count) 5))
-  (setq spacemacs-loading-counter 0)
-  (setq spacemacs-loading-value 0)
-  (spacemacs-buffer/set-mode-line (make-string spacemacs-loading-dots-count
-                                               spacemacs-loading-char-light))
-  (spacemacs//redisplay))
+  (when dotspacemacs-loading-progress-bar
+    (setq spacemacs-loading-dots-count (window-total-size nil 'width))
+    (setq spacemacs-loading-dots-chunk-size (/ spacemacs-loading-dots-count
+                                               spacemacs-loading-dots-chunk-count))
+    (setq spacemacs-loading-dots-chunk-threshold
+          (- (/ max spacemacs-loading-dots-chunk-count) 5))
+    (setq spacemacs-loading-counter 0)
+    (setq spacemacs-loading-value 0)
+    (spacemacs-buffer/set-mode-line (make-string spacemacs-loading-dots-count
+                                                 spacemacs-loading-char-light))
+    (spacemacs//redisplay)))
 
 (defun spacemacs/update-progress-bar ()
   "Update progress bar by incrementing its value by 1.
