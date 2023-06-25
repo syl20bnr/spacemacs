6fd09d315e1959055f43d1390867a61c6f9c4deb
Eugene Yaremenko <w3techplayground@gmail.com>
2023-06-25 12:10:41 +0200

    Remove byte-comps from space-doc.el
    fixes #15721

--- layers/+spacemacs/spacemacs-org/local/space-doc/space-doc.el
+++ layers/+spacemacs/spacemacs-org/local/space-doc/space-doc.el
@@ -248,7 +248,6 @@ The character should be one of the markers from `org-emphasis-alist'."
          begin
          end
          '(invisible spacemacs--space-doc-invisible-marker))))))
-(byte-compile 'spacemacs//space-doc-emphasis-region)
 
 (defun spacemacs//space-doc-alternative-emphasis (&optional enable)
   "Emphasis overlays.
@@ -367,7 +366,6 @@ NOTE: Not using `advice-add' because it is global modification.
 FIXME: Find cleaner solution."
   (font-lock-default-fontify-region  start end verbose)
   (spacemacs//space-doc-tags-fontify start end))
-(byte-compile 'spacemacs//space-doc-font-lock-fontify-region-function)
 
 (defun spacemacs//space-doc-alternative-tags-look (&optional enable)
   "Modify meta tag appearance.
