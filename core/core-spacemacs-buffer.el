4489c337f22835674542df0642ed9533882bd1e3
Maxi Wolff <smile13241324@gmail.com>
2023-07-07 00:26:18 +0200

    Make home todo links show org links correctly
    this fix was directly taken from Dietr1ch
    so all hail goes to him.
    
    Fixes #16056

--- core/core-spacemacs-buffer.el
+++ core/core-spacemacs-buffer.el
@@ -1217,7 +1217,12 @@ LIST: list of `org-agenda' entries in the todo list."
                                 (format "- %s -"
                                         (cdr (assoc "time" el)))
                               "-")
-                            (cdr (assoc "text" el)))))
+                            ;; Replace links in org style in todo entries
+                            ;; "[[Link][Name]]" => "[Name]"
+                            (replace-regexp-in-string
+                             "\\[\\[[^][]+\\]\\[\\([^][]+\\)\\]\\]"
+                             "[\\1]"
+                             (cdr (assoc "text" el))))))
               (insert button-prefix)
               (widget-create 'push-button
                              :action `(lambda (&rest ignore)
@@ -1286,7 +1291,7 @@ LIST-SIZE is specified in `dotspacemacs-startup-lists' for recent entries."
   (let (;; we need to remove `org-agenda-files' entries from recent files
         (agenda-files
          (when-let ((default-directory
-                      (or (bound-and-true-p org-directory) "~/org"))
+                     (or (bound-and-true-p org-directory) "~/org"))
                     (files
                      (when (bound-and-true-p org-agenda-files)
                        (if (listp org-agenda-files)
@@ -1599,10 +1604,10 @@ If a prefix argument is given, switch to it in an other, possibly new window."
             (force-mode-line-update)
             (spacemacs-buffer-mode)))
         (if save-line
-           (progn (goto-char (point-min))
-                  (forward-line (1- save-line))
-                  (forward-to-indentation 0))
-         (spacemacs-buffer/goto-link-line)))
+            (progn (goto-char (point-min))
+                   (forward-line (1- save-line))
+                   (forward-to-indentation 0))
+          (spacemacs-buffer/goto-link-line)))
       (unless do-not-switch
         (if current-prefix-arg
             (switch-to-buffer-other-window spacemacs-buffer-name))
