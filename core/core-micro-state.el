b2511bba47aad1913f0080b6a11f3719dba32368
Eastern Li <EasternLi@outlook.com>
2023-12-15 22:14:44 +0800

    *core/core-micro-state.el: enable the eval for plist-get (fix #16205)

--- core/core-micro-state.el
+++ core/core-micro-state.el
@@ -26,8 +26,8 @@
   "Define faces for micro-states."
   (let* ((hname 'spacemacs-micro-state-header-face)
          (bname 'spacemacs-micro-state-binding-face)
-         (box '(:line-width -1 :color (plist-get (face-attribute
-                                                  'mode-line :box) :color)))
+         (box `(:line-width -1 :color ,(plist-get (face-attribute
+                                                   'mode-line :box) :color)))
          (err (face-attribute 'error :foreground)))
     (eval `(defface ,hname '((t ()))
              "Face for micro-state header in echo area.
