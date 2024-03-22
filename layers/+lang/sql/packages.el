ca4c945f1f16fd59f07fb41e16fe664479cc4c56
Aaron Zeng <me@bcc32.com>
2024-03-22 09:49:19 -0400

    Remove outdated forks (#16300)
    * [org] Remove unused local copy of ox-gfm.el
    
    * [org] Use MELPA version of ox-gfm
    
    The fork has fallen far behind the upstream version, and the bug it
    was introduced to address was seemingly fixed by this commit:
    
    https://github.com/larstvei/ox-gfm/commit/32dd3626b6d8efa4b1cbefc0077e737243fb18d4
    
    (The pull request that commit claims to fix is the same one that was
    cherry-picked onto the fork).
    
    * [sql] Use ELPA version of sql-indent
    
    sql-indent is no longer on MELPA, so Spacemacs should fetch it from
    the proper GNU ELPA source instead.
    
    * [gpu] Use MELPA version of company-glsl
    
    Kaali's repo has been inactive for some time, and MELPA is now using
    a fork thereof as the official source.

--- layers/+lang/sql/packages.el
+++ layers/+lang/sql/packages.el
@@ -26,15 +26,7 @@
         company
         org
         sql
-        ;; This mode is more up-to-date than the MELPA one.
-        ;; Turns out that it is available in GNU ELPA but we cannot
-        ;; force Spacemacs to fetch from it for now, it will always
-        ;; pickup the MELPA version. So for now we use an explicit
-        ;; recip to fetch from GitHUb the package.
-        (sql-indent :location (recipe
-                               :fetcher github
-                               :repo "alex-hhh/emacs-sql-indent"
-                               :files ("sql-indent.el")))
+        (sql-indent :location elpa)
         (sqlfmt :location local)
         (sqlup-mode :toggle sql-capitalize-keywords)
         ))
