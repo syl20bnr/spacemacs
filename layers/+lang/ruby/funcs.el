9e03b695bc49915f7ee2fe1ce0a1b5417999d129
Matthew Boston <matthew@matthewboston.com>
2024-04-04 09:45:05 -0600

    Highlight ruby debugger lines appropriately
    Fixes #16357
    
    The change expands the matching regex to only highlight lines where the debugger
    is the first word on a line.
    
    The intended behavior is to highlight lines that are calling a ruby debugger.
    The purpose is to warn developers that might push code to production
    with a debugger enabled. The change expands the matching regex to only highlight
    lines where the debugger is the first word on a line.
    
    However, there are cases in which the regex is highlighting lines as a
    false-positive. I first noticed this in my `Gemfile` when it was highlighting the
    line below:
    
    ```ruby
    gem 'pry-byebug'
    ```
    
    Other cases include: commented lines and defined function names that match a known
    debugger.
    
    See below for cases in which the line should and should not be highlighted.
    
    ```ruby
    
    class Test
      # This line below should not be highlighted
      def byebug
        byebug # this line should be highlighted
        # byebug This comment shouldn't be highlighted
    
        binding.irb # this line should be highlighted
        # binding.irb this line should not be highlighted
    
        binding.pry # this line should be highlighted
        # binding.pry this line should not be highlighted
      end
    end
    ```

--- layers/+lang/ruby/funcs.el
+++ layers/+lang/ruby/funcs.el
@@ -138,9 +138,9 @@ Called interactively it prompts for a directory."
   "Highlight break point lines."
   (interactive)
   (when ruby-highlight-debugger-keywords
-    (highlight-lines-matching-regexp "byebug")
-    (highlight-lines-matching-regexp "binding.irb")
-    (highlight-lines-matching-regexp "binding.pry")))
+    (highlight-lines-matching-regexp "^\s*byebug")
+    (highlight-lines-matching-regexp "^\s*binding.irb")
+    (highlight-lines-matching-regexp "^\s*binding.pry")))
 
 
 ;; Insert text
