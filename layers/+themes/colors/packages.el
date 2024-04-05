9cddd1543ab19099a53fd98e5f0968138c2306f8
Hang Su <tonyfloater@gmail.com>
2024-04-05 09:57:52 -0400

    init commit for removing local copy of nyan-mode

--- layers/+themes/colors/packages.el
+++ layers/+themes/colors/packages.el
@@ -25,7 +25,7 @@
   '(
     ;; not working well for now
     ;; rainbow-blocks
-    (nyan-mode :location local)
+    nyan-mode
     color-identifiers-mode
     rainbow-identifiers
     rainbow-mode))
