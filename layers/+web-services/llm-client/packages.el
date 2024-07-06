4a8bb0df46999143c6f360e05d8da801bb5189eb
Alexander Matyasko <alexander.matyasko@gmail.com>
2024-07-06 19:39:48 +0800

    Add ellama package to llm-client layer
    Add support and keybindings for ellama package in Spacemacs

--- layers/+web-services/llm-client/packages.el
+++ layers/+web-services/llm-client/packages.el
@@ -23,9 +23,94 @@
 
 
 (defconst llm-client-packages
-  '(gptel
+  '((ellama :toggle llm-client-enable-ellama)
+    (gptel :toggle llm-client-enable-gptel)
     org
     window-purpose))
+
+(defun llm-client/init-ellama ()
+  "Initialize the `ellama` package and set up keybindings."
+  (use-package ellama
+    :defer t
+    :ensure t
+    :init
+    (spacemacs/declare-prefix "$e" "Ellama")
+    ;; Code
+    (spacemacs/declare-prefix "$ec" "Code")
+    (spacemacs/set-leader-keys
+      "$ecc" 'ellama-code-complete    ; Complete code in current buffer
+      "$eca" 'ellama-code-add         ; Add code according to description
+      "$ece" 'ellama-code-edit        ; Edit code according to change
+      "$eci" 'ellama-code-improve     ; Improve code
+      "$ecr" 'ellama-code-review)     ; Review code
+    ;; Summarize
+    (spacemacs/declare-prefix "$es" "Summarize")
+    (spacemacs/set-leader-keys
+      "$ess" 'ellama-summarize           ; Summarize selected region or buffer
+      "$esw" 'ellama-summarize-webpage   ; Summarize webpage
+      "$esc" 'ellama-summarize-killring) ; Summarize text from kill ring
+    ;; Session
+    (spacemacs/declare-prefix "$es" "Session")
+    (spacemacs/set-leader-keys
+      "$esl" 'ellama-load-session     ; Load ellama session
+      "$esr" 'ellama-session-rename   ; Rename ellama session
+      "$esd" 'ellama-session-remove   ; Remove ellama session
+      "$esa" 'ellama-session-switch)  ; Switch ellama session
+    ;; Improve
+    (spacemacs/declare-prefix "$ei" "Improve")
+    (spacemacs/set-leader-keys
+      "$eiw" 'ellama-improve-wording      ; Improve wording
+      "$eig" 'ellama-improve-grammar      ; Improve grammar
+      "$eic" 'ellama-improve-conciseness) ; Make text concise
+    ;; Make
+    (spacemacs/declare-prefix "$em" "Make")
+    (spacemacs/set-leader-keys
+      "$eml" 'ellama-make-list        ; Create markdown list
+      "$emt" 'ellama-make-table       ; Create markdown table
+      "$emf" 'ellama-make-format)     ; Render text as specified format
+    ;; Ask
+    (spacemacs/declare-prefix "$ea" "Ask")
+    (spacemacs/set-leader-keys
+      "$eaa" 'ellama-ask-about        ; Ask about selected region or buffer
+      "$eai" 'ellama-chat             ; Chat with ellama
+      "$eal" 'ellama-ask-line         ; Send current line to ellama
+      "$eas" 'ellama-ask-selection)   ; Send selected region to ellama
+    ;; Text
+    (spacemacs/declare-prefix "$et" "Text")
+    (spacemacs/set-leader-keys
+      "$ett" 'ellama-translate                 ; Translate selected region or word at point
+      "$etb" 'ellama-translate-buffer          ; Translate current buffer
+      "$etc" 'ellama-complete                  ; Complete text in current buffer
+      "$ete" 'ellama-chat-translation-enable   ; Enable chat translation
+      "$etd" 'ellama-chat-translation-disable) ; Disable chat translation
+    ;; Define
+    (spacemacs/declare-prefix "$ed" "Define")
+    (spacemacs/set-leader-keys
+      "$edw" 'ellama-define-word)     ; Define current word
+    ;; Context
+    (spacemacs/declare-prefix "$ex" "Context")
+    (spacemacs/set-leader-keys
+      "$exb" 'ellama-context-add-buffer     ; Add buffer to context
+      "$exf" 'ellama-context-add-file       ; Add file to context
+      "$exs" 'ellama-context-add-selection  ; Add selection to context
+      "$exi" 'ellama-context-add-info-node) ; Add info node to context
+    ;; Provider
+    (spacemacs/declare-prefix "$ep" "Provider")
+    (spacemacs/set-leader-keys
+      "$eps" 'ellama-provider-select) ; Select ellama provider
+    ;; General
+    (spacemacs/set-leader-keys
+      "$ea" 'ellama-ask-about         ; Ask about selected region or buffer
+      "$ec" 'ellama-complete          ; Complete text in current buffer
+      "$ed" 'ellama-define-word       ; Define current word
+      "$es" 'ellama-summarize         ; Summarize selected region or buffer
+      "$et" 'ellama-translate         ; Translate selected region or word at point
+      "$ef" 'ellama-translate-buffer  ; Translate current buffer
+      "$ei" 'ellama-instant           ; Prompt ellama for instant reply
+      "$el" 'ellama-load-session      ; Load ellama session
+      "$er" 'ellama-session-remove    ; Remove ellama session
+      "$es" 'ellama-session-switch    ; Switch ellama session
+      "$en" 'ellama-session-rename))) ; Rename ellama session
 
 (defun llm-client/init-gptel ()
   "Initialize the `gptel` package and set up keybindings."
