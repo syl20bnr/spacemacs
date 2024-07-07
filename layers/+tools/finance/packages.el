3dbe33ea307bc34d7e8743b26c67a0cc63b08cac
Aaron L. Zeng <me@bcc32.com>
2024-07-06 23:15:05 -0400

    [finance] Fix packages.el style and indentation

--- layers/+tools/finance/packages.el
+++ layers/+tools/finance/packages.el
@@ -21,19 +21,17 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-(setq finance-packages
-  '(
-    company
+(defconst finance-packages
+  '(company
     flycheck
     (flycheck-ledger :requires flycheck)
     ledger-mode
-    (evil-ledger :toggle (memq dotspacemacs-editing-style '(vim hybrid)))
-    ))
+    (evil-ledger :toggle (memq dotspacemacs-editing-style '(vim hybrid)))))
 
 (defun finance/post-init-company ()
   (spacemacs|add-company-backends
-    :backends company-capf
-    :modes ledger-mode))
+   :backends company-capf
+   :modes ledger-mode))
 
 (defun finance/post-init-flycheck ()
   (spacemacs/enable-flycheck 'ledger-mode))
