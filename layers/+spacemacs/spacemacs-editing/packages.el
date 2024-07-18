ce56deb684add62a35892887e2cbd2e9a8e4cca5
Aaron L. Zeng <me@bcc32.com>
2024-07-18 13:35:55 -0400

    [spacemacs-editing] Reindent packages.el

--- layers/+spacemacs/spacemacs-editing/packages.el
+++ layers/+spacemacs/spacemacs-editing/packages.el
@@ -446,12 +446,12 @@
     (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
     (sp-local-pair 'minibuffer-mode "'" nil :actions nil)
     (sp-pair "{" nil :post-handlers
-              '(:add (spacemacs/smartparens-pair-newline-and-indent "RET")))
+             '(:add (spacemacs/smartparens-pair-newline-and-indent "RET")))
     (sp-pair "[" nil :post-handlers
-              '(:add (spacemacs/smartparens-pair-newline-and-indent "RET")))
+             '(:add (spacemacs/smartparens-pair-newline-and-indent "RET")))
     (when dotspacemacs-smart-closing-parenthesis
       (define-key evil-insert-state-map ")"
-        'spacemacs/smart-closing-parenthesis))))
+                  'spacemacs/smart-closing-parenthesis))))
 
 (defun spacemacs-editing/init-spacemacs-whitespace-cleanup ()
   (use-package spacemacs-whitespace-cleanup
@@ -469,7 +469,7 @@
       :on (let ((spacemacs-whitespace-cleanup-globally t))
             (spacemacs-whitespace-cleanup-mode))
       :off (let ((spacemacs-whitespace-cleanup-globally t))
-              (spacemacs-whitespace-cleanup-mode -1))
+             (spacemacs-whitespace-cleanup-mode -1))
       :on-message (spacemacs-whitespace-cleanup/on-message t)
       :documentation "Global automatic whitespace clean up."
       :evil-leader "t C-S-w")
