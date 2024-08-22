b3e43f15c0cb69222b558da0b6ac6267d49203fe
Maxi Wolff <smile13241324@gmail.com>
2024-08-22 19:23:35 +0200

    [lsp] Remove unmaintained avy integration
    this is not working properly and Spacemacs
    is not the right place for specific integrations
    instead this must be reintroduced via an
    external package.
    
    Closes #15938

--- layers/+tools/lsp/funcs.el
+++ layers/+tools/lsp/funcs.el
@@ -66,8 +66,8 @@
                               "gS" #'helm-lsp-global-workspace-symbol))
    ((configuration-layer/package-usedp 'consult)
     (define-key lsp-mode-map
-      [remap lsp-treemacs-errors-list]
-      #'consult-lsp-diagnostics)
+                [remap lsp-treemacs-errors-list]
+                #'consult-lsp-diagnostics)
     (spacemacs/lsp-define-key lsp-command-map
                               "gs" #'consult-lsp-symbols
                               "gf" #'consult-lsp-file-symbols))))
@@ -97,8 +97,6 @@
     ;; N.B. implementation and references covered by xref bindings / lsp provider...
     "g" "goto"
     "gt" #'lsp-find-type-definition
-    "gk" #'spacemacs/lsp-avy-goto-word
-    "gK" #'spacemacs/lsp-avy-goto-symbol
     "gM" #'lsp-ui-imenu
     ;; help
     "h" "help"
@@ -320,57 +318,3 @@ EXTRA is an additional parameter that's passed to the LSP function"
 (defun spacemacs//lsp-client-server-id ()
   "Return the ID of the LSP server associated with current project."
   (mapcar 'lsp--client-server-id (mapcar 'lsp--workspace-client (lsp-workspaces))))
-
-
-;; avy integration
-
-(defun spacemacs//lsp-avy-document-symbol (all)
-  (interactive)
-  (let ((line 0) (col 0) (w (selected-window))
-        (ccls (and (memq major-mode '(c-mode c++-mode objc-mode)) (eq c-c++-backend 'lsp-ccls)))
-        (start-line (1- (line-number-at-pos (window-start))))
-        (end-line (1- (line-number-at-pos (window-end))))
-        ranges point0 point1
-        candidates)
-    (save-excursion
-      (goto-char 1)
-      (cl-loop for loc in
-               (lsp--send-request
-                (lsp--make-request
-                 "textDocument/documentSymbol"
-                 `(:textDocument ,(lsp--text-document-identifier)
-                                 :all ,(if all t :json-false)
-                                 :startLine ,start-line :endLine ,end-line)))
-               for range = (if ccls
-                               loc
-                             (if (gethash "location" loc)
-                                 (->> loc (gethash "location") (gethash "range"))
-                               (->> loc (gethash "range"))))
-               for range_start = (gethash "start" range)
-               for range_end = (gethash "end" range)
-               for l0 = (gethash "line" range_start)
-               for c0 = (gethash "character" range_start)
-               for l1 = (gethash "line" range_end)
-               for c1 = (gethash "character" range_end)
-               while (<= l0 end-line)
-               when (>= l0 start-line)
-               do
-               (forward-line (- l0 line))
-               (forward-char c0)
-               (setq point0 (point))
-               (forward-line (- l1 l0))
-               (forward-char c1)
-               (setq point1 (point))
-               (setq line l1 col c1)
-               (push `((,point0 . ,point1) . ,w) candidates)))
-    (avy-with avy-document-symbol
-              (avy--process candidates
-                            (avy--style-fn avy-style)))))
-
-(defun spacemacs/lsp-avy-goto-word ()
-  (interactive)
-  (spacemacs//lsp-avy-document-symbol t))
-
-(defun spacemacs/lsp-avy-goto-symbol ()
-  (interactive)
-  (spacemacs//lsp-avy-document-symbol nil))
