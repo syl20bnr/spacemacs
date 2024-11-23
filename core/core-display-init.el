0ca9d00ba6b65c4fb3109262d32e00f2590bc851
Lin Sun <sunlin7@hotmail.com>
2024-11-23 06:50:41 +0000

    * core/core-display-init.el: fix detecting display initialized

--- core/core-display-init.el
+++ core/core-display-init.el
@@ -24,32 +24,34 @@
 (defvar spacemacs--after-display-system-init-list '()
   "List of functions to be run after the display system is initialized.")
 
+(defun spacemacs--display-system-initialized-p ()
+  "Dectect the display system initialized or not."
+  (cond ((boundp 'ns-initialized) ns-initialized)
+        ;; w32-initialized gets set too early, so
+        ;; if we're on Windows, check the list of fonts
+        ;; instead (this is nil until the graphics system
+        ;; is initialized)
+        ((boundp 'w32-initialized) (font-family-list))
+        ((boundp 'x-initialized) x-initialized)
+        ;; fallback to normal loading behavior only if in a GUI
+        (t (display-graphic-p))))
+
 (define-advice server-create-window-system-frame
     (:after (&rest _) spacemacs-init-display)
   "After Emacs server creates a frame, run functions queued in
 `SPACEMACS--AFTER-DISPLAY-SYSTEM-INIT-LIST' to do any setup that needs to have
 the display system initialized."
-  (dolist (fn (reverse spacemacs--after-display-system-init-list))
-    (funcall fn))
-  (advice-remove 'server-create-window-system-frame
-                 #'server-create-window-system-frame@spacemacs-init-display))
+  (when (spacemacs--display-system-initialized-p)
+    (mapc #'funcall (reverse spacemacs--after-display-system-init-list))
+    (advice-remove 'server-create-window-system-frame
+                   #'server-create-window-system-frame@spacemacs-init-display)))
 
 (defmacro spacemacs|do-after-display-system-init (&rest body)
   "If the display-system is initialized, run `BODY', otherwise,
 add it to a queue of actions to perform after the first graphical frame is
 created."
-  `(let ((init (cond ((boundp 'ns-initialized) ns-initialized)
-                     ;; w32-initialized gets set too early, so
-                     ;; if we're on Windows, check the list of fonts
-                     ;; instead (this is nil until the graphics system
-                     ;; is initialized)
-                     ((boundp 'w32-initialized) (font-family-list))
-                     ((boundp 'x-initialized) x-initialized)
-                     ;; fallback to normal loading behavior only if in a GUI
-                     (t (display-graphic-p)))))
-     (if init
-         (progn
-           ,@body)
-       (push (lambda () ,@body) spacemacs--after-display-system-init-list))))
+  `(if (not (spacemacs--display-system-initialized-p))
+       (push (lambda () ,@body) spacemacs--after-display-system-init-list)
+     ,@body))
 
 (provide 'core-display-init)
