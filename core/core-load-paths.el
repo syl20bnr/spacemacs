78eac61faddc02ea366fbf3b5ea567004959b6f8
Lin Sun <sunlin7@hotmail.com>
2024-12-05 06:50:47 +0000

    * core/core-load-paths.el: Support XDG_CACHE_HOME for spacemacs-cache-directory

--- core/core-load-paths.el
+++ core/core-load-paths.el
@@ -27,6 +27,8 @@
 ;;; Code:
 
 ;;;; PATH variables/constants
+(when (version<= emacs-version "28")
+  (eval-when-compile (require 'subr-x))) ; for the 'if-let*'
 
 ;; ~/.emacs.d
 (defvar spacemacs-start-directory
@@ -96,7 +98,11 @@
 ;; because Spacemacs may be installed to a shared location and this directory
 ;; and its children should be per-user.
 (defconst spacemacs-cache-directory
-  (concat user-emacs-directory ".cache/")
+  (if-let* ((cache-dir (getenv "SPACEMACSCACHE")))
+      cache-dir
+    (if-let* ((xdg-cache (getenv "XDG_CACHE_HOME")))
+        (expand-file-name "spacemacs/" xdg-cache)
+      (concat user-emacs-directory ".cache/")))
   "Spacemacs storage area for persistent files.")
 
 ;; ~/.emacs.d/.cache/auto-save
