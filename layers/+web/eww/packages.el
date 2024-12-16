ba9f7b404b5ee628cc7576e4ae9f1b32ab1ef241
Lin Sun <sunlin7@hotmail.com>
2024-12-16 06:52:35 +0000

    * layers/+web/eww/funcs.el: Fix eww transient state key bindings

--- layers/+web/eww/packages.el
+++ layers/+web/eww/packages.el
@@ -29,6 +29,7 @@
     zoom-frm))
 
 (defun eww/post-init-evil ()
+  (setq spacemacs--eww-ts-use-evil t)
   (spacemacs/transient-state-register-add-bindings 'eww
     '(("j" evil-next-line)
       ("k" evil-previous-line)
@@ -116,10 +117,12 @@
     :defer t))
 
 (defun eww/post-init-writeroom-mode ()
+  (setq spacemacs--eww-ts-use-writeroom t)
   (spacemacs/transient-state-register-add-bindings 'eww
     '(("w" writeroom-mode))))
 
 (defun eww/post-init-zoom-frm ()
+  (setq spacemacs--eww-ts-use-zoom-frm t)
   (spacemacs/transient-state-register-add-bindings 'eww
     '(("+" zoom-frm-in)
       ("-" zoom-frm-out)
