4cd97eaea21183d6ad7601d2428b6c912378dcb8
Lin Sun <sunlin7@hotmail.com>
2025-03-19 06:58:52 +0000

    Remove unnessarry `when` from the code

--- layers/+spacemacs/spacemacs-defaults/packages.el
+++ layers/+spacemacs/spacemacs-defaults/packages.el
@@ -388,10 +388,9 @@
     :defer t
     :commands (recentf-save-list)
     :init
-    (when t
-      (add-hook 'find-file-hook (lambda () (unless recentf-mode
-                                             (recentf-mode)
-                                             (recentf-track-opened-file)))))
+    (add-hook 'find-file-hook (lambda () (unless recentf-mode
+                                           (recentf-mode)
+                                           (recentf-track-opened-file))))
     ;; Do not leave dangling timers when reloading the configuration.
     (when (and (boundp 'recentf-auto-save-timer)
                (timerp recentf-auto-save-timer))
