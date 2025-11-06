ed1e9540f8e88cf47ade69d0f625ea8c303658b8
Aaron L. Zeng <me@bcc32.com>
2025-11-06 13:43:49 -0500

    [emacs-lisp] Make spacemacs/ert-run-tests-buffer robust to errors
    spacemacs//find-ert-test-buffer may fail for various reasons, such as
    if a test was defined using eval-expression.  Just do not include such
    tests in the set about to be run.

--- layers/+lang/emacs-lisp/funcs.el
+++ layers/+lang/emacs-lisp/funcs.el
@@ -163,8 +163,12 @@ Intended for use in mode hooks."
 ;; ERT commands
 
 (defun spacemacs//find-ert-test-buffer (ert-test)
-  "Return the buffer where ERT-TEST is defined."
-  (car (find-definition-noselect (ert-test-name ert-test) 'ert--test)))
+  "Return the buffer where ERT-TEST is defined.
+
+Return nil rather than erroring, if the buffer containing the test's
+definition is unknown."
+  (ignore-errors
+    (car (find-definition-noselect (ert-test-name ert-test) 'ert--test))))
 
 (defun spacemacs/ert-run-tests-buffer ()
   "Run all the tests in the current buffer."
