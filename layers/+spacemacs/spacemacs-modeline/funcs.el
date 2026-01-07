653174f7b663a1351b5f34f0837ce65c9157da3c
Aaron L. Zeng <me@bcc32.com>
2026-01-07 15:52:03 -0500

    [modeline] Enable lexical-binding in funcs.el

--- layers/+spacemacs/spacemacs-modeline/funcs.el
+++ layers/+spacemacs/spacemacs-modeline/funcs.el
@@ -1,4 +1,4 @@
-;;; funcs.el --- Spacemacs Mode-line Layer functions File  -*- lexical-binding: nil; -*-
+;;; funcs.el --- Spacemacs Mode-line Layer functions File  -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (c) 2012-2025 Sylvain Benner & Contributors
 ;;
