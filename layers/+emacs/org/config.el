57867b06c29cd3bf48e3feb19add4b5ca7bcf42e
Aaron L. Zeng <me@bcc32.com>
2026-02-02 19:12:20 -0500

    Tweak docstring

--- layers/+emacs/org/config.el
+++ layers/+emacs/org/config.el
@@ -33,7 +33,7 @@
   "If non-nil GitHub related packages are configured.")
 
 (defvar org-enable-mermaid-support nil
-  "If non-nil ob-mermaid are configured.")
+  "If non-nil, enable support for Mermaid source blocks via ob-mermaid.")
 
 (defvar org-enable-reveal-js-support nil
   "If non-nil, enable export to reveal.js.")
