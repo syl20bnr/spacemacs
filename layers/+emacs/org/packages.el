421649a00818b255d03b8747318cc0492421e29f
Lin Sun <sunlin7@hotmail.com>
2026-02-05 06:29:29 +0000

    [org] Remove keybinding "c" from org-src-edit for less conflicting

--- layers/+emacs/org/packages.el
+++ layers/+emacs/org/packages.el
@@ -193,7 +193,6 @@
     (with-eval-after-load 'org-src
       (spacemacs/set-leader-keys-for-minor-mode 'org-src-mode
         dotspacemacs-major-mode-leader-key 'org-edit-src-exit
-        "c" 'org-edit-src-exit
         "a" 'org-edit-src-abort
         "k" 'org-edit-src-abort))
 
