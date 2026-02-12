2e78e811cdc48db66c61be4d315f310501d4a489
Aaron L. Zeng <me@bcc32.com>
2026-02-11 23:37:26 -0500

    [systemd] Enable lexical-binding in packages.el

--- layers/+tools/systemd/packages.el
+++ layers/+tools/systemd/packages.el
@@ -1,4 +1,4 @@
-;;; packages.el --- systemd layer packages file for Spacemacs.  -*- lexical-binding: nil; -*-
+;;; packages.el --- systemd layer packages file for Spacemacs.  -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (c) 2012-2025 Sylvain Benner & Contributors
 ;;
