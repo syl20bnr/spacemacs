136f42291715d656400e0e98d336a0387377df37
Aaron L. Zeng <me@bcc32.com>
2026-08-25 14:12:34 -0400

    [test] Delete unused function directory-files-recursive_ForOldEmacs
    Not only is it unused, but Spacemacs also only supports Emacs 28.2 and
    newer now.

--- tests/doc/org-export-ftest.el
+++ tests/doc/org-export-ftest.el
@@ -25,47 +25,6 @@
 (require 'org)
 (require 'core-documentation)
 
-;; Replacement for directory-files-recursively for emacs < 25.1.1
-(defun directory-files-recursive_ForOldEmacs (directory match maxdepth ignore)
-  "List files in DIRECTORY and in its sub-directories.
-   Return files that match the regular expression MATCH but ignore
-   files and directories that match IGNORE (IGNORE is tested before MATCH. Recurse only
-   to depth MAXDEPTH. If zero or negative, then do not recurse"
-  (let* ((files-list '())
-         (current-directory-list
-          (directory-files directory t)))
-    ;; while we are in the current directory
-    (while current-directory-list
-      (let ((f (car current-directory-list)))
-        (cond
-         ((and
-           ignore ;; make sure it is not nil
-           (string-match ignore f))
-                                        ; ignore
-          nil)
-
-         ((and
-           (file-regular-p f)
-           (file-readable-p f)
-           (not(file-directory-p f))
-           (string-match match f))
-          (setq files-list (cons f files-list)))
-
-         ((and
-           (file-directory-p f)
-           (file-readable-p f)
-           (not (string-equal ".." (substring f -2)))
-           (not (string-equal "." (substring f -1)))
-           (> maxdepth 0))
-          ;; recurse only if necessary
-          (setq files-list (append files-list (directory-files-recursive_ForOldEmacs f match (- maxdepth -1) ignore))))
-
-         (t)))
-
-
-      (setq current-directory-list (cdr current-directory-list)))
-    files-list))
-
 ;; -----------------------------------------------------------------------------
 ;; Spacemacs Documentation HTML Export Test
 ;; Currently checks whether all org documentation files can be converted to html
