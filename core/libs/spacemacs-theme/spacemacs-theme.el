5f92ed2cb8ef843b4c662559e16e8f30e1dda7d6
SpacemacsBot <not@an.actual.email.beep.boop>
2026-09-15 04:54:39 +0000

    [bot] "built_in_updates" Tue Sep 15 04:54:39 UTC 2026

--- core/libs/spacemacs-theme/spacemacs-theme.el
+++ core/libs/spacemacs-theme/spacemacs-theme.el
@@ -92,10 +92,27 @@ to `auto', tags may not be properly aligned. "
   :type 'boolean
   :group 'spacemacs-theme)
 
+(defcustom spacemacs-theme-force-gui-colors nil
+  "If non-nil, use GUI colors in both the GUI and the terminal.
+
+This is ignored if `spacemacs-theme-force-terminal-colors' is non-nil."
+  :type 'boolean
+  :group 'spacemacs-theme)
+
+(defcustom spacemacs-theme-force-terminal-colors nil
+  "If non-nil, use terminal colors in both the GUI and the terminal.
+
+This overrides `spacemacs-theme-force-gui-colors' if both are non-nil."
+  :type 'boolean
+  :group 'spacemacs-theme)
+
+
 (defun create-spacemacs-theme (variant theme-name)
   (let* ((true-color-p (lambda ()
-                         (or (display-graphic-p)
-                             (= (tty-display-color-cells) 16777216))))
+                         (and (not spacemacs-theme-force-terminal-colors)
+                              (or spacemacs-theme-force-gui-colors
+                                  (display-graphic-p)
+                                  (= (tty-display-color-cells) 16777216)))))
          ;; Helper function to get custom color or default value
          (get-color (lambda (color-name default-value)
                       (or (alist-get color-name spacemacs-theme-custom-colors)
